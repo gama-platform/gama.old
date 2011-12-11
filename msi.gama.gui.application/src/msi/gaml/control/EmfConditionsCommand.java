26b0789ace884fc5daaf74df1a95949dac4a593d
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2011-12-11 21:43:52 +0000

    Corrected headers
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@3386 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.gui.application/src/msi/gaml/control/EmfConditionsCommand.java
+++ msi.gama.gui.application/src/msi/gaml/control/EmfConditionsCommand.java
@@ -1,18 +1,19 @@
 /*
  * GAMA - V1.4  http://gama-platform.googlecode.com
  * 
- * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC 
+ * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
  * 
  * Developers :
  * 
- * - Alexis Drogoul, IRD (Kernel, Metamodel, XML-based GAML), 2007-2011
- * - Vo Duc An, IRD & AUF (SWT integration, multi-level architecture), 2008-2011
- * - Patrick Taillandier, AUF & CNRS (batch framework, GeoTools & JTS integration), 2009-2011
- * - Pierrick Koch, IRD (XText-based GAML environment), 2010-2011
- * - Romain Lavaud, IRD (project-based environment), 2010
- * - Francois Sempe, IRD & AUF (EMF behavioral model, batch framework), 2007-2009
- * - Edouard Amouroux, IRD (C++ initial porting), 2007-2008
- * - Chu Thanh Quang, IRD (OpenMap integration), 2007-2008
+ * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2011
+ * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2011
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen  (Batch, GeoTools & JTS), 2009-2011
+ * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2011
+ * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
+ * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
+ * - Francois Sempe, UMI 209 UMMISCO, IRD/UPMC (EMF model, Batch), 2007-2009
+ * - Edouard Amouroux, UMI 209 UMMISCO, IRD/UPMC (C++ initial porting), 2007-2008
+ * - Chu Thanh Quang, UMI 209 UMMISCO, IRD/UPMC (OpenMap integration), 2007-2008
  */
 package msi.gaml.control;
 
