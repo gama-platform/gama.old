b7fe65d65bc7eb51586ef8e04adc5e0b0804949d
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2011-12-21 01:09:59 +0000

    Reorganizes the msi.gama.lang.* projects. All the functionalities of "msi.gama.lang.gaml.io" have been rolled into "msi.gama.lang.gaml"
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@3427 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.lang.gaml/src/msi/gama/lang/utils/Convert.java
@@ -0,0 +1,108 @@
+/*
+ * GAMA - V1.4 http://gama-platform.googlecode.com
+ * 
+ * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
+ * 
+ * Developers :
+ * 
+ * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
+ * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
+ * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
+ * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
+ * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
+ * - Francois Sempe, UMI 209 UMMISCO, IRD/UPMC (EMF model, Batch), 2007-2009
+ * - Edouard Amouroux, UMI 209 UMMISCO, IRD/UPMC (C++ initial porting), 2007-2008
+ * - Chu Thanh Quang, UMI 209 UMMISCO, IRD/UPMC (OpenMap integration), 2007-2008
+ */
+package msi.gama.lang.utils;
+
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+import msi.gama.lang.gaml.descript.*;
+import msi.gama.lang.gaml.gaml.*;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.*;
+import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.jdom.*;
+import org.jdom.output.*;
+
+public class Convert implements IUpdateOnChange {
+
+	// XML -> GAML
+	public static String xml2gaml(final String path) {
+		return XmlUtils.parse(path);
+	}
+
+	// public static String xml2gaml(final Document doc) {
+	// return XmlUtils.parse(doc);
+	// }
+
+	public static String xml2gaml(final InputStream is) {
+		return XmlUtils.parse(is);
+	}
+
+	public static String xml2gaml(final IResource source) {
+		return xml2gaml(GamlDescriptIO.getPath(source));
+	}
+
+	public static String toFormatedString(final ISyntacticElement doc) {
+		final XMLOutputter xo = new XMLOutputter();
+		Format f = xo.getFormat();
+		f.setIndent("\t");
+		f.setLineSeparator("\n");
+		xo.setFormat(f);
+		Document xmlDoc = new Document();
+		xmlDoc.setRootElement((Element) doc.getUnderlyingElement());
+		return xo.outputString(xmlDoc);
+	}
+
+	public static ISyntacticElement parseXml(final File file) throws Exception {
+		return new LineNumberSAXBuilder().parse(file);
+	}
+
+	public static Map<String, ISyntacticElement> getDocMap(final Resource r) throws Exception {
+		ResourceSet rs = r.getResourceSet();
+		if ( !r.getErrors().isEmpty() ) {
+			StringBuilder sb =
+				new StringBuilder("The Resource contains errors! ").append(r.getURI());
+			for ( Diagnostic e : r.getErrors() ) {
+				sb.append('\n').append(e.getMessage());
+			}
+			throw new Exception(sb.toString());
+		}
+		Map<String, ISyntacticElement> docs = new HashMap();
+		// xrs.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+		// EcoreUtil.resolveAll(r);
+		// get include from 'r'
+		Model m = (Model) EcoreUtil.copy(r.getContents().get(0));
+		// convert 'r' + include in 'rs'
+		for ( Import imp : m.getImports() ) {
+			String importUri = imp.getImportURI();
+			if ( !importUri.startsWith("platform:") ) {
+				URI iu = URI.createURI(importUri).resolve(r.getURI());
+				Resource ir = rs.getResource(iu, true);
+				// EcoreUtil.resolveAll(ir);
+				docs.putAll(getDocMap(ir)); // (!) recursive
+			}
+		}
+		URL url = FileLocator.resolve(new URL(r.getURI().toString()));
+		String path = new File(url.getFile()).getAbsolutePath();
+		docs.put(path, Gaml2JDOM.doConvert(m));
+		return docs;
+	}
+
+	/**
+	 * @see msi.gama.lang.gaml.descript.IUpdateOnChange#update(java.lang.String,
+	 *      org.eclipse.emf.ecore.resource.Resource)
+	 */
+	@Override
+	public void update(final String filePath, final Resource r) throws Exception {
+		getDocMap(r);
+	}
+}
---
+++ msi.gama.lang.gaml/src/msi/gama/lang/utils/Xml2Gaml.java
@@ -0,0 +1,191 @@
+/*
+ * GAMA - V1.4 http://gama-platform.googlecode.com
+ * 
+ * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
+ * 
+ * Developers :
+ * 
+ * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
+ * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
+ * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
+ * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
+ * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
+ * - Francois Sempe, UMI 209 UMMISCO, IRD/UPMC (EMF model, Batch), 2007-2009
+ * - Edouard Amouroux, UMI 209 UMMISCO, IRD/UPMC (C++ initial porting), 2007-2008
+ * - Chu Thanh Quang, UMI 209 UMMISCO, IRD/UPMC (OpenMap integration), 2007-2008
+ */
+package msi.gama.lang.utils;
+
+import java.io.*;
+import java.util.List;
+import org.jdom.*;
+import org.jdom.input.SAXBuilder;
+
+public class Xml2Gaml {
+
+	private final StringBuilder gaml;
+	private String tab;
+	private boolean isString;
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) throws Exception {
+		// Xml2Gaml x2g = new Xml2Gaml(args[1]);
+		final Xml2Gaml x2g = new Xml2Gaml();
+		x2g.doConv(args[0]);
+		System.out.println(x2g);
+	}
+
+	public Xml2Gaml() {
+		gaml = new StringBuilder();
+		tab = "";
+	}
+
+	public static void print(final Document o) {
+		final Xml2Gaml x2g = new Xml2Gaml();
+		x2g.doConv(o);
+		System.out.println(x2g.toString());
+	}
+
+	public String doConv(final String path2xml) {
+		try {
+			return doConv(getDocumentJDOM(path2xml));
+		} catch (final JDOMException e) {
+			e.printStackTrace();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String doConv(final InputStream in) {
+		try {
+			return doConv(getDocumentJDOM(in));
+		} catch (final JDOMException e) {
+			e.printStackTrace();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String doConv(final Document doc) {
+		final Element model = doc.getRootElement();
+		gaml.append("model " + model.getAttributeValue("name") + "\n" + "// gen by Xml2Gaml\n" +
+			"import \"platform:/plugin/msi.gama.core/generated/std.gaml\"\n\n");
+
+		appendStatments(model.getChildren());
+		return gaml.toString();
+	}
+
+	@SuppressWarnings("rawtypes")
+	private void appendStatments(final List statments) {
+		for ( final Object statment : statments ) {
+			appendStatment((Element) statment);
+		}
+	}
+
+	private void appendStatment(final Element statment) {
+		gaml.append(tab);
+		final String markupName = statment.getName();
+		if ( markupName.equals("include") ) {
+			gaml.append("import \"" + statment.getAttributeValue("file") + ".gaml\"\n");
+			return;
+		}
+		final Attribute cst = statment.getAttribute("const");
+		final Attribute name = statment.getAttribute("name");
+		final Attribute var = statment.getAttribute("var");
+		final Attribute type = statment.getAttribute("type");
+		if ( cst != null && cst.getValue().equals("true") ) {
+			gaml.append("const ");
+		} else {
+			gaml.append(markupName + " ");
+		}
+		isString = false;
+		if ( type != null && type.getValue().equals("string") ) {
+			isString = true;
+		}
+		if ( markupName.equals("chart") || markupName.equals("inspect") ||
+			markupName.equals("image") ) {
+			gaml.append("name: '" + name.getValue() + "' ");
+		} else if ( name != null ) {
+			gaml.append(name.getValue() + " ");
+		} else if ( var != null ) {
+			gaml.append(var.getValue() + " ");
+		}
+		for ( final Object ofacet : statment.getAttributes() ) {
+			final Attribute facet = (Attribute) ofacet;
+			if ( !facet.getName().equals("name") && !facet.getName().equals("const") ) {
+				appendFacet(facet);
+			}
+		}
+		@SuppressWarnings("rawtypes")
+		final List statments = statment.getChildren();
+		if ( !statments.isEmpty() ) {
+			// if (markupName.equals("do")) {
+			// String s = "(";
+			// for (Object arg : statments) {
+			// if (s.length()>1) s+=", ";
+			// s += ((Element) arg).getAttributeValue("value");
+			// }
+			// gaml.append(s+");\n");
+			// } else
+			{
+				gaml.append("{\n");
+				tab += '\t';
+				appendStatments(statments);
+				tab = tab.substring(1);
+				gaml.append(tab + "}\n");
+			}
+		} else {
+			gaml.append(";\n");
+		}
+	}
+
+	private void appendFacet(final Attribute facet) {
+		final String name = facet.getName();
+		final String value = facet.getValue();
+
+		if ( (isString && (name.equals("value") || name.equals("init")) || name.equals("parameter") || name
+			.equals("category")) && value.indexOf('\'') < 0 ) {
+			gaml.append(name + ": '" + value + "' ");
+		} else if ( name.equals("skills") && value.indexOf(',') > 0 ) {
+			gaml.append(name + ": [" + value + "] ");
+		} else {
+			// http://download.oracle.com/javase/6/docs/api/java/util/regex/Pattern.html#sum
+			String replaced = value.replaceAll("rgb '(\\w+)'", "rgb('$1')");
+			replaced = replaced.replaceAll("rgb '#(\\w+)'", "rgb('#$1')");
+			replaced = replaced.replaceAll("file '(\\S+)'", "file('$1')");
+			replaced = replaced.replaceAll("last (\\w+)", "last($1)");
+			replaced = replaced.replaceAll("first (\\w+)", "first($1)");
+			replaced = replaced.replaceAll("rnd (\\w+)", "rnd($1)");
+			replaced = replaced.replaceAll("flip (\\w+)", "flip($1)");
+			// replaced = replaced.replaceAll("(\\w+) location",
+			// "location as $1");
+			replaced = replaced.replaceAll("list (\\w+)", "$1 as list");
+			gaml.append(name + ": " + replaced + " ");
+		}
+	}
+
+	// ===== UTILS =====
+
+	private Document getDocumentJDOM(final String path2xml) throws JDOMException, IOException {
+		final SAXBuilder sax = new SAXBuilder();
+		final Document doc = sax.build(new File(path2xml));
+		return doc;
+	}
+
+	private Document getDocumentJDOM(final InputStream in) throws JDOMException, IOException {
+		final SAXBuilder sax = new SAXBuilder();
+		final Document doc = sax.build(in);
+		return doc;
+	}
+
+	@Override
+	public String toString() {
+		return gaml.toString();
+	}
+}
