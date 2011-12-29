56c97ea98f11ddefb5c9a4ef52155106af45e3b5
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2011-12-29 23:05:19 +0000

    Removed a GamaRuntimeException thrown in updating the displays.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@3452 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/displays/TextDisplay.java
+++ msi.gama.application/src/msi/gama/gui/displays/TextDisplay.java
@@ -1,5 +1,5 @@
 /*
- * GAMA - V1.4  http://gama-platform.googlecode.com
+ * GAMA - V1.4 http://gama-platform.googlecode.com
  * 
  * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
  * 
@@ -7,7 +7,7 @@
  * 
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
- * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen  (Batch, GeoTools & JTS), 2009-2012
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
  * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
@@ -22,7 +22,6 @@ import java.awt.*;
 import msi.gama.common.interfaces.*;
 import msi.gama.gui.parameters.*;
 import msi.gama.outputs.layers.*;
-import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gaml.expressions.IExpression;
 import msi.gaml.types.*;
 import org.eclipse.swt.widgets.Composite;
@@ -44,8 +43,7 @@ public class TextDisplay extends AbstractDisplay {
 	}
 
 	@Override
-	public void fillComposite(final Composite compo, final IDisplaySurface container)
-		throws GamaRuntimeException {
+	public void fillComposite(final Composite compo, final IDisplaySurface container) {
 		super.fillComposite(compo, container);
 		EditorFactory.createExpression(compo, "Expression:",
 			((TextDisplayLayer) model).getTextExpr(), new EditorListener<IExpression>() {
