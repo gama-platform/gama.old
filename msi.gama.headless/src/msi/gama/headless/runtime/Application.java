5680daaba0f03f3af223b08abf7cc35e92029511
nmarilleau@gmail.com <nmarilleau@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-03-21 15:05:51 +0000

    Share project "msi.gama.headless" into "https://gama-platform.googlecode.com/svn"
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@3770 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.headless/src/msi/gama/headless/runtime/Application.java
@@ -0,0 +1,20 @@
+package msi.gama.headless.runtime;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class Application implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
