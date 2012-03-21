8a666d8a4db0c1470d06bb9bc5e45e77b921c04b
agrignard@gmail.com <agrignard@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-03-21 14:00:15 +0000

    add barabasi generator
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@3765 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/skills/GraphSkill.java
+++ msi.gama.core/src/msi/gaml/skills/GraphSkill.java
@@ -1,5 +1,6 @@
 package msi.gaml.skills;
 
+
 import msi.gama.common.interfaces.IKeyword;
 import msi.gama.precompiler.GamlAnnotations.action;
 import msi.gama.precompiler.GamlAnnotations.args;
@@ -7,15 +8,43 @@ import msi.gama.precompiler.GamlAnnotations.skill;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gama.util.graph.IGraph;
+import msi.gaml.types.IType;
+
+
+import org.graphstream.algorithm.generator.BarabasiAlbertGenerator;
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+
 
 @skill({ IKeyword.GRAPH_SKILL })
 public class GraphSkill extends Skill {
 
 	@action("load_graph_from")
 	@args({ "edge_species", "vertex_species", "file" })
-	public IGraph primLoadGraphFromFile(final IScope scope) throws GamaRuntimeException {
-		
+	public IGraph primLoadGraphFromFile(final IScope scope) throws GamaRuntimeException {		
 		return null;
-		
 	}
+	
+	@action("generate_barabasi_graph")
+	@args({ "nb_links", "nb_nodes"})
+	public IGraph primGenerateBarabasiGraph(final IScope scope) throws GamaRuntimeException {
+		Graph graph = new SingleGraph("Barabˆsi-Albert");
+		// Between 1 and 3 new links per node added.
+		int v= (Integer) scope.getArg("nb_links", IType.INT);
+		int nb_nodes= (Integer) scope.getArg("nb_nodes", IType.INT);	
+		Generator gen = new BarabasiAlbertGenerator(v);
+		// Generate nb_nodes nodes:
+		gen.addSink(graph);
+		gen.begin();
+		for(int i=0; i<nb_nodes; i++) {
+		    gen.nextEvents();
+		}
+		gen.end();
+		graph.display();
+		return null;
+	}
+	
+	
 }
