31b7dfdd9490c820e5a6337a1a78a36ab628591a
agrignard@gmail.com <agrignard@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-03-23 15:03:10 +0000

    add a GLOutput
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@3857 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.core/src/msi/gama/opengl/GLOutput.java
@@ -0,0 +1,49 @@
+package msi.gama.opengl;
+
+import msi.gama.common.interfaces.IKeyword;
+import msi.gama.common.util.GuiUtils;
+import msi.gama.outputs.AbstractDisplayOutput;
+import msi.gama.outputs.LayerDisplayOutput;
+import msi.gama.precompiler.GamlAnnotations.facets;
+import msi.gama.precompiler.GamlAnnotations.inside;
+import msi.gama.precompiler.GamlAnnotations.symbol;
+import msi.gama.precompiler.GamlAnnotations.facet;
+import msi.gama.precompiler.GamlAnnotations.with_sequence;
+import msi.gama.runtime.IScope;
+import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gaml.compilation.ISymbolKind;
+import msi.gaml.descriptions.IDescription;
+import msi.gaml.types.IType;
+
+/**
+ * @author arno: This output is define in the core but it should be (or can be) defined in the plugin jogl
+ *
+ */
+@SuppressWarnings("unchecked")
+@symbol(name = IKeyword.DISPLAY_GL, kind = ISymbolKind.OUTPUT)
+@facets(value = {
+	//@facet(name = IKeyword.DISPLAY_GRAPH, type = IType.STRING_STR, optional = false),
+	@facet(name = IKeyword.NAME, type = IType.STRING_STR, optional = true)
+	}, omissible = IKeyword.NAME)
+@with_sequence
+@inside(symbols = IKeyword.OUTPUT)
+public class GLOutput extends AbstractDisplayOutput {
+
+	public GLOutput(IDescription desc) {
+		super(desc);
+		System.err.println("output created");
+		
+	}
+
+	@Override
+	public String getViewId() {
+		return GuiUtils.GL_VIEW_ID;
+
+	}
+
+	@Override
+	public void compute(IScope scope, int cycle) throws GamaRuntimeException {
+		System.err.println("compute");
+	}
+
+}
