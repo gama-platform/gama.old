ca750eedd4a76a2d43a7a228dbc0a043f6441fe2
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-04-20 17:39:32 +0000

    Installation of a RefreshListener to begin investigating Issue 180
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@3988 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/swt/commands/ShowHideRepositoriesViewHandler.java
+++ msi.gama.application/src/msi/gama/gui/swt/commands/ShowHideRepositoriesViewHandler.java
@@ -1,5 +1,5 @@
 /*
- * GAMA - V1.4  http://gama-platform.googlecode.com
+ * GAMA - V1.4 http://gama-platform.googlecode.com
  * 
  * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
  * 
@@ -7,7 +7,7 @@
  * 
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
- * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen  (Batch, GeoTools & JTS), 2009-2012
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
  * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
@@ -20,6 +20,8 @@ package msi.gama.gui.swt.commands;
 
 import msi.gama.common.util.GuiUtils;
 import org.eclipse.core.commands.*;
+import org.eclipse.team.svn.ui.repository.*;
+import org.eclipse.team.svn.ui.repository.RepositoryTreeViewer.IRefreshListener;
 import org.eclipse.ui.*;
 
 public class ShowHideRepositoriesViewHandler extends AbstractHandler {
@@ -37,7 +39,17 @@ public class ShowHideRepositoriesViewHandler extends AbstractHandler {
 				if ( !GuiUtils.isModelingPerspective() ) {
 					GuiUtils.openModelingPerspective();
 				}
-				activePage.showView(REPOSITORIES_VIEW_ID);
+				IViewPart pp = activePage.showView(REPOSITORIES_VIEW_ID);
+
+				((RepositoriesView) pp).getRepositoryTree().addRefreshListener(
+					new IRefreshListener() {
+
+						@Override
+						public void refreshed(final Object arg0) {
+							GuiUtils.debug("Refresh : " + arg0);
+						}
+					});
+
 			} else {
 				if ( GuiUtils.isModelingPerspective() ) {
 					activePage.hideView((IViewPart) part);
