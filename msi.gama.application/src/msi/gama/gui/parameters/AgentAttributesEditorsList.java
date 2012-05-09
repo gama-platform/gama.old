ba997ea4ac94e84dc15d307ca60cec312391fb76
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-05-09 07:49:56 +0000

    Allows to hide certain variables from the inspect view. This functionality will be extended later to allow for a parametrization of this hidden variables.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@4099 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/parameters/AgentAttributesEditorsList.java
+++ msi.gama.application/src/msi/gama/gui/parameters/AgentAttributesEditorsList.java
@@ -1,5 +1,5 @@
 /*
- * GAMA - V1.4  http://gama-platform.googlecode.com
+ * GAMA - V1.4 http://gama-platform.googlecode.com
  * 
  * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
  * 
@@ -7,7 +7,7 @@
  * 
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
- * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen  (Batch, GeoTools & JTS), 2009-2012
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
  * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
@@ -28,6 +28,8 @@ public class AgentAttributesEditorsList extends EditorsList<IAgent> {
 
 	public static final String DEAD_MARKER = " dead at step ";
 	public static final String AGENT_MARKER = "Agent" + ItemList.SEPARATION_CODE;
+	public static final Set<String> HIDDEN = new HashSet(Arrays.asList(IKeyword.PEERS,
+		IKeyword.MEMBERS, IKeyword.AGENTS));
 
 	@Override
 	public String getItemDisplayName(final IAgent ag, final String name) {
@@ -48,8 +50,10 @@ public class AgentAttributesEditorsList extends EditorsList<IAgent> {
 		if ( addItem(agent) ) {
 			if ( !agent.dead() ) {
 				for ( final IParameter var : params ) {
-					IParameterEditor gp = EditorFactory.getInstance().create(agent, var);
-					categories.get(agent).put(gp.getParam().getName(), gp);
+					if ( !HIDDEN.contains(var.getName()) ) {
+						IParameterEditor gp = EditorFactory.getInstance().create(agent, var);
+						categories.get(agent).put(gp.getParam().getName(), gp);
+					}
 				}
 			}
 		}
