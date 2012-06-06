8286f9ab5be677267f41cfd186b88b5bce4894ba
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-06-06 18:04:17 +0000

    Made isPairs() a static function (to be accessed from outside GamaFile
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@4365 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gama/util/GamaList.java
+++ msi.gama.core/src/msi/gama/util/GamaList.java
@@ -237,7 +237,7 @@ public class GamaList<E> extends ArrayList<E> implements IList<E> {
 
 		// TODO REVOIR CA POUR RENVOYER PLUTOT UNE MAP<INTEGER, E>
 		final GamaMap result = new GamaMap();
-		if ( isPairs() ) {
+		if ( isPairs(this) ) {
 			for ( final E e : this ) {
 				GamaPair pair = (GamaPair) e;
 				result.put(pair.first(), pair.last());
@@ -252,8 +252,8 @@ public class GamaList<E> extends ArrayList<E> implements IList<E> {
 		return result;
 	}
 
-	private boolean isPairs() {
-		for ( final Object obj : this ) {
+	public static boolean isPairs(final IList list) {
+		for ( final Object obj : list ) {
 			if ( !(obj instanceof GamaPair) ) { return false; }
 		}
 		return true;
