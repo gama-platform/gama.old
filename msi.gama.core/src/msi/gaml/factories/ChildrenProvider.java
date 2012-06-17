93f6f6d4b632f0a6d9e5de2bf88b87de64035306
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-06-17 02:43:03 +0000

    Simplified this class (now acts as a proxy for a list of descriptions)
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@4429 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/factories/ChildrenProvider.java
+++ msi.gama.core/src/msi/gaml/factories/ChildrenProvider.java
@@ -6,25 +6,14 @@ import msi.gaml.descriptions.IDescription;
 public class ChildrenProvider implements IChildrenProvider {
 
 	private final List<IDescription> children;
-	private Class javaBase;
 
-	public ChildrenProvider(final List<IDescription> descs) {
+	public ChildrenProvider(final List descs) {
 		children = descs;
-	}
-
-	public ChildrenProvider(final List<IDescription> descs, final Class base) {
-		children = descs;
-		javaBase = base;
 	}
 
 	@Override
 	public List<IDescription> getChildren() {
 		return children == null ? Collections.EMPTY_LIST : children;
-	}
-
-	@Override
-	public Class getJavaBase() {
-		return javaBase;
 	}
 
 }
