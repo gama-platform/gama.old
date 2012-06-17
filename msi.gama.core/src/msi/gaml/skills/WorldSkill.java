94542f52f24d26bab2e86c22836ae21d51458ce7
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-06-17 02:44:46 +0000

    Added four new global variables : project_path, model_path, workspace_path and cycle. Documented most of the variable declarations.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@4431 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/skills/WorldSkill.java
+++ msi.gama.core/src/msi/gaml/skills/WorldSkill.java
@@ -18,11 +18,13 @@
  */
 package msi.gaml.skills;
 
+import java.net.URL;
 import msi.gama.common.interfaces.IKeyword;
 import msi.gama.kernel.simulation.*;
 import msi.gama.metamodel.agent.IAgent;
 import msi.gama.precompiler.GamlAnnotations.action;
 import msi.gama.precompiler.GamlAnnotations.args;
+import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.getter;
 import msi.gama.precompiler.GamlAnnotations.setter;
 import msi.gama.precompiler.GamlAnnotations.skill;
@@ -31,6 +33,7 @@ import msi.gama.precompiler.GamlAnnotations.vars;
 import msi.gama.runtime.*;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gaml.types.IType;
+import org.eclipse.core.runtime.Platform;
 
 /**
  * Written by drogoul Modified on 24 juin 2010
@@ -39,23 +42,53 @@ import msi.gaml.types.IType;
  * 
  */
 
-@vars({ @var(name = GAMA._FATAL, type = IType.BOOL_STR, init = "false"),
+@vars({
+	@var(name = GAMA._FATAL, type = IType.BOOL_STR, init = "false"),
 	@var(name = GAMA._WARNINGS, type = IType.BOOL_STR, init = "false"),
-	@var(name = IKeyword.TIME, type = IType.FLOAT_STR),
-	@var(name = IKeyword.SEED, type = IType.FLOAT_STR),
-	@var(name = IKeyword.RNG, type = IType.STRING_STR, init = "'" + IKeyword.MERSENNE + "'"),
-	@var(name = IKeyword.STEP, type = IType.FLOAT_STR, constant = true),
-	@var(name = WorldSkill.DURATION, type = IType.STRING_STR),
-	@var(name = WorldSkill.TOTAL_DURATION, type = IType.STRING_STR),
-	@var(name = WorldSkill.AVERAGE_DURATION, type = IType.STRING_STR) })
+	@var(name = IKeyword.TIME, type = IType.FLOAT_STR, doc = @doc(value = "Represents the total time passed, in model time, since the beginning of the simulation", comment = "Equal to cycle * step if the user does not arbitrarily initialize it.")),
+	@var(name = IKeyword.SEED, type = IType.FLOAT_STR, doc = @doc(value = "The seed of the random number generator", comment = "Each time it is set, the random number generator is reinitialized")),
+	@var(name = WorldSkill.CYCLE, type = IType.INT_STR, doc = @doc("Returns the current cycle of the simulation")),
+	@var(name = IKeyword.RNG, type = IType.STRING_STR, init = "'" + IKeyword.MERSENNE + "'", doc = @doc("The random number generator to use for this simulation. Four different ones are at the disposal of the modeler: " +
+		IKeyword.MERSENNE +
+		" represents the default generator, based on the Mersenne-Twister algorithm. Very reliable; " +
+		IKeyword.CELLULAR +
+		" is a cellular automaton based generator that should be a bit faster, but less reliable; " +
+		IKeyword.XOR +
+		" is another choice. Much faster than the previous ones, but with short sequences; and " +
+		IKeyword.JAVA + " invokes the standard Java generator")),
+	@var(name = IKeyword.STEP, type = IType.FLOAT_STR, doc = @doc(value = "Represents the value of the interval, in model time, between two simulation cycles", comment = "If not set, its value is equal to 1.0 and, since the default time unit is the second, to 1 second")),
+	@var(name = WorldSkill.DURATION, type = IType.STRING_STR, doc = @doc("Returns a string containing the duration, in milliseconds, of the previous simulation cycle")),
+	@var(name = WorldSkill.TOTAL_DURATION, type = IType.STRING_STR, doc = @doc("Returns a string containing the total duration, in milliseconds, of the simulation since it has been launched ")),
+	@var(name = WorldSkill.AVERAGE_DURATION, type = IType.STRING_STR, doc = @doc("Returns a string containing the average duration, in milliseconds, of a simulation cycle.")),
+	@var(name = WorldSkill.MODEL_PATH, type = IType.STRING_STR, constant = true, doc = @doc(value = "Contains the absolute path to the folder in which the current model is located", comment = "Always terminated with a trailing separator")),
+	@var(name = WorldSkill.WORKSPACE_PATH, type = IType.STRING_STR, constant = true, doc = @doc(value = "Contains the absolute path to the workspace of GAMA", comment = "Always terminated with a trailing separator")),
+	@var(name = WorldSkill.PROJECT_PATH, type = IType.STRING_STR, constant = true, doc = @doc(value = "Contains the absolute path to the project in which the current model is located", comment = "Always terminated with a trailing separator")) })
 @skill(name = IKeyword.GLOBAL, attach_to = IKeyword.WORLD_SPECIES)
 public class WorldSkill extends GeometricSkill {
 
+	public static final String CYCLE = "cycle";
 	public static final String DURATION = "duration";
-
 	public static final String TOTAL_DURATION = "total_duration";
-
 	public static final String AVERAGE_DURATION = "average_duration";
+	public static final String MODEL_PATH = "model_path";
+	public static final String WORKSPACE_PATH = "workspace_path";
+	public static final String PROJECT_PATH = "project_path";
+
+	@getter(MODEL_PATH)
+	public String getModelPath(final IAgent agent) {
+		return GAMA.getModel().getFolderPath() + "/";
+	}
+
+	@getter(value = WORKSPACE_PATH, initializer = true)
+	public String getWorkspacePath(final IAgent agent) {
+		URL url = Platform.getInstanceLocation().getURL();
+		return url.getPath();
+	}
+
+	@getter(PROJECT_PATH)
+	public String getProjectPath(final IAgent agent) {
+		return GAMA.getModel().getProjectPath() + "/";
+	}
 
 	@getter(DURATION)
 	public String getDuration(final IAgent agent) {
@@ -80,6 +113,11 @@ public class WorldSkill extends GeometricSkill {
 	@setter(IKeyword.TIME)
 	public void setTime(final IAgent agent, final double t) throws GamaRuntimeException {
 		SimulationClock.setTime(t);
+	}
+
+	@getter(CYCLE)
+	public Integer getCycle(final IAgent agent) {
+		return SimulationClock.getCycle();
 	}
 
 	@getter(value = GAMA._FATAL, initializer = true)
@@ -132,7 +170,7 @@ public class WorldSkill extends GeometricSkill {
 		GAMA.getRandom().setGenerator(newRng);
 	}
 
-	@action(name="pause")
+	@action(name = "pause", doc = @doc("Allows to pause the current simulation. It can be set to continue with the manual intervention of the user."))
 	@args(names = {})
 	public Object primPause(final IScope scope) {
 		if ( !getSimulation(scope).isPaused() ) {
@@ -141,7 +179,7 @@ public class WorldSkill extends GeometricSkill {
 		return null;
 	}
 
-	@action(name="halt")
+	@action(name = "halt", doc = @doc("Allows to stop the current simulation. It cannot be continued after"))
 	@args(names = {})
 	public Object primHalt(final IScope scope) {
 		if ( getSimulation(scope).isAlive() ) {
