ebb65baede87b8fd3c5d6399c014d7cdec0d42c9
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-07-04 08:59:08 +0000

    Added methods that should not have to be defined in subclasses (unless necessary)
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@4564 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/expressions/JavaExpression.java
+++ msi.gama.core/src/msi/gaml/expressions/JavaExpression.java
@@ -1,5 +1,5 @@
 /*
- * GAMA - V1.4  http://gama-platform.googlecode.com
+ * GAMA - V1.4 http://gama-platform.googlecode.com
  * 
  * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
  * 
@@ -7,7 +7,7 @@
  * 
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
- * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen  (Batch, GeoTools & JTS), 2009-2012
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
  * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
@@ -20,6 +20,7 @@ package msi.gaml.expressions;
 
 import msi.gama.runtime.IScope;
 import msi.gaml.types.*;
+import org.eclipse.emf.common.notify.*;
 
 /**
  * Written by drogoul Modified on 19 oct. 2010
@@ -55,6 +56,43 @@ public abstract class JavaExpression implements IExpression {
 	@Override
 	public IType getType() {
 		return Types.get(IType.NONE);
+	}
+
+	@Override
+	public void dispose() {}
+
+	@Override
+	public String getTitle() {
+		return null;
+	}
+
+	@Override
+	public String getDocumentation() {
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public void unsetTarget(final Notifier oldTarget) {}
+
+	@Override
+	public void notifyChanged(final Notification notification) {}
+
+	@Override
+	public Notifier getTarget() {
+		return null;
+	}
+
+	@Override
+	public void setTarget(final Notifier newTarget) {}
+
+	@Override
+	public boolean isAdapterForType(final Object type) {
+		return false;
 	}
 
 }
