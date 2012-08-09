bc319d2b7f25d370f09f6ba570d492803acf0787
agrignard <agrignard@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-08-09 10:44:16 +0000

    Sandbox for SWT/jogl
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@4916 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.jogl/src/Sandbox/oneTriangleAWT.java
@@ -0,0 +1,74 @@
+package Sandbox;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+//import javax.media.opengl.GLProfile;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLCanvas;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * A minimal program that draws with JOGL in an AWT Frame.
+ *
+ * @author Wade Walker
+ */
+public class oneTriangleAWT {
+
+    static {
+        // setting this true causes window events not to get sent on Linux if you run from inside Eclipse
+        //GLProfile.initSingleton( false );
+    }
+
+    public static void main( String [] args ) {
+        //GLProfile glprofile = GLProfile.getDefault();
+        //GLCapabilities glcapabilities = new GLCapabilities( glprofile );
+        //final GLCanvas glcanvas = new GLCanvas( glcapabilities );
+        
+        final GLCanvas glcanvas = new GLCanvas(  );
+
+        glcanvas.addGLEventListener( new GLEventListener() {
+            
+            @Override
+            public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
+                oneTriangle.setup( glautodrawable.getGL(), width, height );
+            }
+            
+            @Override
+            public void init( GLAutoDrawable glautodrawable ) {
+            }
+            
+           // @Override
+           // public void dispose( GLAutoDrawable glautodrawable ) {
+            //}
+            
+            @Override
+            public void display( GLAutoDrawable glautodrawable ) {
+                oneTriangle.render( glautodrawable.getGL(), glautodrawable.getWidth(), glautodrawable.getHeight() );
+            }
+
+			@Override
+			public void displayChanged(GLAutoDrawable arg0, boolean arg1,
+					boolean arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+
+        final Frame frame = new Frame( "One Triangle AWT" );
+        frame.add( glcanvas );
+        frame.addWindowListener( new WindowAdapter() {
+            public void windowClosing( WindowEvent windowevent ) {
+                frame.remove( glcanvas );
+                frame.dispose();
+                System.exit( 0 );
+            }
+        });
+
+        frame.setSize( 640, 480 );
+        frame.setVisible( true );
+    }
+}
+
---
+++ msi.gama.jogl/src/Sandbox/oneTriangleSWT.java
@@ -0,0 +1,84 @@
+package Sandbox;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.opengl.GLCanvas;
+import org.eclipse.swt.opengl.GLData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+
+//import javax.media.opengl.GLProfile;
+import javax.media.opengl.GLContext;
+import javax.media.opengl.GLDrawableFactory;
+
+/**
+ * A minimal program that draws with JOGL in an SWT Composite.
+ *
+ * @author Wade Walker
+ */
+public class oneTriangleSWT {
+
+    static {
+        // setting this true causes window events not to get sent on Linux if you run from inside Eclipse
+       // GLProfile.initSingleton( false );
+    }
+
+    public static void main( String [] args ) {
+        Display display = new Display();
+        final Shell shell = new Shell( display );
+        shell.setText( "OneTriangle SWT" );
+        shell.setLayout( new FillLayout() );
+        shell.setSize( 640, 480 );
+
+        final Composite composite = new Composite( shell, SWT.NONE );
+        composite.setLayout( new FillLayout() );
+
+        GLData gldata = new GLData();
+        gldata.doubleBuffer = true;
+        // need SWT.NO_BACKGROUND to prevent SWT from clearing the window
+        // at the wrong times (we use glClear for this instead)
+        final GLCanvas glcanvas = new GLCanvas( composite, SWT.NO_BACKGROUND, gldata );
+        glcanvas.setCurrent();
+        //GLProfile glprofile = GLProfile.getDefault();
+        final GLContext glcontext = GLDrawableFactory.getFactory().createExternalGLContext();
+
+        // fix the viewport when the user resizes the window
+        glcanvas.addListener( SWT.Resize, new Listener() {
+            public void handleEvent(Event event) {
+                Rectangle rectangle = glcanvas.getClientArea();
+                glcanvas.setCurrent();
+                glcontext.makeCurrent();
+                oneTriangle.setup( glcontext.getGL(), rectangle.width, rectangle.height );
+                glcontext.release();        
+            }
+        });
+
+        // draw the triangle when the OS tells us that any part of the window needs drawing
+        glcanvas.addPaintListener( new PaintListener() {
+            public void paintControl( PaintEvent paintevent ) {
+                Rectangle rectangle = glcanvas.getClientArea();
+                glcanvas.setCurrent();
+                glcontext.makeCurrent();
+                oneTriangle.render(glcontext.getGL(), rectangle.width, rectangle.height);
+                glcanvas.swapBuffers();
+                glcontext.release();        
+            }
+        });
+
+        shell.open();
+
+        while( !shell.isDisposed() ) {
+            if( !display.readAndDispatch() )
+                display.sleep();
+        }
+
+        glcanvas.dispose();
+        display.dispose();
+    }
+}
