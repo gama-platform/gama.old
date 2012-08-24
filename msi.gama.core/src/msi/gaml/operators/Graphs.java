90d7c5dc8ffcb3ecb681b70c5760f7569117c53b
agrignard@gmail.com <agrignard@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-08-24 04:45:06 +0000

    add in_degree_of, out_degree_of and degree_of operator for graph
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@4963 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/operators/Graphs.java
+++ msi.gama.core/src/msi/gaml/operators/Graphs.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benot Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -247,17 +247,51 @@ public class Graphs {
 		if ( graph.containsVertex(vertex) ) { return new GamaList(graph.incomingEdgesOf(vertex)); }
 		return new GamaList();
 	}
+	
+	
+	@operator(value = "in_degree_of")
+	@doc(
+		value = "returns the in degree of a vertex (right-hand operand) in the graph given as left-hand operand.",
+		examples = {"graphEpidemio in_degree_of (node(3))   --:  2",
+					"graphFromMap in_edges_of node({12,45})  --:  [LineString]"},
+		see = "out_degree_of,dregree_of")
+	public static int inDregreeOf(final IGraph graph, final Object vertex) {
+		if (graph == null) throw new GamaRuntimeException("In the in_degree_of operator, the graph should not be null!");
+		if ( graph.containsVertex(vertex) ) { return graph.inDegreeOf(vertex); }
+		return 0;
+	}
 
 	@operator(value = "out_edges_of")
 	@doc(
 		value = "returns the list of the out-edges of a vertex (right-hand operand) in the graph given as left-hand operand.",
-		examples = {"graphEpidemio out_edges_of (node(3))   --:   []",
-					"graphFromMap out_edges_of {12,45}      --: [LineString]"	},
+		examples = {"graphEpidemio out_edges_of (node(3))"},
 		see = "in_edges_of")
 	public static IList outEdgesOf(final IGraph graph, final Object vertex) {
 		if (graph == null) throw new GamaRuntimeException("In the out_edges_of operator, the graph should not be null!");		
 		if ( graph.containsVertex(vertex) ) { return new GamaList(graph.outgoingEdgesOf(vertex)); }
 		return new GamaList();
+	}
+	
+	@operator(value = "out_degree_of")
+	@doc(
+		value = "returns the out degree of a vertex (right-hand operand) in the graph given as left-hand operand.",
+		examples = {"graphEpidemio out_degree_of (node(3))"},
+		see = "in_degree_of, degree_of")
+	public static int outDregreeOf(final IGraph graph, final Object vertex) {
+		if (graph == null) throw new GamaRuntimeException("In the in_degree_of operator, the graph should not be null!");
+		if ( graph.containsVertex(vertex) ) { return graph.outDegreeOf(vertex); }
+		return 0;
+	}
+	
+	@operator(value = "degree_of")
+	@doc(
+		value = "returns the degree (in+out) of a vertex (right-hand operand) in the graph given as left-hand operand.",
+		examples = {"graphEpidemio degree_of (node(3))"},
+		see = "in_degree_of, out_degree_of")
+	public static int dregreeOf(final IGraph graph, final Object vertex) {
+		if (graph == null) throw new GamaRuntimeException("In the in_degree_of operator, the graph should not be null!");
+		if ( graph.containsVertex(vertex) ) { return graph.degreeOf(vertex); }
+		return 0;
 	}
 
 	@operator(value = "neighbours_of")
@@ -490,32 +524,32 @@ public class Graphs {
 	*/
 	
 	
-	// TODO "complete" (pour crï¿½er un graphe complet)
+	// TODO "complete" (pour crer un graphe complet)
 
 	// vertices_to_graph [vertices] with_weights (vertices collect: each.val) -> renvoie un graphe
-	// construit ï¿½ partir des vertex (edges gï¿½nï¿½rï¿½s soit sous la forme d'une paire vertex::vertex,
-	// soit sous la forme d'un lien gï¿½omï¿½trique)
-	// vertices_to_graph [a1, a2, a3] with_weights ([1, 4, 8]) -> mï¿½me chose
+	// construit  partir des vertex (edges gnrs soit sous la forme d'une paire vertex::vertex,
+	// soit sous la forme d'un lien gomtrique)
+	// vertices_to_graph [a1, a2, a3] with_weights ([1, 4, 8]) -> mme chose
 	// edges_to_graph [edges] with_weights (edges collect: each.length) -> renvoie un graphe
-	// construit ï¿½ partir des edges (vertex gï¿½nï¿½rï¿½s soit sous la forme d'une paire edge::edge, soit
-	// sous la forme d'un point pour les gï¿½omï¿½tries)
-	// edges_to_graph [a1::a2, a2::a3] with_weights ([3.0, 1.3]) -> mï¿½me chose
+	// construit  partir des edges (vertex gnrs soit sous la forme d'une paire edge::edge, soit
+	// sous la forme d'un point pour les gomtries)
+	// edges_to_graph [a1::a2, a2::a3] with_weights ([3.0, 1.3]) -> mme chose
 	// add item: v1 to:g weight: 1 -> ajout d'un vertex
-	// add item: v1::v2 to:g weight:1 -> ajout d'un edge gï¿½nï¿½rï¿½ (et des vertex correspondants si
-	// nï¿½cessaire)
+	// add item: v1::v2 to:g weight:1 -> ajout d'un edge gnr (et des vertex correspondants si
+	// ncessaire)
 	// add item: (v1::v2)::e to: g weight: 1 -> edge (ajout d'un edge explicite et des vertex
-	// correspondants si nï¿½cessaire)
+	// correspondants si ncessaire)
 	// remove item: v1::v2 from: g -> remove edge
 	// remove item: o from: g -> remove edge / vertex
-	// put item: e2 at: v1::v2 in: g -> replace/add an edge (on peut aussi faire la mï¿½me chose pour
+	// put item: e2 at: v1::v2 in: g -> replace/add an edge (on peut aussi faire la mme chose pour
 	// remplacer un vertex)
 
-	// TODO Transformer peu ï¿½ peu toutes les primitives (GeometricFunctions, GeometricSkill, etc.)
-	// en opï¿½rateurs (as_graph, as_network, as_triangle_graph, as_complete_graph -- En crï¿½ant les
+	// TODO Transformer peu ˆ peu toutes les primitives (GeometricFunctions, GeometricSkill, etc.)
+	// en oprateurs (as_graph, as_network, as_triangle_graph, as_complete_graph -- En crant les
 	// liens dynamiques correspondants --, as_weighted_graph ...).
 
-	// TODO Ajouter les opï¿½rateurs d'union, d'intersection, d'ï¿½galitï¿½, de diffï¿½rence
+	// TODO Ajouter les oprateurs d'union, d'intersection, d'galit, de diffrence
 
-	// TODO Ajouter des gï¿½nï¿½rateurs spï¿½cifiques a partir de GraphGenerator (pb: quelles classes pour
+	// TODO Ajouter des gnrateurs spcifiques a partir de GraphGenerator (pb: quelles classes pour
 	// les vertices/edges ??
 }
