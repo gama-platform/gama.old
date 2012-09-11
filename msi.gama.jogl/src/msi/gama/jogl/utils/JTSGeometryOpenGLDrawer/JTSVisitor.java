3a0b03a5006f5d5276b98941c8a68188dcf6a0d9
agrignard@gmail.com <agrignard@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-09-11 09:04:38 +0000

    add JTS visitor class
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@5019 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.jogl/src/msi/gama/jogl/utils/JTSGeometryOpenGLDrawer/JTSVisitor.java
@@ -0,0 +1,44 @@
+package msi.gama.jogl.utils.JTSGeometryOpenGLDrawer;
+
+import javax.media.opengl.GL;
+
+import com.vividsolutions.jts.geom.CoordinateSequence;
+import com.vividsolutions.jts.geom.CoordinateSequenceFilter;
+
+public class JTSVisitor implements CoordinateSequenceFilter{
+	
+	
+	private GL myGl;
+	
+	public JTSVisitor(GL gl){
+		myGl=gl;
+	}
+
+	@Override
+	public void filter(CoordinateSequence seq, int i) {
+		// TODO Auto-generated method stub
+		if (String.valueOf(seq.getCoordinate(i).z).equals("NaN") == true) {
+		  myGl.glVertex3f((float)seq.getX(i%seq.size()), (float)(-seq.getY(i%seq.size())), 0.0f);
+		  myGl.glVertex3f((float)seq.getX((i+1)%seq.size()), (float)(-seq.getY((i+1)%seq.size())), 0.0f);
+		}
+		else{
+		  myGl.glVertex3f((float)seq.getX(i%seq.size()), (float)(-seq.getY(i%seq.size())), (float)seq.getCoordinate(i%seq.size()).z);
+		  myGl.glVertex3f((float)seq.getX((i+1)%seq.size()), (float)(-seq.getY((i+1)%seq.size())), (float)seq.getCoordinate((i+1)%seq.size()).z);
+		}
+		
+		
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isGeometryChanged() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
