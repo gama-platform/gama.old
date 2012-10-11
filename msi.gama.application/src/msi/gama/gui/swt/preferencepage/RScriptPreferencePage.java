0d5c027271e8b45f57cc4a0831645175fe77df8a
hqnghi88@gmail.com <hqnghi88@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-10-11 14:41:22 +0000

    fix R config page for more friendly with other OS
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@5117 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/swt/preferencepage/RScriptPreferencePage.java
+++ msi.gama.application/src/msi/gama/gui/swt/preferencepage/RScriptPreferencePage.java
@@ -9,51 +9,73 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.PlatformUI;
 
-
 public class RScriptPreferencePage extends FieldEditorPreferencePage implements
-    IWorkbenchPreferencePage {
+	IWorkbenchPreferencePage {
+
 	private static Preferences preferences = Preferences.userRoot().node("gama");
 
-  public RScriptPreferencePage() {
-    super(GRID);
+	public RScriptPreferencePage() {
+		super(GRID);
 
-  }
+	}
 
-@Override
-protected void performApply() {
-	// TODO Auto-generated method stub
-//	PlatformUI.getPreferenceStore().putValue("RScript", dfe.getStringValue());
-	preferences.put("RScript", ffe.getStringValue());
-//	Stats.RPath= dfe.getStringValue();
-	super.performApply();
-}
+	@Override
+	protected void performApply() {
+		// TODO Auto-generated method stub
+		// PlatformUI.getPreferenceStore().putValue("RScript", dfe.getStringValue());
+		preferences.put("RScript", ffe.getStringValue());
+		// Stats.RPath= dfe.getStringValue();
+		super.performApply();
+	}
 
-@Override
-public boolean performOk() {
-	return super.performOk();
-}
-FileFieldEditor ffe;
-public void createFieldEditors() {
-	ffe=new FileFieldEditor("RScript", "&Rscript preference:",
-        getFieldEditorParent());
-	ffe.setStringValue(preferences.get("RScript", null));
-    addField(ffe);
+	@Override
+	public boolean performOk() {
+		preferences.put("RScript", ffe.getStringValue());
+		return super.performOk();
+	}
 
-  }
+	FileFieldEditor ffe;
 
-  @Override
-  public void init(IWorkbench workbench) {
-	  if(preferences.get("RScript", null)==null)
-	  {
-		  String os = System.getProperty("os.name");
-			if("Mac OS X".equals(os)){
-				preferences.put("RScript", "/Library/Frameworks/R.framework/Versions/2.15/Resources/bin/exec/x86_64/RScript");
+	public void createFieldEditors() {
+		ffe = new FileFieldEditor("RScript", "&Rscript preference:", getFieldEditorParent());
+		ffe.setStringValue(preferences.get("RScript", null));
+		addField(ffe);
+
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+		if ( preferences.get("RScript", null) == null ) {
+			String os = System.getProperty("os.name");
+			String osbit = System.getProperty("os.arch");
+			if ( os.startsWith("Mac") ) {
+				preferences
+					.put("RScript",
+						"/Library/Frameworks/R.framework/Versions/2.15/Resources/bin/exec/i386/RScript");
+				if ( osbit.endsWith("64") ) {
+					preferences
+						.put("RScript",
+							"/Library/Frameworks/R.framework/Versions/2.15/Resources/bin/exec/x86_64/RScript");
+				}
+			} else if ( os.startsWith("Linux") ) {
+				preferences.put("RScript", "usr/bin/RScript");
+
+				// if(osbit.startsWith("64"))
+				// {
+				// preferences.put("RScript",
+				// "/Library/Frameworks/R.framework/Versions/2.15/Resources/bin/exec/x86_64/RScript");
+				// }
 			}
-			if("Windows".equals(os)){
+			if ( os.startsWith("Windows") ) {
+				System.out.println(osbit);
 				preferences.put("RScript", "C:\\Program Files\\R\\R-2.15.1\\bin\\Rscript.exe");
-			}	
-	  }
+				if ( osbit.endsWith("64") ) {
+					preferences.put("RScript",
+						"C:\\Program Files\\R\\R-2.15.1\\bin\\x64\\Rscript.exe");
+				}
+			}
+		}
 
-    setDescription("A preference page to config RScript path");
-  }
-} 
+		setDescription("A preference page to config RScript path");
+	}
+}
