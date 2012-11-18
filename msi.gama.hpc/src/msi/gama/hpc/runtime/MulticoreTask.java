c47811856edfe4e564c246232ca9bbf5de230711
nhanhothe <nhanhothe@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-11-18 14:31:46 +0000

    multi-core executor
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@5466 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.hpc/src/msi/gama/hpc/runtime/MulticoreTask.java
@@ -0,0 +1,126 @@
+package msi.gama.hpc.runtime;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MulticoreTask implements Runnable {
+	
+	private String path;
+	private String inpFile;
+	private String outDir;
+	
+	private MulticoreRuntime mr;
+	
+	public MulticoreTask(MulticoreRuntime mr, String path, String inpFile, String outDir) {
+		this.path = path;
+		this.inpFile = inpFile;
+		this.outDir = outDir;
+		this.setMr(mr);
+	}
+
+	public void startObservator() {
+		mr.start();
+	}
+	
+	public void stopObservator() {
+		mr.stop();
+	}
+	
+	public String getName(){
+		return inpFile;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		mr.start();
+		launchCommandLineGama(path, inpFile, outDir);
+	}
+	
+	public void launchCommandLineGama(String path, String inpFile, String outDir) {
+		outDir = outDir + File.separator + getDirName(inpFile);
+		System.out.println("inpFile " + inpFile);
+		System.out.println("newOutDir " + outDir);
+		String os = System.getProperty("os.name");
+
+		List<String> commands;
+		if ( os.startsWith("Windows") ) {
+			commands =
+				new ArrayList<String>((Arrays.asList("cmd.exe", "/C", "start gamaHeadless.bat " +
+					inpFile + " " + outDir)));
+		} else {
+
+			commands =
+				new ArrayList<String>((Arrays.asList("sh", "gamaHeadless.sh", inpFile, outDir)));
+		}
+
+		Process process = null;
+		
+		try {
+			ProcessBuilder pb = new ProcessBuilder();
+			pb.directory(new File(path));
+			pb.command(commands);
+			process = pb.start();
+			
+			InputStream is = process.getInputStream();
+			InputStream err = process.getErrorStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+
+			String line;
+			System.out.printf("Output of running %s is:\n", Arrays.toString(commands.toArray()));
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			InputStreamReader isrerr = new InputStreamReader(err);
+			BufferedReader brerr = new BufferedReader(isrerr);
+
+			System.out.printf("Error of running %s is:\n", Arrays.toString(commands.toArray()));
+			while ((line = brerr.readLine()) != null) {
+				System.out.println(line);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			  try {
+				  process.waitFor();
+				  process.getInputStream().close();
+				  process.getOutputStream().close();
+				  process.getErrorStream().close(); 
+				  process.destroy();
+
+			  } catch (Exception ioe) {
+				  ioe.printStackTrace();
+			  }
+		}
+	}
+	
+	public MulticoreRuntime getMr() {
+		return mr;
+	}
+
+	public void setMr(MulticoreRuntime mr) {
+		this.mr = mr;
+	}
+	
+	private static String getDirName(final String fileName) {
+		File file = new File(fileName);
+		String name = null;
+		String s = file.getName();
+		int i = s.lastIndexOf('.');
+		if ( i > 0 && i < s.length() - 1 ) {
+			name = s.substring(0, i);
+		}
+		return name;
+	}
+
+}
