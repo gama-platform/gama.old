8d3dfaa71142771e109e444156ea012ae4b5abe7
hqnghi88@gmail.com <hqnghi88@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2012-11-19 04:01:16 +0000

    Batch chart view
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@5502 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/views/HeadlessParam.java
+++ msi.gama.application/src/msi/gama/gui/views/HeadlessParam.java
@@ -1,58 +1,50 @@
 package msi.gama.gui.views;
 
-import msi.gama.common.interfaces.EditorListener;
 import msi.gama.common.util.GuiUtils;
-import msi.gama.gui.parameters.EditorFactory;
-import msi.gama.gui.parameters.FileEditor;
-import msi.gama.headless.executor.BatchExecutor;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
-import msi.gaml.expressions.IExpression;
-import msi.gaml.types.IType;
-import msi.gaml.types.Types;
-import org.eclipse.core.internal.resources.refresh.win32.Convert;
-import org.eclipse.jface.preference.DirectoryFieldEditor;
-import org.eclipse.jface.preference.FileFieldEditor;
-import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.*;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
-import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.ui.part.ViewPart;
+import msi.gama.headless.executor.BatchExecutor;
+import msi.gama.hpc.gui.*;
 
 public class HeadlessParam extends ViewPart {
 
 	public static final String ID = GuiUtils.HEADLESSPARAM_ID;
-
 
 	public HeadlessParam() {
 		super();
 	}
 
 
-	public void createPartControl(Composite parent) {
-		 GridLayout parentLayout = new GridLayout(3, false);
-		 parentLayout.marginWidth = 0;
-		 parentLayout.marginHeight = 0;
-		 parentLayout.verticalSpacing = 0;
-		 parent.setLayout(parentLayout);
 
-//		RowLayout rowLayout = new RowLayout();
-//		rowLayout.wrap = true;
-//		rowLayout.pack = true;
-//		rowLayout.justify = true;
-//		rowLayout.type = SWT.VERTICAL;
-//		rowLayout.marginLeft = 5;
-//		rowLayout.marginTop = 5;
-//		rowLayout.marginRight = 5;
-//		rowLayout.marginBottom = 5;
-//		rowLayout.spacing = 0;
-//
-//		parent.setLayout(rowLayout);
 
-		// FileFieldEditor fe = new FileFieldEditor("fileSelect", "Select File: ", parent);
-		// String[] extensions = new String[] { "*.*" }; // NON-NLS-1
-		// fe.setFileExtensions(extensions);
+
+	public void createPartControl(final Composite parent) {
+
+		// GridLayout parentLayout = new GridLayout(3, true);
+		// parentLayout.marginWidth = 0;
+		// parentLayout.marginHeight = 0;
+		// parentLayout.verticalSpacing = 0;
+		// parent.setLayout(parentLayout);
+
+		// RowLayout rowLayout = new RowLayout();
+		// rowLayout.wrap = true;
+		// rowLayout.pack = true;
+		// rowLayout.justify = true;
+		// rowLayout.type = SWT.VERTICAL;
+		// rowLayout.marginLeft = 5;
+		// rowLayout.marginTop = 5;
+		// rowLayout.marginRight = 5;
+		// rowLayout.marginBottom = 5;
+		// rowLayout.spacing = 0;
+		//
+		// parent.setLayout(rowLayout);
+
+
 		final DirectoryFieldEditor de_bin =
 			new DirectoryFieldEditor("the GAMA binary folder", "Select GAMA binary folder", parent);
 
@@ -60,12 +52,19 @@ public class HeadlessParam extends ViewPart {
 			new DirectoryFieldEditor("the Input folder", "Select input folder", parent);
 		final DirectoryFieldEditor de_out =
 			new DirectoryFieldEditor("the Output folder", "Select output folder", parent);
-		Label lbl = new Label(parent, 0);
-		lbl.setText("Cores numbers");
-		final IntegerFieldEditor ie=new IntegerFieldEditor("Cores number", "Enter number of cores", parent);
 
+		final IntegerFieldEditor ie =
+			new IntegerFieldEditor("Cores number", "Enter number of cores", parent);
+		// parent.pack();
+		
 		Button b = new Button(parent, SWT.PUSH);
 		b.setText("Run simulation");
+
+		final FileFieldEditor fe = new FileFieldEditor("the XML file for charts display", "Select XML file: ", parent);
+		String[] extensions = new String[] { "*.xml" }; // NON-NLS-1
+		fe.setFileExtensions(extensions);
+
+
 		b.addMouseListener(new MouseListener() {
 
 			@Override
@@ -75,9 +74,9 @@ public class HeadlessParam extends ViewPart {
 			}
 
 			@Override
-			public void mouseDown(MouseEvent e) throws GamaRuntimeException{
+			public void mouseDown(MouseEvent e) throws GamaRuntimeException {
 				// TODO Auto-generated method stub
-				
+
 				BatchExecutor.submitExperiment(de_bin.getStringValue(), de_inp.getStringValue(),
 					de_out.getStringValue(), ie.getIntValue());
 				// BatchExecutor.submitExperiment("C://Users//Administrator//Desktop//GAMA//eclipse",
@@ -92,13 +91,37 @@ public class HeadlessParam extends ViewPart {
 			}
 		});
 
-	}
+		Button b1 = new Button(parent, SWT.PUSH);
+		b1.setText("Draw charts");
 
+		b1.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) throws GamaRuntimeException {
+				HeadlessChart.xmlfilename=fe.getStringValue();
+				GuiUtils.showHeadlessChartView();
+
+			}
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
---
+++ msi.gama.application/src/msi/gama/hpc/gui/Simulation.java
@@ -0,0 +1,56 @@
+package msi.gama.hpc.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import msi.gama.hpc.gui.Result;
+
+public class Simulation {
+
+	/**
+	 * Variable listeners
+	 */
+
+	public ArrayList<Result> result;
+
+	/**
+	 * current step
+	 */
+	private int step;
+
+	/**
+	 * id of current experiment
+	 */
+	private int experimentID;
+	public int maxStep;
+
+	public void addResult(Result p) {
+		 this.result.add(p);
+	}
+
+	public Simulation(int expId) {
+		this.experimentID = expId;
+		result = new ArrayList<Result>();
+		initialize();
+	}
+
+	public void setup() {
+		this.step = 0;
+		// this.model.setup();
+	}
+
+	public int getExperimentID() {
+		return experimentID;
+	}
+
+	public void setExperimentID(int experimentID) {
+		this.experimentID = experimentID;
+	}
+
+	public void initialize() {
+		// results = new Vector<Result>();
+
+	}
+
+}
