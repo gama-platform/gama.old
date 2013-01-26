a17183f350781787a8d08b392ba6872799749bea
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-01-26 09:01:45 +0000

    Further refinements of the implementation of equations (including the EquationStatement).
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@5811 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/statements/SolveStatement.java
+++ msi.gama.core/src/msi/gaml/statements/SolveStatement.java
@@ -9,44 +9,40 @@ import msi.gama.precompiler.GamlAnnotations.symbol;
 import msi.gama.precompiler.*;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
-import msi.gama.util.*;
-import msi.gaml.compilation.ISymbol;
 import msi.gaml.descriptions.IDescription;
 import msi.gaml.types.IType;
-import org.apache.commons.math3.exception.*;
 import org.apache.commons.math3.ode.*;
+import org.apache.commons.math3.ode.nonstiff.ClassicalRungeKuttaIntegrator;
 
-@facets(value = { @facet(name = IKeyword.METHOD, type = IType.STRING_STR /* CHANGE */, optional = false) }, omissible = IKeyword.METHOD)
+@facets(value = { @facet(name = IKeyword.EQUATION, type = IType.ID, optional = false),
+	@facet(name = IKeyword.METHOD, type = IType.STRING_STR /* CHANGE */, optional = false)
+/** Numerous other facets to plan : step, init, etc.) **/
+}, omissible = IKeyword.EQUATION)
 @symbol(name = { IKeyword.SOLVE }, kind = ISymbolKind.SEQUENCE_STATEMENT, with_sequence = true)
-@inside(symbols = IKeyword.SOLVER)
-public class SolveStatement extends AbstractStatementSequence implements
-	FirstOrderDifferentialEquations {
+@inside(kinds = ISymbolKind.SPECIES)
+public class SolveStatement extends AbstractStatementSequence {
 
-	final IList<EquationStatement> equations = new GamaList();
-	IScope currentScope;
 	Solver solver;
+	FirstOrderDifferentialEquations equations;
 
 	// Have the same organization as in DrawStatement :
-	// The statement contains an abstract subclass called "Solver"; Different solvers (1st order,
-	// 2nd order, etc.) are then subclasses of this one. And the statement only calls the one which
-	// has been chosen
+	// The statement contains an abstract subclass called "Solver"; Different solvers (maybe
+	// corresponding to different integrators?) are then subclasses of this one. And the statement
+	// only calls the one which has been chosen at the beginning.
 	// Find a way to declare an initial state (either with the "with:" facet, or using assignments
 	// in the body of "solve")
 
 	public SolveStatement(final IDescription desc) {
 		super(desc);
-		// Based on the facets, choose a solver and init it;
-		solver = new FirstOrderSolver();
-	}
-
-	@Override
-	public void setChildren(final List<? extends ISymbol> commands) {
-		for ( ISymbol s : commands ) {
-			if ( s instanceof EquationStatement ) {
-				equations.add((EquationStatement) s);
+		List<IDescription> statements = desc.getSpeciesContext().getChildren();
+		String eqName = getFacet(IKeyword.EQUATION).literalValue();
+		for ( IDescription s : statements ) {
+			if ( s instanceof SystemOfEquationsStatement && s.getName().equals(eqName) ) {
+				equations = (SystemOfEquationsStatement) s;
 			}
 		}
-		super.setChildren(commands);
+		// Based on the facets, choose a solver and init it;
+		solver = new FirstOrderSolver();
 	}
 
 	@Override
@@ -55,45 +51,18 @@ public class SolveStatement extends AbstractStatementSequence implements
 		return super.privateExecuteIn(scope);
 	}
 
-	@Override
-	public void computeDerivatives(final double arg0, final double[] arg1, final double[] arg2)
-		throws MaxCountExceededException, DimensionMismatchException {
-		for ( int i = 0, n = getDimension(); i < n; i++ ) {
-			EquationStatement s = equations.get(i);
-			arg2[i] = (Double) s.executeOn(currentScope);
-		}
-	}
-
-	@Override
-	public int getDimension() {
-		return equations.size();
-	}
-
-	@Override
-	public void leaveScope(final IScope scope) {
-		currentScope = null;
-		super.leaveScope(scope);
-	}
-
-	@Override
-	public void enterScope(final IScope scope) {
-		super.enterScope(scope);
-		currentScope = scope;
-	}
-
 	abstract class Solver {
 
 		// Declare the integrator using facets values (name, parameters)
 		// Declare a step handler also using facets values
+		// Should the solver implement StepHandler or use a given StepHandler ?
 
-		// Call the integrator, which should call computeDerivatives
+		// Call the integrator, which should call computeDerivatives on the system of equations;
 		abstract void solve(IScope scope);
 
 	}
 
-	class FirstOrderSolver extends Solver
-	/* implements FirstOrderDifferentialEquations */
-	{
+	class FirstOrderSolver extends Solver {
 
 		FirstOrderIntegrator integrator;
 
@@ -101,11 +70,16 @@ public class SolveStatement extends AbstractStatementSequence implements
 			// initialize the integrator, the step handler, etc.
 			// This class has access to the facets of the statement
 			// ie. getFacet(...)
+			//
+
+			// Just a trial
+			integrator = new ClassicalRungeKuttaIntegrator(1.0);
 		}
 
 		@Override
 		void solve(final IScope scope) {
-			// call the integrator
+			// call the integrator.
+			// We need to save the state (previous time the integrator has been solved, etc.)
 		}
 
 	}
