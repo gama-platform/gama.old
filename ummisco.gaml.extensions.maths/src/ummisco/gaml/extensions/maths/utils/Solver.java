52ef2bb237307c509a4578970ca08ca3ac6b232d
hqnghi88 <hqnghi88@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-01-29 04:00:27 +0000

    new Maths plugin
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@5853 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ ummisco.gaml.extensions.maths/src/ummisco/gaml/extensions/maths/utils/Solver.java
@@ -0,0 +1,17 @@
+package ummisco.gaml.extensions.maths.utils;
+
+import ummisco.gaml.extensions.maths.statements.SingleEquationStatement;
+import ummisco.gaml.extensions.maths.statements.SystemOfEquationsStatement;
+import msi.gama.runtime.IScope;
+
+public abstract class Solver {
+
+	// Declare the integrator using facets values (name, parameters)
+	// Declare a step handler also using facets values
+	// Should the solver implement StepHandler or use a given StepHandler ?
+
+	// Call the integrator, which should call computeDerivatives on the system of equations;
+	public abstract void solve(IScope scope, SingleEquationStatement eq);
+
+
+}
