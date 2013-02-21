943c36616e7a5fae1a4b4ee2dffc8512151f7919
thai.truongminh@gmail.com <thai.truongminh@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-02-21 03:56:39 +0000

    Add OLAP testing class
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@6024 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ irit.maelia.gaml.additions/src/irit/gaml/skills/MDXApp.java
@@ -0,0 +1,174 @@
+package irit.gaml.skills;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+
+import org.olap4j.*;
+import org.olap4j.metadata.Member;
+import org.olap4j.metadata.NamedList;
+import org.olap4j.layout.RectangularCellSetFormatter;
+
+
+public class MDXApp { 
+
+	public static void main(String [] args) throws Exception{
+		Connection conn = null;
+		OlapWrapper wrapper;
+		OlapConnection olapConnection;
+		//try{
+			Class.forName("org.olap4j.driver.xmla.XmlaOlap4jDriver");
+			Connection connection =
+			    DriverManager.getConnection(
+//			    			    "jdbc:xmla:Server=http://tmthai/olap/msmdpump.dll;"
+//			    			    + "Cache=org.olap4j.driver.xmla.cache.XmlaOlap4jNamedMemoryCache;"
+//			    			    + "Cache.Mode=LFU;Cache.Timeout=600;Cache.Size=100");	    		
+//			    		"jdbc:xmla:Server=http://tmthai/olap/msmdpump.dll;Catalog=Northwind ASP");
+						"jdbc:xmla:Server=http://tmthai/olap/msmdpump.dll;");
+			    		//"jdbc:xmla:Server=http://localhost/olap/msxisapi.dll;Catalog=Northwind ASP");
+			 wrapper = (OlapWrapper) connection;
+			 olapConnection = wrapper.unwrap(OlapConnection.class);	
+			// OlapStatement statement =   olapConnection.createStatement();
+			 OlapStatement statement = (OlapStatement) connection.createStatement();
+			 System.out.println("OK");
+			 System.out.println(olapConnection.getCatalog());
+			//-------------------------------------------------------------------------------
+//			 CellSet cellSet =
+//					    statement.executeOlapQuery(
+//					        //"SELECT {[Measures].[Price]} ON COLUMNS,"
+//			"SELECT { [Measures].[unitprice], [Measures].[Quantity], [Measures].[Price] } ON COLUMNS,"
+//					        + "  {[Product].[Product Category].[All].CHILDREN} ON ROWS"
+//					        + " FROM [Northwind Star]");
+			 //-------------------------------------------------------------------------------
+//			 CellSet cellSet =
+//					    statement.executeOlapQuery(
+//			 "SELECT { [Product].[Product Category].[All].CHILDREN } ON COLUMNS ,"
+//
+//			 +"{ [Measures].[unitprice], [Measures].[Quantity], [Measures].[Price] } ON ROWS  "
+//
+//			+" FROM [Northwind Star]");
+//			//-------------------------------------------------------------------------------
+//			 CellSet cellSet =
+//					    statement.executeOlapQuery(
+//					 "SELECT { [Measures].[unitprice], [Measures].[Quantity], [Measures].[Price] } ON COLUMNS ,"
+//
+//					 +"{ { { [Time].[Year].[All].CHILDREN } * { [Product].[Product Category].[All].CHILDREN } } } ON ROWS " 
+//
+//					 + "  FROM [Northwind Star]");
+			//-------------------------------------------------------------------------------		
+			 
+			 
+//			 CellSet cellSet =
+//					    statement.executeOlapQuery(			 
+//			 "SELECT { [Measures].[Quantity], [Measures].[Price] } ON COLUMNS ,"
+//
+//			 +"{ { { [Time].[Year].[All].CHILDREN } * { [Product].[Product Category].[All].CHILDREN } } } ON ROWS  "
+//
+//			 +" FROM [Northwind Star] "
+//
+//			 +" WHERE ( [Customer].[Company Name].[All] ) ");
+			 //------------------------------------------------------------------------------------
+			 CellSet cellSet =
+					    statement.executeOlapQuery(			 
+			 "SELECT { [Measures].[Quantity], [Measures].[Price] } ON COLUMNS ,"
+
+			+" { { { [Time].[Year].[All].CHILDREN } * "
+			+" { [Product].[Product Category].[All].CHILDREN } * "
+			+"{ [Customer].[Company Name].&[Alfreds Futterkiste], " +
+			"[Customer].[Company Name].&[Ana Trujillo Emparedados y helados], " +
+			"[Customer].[Company Name].&[Antonio Moreno Taquería] } } } ON ROWS " 
+
+			 +"FROM [Northwind Star] ");
+			//------------------------------------------------------------------------------------
+			 System.out.println("MDX OK");
+			 
+			 List<CellSetAxis> cellSetAxes = cellSet.getAxes();
+			  // Print headings.
+		        System.out.print("\t\t");
+		        CellSetAxis columnsAxis = cellSetAxes.get(Axis.COLUMNS.axisOrdinal());
+		        for (Position position : columnsAxis.getPositions()) {
+		            Member measure = position.getMembers().get(0);
+		            System.out.print(measure.getName()+'\t');
+		        }
+		        System.out.print("\n");
+		        // Print rows.
+		        CellSetAxis rowsAxis = cellSetAxes.get(Axis.ROWS.axisOrdinal());
+		        int cellOrdinal = 0;
+		        for (Position rowPosition : rowsAxis.getPositions()) {
+		            boolean first = true;
+		            // print member on each row
+		            for (Member member : rowPosition.getMembers()) {
+		                if (first) {
+		                    first = false;
+		                } else {
+		                    System.out.print("\t");
+		                }
+		                System.out.print("ROW["+rowPosition.getOrdinal()+","+member.getOrdinal()+"]:"+member.getName());
+		            }
+		            System.out.print("\t\t");
+		            // Print the value of the cell in each column.
+		            for (Position columnPosition : columnsAxis.getPositions()) {
+		                // Access the cell via its ordinal. The ordinal is kept in step
+		                // because we increment the ordinal once for each row and
+		                // column.
+		                Cell cell = cellSet.getCell(cellOrdinal);
+
+		                // Just for kicks, convert the ordinal to a list of coordinates.
+		                // The list matches the row and column positions.
+		                List<Integer> coordList =
+		                    cellSet.ordinalToCoordinates(cellOrdinal);
+		                assert coordList.get(0) == rowPosition.getOrdinal();
+		                assert coordList.get(1) == columnPosition.getOrdinal();
+
+		                ++cellOrdinal;
+
+		                System.out.print('\t');
+		                System.out.print(cell.getFormattedValue());
+		            }
+		            System.out.println();
+		        }
+		     // Now, nicely formatted.
+		        System.out.println();
+		        final PrintWriter pw = new PrintWriter(System.out);
+		        new RectangularCellSetFormatter(false).format(cellSet, pw);
+		        pw.flush();
+		     // Iteration over a two-axis query
+		        List<CellSetAxis> cellSetAxes1 = cellSet.getAxes();
+		        //System.out.println("size"+cellSetAxes1.size());
+		        //Meta Data---------------------------------------------------------------------------
+		        CellSetMetaData cellSetMetaData=cellSet.getMetaData();
+		       // System.out.println("Column Count:"+cellSetMetaData.getColumnCount());
+		        System.out.println("Number of Axes:"+cellSetMetaData.getAxesMetaData().size());
+		        System.out.println("Cube Name:"+cellSetMetaData.getCube().getName());
+		        
+		        //------------------------------------------------------------------------------------
+//		        System.out.println(cellSetAxes1.get( Axis.ROWS.axisOrdinal() ));
+//		        for (
+//		          Position axis_0 
+//		          : cellSet.getAxes().get( Axis.ROWS.axisOrdinal() ).getPositions()) 
+//		        {
+//		          for (
+//		            Position axis_1
+//		            : cellSet.getAxes().get(Axis.COLUMNS.axisOrdinal()).getPositions())
+//		          {
+//		            Object value = cellSet.getCell(axis_0, axis_1).getValue();
+//		            System.out.print("("+ axis_0.getOrdinal() + " ; "+  axis_1.getOrdinal() +")" );	System.out.print('\t');	
+//		            //System.out.print(value.toString()+'\t');	
+//		          }
+//		          System.out.println();
+//		        }
+		        // Close the statement and connection.
+		        statement.close();
+		        connection.close();
+			 //CellSetMetaData cellMetaData = cellSet.getMetaData();
+			 //System.out.println("Column count:"+ Integer.toString(cellMetaData.getColumnCount()));
+			 //System.out.println( (cellMetaData).toString());
+//		}catch (Exception e){
+//			System.err.println("Error:"+e);
+//		}
+		
+	}
+
+
+}
---
+++ irit.maelia.gaml.additions/src/irit/gaml/skills/MDXConnection.java
@@ -0,0 +1,129 @@
+package irit.gaml.skills;
+
+import java.sql.*;
+
+import org.olap4j.*;
+
+/*
+ * @Author  
+ *     TRUONG Minh Thai
+ *     Fredric AMBLARD
+ *     Benoit GAUDOU
+ *     Christophe Sibertin-BLANC
+ * 
+ * 
+ * SQLConnection:   supports the method
+ * - connectDB: make a connection to DBMS.
+ * - selectDB: connect to DBMS and run executeQuery to select data from DBMS.
+ * - executeUpdateDB: connect to DBMS and run executeUpdate to update/insert/delete/drop/create data
+ * on DBMS.
+ * 
+ * Created date: 18-Jan-2013
+ * Modified:
+ *     dd-mm-yyyy: description
+ *        
+ * Last Modified: 18-Jan-2013
+ */
+public class MDXConnection {
+	static final boolean DEBUG = false; // Change DEBUG = false for release version
+	public static final String MONDRIAN ="MONDRIAN";
+	public static final String POSTGRES="postgres";
+	public static final String MSAS ="MSAS"; //Micrsoft SQL Server Analysis Services
+	public static final String SQLITE="sqlite";
+	public static final String GEOMETRYTYPE="GEOMETRY";
+	static final String MONDRIANDriver = new String("mondrian.olap4j.MondrianOlap4jDriver");
+	static final String MSASDriver = new String("org.olap4j.driver.xmla.XmlaOlap4jDriver");
+	
+	static final String SQLITEDriver = new String("org.sqlite.JDBC");
+	static final String POSTGRESDriver= new String("org.postgresql.Driver");
+	
+	static String vender="";
+	static String url="";
+	static String port="";
+	static String dbName="";
+	static String userName="";
+	static String password="";
+	
+	public MDXConnection(String vender)
+	{
+		this.vender=vender;
+	}
+	public MDXConnection(String venderName,String database)
+	{
+		this.vender=venderName;
+		this.dbName=database;
+	}
+	public MDXConnection()
+	{
+	}
+
+	public MDXConnection(String venderName,String url,String port,
+			String dbName, String userName,String password)  
+	{
+		this.vender=venderName;
+		this.url=url;
+		this.port=port;
+		this.dbName=dbName;
+		this.userName=userName;
+		this.password=password;	
+	}
+	
+	/*
+	 * Make a connection to Multidimensional Database Server
+	 * 
+	 * @syntax: do action: connectMDB {
+	 * arg vendorName value: vendorName; //MONDRIAN/MSAS
+	 * arg url value: urlvalue;
+	 * arg port value: portvaluse;
+	 * arg dbName value: dbnamevalue;
+	 * arg usrName value: usrnamevalue;
+	 * arg password value: pwvaluse;
+	 * }
+	 */
+	public OlapConnection connectMDB()
+			throws ClassNotFoundException, InstantiationException, SQLException, IllegalAccessException 
+	{
+			Connection conn = null;
+			OlapWrapper wrapper;
+			OlapConnection olapConnection;
+			
+			try {
+				if ( vender.equalsIgnoreCase(MONDRIAN) ) {
+					Class.forName(MONDRIANDriver).newInstance();
+					conn =
+						DriverManager.getConnection("jdbc:mondrian:Jdbc=jdbc:odbc:" + url + ":" + port + "/" + dbName,
+							userName, password);
+					 wrapper = (OlapWrapper) conn;
+					 olapConnection = wrapper.unwrap(OlapConnection.class);
+				} else if ( vender.equalsIgnoreCase(MSAS) ) {
+					Class.forName("org.olap4j.driver.xmla.XmlaOlap4jDriver");
+					Connection connection =
+					    DriverManager.getConnection(
+					    		"jdbc:xmla:Server=http://localhost/xmla/msxisapi.dll");
+					 wrapper = (OlapWrapper) connection;
+					 olapConnection = wrapper.unwrap(OlapConnection.class);	
+				} else {
+					throw new ClassNotFoundException("MDXConnection.connectMDB: The " 
+				                                       + vender
+				                                       + " is not supported!");
+				}
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				throw new ClassNotFoundException(e.toString());
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new InstantiationException(e.toString());
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new IllegalAccessException(e.toString());
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new SQLException(e.toString());
+			}
+			return olapConnection;
+	}
+
+}
