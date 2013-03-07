a7dbd83350d71b59a6bab071a719460aa5b17d8e
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-03-07 14:14:58 +0000

    Remove commented code.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@6069 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/navigator/GamaNavigator.java
+++ msi.gama.application/src/msi/gama/gui/navigator/GamaNavigator.java
@@ -1,5 +1,5 @@
 /*
- * GAMA - V1.4  http://gama-platform.googlecode.com
+ * GAMA - V1.4 http://gama-platform.googlecode.com
  * 
  * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
  * 
@@ -7,7 +7,7 @@
  * 
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
- * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen  (Batch, GeoTools & JTS), 2009-2012
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
  * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
@@ -24,50 +24,9 @@ public class GamaNavigator extends CommonNavigator {
 
 	String OPEN_BROWSER_COMMAND_ID = "msi.gama.application.commands.OpenBrowser";
 
-	// @Override
-	// public CommonViewer getCommonViewer() {
-	// return super.getCommonViewer();
-	// }
-
 	@Override
 	protected Object getInitialInput() {
 		return new NavigatorRoot();
 	}
-
-	// @Override
-	// protected void handleDoubleClick(DoubleClickEvent anEvent) {
-	// ICommandService commandService = (ICommandService) getSite()
-	// .getService(ICommandService.class);
-	// Command command = commandService.getCommand(OPEN_BROWSER_COMMAND_ID);
-	//
-	// IWorkbenchWindow window = PlatformUI.getWorkbench()
-	// .getActiveWorkbenchWindow();
-	// ISelectionService selectionService = window.getSelectionService();
-	// ISelection iselection = selectionService.getSelection();
-	//
-	// Object selection = ((IStructuredSelection) iselection)
-	// .getFirstElement();
-	//
-	// if (selection instanceof IResource) {
-	// super.handleDoubleClick(anEvent);
-	// return;
-	// } else {
-	// if (command.isEnabled()) {
-	// IHandlerService handlerService = (IHandlerService) getSite()
-	// .getService(IHandlerService.class);
-	// try {
-	// handlerService
-	// .executeCommand(OPEN_BROWSER_COMMAND_ID, null);
-	// } catch (NotDefinedException e) {
-	// throw new RuntimeException("Could not find open command: "
-	// + OPEN_BROWSER_COMMAND_ID);
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// } else {
-	// super.handleDoubleClick(anEvent);
-	// }
-	// }
-	// }
 
 }
