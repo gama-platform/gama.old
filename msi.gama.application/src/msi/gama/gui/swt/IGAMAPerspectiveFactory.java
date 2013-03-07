d136a6b2d0857cf3e2565eb09921e97cdc067700
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-03-07 14:20:08 +0000

    Removed a useless class
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@6074 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/swt/IGAMAPerspectiveFactory.java
+++
@@ -1,9 +0,0 @@
-package msi.gama.gui.swt;
-
-import msi.gama.runtime.IScope;
-import org.eclipse.ui.IPerspectiveFactory;
-
-
-public interface IGAMAPerspectiveFactory extends IPerspectiveFactory {
-	public void initialise(IScope gamaScope);
-}
