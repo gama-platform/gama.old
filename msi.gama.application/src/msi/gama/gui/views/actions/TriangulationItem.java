df4ca5bc60de6fef7d4961c91f5a2d1edd4cda01
agrignard <agrignard@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-04-22 07:50:38 +0000

    Missing files
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@6393 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.application/src/msi/gama/gui/views/actions/TriangulationItem.java
@@ -0,0 +1,45 @@
+/**
+ * Created by drogoul, 19 janv. 2012
+ * 
+ */
+package msi.gama.gui.views.actions;
+
+import msi.gama.gui.views.*;
+import org.eclipse.jface.action.*;
+
+/**
+ * The class SnapshotItem.
+ * 
+ * @author drogoul
+ * @since 19 janv. 2012
+ * 
+ */
+public class TriangulationItem extends GamaViewItem {
+
+	/**
+	 * @param view
+	 */
+	TriangulationItem(final GamaViewPart view) {
+		super(view);
+		if ( !(view instanceof IViewWithZoom) ) { throw new IllegalArgumentException(); }
+	}
+
+	/**
+	 * @see msi.gama.gui.views.actions.GamaViewItem#createItem()
+	 */
+	@Override
+	protected IContributionItem createItem() {
+		IAction action =
+			new GamaAction("Triangle view: Show triangulated shape", IAction.AS_CHECK_BOX,
+				getImageDescriptor("icons/button_triangulation.png")) {
+
+				@Override
+				public void run() {
+					IViewWithZoom view = (IViewWithZoom) getView();
+					if ( view == null ) { return; }
+					view.toggleTriangulation();
+				}
+			};
+		return new ActionContributionItem(action);
+	}
+}
