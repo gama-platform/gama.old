75ad71e751716319d807d8cb70d93313fc41235b
agrignard <agrignard@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-04-25 11:08:35 +0000

    add icon
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@6431 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.application/src/msi/gama/gui/views/actions/SplitLayerItem.java
@@ -0,0 +1,45 @@
+/**
+ * Created by drogoul, 19 janv. 2012
+ * 
+ */
+package msi.gama.gui.views.actions;
+
+import msi.gama.gui.views.*;
+import org.eclipse.jface.action.*;
+
+/**
+ * The class SnapshotItem.
+ * 
+ * @author drogoul
+ * @since 19 janv. 2012
+ * 
+ */
+public class SplitLayerItem extends GamaViewItem {
+
+	/**
+	 * @param view
+	 */
+	SplitLayerItem(final GamaViewPart view) {
+		super(view);
+		if ( !(view instanceof IViewWithZoom) ) { throw new IllegalArgumentException(); }
+	}
+
+	/**
+	 * @see msi.gama.gui.views.actions.GamaViewItem#createItem()
+	 */
+	@Override
+	protected IContributionItem createItem() {
+		IAction action =
+			new GamaAction("Split layer in 3D", IAction.AS_CHECK_BOX,
+				getImageDescriptor("icons/split_layer_button.png")) {
+
+				@Override
+				public void run() {
+					IViewWithZoom view = (IViewWithZoom) getView();
+					if ( view == null ) { return; }
+					view.toggleSplitLayer();
+				}
+			};
+		return new ActionContributionItem(action);
+	}
+}
