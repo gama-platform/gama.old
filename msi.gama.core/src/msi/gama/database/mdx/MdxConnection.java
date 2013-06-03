7096c041429e8ad98c19ec8472424085af11fd7c
thai.truongminh@gmail.com <thai.truongminh@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-06-03 16:36:15 +0000

    Update new methods for MDXConnection
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@6654 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gama/database/mdx/MdxConnection.java
+++ msi.gama.core/src/msi/gama/database/mdx/MdxConnection.java
@@ -3,11 +3,21 @@ package msi.gama.database.mdx;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+
 import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gama.util.GamaList;
+
+import org.olap4j.Axis;
+import org.olap4j.Cell;
 import org.olap4j.CellSet;
+import org.olap4j.CellSetAxis;
 import org.olap4j.OlapConnection;
+import org.olap4j.OlapDatabaseMetaData;
 import org.olap4j.OlapException;
 import org.olap4j.OlapStatement;
+import org.olap4j.Position;
+import org.olap4j.metadata.Member;
 
 /*
  * @Author  
@@ -84,6 +94,27 @@ public abstract class MdxConnection {
 		}
 	}
 	
+	public String getDatabase() throws GamaRuntimeException
+	{
+		try {
+			return olapConnection.getDatabase();
+		} catch (OlapException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw GamaRuntimeException.error(e.toString());
+		}
+	}
+	
+	public OlapDatabaseMetaData getMetaData() throws GamaRuntimeException
+	{
+		try {
+			return olapConnection.getMetaData();
+		} catch (OlapException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw GamaRuntimeException.error(e.toString());
+		}
+	}
 	/*
 	 * Make a connection to Multidimensional Database Server
 	 */
@@ -93,24 +124,20 @@ public abstract class MdxConnection {
 	 * Select data source with connection was established
 	 */
 	
-	public CellSet selectMDB(String selectComm)
+	public CellSet select(String selectComm)
 	{
 		CellSet resultCellSet=null;
 		Connection conn=null;
 		try {
 			conn = connectMDB();
-			resultCellSet = selectMDB(conn, selectComm);
+			resultCellSet = select(conn, selectComm);
 			conn.close();
 		} catch (SQLException e) {
 
 		}
 		return resultCellSet;
 	}
-	
-	/*
-	 * Select data source with connection was established
-	 */
-	public CellSet selectMDB(Connection connection, String selectComm) throws GamaRuntimeException 
+	public CellSet select(Connection connection, String selectComm) throws GamaRuntimeException 
 	{
 		 CellSet resultCellSet=null;
 		 OlapStatement statement;
@@ -129,7 +156,225 @@ public abstract class MdxConnection {
 		 return resultCellSet;
 	}
 	
-	public void cellSet2List(CellSet cellset){
+
+	/*
+	 * Select data source with connection was established
+	 */
+	public GamaList<Object> selectMDB(String selectComm)  
+	{
+		 CellSet cellSet=select(selectComm);
+		 return cellSet2List(cellSet);
+	}
+
+	public GamaList<Object> selectMDB(Connection connection, String selectComm)  
+	{
+		 CellSet cellSet=select(connection,selectComm);
+		 return cellSet2List(cellSet);
+	}
+	/*
+	 *  Format of Olap query result (GamaList<Object>:
+	 *      Result of OLAP query is transformed to Gamalist<Object> with order:
+	 *      (0): GamaList<String>: List of column names.
+	 *      (1): GamaList<Object>: Row data. it contains List of list and it look like a matrix with structure:
+	 *          (0): the first row data
+	 *          (1): the second row data 
+	 *          ...
+	 *          Each row data contains two element:
+	 *           (0): rowMembers (GamaList<String>: this is a list of members in the row. 
+	 *           (1): cellValues (Gamalist<Object>): This is a list of values in cell column or (we can call measures) 
+	 *      
+	 */
+	public GamaList<Object> cellSet2List(CellSet cellSet){
+		 GamaList<Object> olapResult = new GamaList<Object>();
+		 olapResult.add(this.getColumnsName(cellSet));
+		 olapResult.add(this.getRowsData(cellSet));
+		 return olapResult;
+	}
+	
+	public GamaList<Object> selectMBD(String onColumns, String onRows, String from){
+		String mdxStr = "SELECT " + onColumns + " ON COLUMNS, " + onRows + " ON ROWS " 
+				        + " FROM " + from ;
+		return  selectMDB(mdxStr);
+	}
+	
+	public GamaList<Object> selectMDB(Connection connection, String onColumns, String onRows, String from){
+		String mdxStr = "SELECT " + onColumns + " ON COLUMNS, " + onRows + " ON ROWS " 
+				        + " FROM " + from ;
+		return  selectMDB(connection, mdxStr);
+	}
+	
+	public GamaList<Object> selectMDB(String onColumns, String onRows, String from, String where){
+		String mdxStr = "SELECT " + onColumns + " ON COLUMNS, " + onRows + " ON ROWS " 
+				        + " FROM " + from + " WHERE ( " + where  + " )";
+		return  selectMDB(mdxStr);
+	}
+	
+	public GamaList<Object> selectMDB(Connection connection, String onColumns, String onRows, String from, String where){
+		String mdxStr = "SELECT " + onColumns + " ON COLUMNS, " + onRows + " ON ROWS " 
+				        + " FROM " + from + " WHERE ( " + where  + " )";
+		return  selectMDB(connection, mdxStr);
+	}
+	
+
+	protected GamaList<Object> getColumnsName(CellSet cellSet){
+		GamaList<Object> columnsName = new GamaList<Object>();
+		 List<CellSetAxis> cellSetAxes = cellSet.getAxes();
+		  // get headings.
+		 CellSetAxis columnsAxis = cellSetAxes.get(Axis.COLUMNS.axisOrdinal());
+	     for (Position position : columnsAxis.getPositions()) {
+	            Member measure = position.getMembers().get(0);
+	            columnsName.add(measure.getName());
+	      }
+		return columnsName;
+	     
+	}
+	protected GamaList<Object> getRowsData(CellSet cellSet){
+		GamaList<Object> rowsData = new GamaList<Object>();
+		
+		List<CellSetAxis> cellSetAxes = cellSet.getAxes();
+        CellSetAxis columnsAxis = cellSetAxes.get(Axis.COLUMNS.axisOrdinal());
+        CellSetAxis rowsAxis = cellSetAxes.get(Axis.ROWS.axisOrdinal());
+        int cellOrdinal = 0;
+        for (Position rowPosition : rowsAxis.getPositions()) {
+        	GamaList<Object> row = new GamaList<Object>();
+        	GamaList<Object> rowMembers = new GamaList<Object>();
+            // get member on each row
+            for (Member member : rowPosition.getMembers()) {
+            	rowMembers.add(member.getName());
+            }
+            // get value of the cell in each column.
+            GamaList<Object> cellValues = new GamaList<Object>();
+            for (Position columnPosition : columnsAxis.getPositions()) {
+                // Access the cell via its ordinal. The ordinal is kept in step
+                // because we increment the ordinal once for each row and
+                // column.
+                Cell cell = cellSet.getCell(cellOrdinal);
+                // Just for kicks, convert the ordinal to a list of coordinates.
+                // The list matches the row and column positions.
+                List<Integer> coordList =
+                    cellSet.ordinalToCoordinates(cellOrdinal);
+                assert coordList.get(0) == rowPosition.getOrdinal();
+                assert coordList.get(1) == columnPosition.getOrdinal();
+
+                ++cellOrdinal;
+                cellValues.add(cell.getFormattedValue());
+            }
+            // Add member and value to row 
+            row.add(rowMembers);
+            row.add(cellValues);
+            // Add row to rowsData
+            rowsData.add(row);
+        }
+        return rowsData;
+	     
+	}
+	/*
+	 *  Get all column names of OLAP query
+	 */
+	public GamaList<Object> getAllColummsName(GamaList<Object> olapResult){
+		return (GamaList<Object>) olapResult.get(0);
+	}
+	/*
+	 *  Get all column names of OLAP query
+	 */
+	public Object getColummNameAt(GamaList<Object> olapResult, int cIndex){
+		return this.getAllColummsName(olapResult).get(cIndex);
+	}
+
+	/*
+	 * Get  all rows data 
+	 */
+	public GamaList<Object> getAllRowsData(GamaList<Object> olapResult){
+		return (GamaList<Object>) olapResult.get(1);
+	}	
+	/*
+	 * Get  row data (row members + cell values)  at row index(rIndex) 
+	 */
+	public GamaList<Object> getRowDataAt(GamaList<Object> olapResult, int rIndex){
+		return (GamaList<Object>) getAllRowsData(olapResult).get(rIndex);
+	}
+
+	/*
+	 * Get all row members at row(index) 
+	 */
+	public GamaList<Object> getAllMembersAt(GamaList<Object> olapResult, int rIndex){
+		return (GamaList<Object>) getRowDataAt(olapResult,rIndex).get(0);
+	}
+	/*
+	 * Get  row member at  row index:rIndex ,member index:mIndex) 
+	 */
+	public Object getRowMemberAt(GamaList<Object> olapResult, int rIndex, int mIndex){
+		return (Object) getAllMembersAt(olapResult,rIndex).get(mIndex);
+	}
+	
+	/*
+	 * Get all cell values at index row 
+	 */
+	public GamaList<Object> getAllCellValuesAt(GamaList<Object> olapResult, int rIndex){
+		return (GamaList<Object>) getRowDataAt(olapResult,rIndex).get(1);
+	}
+	/*
+	 * Get  cell value at  row index:rIndex ,cell index:cIndex) 
+	 */
+	public Object getCellValueAt(GamaList<Object> olapResult, int rIndex, int cIndex){
+		return (Object) getAllCellValuesAt(olapResult,rIndex).get(cIndex);
+	}
+	
+	/*
+	 *  print all row data
+	 */
+//	public void printOlapResul(GamaList<Object> rowsData){
+//        int m=rowsData.size();
+//        for (int i=0; i<m;++i){
+//       		System.out.print("row"+ i+":\t");
+//       		GamaList<Object> row= (GamaList<Object>) rowsData.get(i);
+//       		GamaList<Object> members= (GamaList<Object>) row.get(0);
+//       		GamaList<Object> values= (GamaList<Object>) row.get(1);
+//       		// print member
+//       		int k = members.size();
+//       		for (int j=0;j<k;j++){
+//       			System.out.print(members.get(j).toString()+"\t");
+//       		}
+//       		//print value
+//       		int l = values.size();
+//       		for (int j=0;j<l;j++){
+//       			System.out.print(values.get(j).toString()+"\t");
+//       		}
+//       		System.out.println();
+//       	}
+//	}
+//	
+	/*
+	 *  print all row data
+	 */
+	public void printRowsData(GamaList<Object> olapResult){
+		
+        int m=this.getAllRowsData(olapResult).size();
+        for (int rIndex=0; rIndex<m;++rIndex){
+       		System.out.print("row"+ rIndex+":\t");
+       		// print member
+       		int k = this.getRowDataAt(olapResult, rIndex).size();
+       		for (int mIndex=0;mIndex<k;mIndex++){
+       			System.out.print(this.getRowMemberAt(olapResult, rIndex, mIndex).toString()+"\t");
+       		}
+       		//print value
+       		int l = this.getAllCellValuesAt(olapResult, rIndex).size();
+       		for (int cIndex=0;cIndex<l;++cIndex){
+       			System.out.print(this.getCellValueAt(olapResult, rIndex, cIndex).toString()+"\t");
+       		}
+       		System.out.println();
+       	}
+	}
+	/*
+	 *  print all column names
+	 */
+	
+	public void prinColumnsName(GamaList<Object> olapResult){
+		int m=this.getAllColummsName(olapResult).size();
+		for (int cIndex=0; cIndex<m; ++ cIndex){
+			System.out.print(this.getColummNameAt(olapResult, cIndex).toString()+"\t");
+		}
 		
 	}
-}
+	
+}// end class
