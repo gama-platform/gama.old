7e7f45abdb383aad92f1ba7deebd5fffca53b87d
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-06-04 16:06:04 +0000

    Fixed compilation error.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@6657 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gaml.architecture.simplebdi/src/msi/gaml/architecture/simplebdi/SimpleBdiStatement.java
+++ msi.gaml.architecture.simplebdi/src/msi/gaml/architecture/simplebdi/SimpleBdiStatement.java
@@ -1,12 +1,11 @@
 package msi.gaml.architecture.simplebdi;
 
 import msi.gama.common.interfaces.IKeyword;
-import msi.gama.precompiler.ISymbolKind;
 import msi.gama.precompiler.GamlAnnotations.facet;
 import msi.gama.precompiler.GamlAnnotations.facets;
 import msi.gama.precompiler.GamlAnnotations.inside;
 import msi.gama.precompiler.GamlAnnotations.symbol;
-import msi.gama.runtime.ExecutionStatus;
+import msi.gama.precompiler.*;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gaml.descriptions.IDescription;
@@ -15,24 +14,24 @@ import msi.gaml.operators.Cast;
 import msi.gaml.statements.AbstractStatementSequence;
 import msi.gaml.types.IType;
 
-@symbol(name = {SimpleBdiArchitecture.PLAN, SimpleBdiArchitecture.PERCEIVE}, kind = ISymbolKind.BEHAVIOR, with_sequence = true)
+@symbol(name = { SimpleBdiArchitecture.PLAN, SimpleBdiArchitecture.PERCEIVE }, kind = ISymbolKind.BEHAVIOR, with_sequence = true)
 @inside(kinds = { ISymbolKind.SPECIES })
 @facets(value = { @facet(name = IKeyword.WHEN, type = IType.BOOL, optional = true),
-		@facet(name = SimpleBdiArchitecture.PRIORITY, type = IType.FLOAT, optional = true),
+	@facet(name = SimpleBdiArchitecture.PRIORITY, type = IType.FLOAT, optional = true),
 	@facet(name = IKeyword.NAME, type = IType.ID, optional = true) }, omissible = IKeyword.NAME)
-public class SimpleBdiStatement extends AbstractStatementSequence{
-	
+public class SimpleBdiStatement extends AbstractStatementSequence {
+
 	private final IExpression _when;
 	private final IExpression _priority;
-	
-	public IExpression getPriorityExpression(){
+
+	public IExpression getPriorityExpression() {
 		return _priority;
 	}
-	
-	public IExpression getContextExpression(){
+
+	public IExpression getContextExpression() {
 		return _when;
 	}
-	
+
 	public SimpleBdiStatement(final IDescription desc) {
 		super(desc);
 		_when = getFacet(IKeyword.WHEN);
@@ -41,15 +40,14 @@ public class SimpleBdiStatement extends AbstractStatementSequence{
 			setName(getLiteral(IKeyword.NAME));
 		}
 	}
-	
+
 	@Override
 	public Object privateExecuteIn(final IScope scope) throws GamaRuntimeException {
-		if ( _when == null || Cast.asBool(scope, _when.value(scope)) ) { return super
-			.privateExecuteIn(scope); }
-		scope.setStatus(ExecutionStatus.skipped);
+		if ( _when == null || Cast.asBool(scope, _when.value(scope)) ) { return super.privateExecuteIn(scope); }
+		// scope.setStatus(ExecutionStatus.skipped);
 		return null;
 	}
-	
+
 	public Double computePriority(final IScope scope) throws GamaRuntimeException {
 		return Cast.asFloat(scope, _priority.value(scope));
 	}
