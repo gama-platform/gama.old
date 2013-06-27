62aa6b0651bab3228f4748af2cff2adbab0be7da
benoit.gaudou@gmail.com <benoit.gaudou@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-06-27 20:23:58 +0000

    test for °
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@6778 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.lang.gaml/src/msi/gama/lang/utils/GamlExpressionCompiler.java
+++ msi.gama.lang.gaml/src/msi/gama/lang/utils/GamlExpressionCompiler.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -124,7 +124,7 @@ public class GamlExpressionCompiler implements IExpressionCompiler<Expression> {
 			return expr;
 		}
 		// The unary "unit" operator should let the value of its child pass through
-		if ( op.equals("¡") ) { return expr; }
+		if ( op.equals("Â°") ) { return expr; }
 		if ( isSpeciesName(op) ) { return factory.createOperator(AS, context, expr, species(op)); }
 		if ( isSkillName(op) ) { return factory.createOperator(AS, context, expr, skill(op)); }
 		return factory.createOperator(op, context, expr);
