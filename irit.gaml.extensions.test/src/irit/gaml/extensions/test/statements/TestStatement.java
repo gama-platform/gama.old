b45ad6c7159842b6eec6ebe4a44f3809f21b3c5b
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-07-15 10:50:31 +0000

    Corrected the access to the SetupStatement
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7003 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- irit.gaml.extensions.test/src/irit/gaml/extensions/test/statements/TestStatement.java
+++ irit.gaml.extensions.test/src/irit/gaml/extensions/test/statements/TestStatement.java
@@ -1,16 +1,15 @@
 package irit.gaml.extensions.test.statements;
 
 import msi.gama.common.interfaces.IKeyword;
-import msi.gama.precompiler.ISymbolKind;
 import msi.gama.precompiler.GamlAnnotations.facet;
 import msi.gama.precompiler.GamlAnnotations.facets;
 import msi.gama.precompiler.GamlAnnotations.inside;
 import msi.gama.precompiler.GamlAnnotations.symbol;
+import msi.gama.precompiler.*;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gaml.descriptions.IDescription;
 import msi.gaml.statements.AbstractStatementSequence;
-import msi.gaml.statements.IStatement;
 import msi.gaml.types.IType;
 
 @symbol(name = { "test" }, kind = ISymbolKind.SEQUENCE_STATEMENT, with_sequence = true, unique_name = true)
@@ -18,21 +17,25 @@ import msi.gaml.types.IType;
 @facets(value = { @facet(name = IKeyword.NAME, type = IType.ID, optional = true) }, omissible = IKeyword.NAME)
 public class TestStatement extends AbstractStatementSequence {
 
-	public TestStatement(final IDescription desc){
-		super(desc);	
+	SetUpStatement setup = null;
+	boolean setupLookedFor = false;
+
+	public TestStatement(final IDescription desc) {
+		super(desc);
 		if ( hasFacet(IKeyword.NAME) ) {
-			setName("test"+getLiteral(IKeyword.NAME));
+			setName("test" + getLiteral(IKeyword.NAME));
 		}
 	}
 
 	@Override
 	public Object privateExecuteIn(final IScope scope) throws GamaRuntimeException {
-		for(IStatement s : scope.getAgentScope().getSpecies().getBehaviors()){
-			if("setUp".equals(s.getName())){
-				s.executeOn(scope);
-			}
-		}		
-		return super.privateExecuteIn(scope); 
+		if ( setup == null && !setupLookedFor ) {
+			setupLookedFor = true;
+			setup = scope.getAgentScope().getSpecies().getStatement(SetUpStatement.class, null);
+		} else {
+			setup.executeOn(scope);
+		}
+		return super.privateExecuteIn(scope);
 	}
 
 }
