39bcf72753bbef48c8717bb9f8e91379619befe7
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-08-06 16:32:07 +0000

    Makes the layers compute their display box inside the correct scope.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7191 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gama/outputs/layers/AbstractLayerStatement.java
+++ msi.gama.core/src/msi/gama/outputs/layers/AbstractLayerStatement.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -55,7 +55,7 @@ public abstract class AbstractLayerStatement extends Symbol implements ILayerSta
 
 	@Override
 	public final boolean init(final IScope scope) {
-		getBox().compute(scope);
+		// getBox().compute(scope);
 		return _init(scope);
 	}
 
@@ -75,7 +75,7 @@ public abstract class AbstractLayerStatement extends Symbol implements ILayerSta
 
 	@Override
 	public final boolean step(final IScope scope) throws GamaRuntimeException {
-		getBox().compute(scope);
+		// getBox().compute(scope);
 		return _step(scope);
 	}
 
