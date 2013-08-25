941e73b5e8d4d4e5fc5fa27cc94e983a35dc74e8
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-08-25 17:17:29 +0000

    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7366 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/swt/swing/experimental/internal/ComponentDebugging.java
+++ msi.gama.application/src/msi/gama/gui/swt/swing/experimental/internal/ComponentDebugging.java
@@ -4,16 +4,15 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     SAS Institute Inc. - initial API and implementation
- *     ILOG S.A. - initial API and implementation
+ * SAS Institute Inc. - initial API and implementation
+ * ILOG S.A. - initial API and implementation
  *******************************************************************************/
 package msi.gama.gui.swt.swing.experimental.internal;
 
 import java.awt.Component;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
+import java.awt.event.*;
 
 /**
  * This class contains utility functions for debugging issues at Component
@@ -21,27 +20,35 @@ import java.awt.event.ComponentListener;
  */
 public class ComponentDebugging {
 
-    /**
-     * Adds a listener for debugging size at Component events.
-     */
-    public static void addComponentSizeDebugListeners(final Component comp) {
-        comp.addComponentListener(
-            new ComponentListener() {
-                private void log(ComponentEvent event) {
-                    System.err.println("Size: "+comp.getWidth()+" x "+comp.getHeight()+" after "+event);
-                }
-                public void componentHidden(ComponentEvent event) {
-                    log(event);
-                }
-                public void componentMoved(ComponentEvent event) {
-                    log(event);
-                }
-                public void componentResized(ComponentEvent event) {
-                    log(event);
-                }
-                public void componentShown(ComponentEvent event) {
-                    log(event);
-                }
-            });
-    }
+	/**
+	 * Adds a listener for debugging size at Component events.
+	 */
+	public static void addComponentSizeDebugListeners(final Component comp) {
+		comp.addComponentListener(new ComponentListener() {
+
+			private void log(final ComponentEvent event) {
+				System.err.println("Size: " + comp.getWidth() + " x " + comp.getHeight() + " after " + event);
+			}
+
+			@Override
+			public void componentHidden(final ComponentEvent event) {
+				log(event);
+			}
+
+			@Override
+			public void componentMoved(final ComponentEvent event) {
+				log(event);
+			}
+
+			@Override
+			public void componentResized(final ComponentEvent event) {
+				log(event);
+			}
+
+			@Override
+			public void componentShown(final ComponentEvent event) {
+				log(event);
+			}
+		});
+	}
 }
