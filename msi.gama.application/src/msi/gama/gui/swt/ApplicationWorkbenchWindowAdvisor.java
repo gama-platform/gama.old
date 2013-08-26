4c3e9c6a702ff28514c71fca7df764fdc42debe9
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-08-26 12:05:12 +0000

    Fixes Issue 545, by removing the custom preWindowShellClose() method in ApplicationWorkbenchWindowAdvisor.java
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7380 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/swt/ApplicationWorkbenchWindowAdvisor.java
+++ msi.gama.application/src/msi/gama/gui/swt/ApplicationWorkbenchWindowAdvisor.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -19,15 +19,10 @@
 package msi.gama.gui.swt;
 
 import java.util.*;
-import msi.gama.common.interfaces.IGui;
 import msi.gama.runtime.GAMA;
 import msi.gama.util.GamaList;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.preferences.*;
 import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.*;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.*;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.internal.*;
@@ -36,7 +31,6 @@ import org.eclipse.ui.internal.ide.EditorAreaDropAdapter;
 import org.eclipse.ui.internal.ide.application.IDEWorkbenchWindowAdvisor;
 import org.eclipse.ui.internal.wizards.AbstractExtensionWizardRegistry;
 import org.eclipse.ui.wizards.*;
-import org.osgi.service.prefs.BackingStoreException;
 
 @SuppressWarnings("restriction")
 public class ApplicationWorkbenchWindowAdvisor extends IDEWorkbenchWindowAdvisor {
@@ -69,28 +63,28 @@ public class ApplicationWorkbenchWindowAdvisor extends IDEWorkbenchWindowAdvisor
 		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().setMaximized(true);
 	}
 
-	@Override
-	public boolean preWindowShellClose() {
-		final IEclipsePreferences preferenceStore = InstanceScope.INSTANCE.getNode(IGui.PLUGIN_ID);
-		final boolean promptOnExit = preferenceStore.getBoolean(PROMPT_ON_EXIT, false);
-
-		if ( !promptOnExit ) {
-			final MessageDialogWithToggle dlg =
-				MessageDialogWithToggle.openOkCancelConfirm(Display.getDefault().getActiveShell(), "Confirm Exit",
-					"Exit " + Platform.getProduct().getName() + " ?", "Always exit without prompt", false, null, null);
-
-			if ( dlg.getReturnCode() != IDialogConstants.OK_ID ) { return false; }
-
-			InstanceScope.INSTANCE.getNode(IGui.PLUGIN_ID).putBoolean(PROMPT_ON_EXIT, dlg.getToggleState());
-			try {
-				InstanceScope.INSTANCE.getNode(IGui.PLUGIN_ID).flush();
-			} catch (final BackingStoreException e) {
-				System.out.println("Preferences can not be saved");
-				e.printStackTrace();
-			}
-		}
-		return super.preWindowShellClose();
-	}
+	// @Override
+	// public boolean preWindowShellClose() {
+	// final IEclipsePreferences preferenceStore = InstanceScope.INSTANCE.getNode(IGui.PLUGIN_ID);
+	// final boolean promptOnExit = preferenceStore.getBoolean(PROMPT_ON_EXIT, false);
+	//
+	// if ( !promptOnExit ) {
+	// final MessageDialogWithToggle dlg =
+	// MessageDialogWithToggle.openOkCancelConfirm(Display.getDefault().getActiveShell(), "Confirm Exit",
+	// "Exit " + Platform.getProduct().getName() + " ?", "Always exit without prompt", false, null, null);
+	//
+	// if ( dlg.getReturnCode() != IDialogConstants.OK_ID ) { return false; }
+	//
+	// InstanceScope.INSTANCE.getNode(IGui.PLUGIN_ID).putBoolean(PROMPT_ON_EXIT, dlg.getToggleState());
+	// try {
+	// InstanceScope.INSTANCE.getNode(IGui.PLUGIN_ID).flush();
+	// } catch (final BackingStoreException e) {
+	// System.out.println("Preferences can not be saved");
+	// e.printStackTrace();
+	// }
+	// }
+	// return super.preWindowShellClose();
+	// }
 
 	private static List<String> CATEGORIES_TO_REMOVE = new GamaList(new String[] { "org.eclipse.jdt.debug.ui.java",
 		"org.eclipse.jdt.junit", "org.eclipse.pde.PDE", /* "org.eclipse.ui.Basic", */
