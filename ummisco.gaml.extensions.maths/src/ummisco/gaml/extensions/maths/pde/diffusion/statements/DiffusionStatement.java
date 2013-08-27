d1301203ef1a1e0a6a4c8fb6ffe2dc979261362f
hqnghi88@gmail.com <hqnghi88@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-08-27 15:12:45 +0000

    EDP  -  integration torus diffusion (depend on torus attribute of environment)
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7386 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- ummisco.gaml.extensions.maths/src/ummisco/gaml/extensions/maths/pde/diffusion/statements/DiffusionStatement.java
+++ ummisco.gaml.extensions.maths/src/ummisco/gaml/extensions/maths/pde/diffusion/statements/DiffusionStatement.java
@@ -1,6 +1,7 @@
 package ummisco.gaml.extensions.maths.pde.diffusion.statements;
 
 import msi.gama.common.interfaces.IKeyword;
+import msi.gama.common.util.GuiUtils;
 import msi.gama.metamodel.agent.IAgent;
 import msi.gama.metamodel.topology.grid.GamaSpatialMatrix.GridPopulation;
 import msi.gama.precompiler.GamlAnnotations.facet;
@@ -17,10 +18,12 @@ import msi.gaml.expressions.IExpression;
 import msi.gaml.statements.AbstractStatementSequence;
 import msi.gaml.types.IType;
 
-@facets(value = { @facet(name = "var", type = IType.ID, optional = false),
+@facets(value = {
+		@facet(name = "var", type = IType.ID, optional = false),
 		@facet(name = "on", type = IType.ID, optional = false),
 		@facet(name = "mat_diffu", type = IType.MATRIX, optional = false),
-		@facet(name = "method", type = IType.ID, optional = true, values={"convolution","dot_product"}),
+		@facet(name = "method", type = IType.ID, optional = true, values = {
+				"convolution", "dot_product" }),
 		@facet(name = "mask", type = IType.MATRIX, optional = true),
 		@facet(name = "cycle_length", type = IType.INT, optional = true) }, omissible = IKeyword.EQUATION)
 @symbol(name = { "diffusion" }, kind = ISymbolKind.SEQUENCE_STATEMENT, with_sequence = true)
@@ -43,168 +46,190 @@ public class DiffusionStatement extends AbstractStatementSequence {
 		// Based on the facets, choose a solver and init it;
 
 	}
-	
 
 	public void doDiffusion2(IScope scope, String varName, String speciesName,
-			 double[][] mat_diffu, double[][] mask) {
+			double[][] mat_diffu, double[][] mask) {
 
 		GridPopulation pop = (GridPopulation) scope.getAgentScope()
 				.getPopulationFor(speciesName);
+		boolean isTorus = pop.getTopology().isTorus();
+
 		IAgent[] lstAgents = pop.toArray();
 		IMatrix mmm = pop.matrixValue(scope);
 		int cols = mmm.getCols(scope);
 		int rows = mmm.getRows(scope);
-		
 
-//		IMatrix<Double> mask = new GamaFloatMatrix(scope,cols, rows);
-		
-//		if (getFacet("mask") != null) {
-//			mask = (IMatrix) getFacet("mask").value(scope);
-//		}
-		
+		// IMatrix<Double> mask = new GamaFloatMatrix(scope,cols, rows);
 
-		int kRows=mat_diffu.length;
-		int kCols=mat_diffu[0].length;
+		// if (getFacet("mask") != null) {
+		// mask = (IMatrix) getFacet("mask").value(scope);
+		// }
 
-		
+		int kRows = mat_diffu.length;
+		int kCols = mat_diffu[0].length;
+
 		int xcenter = kRows / 2;
 		int ycenter = kCols / 2;
-//		GamaFloatMatrix tmp = new GamaFloatMatrix(scope, cols, rows);
-		double[][] tmp=new double[cols][rows];
+		// GamaFloatMatrix tmp = new GamaFloatMatrix(scope, cols, rows);
+		double[][] tmp = new double[rows][cols];
 
 		// System.out.println(xcenter+" "+ycenter);
 		// find center position of kernel (half of kernel size)
-		
-
 
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
 				double currentValue = Double.parseDouble(""
 						+ lstAgents[i * rows + j].getAttribute(varName));
-//				if (currentValue <= Double.MAX_VALUE
-//						&& currentValue >= - (Double.MAX_VALUE)) 
-//				{ 
-//						&& i >= xcenter
-//						&& j >= ycenter && i < cols - xcenter
-//						&& j < rows - ycenter) {
+				// if (currentValue <= Double.MAX_VALUE
+				// && currentValue >= - (Double.MAX_VALUE))
+				// {
+				// && i >= xcenter
+				// && j >= ycenter && i < cols - xcenter
+				// && j < rows - ycenter) {
 
-					for (int u = i - xcenter; u <= i + xcenter; u++) {
-						for (int v = j - ycenter; v <= j + ycenter; v++) {
-							if(u<0 || v<0 || u>=cols || v>=rows){
-								continue;
+				int um = 0;
+				for (int uu = i - xcenter; uu <= i + xcenter; uu++) {
+					int vm = 0;
+					for (int vv = j - ycenter; vv <= j + ycenter; vv++) {
+						int u=uu;
+						int v=vv;
+						double currentMask = (mask != null) ? ((mask[i][j] < -1) ? 0
+								: 1)
+								: 1;
+						if (isTorus) {
+							if (u < 0) {
+								u = rows + u;
+							} else if (u >= rows) {
+								u = u - rows;
 							}
-							// System.out.println(u+" "+v+" | "+( u - i +
-							// xcenter)+" "+(v - j + ycenter));
-							double currentMatValue = 
-									+ mat_diffu[u - i + xcenter][v
-											- j + ycenter];
-		                	double currentMask=(mask!=null)?((mask[i][j]<-1)?0:1):1;
 
-//							System.out.println(currentMask);
-							tmp[u][v] = tmp[u][v]
-									+ currentValue * currentMatValue * currentMask;
-							
+							if (v < 0) {
+								v = cols + v;
+							} else if (v >= cols) {
+								v = v - cols;
+							}
 						}
-//					}
-					// System.out.println();
+						else if (u < 0 || v < 0 || u >= cols || v >= rows) {
+							continue;
+						}
+						System.out.println(um + " " + vm +" | "+u+" "+v);
+						// System.out.println(currentMask);
+						tmp[u][v] = tmp[u][v] + currentValue
+								* mat_diffu[um][vm] * currentMask;
+						vm++;
+					}
+					um++;
+					// }
+					 System.out.println();
 				}
 			}
 		}
 
-
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				lstAgents[i * rows + j].setAttribute(varName,
-						tmp[i][j]);
+				lstAgents[i * rows + j].setAttribute(varName, tmp[i][j]);
 			}
 		}
 
 	}
 
-
 	public void doDiffusion1(IScope scope, String varName, String speciesName,
-			double[][]  mat_diffu, double[][] mask) {
+			double[][] mat_diffu, double[][] mask) {
 
 		GridPopulation pop = (GridPopulation) scope.getAgentScope()
 				.getPopulationFor(speciesName);
-//		System.out.println(pop.getTopology().isTorus());
+		// GuiUtils.informConsole(""+pop.getTopology().isTorus());
+		boolean isTorus = pop.getTopology().isTorus();
 		IAgent[] lstAgents = pop.toArray();
 		IMatrix mmm = pop.matrixValue(scope);
 		int cols = mmm.getCols(scope);
 		int rows = mmm.getRows(scope);
-		
 
+		int kRows = mat_diffu.length;
+		int kCols = mat_diffu[0].length;
+		// double[][] mat_diffu=translateMatrix(scope, mat_diffusion);
 
-		int kRows=mat_diffu.length;
-		int kCols=mat_diffu[0].length;
-//		double[][] mat_diffu=translateMatrix(scope, mat_diffusion);
-		
-
-		
 		int kCenterX = kRows / 2;
 		int kCenterY = kCols / 2;
-//		GamaFloatMatrix tmp = new GamaFloatMatrix(scope, cols, rows);
-		double[][] tmp=new double[cols][rows];
+		// GamaFloatMatrix tmp = new GamaFloatMatrix(scope, cols, rows);
+		double[][] tmp = new double[rows][cols];
 		// System.out.println(xcenter+" "+ycenter);
 		// find center position of kernel (half of kernel size)
-//		float sum=0;
-		int mm=0, nn=0, ii=0, jj=0;
+		// float sum=0;
+		int mm = 0, nn = 0, ii = 0, jj = 0;
 
-		for(int i=0; i < rows; ++i)              // rows
+		for (int i = 0; i < rows; ++i) // rows
 		{
-		    for(int j=0; j < cols; ++j)          // columns
-		    {
-        	
-//		        sum = 0;                     // init to 0 before sum
-		        for(int m=0; m < kRows; ++m)     // kernel rows
-		        {
-		            mm = kRows - 1 - m;      // row index of flipped kernel
-		            for(int n=0; n < kCols; ++n) // kernel columns
-		            {
-		                nn = kCols - 1 - n;  // column index of flipped kernel
+			for (int j = 0; j < cols; ++j) // columns
+			{
 
-		                // index of input signal, used for checking boundary
-		                ii = i + (m - kCenterY);
-		                jj = j + (n - kCenterX);
+				// sum = 0; // init to 0 before sum
+				for (int m = 0; m < kRows; ++m) // kernel rows
+				{
+					mm = kRows - 1 - m; // row index of flipped kernel
+					for (int n = 0; n < kCols; ++n) // kernel columns
+					{
+						nn = kCols - 1 - n; // column index of flipped kernel
 
-		                // ignore input samples which are out of bound
-		                if( ii >= 0 && ii < rows && jj >= 0 && jj < cols ){
-//		                out[i][j] = out[i][j] + in[ii][jj] * kernel[mm][nn];
-		                	double inValue=Double.parseDouble(""+lstAgents[ii * rows + jj].getAttribute(varName));
-		                	
-		                	double currentMask=(mask!=null)?((mask[i][j]<-1)?0:1):1;
-		                	double currentMatValue=mat_diffu[mm][nn];
-		                	tmp[i][j]=	tmp[i][j]+ inValue * currentMatValue * currentMask;
-		                	
-		                }
-		            }
-		        }
-		    }
+						// index of input signal, used for checking boundary
+						ii = i + (m - kCenterX);
+						jj = j + (n - kCenterY);
+
+						// ignore input samples which are out of bound
+						if (isTorus) {
+							if (ii < 0) {
+								ii = rows + ii;
+							} else if (ii >= rows) {
+								ii = ii - rows;
+							}
+
+							if (jj < 0) {
+								jj = cols + jj;
+							} else if (jj >= cols) {
+								jj = jj - cols;
+							}
+						}
+						if (ii >= 0 && ii < rows && jj >= 0 && jj < cols) {
+							// out[i][j] = out[i][j] + in[ii][jj] *
+							// kernel[mm][nn];
+
+							double inValue = Double.parseDouble(""
+									+ lstAgents[ii * rows + jj]
+											.getAttribute(varName));
+
+							double currentMask = (mask != null) ? ((mask[i][j] < -1) ? 0
+									: 1)
+									: 1;
+							double currentMatValue = mat_diffu[mm][nn];
+							tmp[i][j] = tmp[i][j] + inValue * currentMatValue
+									* currentMask;
+						}
+
+					}
+				}
+			}
 		}
-
-
 
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				lstAgents[i * rows + j].setAttribute(varName,
-						tmp[i][j]);
+				lstAgents[i * rows + j].setAttribute(varName, tmp[i][j]);
 			}
 		}
 
 	}
 
-	public double[][] translateMatrix(IScope scope, IMatrix mm){
-		int rows=mm.getRows(scope);
-		int cols=mm.getCols(scope);
-		double[][] res=new double[rows][cols];
-		for(int i=0; i<rows; i++) {
-			for(int j=0; j<cols; j++) {
-				res[i][j]=Double.parseDouble(""+mm.get(scope, i, j));
+	public double[][] translateMatrix(IScope scope, IMatrix mm) {
+		int rows = mm.getRows(scope);
+		int cols = mm.getCols(scope);
+		double[][] res = new double[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				res[i][j] = Double.parseDouble("" + mm.get(scope, i, j));
 			}
 		}
 		return res;
 	}
-	
+
 	@Override
 	public Object privateExecuteIn(final IScope scope)
 			throws GamaRuntimeException {
@@ -215,21 +240,21 @@ public class DiffusionStatement extends AbstractStatementSequence {
 
 		String varName = (String) getFacet("var").value(scope);
 		String speciesName = (String) getFacet("on").value(scope);
-//		IMatrix mat_diffu = (IMatrix) getFacet("mat_diffu").value(scope);
-		double[][] mat_diffu=translateMatrix(scope, (IMatrix) getFacet("mat_diffu").value(scope));
+		// IMatrix mat_diffu = (IMatrix) getFacet("mat_diffu").value(scope);
+		double[][] mat_diffu = translateMatrix(scope,
+				(IMatrix) getFacet("mat_diffu").value(scope));
 
-		double[][] mask=null;
-		
+		double[][] mask = null;
+
 		if (getFacet("mask") != null) {
-			mask= translateMatrix(scope, (IMatrix) getFacet("mask").value(scope));
-			
-			
+			mask = translateMatrix(scope,
+					(IMatrix) getFacet("mask").value(scope));
+
 		}
-		
-		
-		IExpression method_diffu=getFacet("method");
-		if(method_diffu!=null){
-			if(method_diffu.value(scope).equals("dot_product")){
+
+		IExpression method_diffu = getFacet("method");
+		if (method_diffu != null) {
+			if (method_diffu.value(scope).equals("dot_product")) {
 				for (int time = 0; time < cLen; time++) {
 					doDiffusion2(scope, varName, speciesName, mat_diffu, mask);
 				}
