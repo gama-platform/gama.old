e989d1bb8f44e88b98cf6a4b455c41ba80ac535f
nmarilleau@gmail.com <nmarilleau@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-08-28 06:55:17 +0000

    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7394 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.hpc/src/msi/gama/hpc/gui/perspective/HPCPerspectiveFactory.java
+++ msi.gama.hpc/src/msi/gama/hpc/gui/perspective/HPCPerspectiveFactory.java
@@ -4,6 +4,8 @@ import msi.gama.common.util.GuiUtils;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IPlaceholderFolderLayout;
+import org.eclipse.ui.IViewLayout;
 
 public class HPCPerspectiveFactory implements IPerspectiveFactory {
 
@@ -13,7 +15,21 @@ public class HPCPerspectiveFactory implements IPerspectiveFactory {
 	
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		// TODO Auto-generated method stub
+		layout.setEditorAreaVisible(false);
+		System.out.println("stttaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		String editorId = layout.getEditorArea();
+		layout.addView("msi.gama.hpc.gui.navigator.OutputNavigator", IPageLayout.LEFT, 0.23f, editorId);
+		layout.addView("msi.gama.hpc.gui.experiment.navigator.ExperimentNavigator", IPageLayout.BOTTOM, 0.23f, editorId);
+		//layout.getViewLayout("msi.gama.hpc.gui.experiment.navigator.ExperimentNavigator");
+	//	IViewLayout nav = layout.getViewLayout("msi.gama.hpc.chartView");
+	//	nav.setMoveable(false);
+
+		layout.addView("msi.gama.hpc.chartView", IPageLayout.TOP, 0.70f,
+				editorId);
+
+
+		/*layout.addView("msi.gama.application.view.ConsoleView", IPageLayout.BOTTOM, 0.70f,
+			"msi.gama.gui.view.GamaNavigator");*/
 	}
 
 }
