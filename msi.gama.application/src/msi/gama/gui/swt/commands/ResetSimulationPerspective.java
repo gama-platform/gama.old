8cbec7efe8ad42898f98dff780c862a9f65b6c5e
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-09-03 04:40:35 +0000

    Fixes Issue 551 by implementing a new handler for the "Reset Perspective..." menu item when the Simulation perspective is the current one. Warns the user about the fact that the experiment will be closed and allows him/her to cancel the operation.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7463 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ msi.gama.application/src/msi/gama/gui/swt/commands/ResetSimulationPerspective.java
@@ -0,0 +1,39 @@
+package msi.gama.gui.swt.commands;
+
+import msi.gama.runtime.GAMA;
+import org.eclipse.core.commands.*;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.ui.*;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.internal.*;
+
+public class ResetSimulationPerspective extends AbstractHandler {
+
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+		if ( activeWorkbenchWindow != null ) {
+			WorkbenchPage page = (WorkbenchPage) activeWorkbenchWindow.getActivePage();
+			if ( page != null ) {
+				IPerspectiveDescriptor descriptor = page.getPerspective();
+				if ( descriptor != null ) {
+					String message =
+						"Resetting the simulation perspective will close the current simulation. Do you want to proceed ?";
+					boolean result =
+						MessageDialog.open(MessageDialog.QUESTION, activeWorkbenchWindow.getShell(),
+							WorkbenchMessages.ResetPerspective_title, message, SWT.SHEET);
+					if ( result ) {
+						GAMA.controller.closeExperiment();
+						page.resetPerspective();
+					}
+
+				}
+			}
+		}
+
+		return null;
+
+	}
+
+}
