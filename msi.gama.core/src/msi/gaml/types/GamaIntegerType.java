c10aeac074eddeba49a429deab4005f6831570c5
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-09-06 10:38:58 +0000

    Cleansing of code.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7502 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/types/GamaIntegerType.java
+++ msi.gama.core/src/msi/gaml/types/GamaIntegerType.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -28,7 +28,7 @@ import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gaml.descriptions.IDescription;
 
 /**
- * Written by drogoul Modified on 1 aožt 2010
+ * Written by drogoul Modified on 1 aoï¿½t 2010
  * 
  * @todo Description
  * 
@@ -37,8 +37,7 @@ import msi.gaml.descriptions.IDescription;
 public class GamaIntegerType extends GamaType<Integer> {
 
 	@Override
-	public Integer cast(final IScope scope, final Object obj, final Object param)
-		throws GamaRuntimeException {
+	public Integer cast(final IScope scope, final Object obj, final Object param) throws GamaRuntimeException {
 		return staticCast(scope, obj, param);
 	}
 
@@ -90,7 +89,7 @@ public class GamaIntegerType extends GamaType<Integer> {
 	}
 
 	@Override
-	public IType findCommonSupertypeWith(IType type) {
+	public IType findCommonSupertypeWith(final IType type) {
 		return type == this ? this : type.id() == IType.FLOAT ? type : Types.NO_TYPE;
 	}
 
