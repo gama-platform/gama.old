c30d73059502eb5a9b7247a15470a51dae72af62
nmarilleau@gmail.com <nmarilleau@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-09-15 07:53:49 +0000

    macro model of traffic skill
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7640 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ ummisco.miro.extension/src/ummisco/miro/extension/moving/RoadTrafficManagement.java
@@ -0,0 +1,16 @@
+package ummisco.miro.extension.moving;
+
+import msi.gama.precompiler.GamlAnnotations.skill;
+import msi.gama.precompiler.GamlAnnotations.var;
+import msi.gama.precompiler.GamlAnnotations.vars;
+import msi.gaml.skills.Skill;
+import msi.gaml.types.IType;
+
+
+@vars({ @var(name = "speed", type = IType.FLOAT, init = "1.0"),
+	@var(name = "nbVehicle", type = IType.INT, init = "0")
+})
+@skill(name = "roadTrafficManagement")
+public class RoadTrafficManagement extends Skill {	
+
+}
