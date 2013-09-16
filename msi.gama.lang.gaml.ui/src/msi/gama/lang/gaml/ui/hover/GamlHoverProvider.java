8f085f4166d2f97e3c0525ae6c699473b61e0359
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-09-16 19:31:59 +0000

    More work on the hover provider.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7666 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.lang.gaml.ui/src/msi/gama/lang/gaml/ui/hover/GamlHoverProvider.java
+++ msi.gama.lang.gaml.ui/src/msi/gama/lang/gaml/ui/hover/GamlHoverProvider.java
@@ -4,19 +4,21 @@
  */
 package msi.gama.lang.gaml.ui.hover;
 
-import msi.gama.common.util.GuiUtils;
 import msi.gama.lang.gaml.gaml.*;
 import msi.gama.lang.utils.EGaml;
 import msi.gaml.factories.*;
 import msi.gaml.factories.DescriptionFactory.Documentation;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.internal.text.html.BrowserInformationControl;
 import org.eclipse.jface.text.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.ui.editors.text.EditorsUI;
 import org.eclipse.xtext.Keyword;
 import org.eclipse.xtext.nodemodel.*;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.*;
 import org.eclipse.xtext.ui.editor.hover.DispatchingEObjectTextHover;
-import org.eclipse.xtext.ui.editor.hover.html.DefaultEObjectHoverProvider;
+import org.eclipse.xtext.ui.editor.hover.html.*;
 import org.eclipse.xtext.util.*;
 import org.eclipse.xtext.util.Pair;
 import com.google.inject.Inject;
@@ -92,21 +94,95 @@ public class GamlHoverProvider extends DefaultEObjectHoverProvider {
 			return super.getHoverInfo(first, textViewer, hoverRegion);
 		}
 
-		@Override
-		public IInformationControlCreator getHoverControlCreator() {
-			IInformationControlCreator c = super.getHoverControlCreator();
-			return c;
+	}
+
+	public class GamlHoverControlCreator extends HoverControlCreator {
+
+		/**
+		 * @param informationPresenterControlCreator
+		 */
+		public GamlHoverControlCreator(final IInformationControlCreator informationPresenterControlCreator) {
+			super(informationPresenterControlCreator);
 		}
 
+		public class GamlInformationControl extends XtextBrowserInformationControl {
+
+			@Override
+			public void setSize(final int width, final int height) {
+				super.setSize(width, height);
+				org.eclipse.swt.graphics.Point p = Display.getDefault().getCursorLocation();
+				p.x -= 5;
+				p.y += 15;
+				setLocation(p);
+			}
+
+			/**
+			 * @param parent
+			 * @param symbolicFontName
+			 * @param statusFieldText
+			 */
+			public GamlInformationControl(final Shell parent, final String symbolicFontName,
+				final String statusFieldText) {
+				super(parent, symbolicFontName, statusFieldText);
+			}
+
+			/*
+			 * @see
+			 * org.eclipse.jface.text.IInformationControlExtension5#getInformationPresenterControlCreator()
+			 */
+			@Override
+			public IInformationControlCreator getInformationPresenterControlCreator() {
+				return GamlHoverProvider.this.getInformationPresenterControlCreator();
+			}
+		}
+
+		@Override
+		public IInformationControl doCreateInformationControl(final Shell parent) {
+
+			String tooltipAffordanceString = EditorsUI.getTooltipAffordanceString();
+			if ( BrowserInformationControl.isAvailable(parent) ) {
+				String font = "org.eclipse.jdt.ui.javadocfont"; // FIXME: PreferenceConstants.APPEARANCE_JAVADOC_FONT;
+				IXtextBrowserInformationControl iControl =
+					new GamlInformationControl(parent, font, tooltipAffordanceString) {
+
+					};
+				addLinkListener(iControl);
+				return iControl;
+			} else {
+				return new DefaultInformationControl(parent, tooltipAffordanceString);
+			}
+
+			// final XtextBrowserInformationControl c =
+			// (XtextBrowserInformationControl) super.doCreateInformationControl(parent);
+			// c.addFocusListener(new FocusListener() {
+			//
+			// @Override
+			// public void focusGained(final FocusEvent e) {
+			// c.setLocation(Display.getDefault().getCursorLocation());
+			// }
+			//
+			// @Override
+			// public void focusLost(final FocusEvent e) {}
+			//
+			// });
+			// return c;
+		}
+	}
+
+	private IInformationControlCreator creator;
+
+	@Override
+	public IInformationControlCreator getHoverControlCreator() {
+		if ( creator == null ) {
+			creator = new GamlHoverControlCreator(getInformationPresenterControlCreator());
+		}
+		return creator;
 	}
 
 	@Override
 	public IInformationControlCreatorProvider getHoverInfo(final EObject first, final ITextViewer textViewer,
 		final IRegion hoverRegion) {
-		if ( first instanceof Keyword ) {
-			GuiUtils.debug("Implement getHoverInfo for keywords");
 
-		}
 		return super.getHoverInfo(first, textViewer, hoverRegion);
 
 	}
