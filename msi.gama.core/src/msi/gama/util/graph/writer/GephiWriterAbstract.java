87d18b6f0dc89c09307db01d8ad4ff214dce853f
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-09-23 15:04:46 +0000

    Cleansed a bit the code, removing from the build path the graphstream view.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7727 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gama/util/graph/writer/GephiWriterAbstract.java
+++ msi.gama.core/src/msi/gama/util/graph/writer/GephiWriterAbstract.java
@@ -1,14 +1,10 @@
 package msi.gama.util.graph.writer;
 
-import java.io.File;
-import java.io.IOException;
-
+import java.io.*;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gama.util.file.GamaFile;
-import msi.gama.util.graph.GraphUtilsGephi;
-import msi.gama.util.graph.IGraph;
-
+import msi.gama.util.graph.*;
 import org.gephi.io.exporter.api.ExportController;
 import org.gephi.io.exporter.spi.GraphExporter;
 import org.gephi.project.api.Workspace;
@@ -16,53 +12,48 @@ import org.openide.util.Lookup;
 
 public abstract class GephiWriterAbstract implements IGraphWriter {
 
-	protected void initializeExporter(GraphExporter exporter) {}
+	protected void initializeExporter(final GraphExporter exporter) {}
 
-	private final void writeGraph(String filename, Workspace gephiWorkspace) {
+	private final void writeGraph(final String filename, final Workspace gephiWorkspace) {
 
 		// retrieve exporter
-        ExportController controller = null;
-        GraphExporter exporter = null;
+		ExportController controller = null;
+		GraphExporter exporter = null;
 		synchronized (GraphUtilsGephi.gephiStaticLocker) {
-        	controller = Lookup.getDefault().lookup(ExportController.class);
-			exporter = (GraphExporter) controller.getExporter(getFormat());     //Get GEXF exporter
-			
+			controller = Lookup.getDefault().lookup(ExportController.class);
+			exporter = (GraphExporter) controller.getExporter(getFormat()); // Get GEXF exporter
+
 		}
-		if (exporter == null) {
-			throw GamaRuntimeException.error("unable to find an exporter for format "+getFormat());
-		}
-		
+		if ( exporter == null ) { throw GamaRuntimeException.error("unable to find an exporter for format " +
+			getFormat()); }
+
 		// configure the exporter
-        exporter.setExportVisible(false);  //Only exports the visible (filtered) graph
-        exporter.setWorkspace(gephiWorkspace);
+		exporter.setExportVisible(false); // Only exports the visible (filtered) graph
+		exporter.setWorkspace(gephiWorkspace);
 
-        try {
-        	controller.exportFile(
-        			new File(filename), 
-        			exporter
-        			);
-        } catch (RuntimeException e) {
-        	e.printStackTrace();
-        	 throw GamaRuntimeException.error("Unable to export the graph in file "+filename+" ("+ e.getMessage()+")");
-        } catch (IOException e) {
-        	e.printStackTrace();
-       	 	throw GamaRuntimeException.error("Unable to export the graph in file "+filename+" ("+ e.getMessage()+")");
-        } 
+		try {
+			controller.exportFile(new File(filename), exporter);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			throw GamaRuntimeException.error("Unable to export the graph in file " + filename + " (" + e.getMessage() +
+				")");
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw GamaRuntimeException.error("Unable to export the graph in file " + filename + " (" + e.getMessage() +
+				")");
+		}
 	}
-	
-	protected abstract String getFormat();
-	
-	@Override
-	public void writeGraph(IScope scope, IGraph<?, ?> gamaGraph,
-			GamaFile<?, ?> gamaFile, String filename) {
-		
-			// translate the gama graph as a gephi graph (that is, stored into a gephi workspace)
-			Workspace gephiWorkspace = GraphUtilsGephi.loadIntoAGephiWorkspace(gamaGraph);
 
-			writeGraph(
-					filename,
-					gephiWorkspace
-					);
+	protected abstract String getFormat();
+
+	@Override
+	public void writeGraph(final IScope scope, final IGraph<?, ?> gamaGraph, final GamaFile<?, ?> gamaFile,
+		final String filename) {
+
+		// translate the gama graph as a gephi graph (that is, stored into a gephi workspace)
+		Workspace gephiWorkspace = GraphUtilsGephi.loadIntoAGephiWorkspace(gamaGraph);
+
+		writeGraph(filename, gephiWorkspace);
 	}
 
 }
