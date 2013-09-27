25ef5377319e0deecd9d0832ddf1c94844a126ff
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-09-27 16:40:35 +0000

    Always run the code in the main thread.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7795 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/navigator/commands/RefreshHandler.java
+++ msi.gama.application/src/msi/gama/gui/navigator/commands/RefreshHandler.java
@@ -1,5 +1,5 @@
 /*
- * GAMA - V1.4  http://gama-platform.googlecode.com
+ * GAMA - V1.4 http://gama-platform.googlecode.com
  * 
  * (c) 2007-2011 UMI 209 UMMISCO IRD/UPMC & Partners (see below)
  * 
@@ -7,8 +7,8 @@
  * 
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
- * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen  (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -19,26 +19,27 @@
 package msi.gama.gui.navigator.commands;
 
 import msi.gama.gui.navigator.GamaNavigator;
-
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.core.commands.*;
+import org.eclipse.ui.*;
 
 public class RefreshHandler extends AbstractHandler {
 
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		final IViewPart view = page.findView("msi.gama.gui.view.GamaNavigator");
-		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
-		 	@Override
-			public void run() {
-		 		((GamaNavigator) view).getCommonViewer().refresh();
-		 	}
-		});
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		run();
 		return null;
+	}
+
+	public static void run() {
+
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				final IViewPart view = page.findView("msi.gama.gui.view.GamaNavigator");
+				((GamaNavigator) view).getCommonViewer().refresh();
+			}
+		});
 	}
 }
