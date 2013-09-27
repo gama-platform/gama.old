29075334cf02aebe069bbb731f03cff5604c8548
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-09-27 17:05:16 +0000

    Removes references to HelpView.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7798 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/swt/perspectives/SimulationPerspective.java
+++ msi.gama.application/src/msi/gama/gui/swt/perspectives/SimulationPerspective.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -43,7 +43,7 @@ public class SimulationPerspective implements IPerspectiveFactory {
 		IPlaceholderFolderLayout inspectorsFolder =
 			layout.createPlaceholderFolder("inspectorsFolder", IPageLayout.RIGHT, 0.52f, "layersFolder");
 
-		inspectorsFolder.addPlaceholder("msi.gama.application.view.SpeciesInspectView");
+		// inspectorsFolder.addPlaceholder("msi.gama.application.view.SpeciesInspectView");
 		inspectorsFolder.addPlaceholder("msi.gama.application.view.MonitorView");
 
 		IPlaceholderFolderLayout inspectorsFolder2 =
