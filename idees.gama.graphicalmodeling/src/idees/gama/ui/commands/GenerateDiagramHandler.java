057ce5ae188e917411f263103089acff53e1fcdd
patrick.taillandier@gmail.com <patrick.taillandier@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-10-01 21:20:07 +0000

    fix the graphical modeling plug-in
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7850 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- idees.gama.graphicalmodeling/src/idees/gama/ui/commands/GenerateDiagramHandler.java
+++ idees.gama.graphicalmodeling/src/idees/gama/ui/commands/GenerateDiagramHandler.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen  (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -57,6 +57,7 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
@@ -69,7 +70,8 @@ public class GenerateDiagramHandler extends AbstractHandler {
 
 				@Override
 				public IModel exec(final XtextResource state) throws Exception {
-					return editor.getValidator().build((GamlResource) state);
+					GamlJavaValidator v = IResourceServiceProvider.Registry.INSTANCE.getResourceServiceProvider(state.getURI()).get(GamlJavaValidator.class);
+					return v.build((GamlResource) state);
 				}
 
 			});
