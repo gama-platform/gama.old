4abb03038327a5af29dcf9058a610a78b137cc1d
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-10-03 03:56:46 +0000

    Changes the wording of the monitor message.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7856 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/navigator/commands/ImportProjectsFromSVNHandler.java
+++ msi.gama.application/src/msi/gama/gui/navigator/commands/ImportProjectsFromSVNHandler.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -45,7 +45,7 @@ public class ImportProjectsFromSVNHandler extends AbstractHandler {
 			protected IStatus run(final IProgressMonitor monitor) {
 				final SVNAccess access = new SVNAccess();
 				String url = "https://gama-models.googlecode.com/svn/trunk/";
-				monitor.beginTask("Import library of models from " + url, 5000);
+				monitor.beginTask("Importing shared models from " + url, 5000);
 				try {
 					access.createAccessToRepositoryLocation(url);
 					monitor.worked(500);
