6edbc515068a82a9cf04a277c8845d29fef4abf2
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-10-28 05:56:48 +0000

    Removed obsolete facets.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@7995 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/variables/NumberVariable.java
+++ msi.gama.core/src/msi/gaml/variables/NumberVariable.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -47,9 +47,6 @@ import msi.gaml.types.IType;
 	@facet(name = IKeyword.MIN, type = IType.INT, optional = true),
 	@facet(name = IKeyword.MAX, type = IType.INT, optional = true),
 	@facet(name = IKeyword.STEP, type = IType.INT, optional = true),
-	// @facet(name = IKeyword.INITER, type = IType.LABEL, optional = true),
-	// @facet(name = IKeyword.GETTER, type = IType.LABEL, optional = true),
-	// @facet(name = IKeyword.SETTER, type = IType.LABEL, optional = true),
 	@facet(name = IKeyword.AMONG, type = IType.LIST, optional = true) }, omissible = IKeyword.NAME)
 @symbol(kind = ISymbolKind.Variable.NUMBER, with_sequence = false)
 @inside(kinds = { ISymbolKind.SPECIES, ISymbolKind.EXPERIMENT, ISymbolKind.MODEL })
