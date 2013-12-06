cbe612cf922f6de4a98748793b88672075b1913d
patrick.taillandier@gmail.com <patrick.taillandier@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2013-12-06 17:58:59 +0000

    New version of the model for map comparison (fuzzy kappa sim)
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@8171 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- idees.gama.mapcomparison/src/idees/gama/operators/MapComparison.java
+++ idees.gama.mapcomparison/src/idees/gama/operators/MapComparison.java
@@ -3,8 +3,10 @@ package idees.gama.operators;
 
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import msi.gama.metamodel.agent.IAgent;
 import msi.gama.metamodel.population.IPopulation;
@@ -118,7 +120,6 @@ public class MapComparison {
 				contigencyOS[j][k] /= O[j];
 			}
 			O[j] /= nb;
-			
 		}
 		double po = 0;
 		double pe = 0;
@@ -174,34 +175,34 @@ public class MapComparison {
 		return (meanSimilarity - similarityExpected) / (1 - similarityExpected);
 	}
 	
-	/*@operator(value = { "fuzzy_kappa_sim"}, content_type = IType.FLOAT)
-	@doc(value = "fuzzy kappa simulation indicator for 2 map comparisons: fuzzy_kappa_sim(agents_list,list_vals1,list_vals2, output_similarity_per_agents,fuzzy_categories_matrix, fuzzy_distance)", examples = { "fuzzy_kappa([ag1, ag2, ag3, ag4, ag5], [1,3,2,8,2],[10,2,3,8,2], similarity_per_agents,[cat1,cat2,cat3],[[1,0,0],[0,1,0],[0,0,1]], 2)" })
+	@operator(value = { "fuzzy_kappa_sim"}, content_type = IType.FLOAT)
+	@doc(value = "fuzzy kappa simulation indicator for 2 map comparisons: fuzzy_kappa_sim(agents_list,list_vals1,list_vals2, output_similarity_per_agents,fuzzy_transitions_matrix, fuzzy_distance). Reference: Jasper van Vliet, Alex Hagen-Zanker, Jelle Hurkens, Hedwig van Delden, A fuzzy set approach to assess the predictive accuracy of land use simulations, Ecological Modelling, Volumes 261–262, 24 July 2013, Pages 32-42, ISSN 0304-3800, ", examples = { "fuzzy_kappa_sim([ag1, ag2, ag3, ag4, ag5], [cat1,cat1,cat2,cat3,cat2],[cat2,cat1,cat2,cat1,cat2], similarity_per_agents,[cat1,cat2,cat3],[[1,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0],[0,0,1,0,0,0,0,0,0],[0,0,0,1,0,0,0,0,0],[0,0,0,0,1,0,0,0,0],[0,0,0,0,0,1,0,0,0],[0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,0,1]], 2)" })
 	public static double fuzzyKappaSimulation(final IScope scope,
-		final IContainer<Integer, IAgent> agents, final IList<Object> valsInit, final IList<Object> valsObs, final IList<Object> valsSim, final IList<Double> observeAgreements, final IList<Double> expectedAgreements, final List<Object> categories, final GamaMatrix<Double> fuzzycategories, final Double distance) {
+		final IContainer<Integer, IAgent> agents, final IList<Object> valsInit, final IList<Object> valsObs, final IList<Object> valsSim,  final IList<Double> similarities, final List<Object> categories, final GamaMatrix<Double> fuzzytransitions, final Double distance) {
 		if (agents == null)
 			return 1;
 		int nb = agents.length(scope);
 		if (nb  < 1)
 			return 1;
 		int nbCat = categories.size();
-		boolean[] sim = new boolean[nb]; 
-		double[][] crispVector1 = new double[nb][nbCat];
-		double[][] crispVector2 = new double[nb][nbCat];
-		double[][] fuzzyVector1 = new double[nb][nbCat];
-		double[][] fuzzyVector2 = new double[nb][nbCat];
 		double[] nbObs = new double[nbCat];
 		double[] nbSim = new double[nbCat];
 		double[] nbInit = new double[nbCat];
 		double[][] nbInitObs = new double[nbCat][nbCat];
 		double[][] nbInitSim = new double[nbCat][nbCat];
 		GamaMap<Object, Integer> categoriesId = new GamaMap<Object, Integer>();
+
+		GamaMap<List<Integer>, GamaMap<Double, Integer>> XaPerTransition = new GamaMap<List<Integer>, GamaMap<Double,Integer>>();
+		GamaMap<List<Integer>, GamaMap<Double, Integer>> XsPerTransition = new GamaMap<List<Integer>, GamaMap<Double,Integer>>();
+		Set<Double> Xvals= new HashSet<Double>();
 		for (int i = 0; i < nbCat; i++) {
 			categoriesId.put(categories.get(i), i);
 		}
 		
-		computeXYCrispVector(scope, categoriesId,categories, valsObs, valsSim, fuzzycategories, nbCat, nb, crispVector1 ,crispVector2, nbObs, nbSim, sim);
 		for (int i = 0 ; i < nbCat; i++ ) {
 			nbInit[i] = 0;
+			nbObs[i] = 0;
+			nbSim[i] = 0;
 			for (int j = 0 ; j < nbCat; j++ ) {
 				nbInitObs[i][j] = 0;
 				nbInitSim[i][j] = 0;
@@ -217,36 +218,173 @@ public class MapComparison {
 	
 		for (int i = 0; i < nb; i++) {
 			int idCatInit = categoriesId.get(valsInit.get(i));
-			nbInit[idCatInit] += 1;
 			int idCatObs = categoriesId.get(valsObs.get(i));
 			int idCatSim = categoriesId.get(valsSim.get(i));
+			nbInit[idCatInit] += 1;
+			nbSim[idCatSim] += 1;
+			nbObs[idCatObs] += 1;
 			nbInitObs[idCatInit][idCatObs] += 1;
 			nbInitSim[idCatInit][idCatSim] += 1;
-		}
-		double po = computeSimilarity(scope, filter,distance, valsObs, valsSim, agents, nbCat, nb, crispVector1 ,crispVector2,sim,fuzzyVector1, fuzzyVector2,observeAgreements);
-		
-		Map<Set<Object>, Double> listE = new GamaMap<Set<Object>, Double>();
-		Map<Set<Object>, Double> listP = new GamaMap<Set<Object>, Double>();
+		}		
+		double po = computePo(scope, filter, categoriesId,fuzzytransitions,distance, valsInit,valsObs, valsSim, agents, nbCat, nb, similarities,XaPerTransition, XsPerTransition, Xvals);
+		double pe = 0;
+		computeXaXsTransitions(scope, filter,fuzzytransitions,distance,agents, nbCat, XaPerTransition, XsPerTransition, Xvals);
 		for (int i = 0 ; i < nbCat; i++ ) {
-			Object o = categories.get(i);
 			for (int j = 0 ; j < nbCat; j++ ) {
-				Object a = categories.get(j);
 				for (int k = 0 ; k < nbCat; k++ ) {
-					Object s = categories.get(k);
-					Set<Object> set = new HashSet<Object>();
-					set.add(o); set.add(a); set.add(s);
-					double poas = nbInitObs[i][j]/nbInit[i] * nbInitSim[i][k]/nbInit[i] * nbInit[i] / nb;
-					listP.put(set, poas);
+					List<Integer> ca = new GamaList<Integer>();
+					ca.add(i); ca.add(j);ca.add(k);
+					GamaMap<Double, Integer> pmuXa = XaPerTransition.get(ca);
+					GamaMap<Double, Integer> pmuXs = XsPerTransition.get(ca);
+					double emu = 0;
+					for (Double xval : Xvals) {
+						double XaVal = pmuXa == null || ! pmuXa.containsKey(xval) ? 0:pmuXa.get(xval);
+						double XsVal = pmuXs == null || ! pmuXs.containsKey(xval) ? 0 :pmuXs.get(xval);
+						double proba = xval*XaVal*XsVal;
+						emu += proba;
+					}
+					double poas = nbInitObs[i][j]/nbInit[i]  * nbInitSim[i][k]/nbInit[i] * nbInit[i]/nb;
+					pe += emu * poas;
 				}
 			}
 		}
-		double pe = 0;
-		for (Set<Object> val : listE.keySet()) {
-			pe += (listE.get(val) * listP.get(val));
-		}
-	
 		return (po - pe) / (1 - pe);
-	}*/
+	}
+	
+	private static double computePo(final IScope scope, In filter, GamaMap<Object, Integer> categoriesId, final GamaMatrix<Double> fuzzytransitions,final Double distance, final IList<Object> valsInit,final IList<Object> valsObs, final IList<Object> valsSim, 
+			final IContainer<Integer, IAgent> agents, int nbCat, int nb, final IList<Double> similarities,GamaMap<List<Integer>, GamaMap<Double, Integer>> XaPerTransition,GamaMap<List<Integer>, GamaMap<Double, Integer>> XsPerTransition, Set<Double> Xvals){
+		GamaMap<IAgent, Integer> agsId = new GamaMap<IAgent, Integer>();
+		for (int i = 0; i < agents.length(scope); i++) {
+			agsId.put(agents.get(scope, i), i);
+		}
+		
+		for (int i = 0; i < nb; i++) {
+			Object valObs  = valsObs.get(i);
+			Object valSim  = valsSim.get(i);
+			Object valInit = valsInit.get(i);
+			int valObsId  = categoriesId.get(valObs);
+			int valSimId  = categoriesId.get(valSim);
+			int valInitId = categoriesId.get(valInit);
+			IAgent agent = agents.get(scope, i);
+			double[] XaXs = computeXaXs(scope, filter, categoriesId, agsId, valObsId, valSimId, valInitId,fuzzytransitions,distance,agent, valsInit,valsObs, valsSim, agents, nbCat);
+			similarities.add(Math.min(XaXs[0],XaXs[1]));
+		}
+		double meanSimilarity = 0;
+		for (Double val : similarities) {
+			meanSimilarity += val;
+		}
+		meanSimilarity /= similarities.size();
+		return meanSimilarity;
+	}
+	
+	private static double[] computeXaXs(final IScope scope, In filter, GamaMap<Object, Integer> categoriesId, GamaMap<IAgent, Integer> agsId, int valObsId, int valSimId, int valInitId,final GamaMatrix<Double> fuzzytransitions,final Double distance, IAgent agent, final IList<Object> valsInit,final IList<Object> valsObs, 
+			final IList<Object> valsSim, final IContainer<Integer, IAgent> agents, int nbCat) {
+		double xa = 0.0;
+		double xs = 0.0;
+		double[] XaXs = new double[2]; 
+		double sizeNorm = Math.sqrt(agent.getEnvelope().getArea());
+		GamaList<IAgent> neighbours = distance == 0 ?  new GamaList<IAgent>() : new GamaList<IAgent>(scope.getTopology().getNeighboursOf(scope, agent, distance, filter));
+		
+		GamaMap<IAgent, Double> distancesCoeff = new GamaMap<IAgent, Double>();
+		distancesCoeff.put(agent, 1.0);
+		for (IAgent ag : neighbours) {
+			double euclidDist = agent.getLocation().euclidianDistanceTo(ag.getLocation());
+			distancesCoeff.put(ag, 1 /(1.0 + (euclidDist/sizeNorm))); 
+		}
+		for (IAgent ag : distancesCoeff.getKeys())  {
+			int id = agsId.get(ag);
+			Object valI = valsInit.get(id);
+			Object valO = valsObs.get(id);
+			Object valS = valsSim.get(id);
+			int valOId  = categoriesId.get(valO);
+			int valSId  = categoriesId.get(valS);
+			int valIId = categoriesId.get(valI);
+			double dist = distancesCoeff.get(ag);
+			double valxatmp = fuzzyTransition(scope, fuzzytransitions, nbCat, valInitId,valSimId,valIId,valOId) *dist ;
+			double valxstmp = fuzzyTransition(scope, fuzzytransitions, nbCat, valInitId,valObsId,valIId,valSId) *dist ;
+				
+			if (valxatmp > xa) xa = valxatmp; 
+			if (valxstmp > xs) xs = valxstmp; 	
+		}
+
+		XaXs[0] = xa;
+		XaXs[1] = xs;
+		return XaXs;
+	}
+	private static double fuzzyTransition(final IScope scope,final GamaMatrix<Double> fuzzytransitions, int nbCat,int from1, int to1, int from2, int to2){
+		return fuzzytransitions.get(scope, from1 + (nbCat * to1),  from2 + (nbCat * to2));
+	}
+	
+	private static void computeXaXsTransitions(final IScope scope, In filter, final GamaMatrix<Double> fuzzytransitions,final Double distance,
+			 final IContainer<Integer, IAgent> agents, int nbCat, GamaMap<List<Integer>, GamaMap<Double, Integer>> XaPerTransition, GamaMap<List<Integer>, GamaMap<Double, Integer>> XsPerTransition, Set<Double> Xvals) {
+		
+		GamaList<ILocation> locs = new GamaList<ILocation>();
+		for (IAgent ag : agents) {
+			locs.add(ag.getLocation());
+		}
+		ILocation centralLoc = (ILocation) Stats.getMean(scope,locs);
+		IAgent centralAg =  scope.getTopology().getAgentClosestTo(scope, centralLoc, filter);
+		GamaList<IAgent> neighbours = distance == 0 ?  new GamaList<IAgent>() : new GamaList<IAgent>(scope.getTopology().getNeighboursOf(scope, centralAg, distance, filter));
+		double sizeNorm = Math.sqrt(centralAg.getEnvelope().getArea());
+		
+		GamaMap<IAgent, Double> distancesCoeff = new GamaMap<IAgent, Double>();
+		distancesCoeff.put(centralAg, 1.0);
+		for (IAgent ag : neighbours) {
+			double euclidDist = centralAg.getLocation().euclidianDistanceTo(ag.getLocation());
+			distancesCoeff.put(ag, 1 /(1.0 + (euclidDist/sizeNorm))); 
+		}
+		
+		for (int i = 0; i < nbCat; i++) {
+			for (int j = 0; j < nbCat; j++) {
+				for (int k = 0; k < nbCat; k++) {
+					List<Integer> ca = new GamaList<Integer>();
+					ca.add(i); ca.add(j); ca.add(k);
+					double xa = 0;
+					double xs = 0;
+					for (IAgent ag : distancesCoeff.getKeys())  {
+						double dist = distancesCoeff.get(ag);
+						double valxatmp = fuzzyTransition(scope, fuzzytransitions, nbCat, i,k,i,j) *dist ;
+						double valxstmp = fuzzyTransition(scope, fuzzytransitions, nbCat, i,j,i,k) *dist ;
+						if (valxatmp > xa) xa = valxatmp; 
+						if (valxstmp > xs) xs = valxstmp; 	
+					}
+					if (xa > 0) {
+						GamaMap<Double, Integer> mapxa = XaPerTransition.get(ca);
+						if (mapxa == null) {
+							mapxa = new GamaMap<Double, Integer>();
+							mapxa.put(xa, 1);
+							XaPerTransition.put(ca, mapxa);
+						} else {
+							if (mapxa.containsKey(xa)) {
+								mapxa.put(xa, mapxa.get(xa) + 1);
+							} else {
+								mapxa.put(xa, 1);
+							}
+						}
+						Xvals.add(xa);
+					}
+					if (xs > 0) {
+						GamaMap<Double, Integer> mapxs = XsPerTransition.get(ca);
+						if (mapxs == null) {
+							mapxs = new GamaMap<Double, Integer>();
+							mapxs.put(xs, 1);
+							XsPerTransition.put(ca, mapxs);
+						} else {
+							if (mapxs.containsKey(xa)) {
+								mapxs.put(xs, mapxs.get(xs) + 1);
+							} else {
+								mapxs.put(xs, 1);
+							}
+						}
+						Xvals.add(xs);
+					}
+						
+				}
+				}
+			}
+		}
+
+		
 	
 	private static double p(double dist, int a, int b,double[] X,double[] Y,Map<Double,Integer> ringsPn) {
 		int n = 0;
@@ -292,8 +430,8 @@ public class MapComparison {
 				similarities.add(1.0);
 			} else {
 				IAgent agent = agents.get(scope, i);
-				double sizeNorm = agent.getPerimeter() / 4.0;
-				
+				//double sizeNorm = agent.getPerimeter() / 4.0;
+				double sizeNorm = Math.sqrt(agent.getEnvelope().getArea());
 				GamaList<IAgent> neighbours = distance == 0 ?  new GamaList<IAgent>() : new GamaList<IAgent>(scope.getTopology().getNeighboursOf(scope, agent, distance, filter));
 				
 				
@@ -301,7 +439,7 @@ public class MapComparison {
 				distancesCoeff.put(agent, 1.0);
 				for (IAgent ag : neighbours) {
 					double euclidDist = agent.getLocation().euclidianDistanceTo(ag.getLocation());
-					distancesCoeff.put(ag, 1 /(1.0 + (euclidDist)/sizeNorm)); 
+					distancesCoeff.put(ag, 1 /(1.0 + (euclidDist/sizeNorm))); 
 				}
 				for (int j = 0; j < nbCat; j++) {
 					double max1 = 0.0;
@@ -421,6 +559,5 @@ public class MapComparison {
 	
 	}
 
-	
 
 }
