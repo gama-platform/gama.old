becdf9dace95ddb8e990d453cd0ef0dee496e7ec
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-02-14 06:54:39 +0000

    Bug in the dotproduct of points.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@8551 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gama/metamodel/shape/GamaPoint.java
+++ msi.gama.core/src/msi/gama/metamodel/shape/GamaPoint.java
@@ -357,7 +357,7 @@ public class GamaPoint extends Coordinate implements ILocation {
 	}
 
 	public final static double dotProduct(final GamaPoint v1, final GamaPoint v2) {
-		return v1.x * v2.x + v1.y * v2.y + v1.y * v2.y;
+		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 	}
 
 }
