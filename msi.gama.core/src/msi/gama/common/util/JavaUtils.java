30a0fbe111007ef00a6df447309098cbc9844cd3
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-03-05 04:41:00 +0000

    Adds the case where the base class when collecting the implementation classes of a species is null.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@8780 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gama/common/util/JavaUtils.java
+++ msi.gama.core/src/msi/gama/common/util/JavaUtils.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -22,7 +22,7 @@ import java.util.*;
 import com.google.common.collect.*;
 
 /**
- * Written by drogoul Modified on 28 dŽc. 2010
+ * Written by drogoul Modified on 28 dï¿½c. 2010
  * 
  * Provides some utilities for dealing with reflection.
  * 
@@ -80,7 +80,9 @@ public class JavaUtils {
 	public static List<Class> collectImplementationClasses(final Class baseClass, final Set<Class> skillClasses,
 		final Set<Class> in) {
 		final Set<Class> classes = new HashSet();
-		classes.add(baseClass);
+		if ( baseClass != null ) {
+			classes.add(baseClass);
+		}
 		classes.addAll(skillClasses);
 		final Set<Class> key = new HashSet(classes);
 		if ( IMPLEMENTATION_CLASSES.containsKey(key) ) { return IMPLEMENTATION_CLASSES.get(key); }
