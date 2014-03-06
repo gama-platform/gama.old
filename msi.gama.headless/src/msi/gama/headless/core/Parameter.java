942d561d14a46da4129755944e68e673098898f5
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-03-06 17:12:32 +0000

    Adaptation to the new ITyped API
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@8831 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.headless/src/msi/gama/headless/core/Parameter.java
+++ msi.gama.headless/src/msi/gama/headless/core/Parameter.java
@@ -1,39 +1,36 @@
 package msi.gama.headless.core;
 
 import msi.gama.headless.common.DataType;
-import msi.gama.headless.common.DataTypeFactory;
-
-
 
 public class Parameter {
+
 	private String name;
 	private Object value;
-	private DataType type;
-	
-	
-	public Parameter(String name, Object value, DataType type) {
+
+	// private DataType type;
+
+	public Parameter(final String name, final Object value, final DataType type) {
 		super();
 		this.name = name;
 		this.value = value;
-		this.type = type;
+		// this.type = type;
 	}
+
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+
+	public void setName(final String name) {
 		this.name = name;
 	}
-	
-	
+
 	public Object getValue() {
 		return this.value;
 	}
-	
-	
-	public void setValue(Object value) {
-		this.type=DataTypeFactory.getObjectMetaData(value);
+
+	public void setValue(final Object value) {
+		// this.type=DataTypeFactory.getObjectMetaData(value);
 		this.value = value;
 	}
-
 
 }
