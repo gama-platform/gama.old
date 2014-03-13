16420253544cae919ea00a5a76a4cb81bebcc070
agrignard <agrignard@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-03-13 10:41:09 +0000

    fix issue 856
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@8864 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.jogl/src/msi/gama/jogl/utils/Camera/AbstractCamera.java
+++ msi.gama.jogl/src/msi/gama/jogl/utils/Camera/AbstractCamera.java
@@ -84,7 +84,7 @@ public abstract class AbstractCamera implements ICamera {
 	@Override
 	public final void updateCamera(final GL gl, final GLU glu, final int width, final int height) {
 		float aspect = (float) width / (height == 0 ? 1 : height);
-		glu.gluPerspective(45.0f, aspect, getMaxDim() / 100, getMaxDim() * 100);
+		glu.gluPerspective(45.0f, aspect, getMaxDim() / 1000, getMaxDim() * 10);
 		makeGluLookAt(glu);
 		animate();
 	}
