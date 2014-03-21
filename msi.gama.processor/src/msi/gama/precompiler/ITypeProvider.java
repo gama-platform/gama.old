32da625e692699c3014c201ae37e16d48b0065cb
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-03-21 17:01:50 +0000

    Provides 3 new symbols to indicate the type/contents type/key type returned by operators
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@8963 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.processor/src/msi/gama/precompiler/ITypeProvider.java
+++ msi.gama.processor/src/msi/gama/precompiler/ITypeProvider.java
@@ -8,7 +8,7 @@
  * - Alexis Drogoul, UMI 209 UMMISCO, IRD/UPMC (Kernel, Metamodel, GAML), 2007-2012
  * - Vo Duc An, UMI 209 UMMISCO, IRD/UPMC (SWT, multi-level architecture), 2008-2012
  * - Patrick Taillandier, UMR 6228 IDEES, CNRS/Univ. Rouen (Batch, GeoTools & JTS), 2009-2012
- * - Beno”t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
+ * - Benoï¿½t Gaudou, UMR 5505 IRIT, CNRS/Univ. Toulouse 1 (Documentation, Tests), 2010-2012
  * - Phan Huy Cuong, DREAM team, Univ. Can Tho (XText-based GAML), 2012
  * - Pierrick Koch, UMI 209 UMMISCO, IRD/UPMC (XText-based GAML), 2010-2011
  * - Romain Lavaud, UMI 209 UMMISCO, IRD/UPMC (RCP environment), 2010
@@ -19,7 +19,7 @@
 package msi.gama.precompiler;
 
 /**
- * Written by drogoul Modified on 2 aožt 2010
+ * Written by drogoul Modified on 2 aoï¿½t 2010
  * 
  * @todo Description
  * 
@@ -47,7 +47,7 @@ public interface ITypeProvider {
 	static final int SECOND_CONTENT_TYPE_OR_TYPE = -25;
 	static final int FIRST_CONTENT_TYPE_OR_TYPE = -26;
 	/*
-	 * The content type is provided by the type of of the expression itself (i.e. species)
+	 * The content type is provided by the type of the expression itself (i.e. species)
 	 */
 	static final int TYPE = -14;
 	/*
@@ -65,5 +65,15 @@ public interface ITypeProvider {
 	 * e.g. : matrix ([[4, 5, 6],[1, 2, 4]]) should get int as a content type.
 	 */
 	static final int FIRST_ELEMENT_CONTENT_TYPE = -22;
+
+	/**
+	 * The type, content type, key type are provided by the element computed by its index. 0 for the first element, 1
+	 * for the second, etc.
+	 * For instance, TYPE_AT_INDEX + 2 will represent the third argument
+	 */
+	static final int INDEXED_TYPES = -100;
+	static final int TYPE_AT_INDEX = -200;
+	static final int CONTENT_TYPE_AT_INDEX = -300;
+	static final int KEY_TYPE_AT_INDEX = -400;
 
 }
