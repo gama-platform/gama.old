37226ebd7b405f2b0422d8f89fbb6e5e3f653e48
voducanvn@gmail.com <voducanvn@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-03-26 11:37:02 +0000

    add basic player source code for debug purpose.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9100 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- ummisco.gaml.extensions.music/src/ummisco/gaml/extensions/sound/GamaSoundPlayer.java
+++ ummisco.gaml.extensions.music/src/ummisco/gaml/extensions/sound/GamaSoundPlayer.java
@@ -147,4 +147,9 @@ public class GamaSoundPlayer {
 	public boolean canBeReused() {
 		return !repeat && playerStopped;
 	}
+
+
+	public Thread getBasicPlayerMThread() {
+		return basicPlayer.getMThread();
+	}
 }
