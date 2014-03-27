0b10acf71705bf1901439b458316dc161affe4b5
agrignard <agrignard@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-03-27 04:29:12 +0000

    add colorbrewer in graphic skills
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9132 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/skills/GraphicSkill.java
+++ msi.gama.core/src/msi/gaml/skills/GraphicSkill.java
@@ -18,7 +18,10 @@
  */
 package msi.gaml.skills;
 
+import java.awt.Color;
 import java.util.Map;
+
+
 import msi.gama.common.interfaces.*;
 import msi.gama.common.util.*;
 import msi.gama.metamodel.agent.IAgent;
@@ -47,6 +50,20 @@ import msi.gaml.types.*;
 import com.vividsolutions.jts.geom.*;
 import com.vividsolutions.jts.precision.GeometryPrecisionReducer;
 
+import org.geotools.brewer.color.*;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.filter.function.Classifier;
+import org.geotools.filter.function.ExplicitClassifier;
+import org.geotools.filter.function.RangedClassifier;
+import org.geotools.styling.FeatureTypeStyle;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.expression.Function;
+import org.opengis.filter.expression.PropertyName;
+
+
+
 /**
  * GraphicSkill : This class is intended to define the minimal set of behaviours required from a
  * graphical agent. Each member that has a meaning in GAML is annotated with the
@@ -61,6 +78,14 @@ import com.vividsolutions.jts.precision.GeometryPrecisionReducer;
 	@var(name = IKeyword.TRANSPARENCY, type = IType.FLOAT, init = "1.0", doc = @doc("the transparency of the agent (between 0.0 and 1.0)")),})
 @skill(name = IKeyword.GRAPHIC_SKILL)
 public class GraphicSkill extends Skill {
+	
+	
+	public static final PaletteType ALL = new PaletteType(true, true, "ALL");
+    public static final PaletteType SUITABLE_RANGED = new PaletteType(true, false);
+    public static final PaletteType SUITABLE_UNIQUE = new PaletteType(false, true);
+    public static final PaletteType SEQUENTIAL = new PaletteType(true, false, "SEQUENTIAL");
+    public static final PaletteType DIVERGING = new PaletteType(true, false, "DIVERGING");
+    public static final PaletteType QUALITATIVE = new PaletteType(false, true, "QUALITATIVE");
 
 
 	@getter(IKeyword.TRANSPARENCY)
@@ -76,7 +101,7 @@ public class GraphicSkill extends Skill {
 
 	@action(name = "twinkle", 
 		args = { @arg(name = "period", type = IType.INT, 
-					  doc = @doc("make the agent twinkle with a given period")) }, 
+		doc = @doc("make the agent twinkle with a given period")) }, 
 		doc = @doc(examples = { @example("do twinkle period: 10;") } ))
 	public void twinkle(final IScope scope) throws GamaRuntimeException {
 		final IAgent agent = getCurrentAgent(scope);
@@ -93,5 +118,84 @@ public class GraphicSkill extends Skill {
 		scope.getGraphics().setOpacity(curTrans);
 		return;
 	}
+	
+	@action(name = "brewer_color", 
+			args = { @arg(name = "type", type = IType.STRING, doc = @doc("Palette Type (Sequential, Diverging, Qualitative)")),
+			@arg(name = "class", type = IType.INT, optional = false, doc = @doc("Number of class")),
+			@arg(name = "index", type = IType.INT,optional = false, doc = @doc("index")) }, 
+			doc = @doc(examples = { @example(" color <- self.colors(1);") } ))
+	public GamaColor getBrewerColors(final IScope scope){
+		String type = scope.getStringArg("type");
+		int nbClass = scope.getIntArg("class");
+		int index = scope.getIntArg("index");
+		
+		GamaColor c = null;
+		
+	    ColorBrewer brewer = ColorBrewer.instance();
 
+	    
+	    // STEP 2 - look up a predefined palette from color brewer
+	    String paletteName = "";
+	    
+	    if("Sequential".equals(type) || "sequential".equals(type)){
+	    	BrewerPalette[] spallettes = brewer.getPalettes(SEQUENTIAL,nbClass);
+	    	//java.lang.System.out.println("sequential palette");
+		    for(int i=0;i<spallettes.length;i++){  	
+		    	//java.lang.System.out.println(spallettes[i].getName());
+		    	//java.lang.System.out.println(spallettes[i].getDescription());
+		    	c = new GamaColor(spallettes[i].getColors()[index]);
+		    }
+	    }
+	    
+        if("Diverging".equals(type) || "diverging".equals(type)){
+        	BrewerPalette[] spallettes = brewer.getPalettes(DIVERGING,nbClass);
+	    	//java.lang.System.out.println("diverging palette");
+		    for(int i=0;i<spallettes.length;i++){  	
+		    	//java.lang.System.out.println(spallettes[i].getName());
+		    	//java.lang.System.out.println(spallettes[i].getDescription());
+		    	c = new GamaColor(spallettes[i].getColors()[index]);
+		    	for(int j=0;j<spallettes[i].getColors().length;j++){
+		    		//java.lang.System.out.println(spallettes[i].getColors());	
+		    	}	
+		    }	
+	    }
+        
+        if("Qualitative".equals(type) || "qualitative".equals(type)){
+        	BrewerPalette[] spallettes = brewer.getPalettes(QUALITATIVE,nbClass);
+	    	//java.lang.System.out.println("qualitative palette");
+		    for(int i=0;i<spallettes.length;i++){  	
+		    	//java.lang.System.out.println(spallettes[i].getName());
+		    	//java.lang.System.out.println(spallettes[i].getDescription());
+		    	c = new GamaColor(spallettes[i].getColors()[index]);
+		    	for(int j=0;j<spallettes[i].getColors().length;j++){
+		    		//java.lang.System.out.println(spallettes[i].getColors());	
+		    	}	
+		    } 	
+	    }
+	    return  c;				
+	}
+	
+	@action(name = "brewer_palette", 
+			args = { @arg(name = "type", type = IType.STRING, doc = @doc("Palette Type (Sequential, Diverging, Qualitative)")) }, 
+			doc = @doc(examples = { @example("list<rgb> colors <- brewer_palette(\"6-class Blues\");") } , comment =  "You can get the type of the palette form this websiten http://colorbrewer2.org/" ))
+	public GamaList<Color> getBrewerPaletteColors(final IScope scope){
+		String type = scope.getStringArg("type");
+		
+		GamaList<Color>colors = new GamaList<Color>();
+		
+	    ColorBrewer brewer = ColorBrewer.instance();
+
+	    String paletteName = type;
+	    
+	    if(brewer.getPalette(paletteName) != null){
+	    	for (int i=0;i<brewer.getPalette(paletteName).getColors().length;i++){
+	    		colors.add(brewer.getPalette(paletteName).getColors()[i]);
+	    	}
+	    }
+	    else{
+	    	throw GamaRuntimeException.error(type + "does not exist");
+	    }
+	    return  colors;				
+	}
+	
 }
