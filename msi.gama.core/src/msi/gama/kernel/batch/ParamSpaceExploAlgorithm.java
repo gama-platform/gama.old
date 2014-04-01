da8972e05282b350e8d8c209f7b13e0aced8e6ad
patrick.taillandier@gmail.com <patrick.taillandier@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-04-01 01:25:54 +0000

    Fix a bug when there were no maximize/minimize facets (exhaustive search)
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9250 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gama/kernel/batch/ParamSpaceExploAlgorithm.java
+++ msi.gama.core/src/msi/gama/kernel/batch/ParamSpaceExploAlgorithm.java
@@ -113,10 +113,10 @@ public abstract class ParamSpaceExploAlgorithm extends Symbol implements IExplor
 			public Object value() {
 				List<Class> classes = new GamaList(CLASSES);
 				String name = IKeyword.METHODS[classes.indexOf(ParamSpaceExploAlgorithm.this.getClass())];
-				String fit = "fitness = " + (isMaximize ? " maximize " : " minimize ") + fitnessExpression.toGaml();
-				String sim =
+				String fit = fitnessExpression == null ? "" : ("fitness = " + (isMaximize ? " maximize " : " minimize ") + fitnessExpression.toGaml());
+				String sim =fitnessExpression == null ? "" :(
 					(combination == C_MAX ? " max " : combination == C_MIN ? " min " : " average ") + "of " +
-						agent.getSeeds().length + " simulations";
+						agent.getSeeds().length + " simulations");
 				return "Method " + name + " | " + fit + " | " + "compute the" + sim + " for each solution";
 			}
 
