f56bba4616bb61efc1c7fd5ca5c8359314330498
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-04-14 23:52:10 +0000

    architecture.simplebdi for 1.6.1PR3
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9374 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gaml.architecture.simplebdi/src/msi/gaml/architecture/simplebdi/SimpleBdiArchitecture.java
+++ msi.gaml.architecture.simplebdi/src/msi/gaml/architecture/simplebdi/SimpleBdiArchitecture.java
@@ -1,3 +1,14 @@
+/*********************************************************************************************
+ * 
+ *
+ * 'SimpleBdiArchitecture.java', in plugin 'msi.gaml.architecture.simplebdi', is part of the source code of the 
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 
+ * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
+ * 
+ * 
+ **********************************************************************************************/
 package msi.gaml.architecture.simplebdi;
 
 import java.util.*;
