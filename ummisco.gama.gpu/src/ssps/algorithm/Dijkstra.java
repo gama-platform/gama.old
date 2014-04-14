370cce23dfe5d99e10774e601392907cf641e220
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-04-14 23:45:26 +0000

    gama.gpu for 1.6.1 PR3
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9364 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- ummisco.gama.gpu/src/ssps/algorithm/Dijkstra.java
+++ ummisco.gama.gpu/src/ssps/algorithm/Dijkstra.java
@@ -1,7 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*********************************************************************************************
+ * 
+ *
+ * 'Dijkstra.java', in plugin 'ummisco.gama.gpu', is part of the source code of the 
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 
+ * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
+ * 
+ * 
+ **********************************************************************************************/
 package ssps.algorithm;
 
 import static org.jocl.CL.CL_CONTEXT_PLATFORM;
--- ummisco.gama.gpu/src/ssps/operators/GPUGraphOperators.java
+++ ummisco.gama.gpu/src/ssps/operators/GPUGraphOperators.java
@@ -1,3 +1,14 @@
+/*********************************************************************************************
+ * 
+ *
+ * 'GPUGraphOperators.java', in plugin 'ummisco.gama.gpu', is part of the source code of the 
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 
+ * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
+ * 
+ * 
+ **********************************************************************************************/
 package ssps.operators;
 
 import java.util.Set;
