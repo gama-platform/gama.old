9692f9493ff2472290c12f553a5f8d73cff29cde
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-04-15 00:29:23 +0000

    maelia additions (still active?) for 1.6.1PR3
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9384 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- irit.maelia.gaml.additions/src/irit/gaml/skills/MAELIASkill.java
+++ irit.maelia.gaml.additions/src/irit/gaml/skills/MAELIASkill.java
@@ -1,3 +1,14 @@
+/*********************************************************************************************
+ * 
+ *
+ * 'MAELIASkill.java', in plugin 'irit.maelia.gaml.additions', is part of the source code of the 
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 
+ * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
+ * 
+ * 
+ **********************************************************************************************/
 package irit.gaml.skills;
 
 /*
