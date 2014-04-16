bd25430259da9b87fb5fb056f0a2003b89e97bd1
agrignard <agrignard@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-04-16 15:50:03 +0000

    make movingskil3D inherits from movingSkil
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9422 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/skills/MovingSkill3D.java
+++ msi.gama.core/src/msi/gaml/skills/MovingSkill3D.java
@@ -56,7 +56,7 @@ import com.vividsolutions.jts.precision.GeometryPrecisionReducer;
 	@var(name = IKeyword.DESTINATION, type = IType.POINT, depends_on = { IKeyword.SPEED, IKeyword.HEADING,
 		IKeyword.LOCATION }, doc = @doc("continuously updated destination of the agent with respect to its speed and heading (read-only)")) })
 @skill(name = IKeyword.MOVING_3D_SKILL)
-public class MovingSkill3D extends Skill {
+public class MovingSkill3D extends MovingSkill {
 
 	
 	@getter(IKeyword.DESTINATION)
@@ -167,7 +167,7 @@ public class MovingSkill3D extends Skill {
 
 
 	//FIXME: BOUNDS NOT WORKING YET
-	@action(name = "move", args = {
+	@action(name = "move3D", args = {
 			@arg(name = IKeyword.SPEED, type = IType.FLOAT, optional = true, doc = @doc("the speed to use for this move (replaces the current value of speed)")),
 			@arg(name = IKeyword.HEADING, type = IType.INT, optional = true, doc = @doc("int, optional, the direction to take for this move (replaces the current value of heading)")),
 			@arg(name = IKeyword.PITCH, type = IType.INT, optional = true, doc = @doc("int, optional, the direction to take for this move (replaces the current value of pitch)")),
@@ -175,7 +175,7 @@ public class MovingSkill3D extends Skill {
 			@arg(name = IKeyword.BOUNDS, type = { IType.GEOMETRY, IType.AGENT }, optional = true, doc = @doc("the geometry (the localized entity geometry) that restrains this move (the agent moves inside this geometry")) }, 
 			doc = @doc(examples = { @example("do move speed: speed - 10 heading: heading + rnd (30) bounds: agentA;") }, 
 			value = "moves the agent forward, the distance being computed with respect to its speed and heading. The value of the corresponding variables are used unless arguments are passed."))
-	public IPath primMoveForward3D(final IScope scope) throws GamaRuntimeException {
+	public IPath primMoveForward3D  (final IScope scope) throws GamaRuntimeException {
 			final IAgent agent = getCurrentAgent(scope);
 			final ILocation location = agent.getLocation();
 			final double dist = computeDistance(scope, agent);
@@ -201,7 +201,7 @@ public class MovingSkill3D extends Skill {
 			return null;
 		}
 
-	@action(name = "wander", args = {
+	@action(name = "wander3D", args = {
 		@arg(name = IKeyword.SPEED, type = IType.FLOAT, optional = true, doc = @doc("the speed to use for this move (replaces the current value of speed)")),
 		@arg(name = "amplitude", type = IType.INT, optional = true, doc = @doc("a restriction placed on the random heading choice. The new heading is chosen in the range (heading - amplitude/2, heading+amplitude/2)")),
 		@arg(name = IKeyword.BOUNDS, type = { IType.AGENT, IType.GEOMETRY }, optional = true, doc = @doc("the geometry (the localized entity geometry) that restrains this move (the agent moves inside this geometry")) }, doc = @doc(examples = { @example("do wander speed: speed - 10 amplitude: 120 bounds: agentA;") }, value = "Moves the agent towards a random location at the maximum distance (with respect to its speed). The heading of the agent is chosen randomly if no amplitude is specified. This action changes the value of heading."))
