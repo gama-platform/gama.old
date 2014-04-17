69730f72a9cdfd7b281645f1678deb0911511515
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-04-17 03:43:58 +0000

    Changed the call to setLocation()
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9441 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- ummisco.miro.extension/src/ummisco/miro/extension/moving/TrafficMovingSkill.java
+++ ummisco.miro.extension/src/ummisco/miro/extension/moving/TrafficMovingSkill.java
@@ -22,14 +22,31 @@ import com.vividsolutions.jts.geom.Coordinate;
 @skill(name = "trafficMoving")
 public class TrafficMovingSkill extends MovingSkill {
 
-	@action(name = "goto_traffic", args = {
-		@arg(name = "target", type = { IType.POINT, IType.GEOMETRY, IType.AGENT }, optional = false, doc = @doc("the location or entity towards which to move.")),
-		@arg(name = IKeyword.SPEED, type = IType.FLOAT, optional = true, doc = @doc("the speed to use for this move (replaces the current value of speed)")),
-		@arg(name = "on", type = { IType.LIST, IType.AGENT, IType.GRAPH, IType.GEOMETRY }, optional = true, doc = @doc("list, agent, graph, geometry that restrains this move (the agent moves inside this geometry)")),
-		@arg(name = "duration", type = IType.FLOAT, optional = true, doc = @doc("duration of the moving")),
-		@arg(name = "max_speed", type = IType.FLOAT, optional = true, doc = @doc("speedMoving")),
-		@arg(name = "return_path", type = IType.BOOL, optional = true, doc = @doc("if true, return the path followed (by default: false)")) }, doc = @doc(value = "moves the agent towards the target passed in the arguments while considering the other agents in the network (only for graph topology)", returns = "optional: the path followed by the agent.", examples = { @example("do gotoTraffic target: one_of (list (species (self))) speed: speed * 2 on: road_network living_space: 2.0;") }))
-	public void primGotoTraffic(final IScope scope) throws GamaRuntimeException {
+	@action(name = "goto_traffic",
+		args = {
+			@arg(name = "target",
+				type = { IType.POINT, IType.GEOMETRY, IType.AGENT },
+				optional = false,
+				doc = @doc("the location or entity towards which to move.")),
+			@arg(name = IKeyword.SPEED,
+				type = IType.FLOAT,
+				optional = true,
+				doc = @doc("the speed to use for this move (replaces the current value of speed)")),
+			@arg(name = "on",
+				type = { IType.LIST, IType.AGENT, IType.GRAPH, IType.GEOMETRY },
+				optional = true,
+				doc = @doc("list, agent, graph, geometry that restrains this move (the agent moves inside this geometry)")),
+			@arg(name = "duration", type = IType.FLOAT, optional = true, doc = @doc("duration of the moving")),
+			@arg(name = "max_speed", type = IType.FLOAT, optional = true, doc = @doc("speedMoving")),
+			@arg(name = "return_path",
+				type = IType.BOOL,
+				optional = true,
+				doc = @doc("if true, return the path followed (by default: false)")) },
+		doc = @doc(value = "moves the agent towards the target passed in the arguments while considering the other agents in the network (only for graph topology)",
+			returns = "optional: the path followed by the agent.",
+			examples = { @example("do gotoTraffic target: one_of (list (species (self))) speed: speed * 2 on: road_network living_space: 2.0;") }))
+	public
+		void primGotoTraffic(final IScope scope) throws GamaRuntimeException {
 		final IAgent agent = getCurrentAgent(scope);
 		ILocation source = agent.getLocation().copy(scope);
 
@@ -82,12 +99,26 @@ public class TrafficMovingSkill extends MovingSkill {
 		return;
 	}
 
-	@action(name = "theorical_duration", args = {
-		@arg(name = "from", type = { IType.POINT, IType.GEOMETRY, IType.AGENT }, optional = false, doc = @doc("the location or entity towards which to move.")),
-		@arg(name = "to", type = { IType.POINT, IType.GEOMETRY, IType.AGENT }, optional = false, doc = @doc("the location or entity towards which to move.")),
-		@arg(name = "on", type = { IType.LIST, IType.AGENT, IType.GRAPH, IType.GEOMETRY }, optional = true, doc = @doc("list, agent, graph, geometry that restrains this move (the agent moves inside this geometry)")),
-		@arg(name = "max_speed", type = IType.FLOAT, optional = true, doc = @doc("speedMoving")) }, doc = @doc(value = "moves the agent towards the target passed in the arguments while considering the other agents in the network (only for graph topology)", returns = "optional: the path followed by the agent.", examples = { @example("do gotoTraffic target: one_of (list (species (self))) speed: speed * 2 on: road_network living_space: 2.0;") }))
-	public Float computeTheoricalTransportationTime(final IScope scope) throws GamaRuntimeException {
+	@action(name = "theorical_duration",
+		args = {
+			@arg(name = "from",
+				type = { IType.POINT, IType.GEOMETRY, IType.AGENT },
+				optional = false,
+				doc = @doc("the location or entity towards which to move.")),
+			@arg(name = "to",
+				type = { IType.POINT, IType.GEOMETRY, IType.AGENT },
+				optional = false,
+				doc = @doc("the location or entity towards which to move.")),
+			@arg(name = "on",
+				type = { IType.LIST, IType.AGENT, IType.GRAPH, IType.GEOMETRY },
+				optional = true,
+				doc = @doc("list, agent, graph, geometry that restrains this move (the agent moves inside this geometry)")),
+			@arg(name = "max_speed", type = IType.FLOAT, optional = true, doc = @doc("speedMoving")) },
+		doc = @doc(value = "moves the agent towards the target passed in the arguments while considering the other agents in the network (only for graph topology)",
+			returns = "optional: the path followed by the agent.",
+			examples = { @example("do gotoTraffic target: one_of (list (species (self))) speed: speed * 2 on: road_network living_space: 2.0;") }))
+	public
+		Float computeTheoricalTransportationTime(final IScope scope) throws GamaRuntimeException {
 		final IAgent agent = getCurrentAgent(scope);
 		ILocation source = computeFrom(scope, agent);
 		final ILocation goal = computeTo(scope, agent);
@@ -283,9 +314,9 @@ public class TrafficMovingSkill extends MovingSkill {
 		path.setIndexSegementOf(agent, indexSegment);
 		path.setIndexOf(agent, index);
 
-		agent.setLocation(currentLocation);
+		setLocation(agent, currentLocation);
 		path.setSource(currentLocation.copy(scope));
-		agent.setLocation(currentLocation);
+		setLocation(agent, currentLocation);
 		if ( segments.isEmpty() ) { return null; }
 		IPath followedPath =
 			PathFactory.newInstance(agent.getTopology(), startLocation, currentLocation, segments, false);
