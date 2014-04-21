234e3f105a38b5d8d2c83d4c4fbb796be1bb2b95
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-04-21 16:20:30 +0000

    Removed all mentions of "pertinence"
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9525 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/statements/ActionStatement.java
+++ msi.gama.core/src/msi/gaml/statements/ActionStatement.java
@@ -14,13 +14,13 @@ package msi.gaml.statements;
 import gnu.trove.set.hash.TLinkedHashSet;
 import java.util.Set;
 import msi.gama.common.interfaces.*;
+import msi.gama.precompiler.GamlAnnotations.doc;
+import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.facet;
 import msi.gama.precompiler.GamlAnnotations.facets;
 import msi.gama.precompiler.GamlAnnotations.inside;
 import msi.gama.precompiler.GamlAnnotations.symbol;
-import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.usage;
-import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.validator;
 import msi.gama.precompiler.*;
 import msi.gama.runtime.IScope;
@@ -38,20 +38,51 @@ import msi.gaml.types.*;
  */
 @symbol(name = IKeyword.ACTION, kind = ISymbolKind.ACTION, with_sequence = true, with_args = true, unique_name = true)
 @inside(kinds = { ISymbolKind.SPECIES, ISymbolKind.EXPERIMENT, ISymbolKind.MODEL })
-@facets(value = { @facet(name = IKeyword.NAME, type = IType.ID, optional = false, doc = @doc("identifier of the action")),
-	@facet(name = IKeyword.TYPE, type = IType.TYPE_ID, optional = true, doc = @doc("the action returned type"), internal = true),
-	@facet(name = IKeyword.OF, type = IType.TYPE_ID, optional = true, doc = @doc("if the action returns a container, the type of its elements"), internal = true),
-	@facet(name = IKeyword.INDEX, type = IType.TYPE_ID, optional = true, doc = @doc("if the action returns a map, the type of its keys"), internal = true),
-	@facet(name = IKeyword.VIRTUAL, type = IType.BOOL, optional = true, doc = @doc("whether the action is virtual (defined without a set of instructions) (false by default)")),
-	@facet(name = IKeyword.PERTINENCE, type = IType.FLOAT, optional = true, doc = @doc("the action pertinence")) }, omissible = IKeyword.NAME)
-@doc(value="Allows to define in a species, model or experiment a new action that can be called elsewhere.", usages = {
-	@usage(value="The simplest syntax to define an action that does not take any parameter and does not return anything is:", examples= {@example(value="action simple_action {",isExecutable=false),@example(value="   // [set of statements]",isExecutable=false),@example(value="}",isExecutable=false)}),
-	@usage(value="If the action needs some parameters, they can be specified betwee, braquets after the identifier of the action:",examples= {@example(value="action action_parameters(int i, string s){",isExecutable=false),@example(value="   // [set of statements using i and s]",isExecutable=false),@example(value="}",isExecutable=false)}),
-	@usage(value="If the action returns any value, the returned type should be used instead of the \"action\" keyword. A return statement inside the body of the action statement is mandatory.",examples= {@example(value="int action_return_val(int i, string s){",isExecutable=false),@example(value="   // [set of statements using i and s]",isExecutable=false),@example(value="   return i + i;",isExecutable=false),@example(value="}",isExecutable=false)}),
-	@usage(value="If virtual: is true, then the action is abstract, which means that the action is defined without body. A species containing at least one abstract action is abstract. Agents of this species cannot be created. The common use of an abstract action is to define an action that can be used by all its sub-species, which should redefine all abstract actions and implements its body.", examples= {
-			@example(value="species parent_species {", isExecutable = false),@example(value="   int virtual_action(int i, string s);", isExecutable = false),@example(value="}", isExecutable = false),@example(value="", isExecutable = false),@example(value="species children parent: parent_species {", isExecutable = false),@example(value="   int virtual_action(int i, string s) {", isExecutable = false),@example(value="      return i + i;", isExecutable = false),@example(value="   }", isExecutable = false),@example(value="}", isExecutable = false)
-	})
-}, see={"do"})
+@facets(value = {
+	@facet(name = IKeyword.NAME, type = IType.ID, optional = false, doc = @doc("identifier of the action")),
+	@facet(name = IKeyword.TYPE,
+		type = IType.TYPE_ID,
+		optional = true,
+		doc = @doc("the action returned type"),
+		internal = true),
+	@facet(name = IKeyword.OF,
+		type = IType.TYPE_ID,
+		optional = true,
+		doc = @doc("if the action returns a container, the type of its elements"),
+		internal = true),
+	@facet(name = IKeyword.INDEX,
+		type = IType.TYPE_ID,
+		optional = true,
+		doc = @doc("if the action returns a map, the type of its keys"),
+		internal = true),
+	@facet(name = IKeyword.VIRTUAL,
+		type = IType.BOOL,
+		optional = true,
+		doc = @doc("whether the action is virtual (defined without a set of instructions) (false by default)")), },
+	omissible = IKeyword.NAME)
+@doc(value = "Allows to define in a species, model or experiment a new action that can be called elsewhere.",
+	usages = {
+		@usage(value = "The simplest syntax to define an action that does not take any parameter and does not return anything is:",
+			examples = { @example(value = "action simple_action {", isExecutable = false),
+				@example(value = "   // [set of statements]", isExecutable = false),
+				@example(value = "}", isExecutable = false) }),
+		@usage(value = "If the action needs some parameters, they can be specified betwee, braquets after the identifier of the action:",
+			examples = { @example(value = "action action_parameters(int i, string s){", isExecutable = false),
+				@example(value = "   // [set of statements using i and s]", isExecutable = false),
+				@example(value = "}", isExecutable = false) }),
+		@usage(value = "If the action returns any value, the returned type should be used instead of the \"action\" keyword. A return statement inside the body of the action statement is mandatory.",
+			examples = { @example(value = "int action_return_val(int i, string s){", isExecutable = false),
+				@example(value = "   // [set of statements using i and s]", isExecutable = false),
+				@example(value = "   return i + i;", isExecutable = false), @example(value = "}", isExecutable = false) }),
+		@usage(value = "If virtual: is true, then the action is abstract, which means that the action is defined without body. A species containing at least one abstract action is abstract. Agents of this species cannot be created. The common use of an abstract action is to define an action that can be used by all its sub-species, which should redefine all abstract actions and implements its body.",
+			examples = { @example(value = "species parent_species {", isExecutable = false),
+				@example(value = "   int virtual_action(int i, string s);", isExecutable = false),
+				@example(value = "}", isExecutable = false), @example(value = "", isExecutable = false),
+				@example(value = "species children parent: parent_species {", isExecutable = false),
+				@example(value = "   int virtual_action(int i, string s) {", isExecutable = false),
+				@example(value = "      return i + i;", isExecutable = false),
+				@example(value = "   }", isExecutable = false), @example(value = "}", isExecutable = false) }) },
+	see = { "do" })
 @validator(ActionValidator.class)
 public class ActionStatement extends AbstractStatementSequence implements IStatement.WithArgs {
 
@@ -120,9 +151,7 @@ public class ActionStatement extends AbstractStatementSequence implements IState
 		if ( hasFacet(IKeyword.NAME) ) {
 			name = getLiteral(IKeyword.NAME);
 		}
-		if ( hasFacet(IKeyword.PERTINENCE) ) {
-			pertinence = getFacet(IKeyword.PERTINENCE);
-		}
+
 	}
 
 	@Override
