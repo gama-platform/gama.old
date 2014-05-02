b6bacf5b9648d4b8f821606435aa52d608883a34
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-05-02 19:25:53 +0000

    Imposes the use of the encoding in file names for all URIs manipulated in GAMA.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9782 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.lang.gaml.ui/src/msi/gama/lang/gaml/ui/editor/GamlHyperlinkDetector.java
+++ msi.gama.lang.gaml.ui/src/msi/gama/lang/gaml/ui/editor/GamlHyperlinkDetector.java
@@ -1,7 +1,7 @@
 /*********************************************************************************************
  * 
- *
- * 'GamlHyperlinkDetector.java', in plugin 'msi.gama.lang.gaml.ui', is part of the source code of the 
+ * 
+ * 'GamlHyperlinkDetector.java', in plugin 'msi.gama.lang.gaml.ui', is part of the source code of the
  * GAMA modeling and simulation platform.
  * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
  * 
@@ -12,7 +12,7 @@
 package msi.gama.lang.gaml.ui.editor;
 
 import msi.gama.lang.gaml.gaml.Import;
-import msi.gama.lang.gaml.ui.*;
+import msi.gama.lang.gaml.ui.FileOpener;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.*;
@@ -48,8 +48,7 @@ public class GamlHyperlinkDetector extends DefaultHyperlinkDetector {
 
 			@Override
 			public IHyperlink[] exec(final XtextResource resource) {
-				return getHelper().createHyperlinksByOffset(resource, region.getOffset(),
-					canShowMultipleHyperlinks);
+				return getHelper().createHyperlinksByOffset(resource, region.getOffset(), canShowMultipleHyperlinks);
 			}
 		});
 	}
@@ -59,14 +58,13 @@ public class GamlHyperlinkDetector extends DefaultHyperlinkDetector {
 
 			@Override
 			public IHyperlink[] exec(final XtextResource resource) {
-				EObject resolved =
-					eObjectAtOffsetHelper.resolveElementAt(resource, region.getOffset());
+				EObject resolved = eObjectAtOffsetHelper.resolveElementAt(resource, region.getOffset());
 				if ( !(resolved instanceof Import) ) { return NO_HYPERLINKS; }
 				Import anImport = (Import) resolved;
 				// if ( !imports.isResolved(anImport) ) { return NO_HYPERLINKS; }
 				String importUri = anImport.getImportURI();
 				if ( importUri == null ) { return NO_HYPERLINKS; }
-				URI iu = URI.createURI(importUri).resolve(resource.getURI());
+				URI iu = URI.createURI(importUri, false).resolve(resource.getURI());
 				IRegion importUriRegion;
 				try {
 					importUriRegion = importUriRegion(document, region.getOffset(), importUri);
@@ -80,8 +78,8 @@ public class GamlHyperlinkDetector extends DefaultHyperlinkDetector {
 		});
 	}
 
-	private IRegion importUriRegion(final IXtextDocument document, final int offset,
-		final String importUri) throws BadLocationException {
+	private IRegion importUriRegion(final IXtextDocument document, final int offset, final String importUri)
+		throws BadLocationException {
 		int lineNumber = document.getLineOfOffset(offset);
 		int lineLength = document.getLineLength(lineNumber);
 		int lineOffset = document.getLineOffset(lineNumber);
