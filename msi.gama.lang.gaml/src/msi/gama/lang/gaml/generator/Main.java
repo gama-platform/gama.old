45e6f6098e3882b206b4565bedf49d6e676bd927
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-05-02 19:19:05 +0000

    Imposes the use of the encoding in file names for all URIs manipulated in GAMA.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9774 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.lang.gaml/src/msi/gama/lang/gaml/generator/Main.java
+++ msi.gama.lang.gaml/src/msi/gama/lang/gaml/generator/Main.java
@@ -1,7 +1,7 @@
 /*********************************************************************************************
  * 
- *
- * 'Main.java', in plugin 'msi.gama.lang.gaml', is part of the source code of the 
+ * 
+ * 'Main.java', in plugin 'msi.gama.lang.gaml', is part of the source code of the
  * GAMA modeling and simulation platform.
  * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
  * 
@@ -12,25 +12,17 @@
 package msi.gama.lang.gaml.generator;
 
 import java.util.List;
-
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.xtext.generator.IGenerator;
-import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
+import org.eclipse.emf.ecore.resource.*;
+import org.eclipse.xtext.generator.*;
 import org.eclipse.xtext.util.CancelIndicator;
-import org.eclipse.xtext.validation.CheckMode;
-import org.eclipse.xtext.validation.IResourceValidator;
-import org.eclipse.xtext.validation.Issue;
-
-import com.google.inject.Inject;
-import com.google.inject.Injector;
-import com.google.inject.Provider;
+import org.eclipse.xtext.validation.*;
+import com.google.inject.*;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		if (args.length==0) {
+
+	public static void main(final String[] args) {
+		if ( args.length == 0 ) {
 			System.err.println("Aborting: no path to EMF resource provided!");
 			return;
 		}
@@ -38,37 +30,37 @@ public class Main {
 		Main main = injector.getInstance(Main.class);
 		main.runGenerator(args[0]);
 	}
-	
-	@Inject 
+
+	@Inject
 	private Provider<ResourceSet> resourceSetProvider;
-	
+
 	@Inject
 	private IResourceValidator validator;
-	
+
 	@Inject
 	private IGenerator generator;
-	
-	@Inject 
+
+	@Inject
 	private JavaIoFileSystemAccess fileAccess;
 
-	protected void runGenerator(String string) {
+	protected void runGenerator(final String string) {
 		// load the resource
 		ResourceSet set = resourceSetProvider.get();
-		Resource resource = set.getResource(URI.createURI(string), true);
-		
+		Resource resource = set.getResource(URI.createURI(string, false), true);
+
 		// validate the resource
 		List<Issue> list = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
-		if (!list.isEmpty()) {
-			for (Issue issue : list) {
+		if ( !list.isEmpty() ) {
+			for ( Issue issue : list ) {
 				System.err.println(issue);
 			}
 			return;
 		}
-		
+
 		// configure and start the generator
 		fileAccess.setOutputPath("src-gen/");
 		generator.doGenerate(resource, fileAccess);
-		
+
 		System.out.println("Code generation finished.");
 	}
 }
