d2918acfa5b23416522dfc519e378ef8e4034a35
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-05-05 06:38:13 +0000

    Fixes warnings when using points for the "value:" facet
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@9943 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gama/outputs/layers/ChartDataStatement.java
+++ msi.gama.core/src/msi/gama/outputs/layers/ChartDataStatement.java
@@ -13,7 +13,6 @@ package msi.gama.outputs.layers;
 
 import java.awt.Shape;
 import java.util.ArrayList;
-
 import msi.gama.common.interfaces.IKeyword;
 import msi.gama.metamodel.shape.GamaPoint;
 import msi.gama.precompiler.GamlAnnotations.facet;
@@ -29,48 +28,49 @@ import msi.gaml.expressions.IExpression;
 import msi.gaml.operators.Cast;
 import msi.gaml.statements.AbstractStatement;
 import msi.gaml.types.IType;
-
 import org.jfree.chart.renderer.AbstractRenderer;
 import org.jfree.chart.renderer.category.*;
 import org.jfree.chart.renderer.xy.*;
-import org.jfree.chart.plot.DefaultDrawingSupplier;
 
 @symbol(name = IKeyword.DATA, kind = ISymbolKind.SINGLE_STATEMENT, with_sequence = false)
 @inside(symbols = IKeyword.CHART, kinds = ISymbolKind.SEQUENCE_STATEMENT)
 @facets(value = {
-	@facet(name = IKeyword.VALUE, type = { IType.FLOAT, IType.LIST }, optional = false),
+	@facet(name = IKeyword.VALUE, type = { IType.FLOAT, IType.LIST, IType.POINT }, optional = false),
 	@facet(name = IKeyword.NAME, type = IType.ID, optional = true),
 	@facet(name = IKeyword.LEGEND, type = IType.STRING, optional = true),
 	@facet(name = IKeyword.COLOR, type = IType.COLOR, optional = true),
 	@facet(name = ChartDataStatement.LINE_VISIBLE, type = IType.BOOL, optional = true),
 	@facet(name = ChartDataStatement.MARKER, type = IType.BOOL, optional = true),
-	@facet(name = ChartDataStatement.MARKERSHAPE, type = IType.ID,values={ChartDataStatement.MARKER_EMPTY,ChartDataStatement.MARKER_SQUARE,ChartDataStatement.MARKER_CIRCLE,ChartDataStatement.MARKER_UP_TRIANGLE,
-			ChartDataStatement.MARKER_DIAMOND,ChartDataStatement.MARKER_HOR_RECTANGLE,ChartDataStatement.MARKER_DOWN_TRIANGLE,ChartDataStatement.MARKER_HOR_ELLIPSE,ChartDataStatement.MARKER_RIGHT_TRIANGLE,
-			ChartDataStatement.MARKER_VERT_RECTANGLE,ChartDataStatement.MARKER_LEFT_TRIANGLE}, optional = true),
+	@facet(name = ChartDataStatement.MARKERSHAPE, type = IType.ID, values = { ChartDataStatement.MARKER_EMPTY,
+		ChartDataStatement.MARKER_SQUARE, ChartDataStatement.MARKER_CIRCLE, ChartDataStatement.MARKER_UP_TRIANGLE,
+		ChartDataStatement.MARKER_DIAMOND, ChartDataStatement.MARKER_HOR_RECTANGLE,
+		ChartDataStatement.MARKER_DOWN_TRIANGLE, ChartDataStatement.MARKER_HOR_ELLIPSE,
+		ChartDataStatement.MARKER_RIGHT_TRIANGLE, ChartDataStatement.MARKER_VERT_RECTANGLE,
+		ChartDataStatement.MARKER_LEFT_TRIANGLE }, optional = true),
 	@facet(name = ChartDataStatement.FILL, type = IType.BOOL, optional = true),
 	@facet(name = IKeyword.STYLE, type = IType.ID, values = { IKeyword.LINE, IKeyword.WHISKER, IKeyword.AREA,
-		IKeyword.BAR, IKeyword.DOT, IKeyword.STEP, IKeyword.SPLINE, IKeyword.STACK,IKeyword.THREE_D, IKeyword.RING,
+		IKeyword.BAR, IKeyword.DOT, IKeyword.STEP, IKeyword.SPLINE, IKeyword.STACK, IKeyword.THREE_D, IKeyword.RING,
 		IKeyword.EXPLODED }, optional = true) }, omissible = IKeyword.LEGEND)
 public class ChartDataStatement extends AbstractStatement {
 
 	public static final String MARKER = "marker";
 	public static final String MARKERSHAPE = "marker_shape";
 	public static final String FILL = "fill";
+	public static final String LINE_VISIBLE = "line_visible";
+	public static final String MARKER_EMPTY = "marker_empty";
+	public static final String MARKER_SQUARE = "marker_sqaure";
+	public static final String MARKER_CIRCLE = "marker_square";
+	public static final String MARKER_UP_TRIANGLE = "marker_up_triangle";
+	public static final String MARKER_DIAMOND = "marker_diamond";
+	public static final String MARKER_HOR_RECTANGLE = "marker_hor_rectangle";
+	public static final String MARKER_DOWN_TRIANGLE = "marker_down_triangle";
+	public static final String MARKER_HOR_ELLIPSE = "marker_hor_ellipse";
+	public static final String MARKER_RIGHT_TRIANGLE = "marker_right_triangle";
+	public static final String MARKER_VERT_RECTANGLE = "marker_vert_rectangle";
+	public static final String MARKER_LEFT_TRIANGLE = "marker_left_triangle";
 
-	public static final String LINE_VISIBLE="line_visible";
-	public static final String MARKER_EMPTY="marker_empty";
-	public static final String MARKER_SQUARE="marker_sqaure";
-	public static final String MARKER_CIRCLE="marker_square";
-	public static final String MARKER_UP_TRIANGLE="marker_up_triangle";
-	public static final String MARKER_DIAMOND="marker_diamond";
-	public static final String MARKER_HOR_RECTANGLE="marker_hor_rectangle";
-	public static final String MARKER_DOWN_TRIANGLE="marker_down_triangle";
-	public static final String MARKER_HOR_ELLIPSE="marker_hor_ellipse";
-	public static final String MARKER_RIGHT_TRIANGLE="marker_right_triangle";
-	public static final String MARKER_VERT_RECTANGLE="marker_vert_rectangle";
-	public static final String MARKER_LEFT_TRIANGLE="marker_left_triangle";
-	
-	public static final Shape[] defaultmarkers=org.jfree.chart.plot.DefaultDrawingSupplier.createStandardSeriesShapes();
+	public static final Shape[] defaultmarkers = org.jfree.chart.plot.DefaultDrawingSupplier
+		.createStandardSeriesShapes();
 
 	public static class ChartData {
 
@@ -128,7 +128,7 @@ public class ChartDataStatement extends AbstractStatement {
 
 	public static final String DATAS = "chart_datas";
 	protected int dataNumber = 0;
-	
+
 	public ChartDataStatement(final IDescription desc) {
 		super(desc);
 	}
@@ -148,61 +148,38 @@ public class ChartDataStatement extends AbstractStatement {
 		boolean showMarkers = getFacetValue(scope, MARKER, true);
 		boolean showLine = getFacetValue(scope, LINE_VISIBLE, true);
 		boolean fillMarkers = getFacetValue(scope, FILL, true);
-		String shapeMarker=getFacetValue(scope,MARKERSHAPE,null);
-		
+		String shapeMarker = getFacetValue(scope, MARKERSHAPE, null);
 
 		AbstractRenderer r = null;
 		if ( style.equals(IKeyword.LINE) ) {
 			r = new XYLineAndShapeRenderer(true, showMarkers);
 			r.setSeriesPaint(0, color);
 			((XYLineAndShapeRenderer) r).setBaseShapesFilled(fillMarkers);
-			if (shapeMarker!=null)
-			{
-				if (shapeMarker.equals(MARKER_SQUARE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[0]);					
+			if ( shapeMarker != null ) {
+				if ( shapeMarker.equals(MARKER_SQUARE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[0]);
+				} else if ( shapeMarker.equals(MARKER_CIRCLE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[1]);
+				} else if ( shapeMarker.equals(MARKER_UP_TRIANGLE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[2]);
+				} else if ( shapeMarker.equals(MARKER_DIAMOND) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[3]);
+				} else if ( shapeMarker.equals(MARKER_HOR_RECTANGLE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[4]);
+				} else if ( shapeMarker.equals(MARKER_DOWN_TRIANGLE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[5]);
+				} else if ( shapeMarker.equals(MARKER_HOR_ELLIPSE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[6]);
+				} else if ( shapeMarker.equals(MARKER_RIGHT_TRIANGLE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[7]);
+				} else if ( shapeMarker.equals(MARKER_VERT_RECTANGLE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[8]);
+				} else if ( shapeMarker.equals(MARKER_LEFT_TRIANGLE) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[9]);
+				} else if ( shapeMarker.equals(MARKER_EMPTY) ) {
+					((XYLineAndShapeRenderer) r).setSeriesShapesVisible(0, false);
 				}
-				else if (shapeMarker.equals(MARKER_CIRCLE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[1]);					
-				}
-				else if (shapeMarker.equals(MARKER_UP_TRIANGLE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[2]);					
-				}
-				else if (shapeMarker.equals(MARKER_DIAMOND))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[3]);					
-				}
-				else if (shapeMarker.equals(MARKER_HOR_RECTANGLE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[4]);					
-				}
-				else if (shapeMarker.equals(MARKER_DOWN_TRIANGLE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[5]);					
-				}
-				else if (shapeMarker.equals(MARKER_HOR_ELLIPSE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[6]);					
-				}
-				else if (shapeMarker.equals(MARKER_RIGHT_TRIANGLE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[7]);					
-				}
-				else if (shapeMarker.equals(MARKER_VERT_RECTANGLE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[8]);					
-				}
-				else if (shapeMarker.equals(MARKER_LEFT_TRIANGLE))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShape(0, defaultmarkers[9]);					
-				}
-				else if (shapeMarker.equals(MARKER_EMPTY))
-				{
-					((XYLineAndShapeRenderer) r).setSeriesShapesVisible(0, false); 
-				}
-				
+
 			}
 			((XYLineAndShapeRenderer) r).setSeriesLinesVisible(0, showLine);
 		} else if ( style.equals(IKeyword.AREA) ) {
