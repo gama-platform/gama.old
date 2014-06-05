13c7d48ede8ea04ad450e24ebdf4f7c2fbbee96e
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-06-05 13:12:31 +0000

    Addresses Issue 1036 (#3) by explicitly passing the "user location" to the menu context
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@11088 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/statements/UserCommandStatement.java
+++ msi.gama.core/src/msi/gaml/statements/UserCommandStatement.java
@@ -1,7 +1,7 @@
 /*********************************************************************************************
  * 
- *
- * 'UserCommandStatement.java', in plugin 'msi.gama.core', is part of the source code of the 
+ * 
+ * 'UserCommandStatement.java', in plugin 'msi.gama.core', is part of the source code of the
  * GAMA modeling and simulation platform.
  * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
  * 
@@ -13,13 +13,13 @@ package msi.gaml.statements;
 
 import java.util.*;
 import msi.gama.common.interfaces.IKeyword;
+import msi.gama.precompiler.GamlAnnotations.doc;
+import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.facet;
 import msi.gama.precompiler.GamlAnnotations.facets;
 import msi.gama.precompiler.GamlAnnotations.inside;
 import msi.gama.precompiler.GamlAnnotations.symbol;
-import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.usage;
-import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.*;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
@@ -39,16 +39,30 @@ import msi.gaml.types.IType;
  */
 @symbol(name = { IKeyword.USER_COMMAND }, kind = ISymbolKind.SEQUENCE_STATEMENT, with_sequence = true, with_args = true)
 @inside(kinds = { ISymbolKind.SPECIES, ISymbolKind.EXPERIMENT, ISymbolKind.MODEL }, symbols = IKeyword.USER_PANEL)
-@facets(value = { @facet(name = IKeyword.ACTION, type = IType.ID, optional = true, doc = @doc("the identifier of the action to be executed")),
+@facets(value = {
+	@facet(name = IKeyword.ACTION,
+		type = IType.ID,
+		optional = true,
+		doc = @doc("the identifier of the action to be executed")),
 	@facet(name = IKeyword.NAME, type = IType.LABEL, optional = false, doc = @doc("the identifier of the user_command")),
-	@facet(name = IKeyword.WHEN, type = IType.BOOL, optional = true, doc = @doc("the condition that should be fulfille in order that the action is executed")),
-	@facet(name = IKeyword.WITH, type = IType.MAP, optional = true, doc = @doc("the map of the parameters::values that requires the action")) }, omissible = IKeyword.NAME)
-@doc(value="Anywhere in the global block, in a species or in an (GUI) experiment, user_command statements allows to either call directly an existing action (with or without arguments) or to be followed by a block that describes what to do when this command is run.", usages={
-		@usage(value="The general syntax is for example:", examples = @example(value="user_command kill_myself action: some_action with: [arg1::val1, arg2::val2, ...];", isExecutable=false))
-	}, see = {IKeyword.USER_INIT,IKeyword.USER_PANEL, IKeyword.USER_INPUT})
+	@facet(name = IKeyword.WHEN,
+		type = IType.BOOL,
+		optional = true,
+		doc = @doc("the condition that should be fulfille in order that the action is executed")),
+	@facet(name = IKeyword.WITH,
+		type = IType.MAP,
+		optional = true,
+		doc = @doc("the map of the parameters::values that requires the action")) },
+	omissible = IKeyword.NAME)
+@doc(value = "Anywhere in the global block, in a species or in an (GUI) experiment, user_command statements allows to either call directly an existing action (with or without arguments) or to be followed by a block that describes what to do when this command is run.",
+	usages = { @usage(value = "The general syntax is for example:",
+		examples = @example(value = "user_command kill_myself action: some_action with: [arg1::val1, arg2::val2, ...];",
+			isExecutable = false)) },
+	see = { IKeyword.USER_INIT, IKeyword.USER_PANEL, IKeyword.USER_INPUT })
 public class UserCommandStatement extends AbstractStatementSequence implements IStatement.WithArgs {
 
 	Arguments args;
+	Arguments runtimeArgs;
 	final String actionName;
 	final IExpression when;
 	List<UserInputStatement> inputs = new ArrayList();
@@ -83,11 +97,23 @@ public class UserCommandStatement extends AbstractStatementSequence implements I
 	@Override
 	public Object privateExecuteIn(final IScope scope) throws GamaRuntimeException {
 		if ( isEnabled(scope) ) {
-			if ( actionName == null ) { return super.privateExecuteIn(scope); }
+			if ( actionName == null ) {
+				if ( runtimeArgs != null ) {
+					scope.stackArguments(runtimeArgs);
+				}
+				Object result = super.privateExecuteIn(scope);
+				runtimeArgs = null;
+				return result;
+			}
 			final ISpecies context = scope.getAgentScope().getSpecies();
 			final IStatement.WithArgs executer = context.getAction(actionName);
-			executer.setRuntimeArgs(args);
+			Arguments tempArgs = new Arguments(args);
+			if ( runtimeArgs != null ) {
+				tempArgs.complementWith(runtimeArgs);
+			}
+			executer.setRuntimeArgs(tempArgs);
 			final Object result = executer.executeOn(scope);
+			runtimeArgs = null;
 			return result;
 		}
 		// scope.setStatus(ExecutionStatus.success);
@@ -95,7 +121,9 @@ public class UserCommandStatement extends AbstractStatementSequence implements I
 	}
 
 	@Override
-	public void setRuntimeArgs(final Arguments args) {}
+	public void setRuntimeArgs(final Arguments args) {
+		this.runtimeArgs = args;
+	}
 
 	// @Override
 	// public IType getType() {
