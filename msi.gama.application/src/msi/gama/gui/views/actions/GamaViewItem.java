31bc67aae4b2c35e20c649ab46bef09bc5012bc9
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-07-01 04:03:12 +0000

    Cosmetic changes.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@11392 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.application/src/msi/gama/gui/views/actions/GamaViewItem.java
+++ msi.gama.application/src/msi/gama/gui/views/actions/GamaViewItem.java
@@ -1,13 +1,13 @@
 /*********************************************************************************************
- * 
- * 
+ *
+ *
  * 'GamaViewItem.java', in plugin 'msi.gama.application', is part of the source code of the
  * GAMA modeling and simulation platform.
  * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
- * 
+ *
  * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
- * 
- * 
+ *
+ *
  **********************************************************************************************/
 package msi.gama.gui.views.actions;
 
@@ -17,10 +17,10 @@ import org.eclipse.ui.*;
 
 /**
  * The class GamaContributionItem.
- * 
+ *
  * @author drogoul
  * @since 19 janv. 2012
- * 
+ *
  */
 public abstract class GamaViewItem implements IContributionItem {
 
