gama-platform/gama.old

commit 5bcc26c23cde5648c9913f643aec3a6ee9f7e592

Allows an agent to directly manipulate a variable of its host (in particular, allows a simulation to manipulate an experiment variable, like "seed").
git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@11599 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

msi.gama.core/src/msi/gama/metamodel/agent/GamlAgent.java
@@ -41,26 +41,26 @@ public class GamlAgent extends MinimalAgent implements IMacroAgent {
 
 	/** The population that this agent belongs to. */
 	protected final IPopulation population;
-	//hqnghi manipulate micro-models
+	// hqnghi manipulate micro-models
 	protected GamaMap<String, IPopulation> externMicroPopulations = new GamaMap<String, IPopulation>();
-	
+
 	@Override
 	public void addExternMicroPopulation(final String expName, final IPopulation pop) {
 		externMicroPopulations.put(expName, pop);
 	}
-	
+
 	@Override
 	public IPopulation getExternMicroPopulationFor(final String expName) {
-		if(externMicroPopulations.size()>0)
-			return externMicroPopulations.get(expName);
+		if ( externMicroPopulations.size() > 0 ) { return externMicroPopulations.get(expName); }
 		return null;
 	}
-	
+
 	@Override
 	public GamaMap<String, IPopulation> getExternMicroPopulations() {
 		return externMicroPopulations;
 	}
-	//end-hqnghi
+
+	// end-hqnghi
 
 	protected IShape geometry;
 	protected String name;
@@ -98,12 +98,25 @@ public class GamlAgent extends MinimalAgent implements IMacroAgent {
 			final IVariable varOfHost = host.getPopulation().getVar(host, n);
 			if ( varOfHost != null ) { return varOfHost.value(scope, host); }
 		}
+		// TODO: else ? launch an error ?
 		return null;
 	}
 
 	@Override
 	public void setDirectVarValue(final IScope scope, final String s, final Object v) throws GamaRuntimeException {
-		population.getVar(this, s).setVal(scope, this, v);
+		final IVariable var = population.getVar(this, s);
+		if ( var != null ) {
+			var.setVal(scope, this, v);
+		}
+		final IAgent host = this.getHost();
+		if ( host != null ) {
+			final IVariable varOfHost = host.getPopulation().getVar(host, s);
+			if ( varOfHost != null ) {
+				varOfHost.setVal(scope, host, v);
+			}
+		}
+		// TODO: else ? launch an error ?
+		// population.getVar(this, s).setVal(scope, this, v);
 	}
 
 	/**
@@ -633,17 +646,19 @@ public class GamlAgent extends MinimalAgent implements IMacroAgent {
 
 	@Override
 	public IPopulation getPopulationFor(final ISpecies species) {
-		//hqnghi adjust to get population for species which come from main as well micro models
+		// hqnghi adjust to get population for species which come from main as well micro models
 		ModelDescription micro = species.getDescription().getModelDescription();
-		ModelDescription main  = (ModelDescription) this.getModel().getDescription();		
+		ModelDescription main = (ModelDescription) this.getModel().getDescription();
 		IPopulation microPopulation = null;
-		if( main.getMicroModel(micro.getAlias()) == null ) {
+		if ( main.getMicroModel(micro.getAlias()) == null ) {
 			microPopulation = this.getMicroPopulation(species);
-			if ( microPopulation == null && getHost() != null ) { microPopulation =  getHost().getPopulationFor(species); }
-		}else {
-				microPopulation = this.getExternMicroPopulationFor(species.getName());
+			if ( microPopulation == null && getHost() != null ) {
+				microPopulation = getHost().getPopulationFor(species);
+			}
+		} else {
+			microPopulation = this.getExternMicroPopulationFor(species.getName());
 		}
-		//end-hqnghi
+		// end-hqnghi
 		return microPopulation;
 	}
 

commit 33563ef52b2757bac2b5f25f507e33403b6144a1

Fixes Issue 1120 where an error has been introduced in revision r11599 (the variables of the host was systematically set in addition to the variable of the agent).
git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@11696 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

msi.gama.core/src/msi/gama/metamodel/agent/GamlAgent.java
@@ -107,12 +107,13 @@ public class GamlAgent extends MinimalAgent implements IMacroAgent {
 		final IVariable var = population.getVar(this, s);
 		if ( var != null ) {
 			var.setVal(scope, this, v);
-		}
-		final IAgent host = this.getHost();
-		if ( host != null ) {
-			final IVariable varOfHost = host.getPopulation().getVar(host, s);
-			if ( varOfHost != null ) {
-				varOfHost.setVal(scope, host, v);
+		} else {
+			final IAgent host = this.getHost();
+			if ( host != null ) {
+				final IVariable varOfHost = host.getPopulation().getVar(host, s);
+				if ( varOfHost != null ) {
+					varOfHost.setVal(scope, host, v);
+				}
 			}
 		}
 		// TODO: else ? launch an error ?