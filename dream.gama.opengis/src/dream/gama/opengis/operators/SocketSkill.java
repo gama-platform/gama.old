de45da93a870bdc0eaf616e5296e34aadc06b6ab
hqnghi88@gmail.com <hqnghi88@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2014-10-04 08:44:24 +0000

    1. SocketSkill - communicate online through socket TCP/IP 2. URLFIle - read file from http URL link and save to data 3. Several experimental example of CoModels.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@11708 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

---
+++ dream.gama.opengis/src/dream/gama/opengis/operators/SocketSkill.java
@@ -0,0 +1,188 @@
+/*********************************************************************************************
+ * 
+ * 
+ * 'MovingSkill.java', in plugin 'msi.gama.core', is part of the source code of the
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 
+ * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
+ * 
+ * 
+ **********************************************************************************************/
+package dream.gama.opengis.operators;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import msi.gama.common.util.GuiUtils;
+import msi.gama.metamodel.agent.IAgent;
+import msi.gama.metamodel.shape.*;
+import msi.gama.precompiler.GamlAnnotations.action;
+import msi.gama.precompiler.GamlAnnotations.arg;
+import msi.gama.precompiler.GamlAnnotations.doc;
+import msi.gama.precompiler.GamlAnnotations.example;
+import msi.gama.precompiler.GamlAnnotations.getter;
+import msi.gama.precompiler.GamlAnnotations.setter;
+import msi.gama.precompiler.GamlAnnotations.skill;
+import msi.gama.precompiler.GamlAnnotations.var;
+import msi.gama.precompiler.GamlAnnotations.vars;
+import msi.gama.runtime.IScope;
+import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gaml.skills.Skill;
+import msi.gaml.types.*;
+
+@doc("k")
+@vars({ @var(name = "msg", type = IType.STRING),
+		@var(name = "port", type = IType.INT, doc = @doc("t)")),
+		@var(name = "ip", type = IType.STRING, doc = @doc("cs")) })
+@skill(name = "socket")
+public class SocketSkill extends Skill {
+	private Integer myPort;
+	private String myIP = "127.0.0.1";
+	private String sendMessage;
+	private String recMessage;
+
+	@getter("msg")
+	public String getMessage(final IAgent agent) {
+		return recMessage;
+	}
+
+	@setter("msg")
+	public void setMesage(final IAgent agent, final String s) {
+		recMessage = s;
+	}
+
+	@getter("ip")
+	public String getIP(final IAgent agent) {
+		return  myIP;
+	}
+
+	@setter("ip")
+	public void setIP(final IAgent agent, final String myip) {
+		myIP = myip;
+	}
+
+	@getter("port")
+	// @getter(value = IKeyword.LOCATION, initializer = true)
+	public Integer getPort(final IAgent agent) {
+		return myPort;
+	}
+
+	@setter("port")
+	public void setPort(final IAgent agent, final Integer p) {
+		myPort = p;
+	}
+
+	private ServerSocket sersock = null;
+	private Socket sock = null;
+	
+	private void openSocket(final Integer socket) {
+		if (sersock == null || sersock.isClosed()) {
+			try {				
+				sersock = new ServerSocket(socket);
+				sock = sersock.accept();
+			} catch (Exception e) {
+				GuiUtils.errorStatus(e.getMessage());
+			}			
+		}
+	}
+	
+	@action(name = "open_socket", doc = @doc(examples = { @example("d;") }, value = "."))
+	public void primOpenSocket(final IScope scope) throws GamaRuntimeException {
+		// return null;
+		// ServerSocket sersock = null;
+		// Socket sock = null;
+		openSocket(myPort);
+	}
+	
+	@action(name = "listen_client", doc = @doc(examples = { @example("d;") }, value = "."))
+	public void primListenClient(final IScope scope) throws GamaRuntimeException {
+		
+		try {
+//			BufferedReader keyRead = new BufferedReader(new InputStreamReader(
+//					java.lang.System.in));
+			// sending to client (pwrite object)
+
+			InputStream istream = sock.getInputStream();
+			BufferedReader receiveRead = new BufferedReader(
+					new InputStreamReader(istream));
+			String receiveMessage;
+			if ((receiveMessage = receiveRead.readLine()) != null) {
+//				GuiUtils.informConsole(receiveMessage);
+				recMessage = receiveMessage;
+			}
+		} catch (Exception e) {
+			GuiUtils.errorStatus(e.getMessage());
+		}
+	}
+	
+	@action(name = "send_to_client", args = { @arg(name = "msg", type = IType.STRING, optional = false, doc = @doc("td)")) }, doc = @doc(examples = { @example("tA;") }, value = "M."))
+	public void primSendToClient(final IScope scope)
+			throws GamaRuntimeException {
+		String msg = scope.getStringArg("msg");
+
+		try {
+			OutputStream ostream = sock.getOutputStream();
+			PrintWriter pwrite = new PrintWriter(ostream, true);
+			pwrite.println(msg);
+			pwrite.flush();
+
+		}catch (Exception e) {
+			GuiUtils.errorStatus(e.getMessage());
+		}
+	}	
+	
+	private Socket cSock = null;
+
+	@action(name = "listen_server", doc = @doc(examples = { @example("d;") }, value = "."))
+	public void primListenServer(final IScope scope) throws GamaRuntimeException {
+		
+		try {
+//			BufferedReader keyRead = new BufferedReader(new InputStreamReader(
+//					java.lang.System.in));
+			// sending to client (pwrite object)
+
+
+			InputStream istream = cSock.getInputStream();
+			BufferedReader receiveRead = new BufferedReader(
+					new InputStreamReader(istream));
+			String receiveMessage;
+			if ((receiveMessage = receiveRead.readLine()) != null) {
+//				GuiUtils.informConsole(receiveMessage); // displaying at DOS prompt
+				recMessage  = receiveMessage;
+			}
+		} catch (Exception e) {
+			GuiUtils.errorStatus(e.getMessage());
+		}
+	}
+	@action(name = "send_to_server", args = { @arg(name = "msg", type = IType.STRING, optional = false, doc = @doc("td)")) }, doc = @doc(examples = { @example("tA;") }, value = "M."))
+	public void primSendToServer(final IScope scope)
+			throws GamaRuntimeException {
+		String msg = scope.getStringArg("msg");
+		if (cSock == null) {
+			try {
+				cSock = new Socket(myIP, myPort);
+			} catch (Exception e) {
+				GuiUtils.errorStatus(e.getMessage());
+			}
+
+		}
+		try {
+			OutputStream ostream = cSock.getOutputStream();
+			PrintWriter pwrite = new PrintWriter(ostream, true);
+			pwrite.println(msg); // sending to server
+			pwrite.flush(); // flush the data
+			
+		} catch (Exception e) {
+			GuiUtils.errorStatus(e.getMessage());
+		}
+	}
+
+}
---
+++ msi.gama.core/src/msi/gama/util/file/URLFile.java
@@ -0,0 +1,145 @@
+/*********************************************************************************************
+ * 
+ * 
+ * 'GamaTextFile.java', in plugin 'msi.gama.core', is part of the source code of the
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 
+ * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
+ * 
+ * 
+ **********************************************************************************************/
+package msi.gama.util.file;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import msi.gama.common.GamaPreferences;
+import msi.gama.common.util.*;
+import msi.gama.precompiler.GamlAnnotations.file;
+import msi.gama.runtime.IScope;
+import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gama.util.*;
+import msi.gaml.operators.Cast;
+import msi.gaml.types.IType;
+import rcaller.*;
+import com.vividsolutions.jts.geom.Envelope;
+
+@file(name = "URL",
+	extensions = { "txt" },
+	buffer_type = IType.LIST, 
+	buffer_content = IType.STRING)
+public class URLFile extends GamaFile<IList<String>, String, Integer, String> {
+
+	private final String URL;
+
+	public URLFile(final IScope scope, final String pathName) {
+		super(scope, pathName);
+		URL="";
+	}
+	public URLFile(final IScope scope, final String pathName, final String u) {
+		super(scope, pathName);
+		URL = u;
+	}
+
+	@Override
+	public String _stringValue(final IScope scope) throws GamaRuntimeException {
+		getContents(scope);
+		StringBuilder sb = new StringBuilder(getBuffer().length(scope) * 200);
+		for ( String s : getBuffer().iterable(scope) ) {
+			sb.append(s).append("\n"); // TODO Factorize the different calls to "new line" ...
+		}
+		sb.setLength(sb.length() - 1);
+		return sb.toString();
+	}
+	
+	public  GamaList<String>  getURLContent(final String u_str) {
+		URL url;
+
+		final GamaList<String> allLines = new GamaList();
+		try {
+			// get URL content
+			url = new URL(u_str);
+			URLConnection conn = url.openConnection();
+
+			// open the stream and put it into BufferedReader
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+
+			String inputLine;
+
+			// save to this filename
+//			String fileName = "/users/mkyong/test.html";
+			File file = new File(this.getPath());
+
+//			if (!file.exists()) {
+//				file.createNewFile();
+//			}
+
+			// use FileWriter to write file
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			while ((inputLine = br.readLine()) != null) {
+				bw.write(inputLine+"\n");
+
+				allLines.add(inputLine);
+			}
+
+			bw.close();
+			br.close();
+
+
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return allLines; 
+	}
+	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see msi.gama.util.GamaFile#fillBuffer()
+	 */
+	@Override
+	protected void fillBuffer(final IScope scope) throws GamaRuntimeException {
+		if ( getBuffer() != null ) { return; }
+	
+
+
+
+		setBuffer(getURLContent(this.URL));
+//		GuiUtils.informConsole(""+getURLContent(this.URL));
+
+
+	}
+
+
+	private static String computeVariable(final String string) {
+		String[] tokens = string.split("<-");
+		return tokens[0];
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see msi.gama.util.GamaFile#flushBuffer()
+	 */
+	@Override
+	protected void flushBuffer() throws GamaRuntimeException {
+		// TODO A faire.
+
+	}
+
+	@Override
+	public Envelope computeEnvelope(final IScope scope) {
+		return null;
+	}
+
+}
