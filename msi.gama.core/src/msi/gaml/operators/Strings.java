f46cd666d97da1df17189aad8a065efd6a6b0abc
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2015-05-06 17:06:30 +0000

    Adresses Issue 1200 and 1206 by adding two new operators: as_system_time and as_system_date, which output the current time and date of the system for a given number of milliseconds.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@12194 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/operators/Strings.java
+++ msi.gama.core/src/msi/gaml/operators/Strings.java
@@ -40,10 +40,14 @@ public class Strings {
 	public static final String LN = java.lang.System.getProperty("line.separator");
 	public static final String TAB = "\t";
 
-	static Pattern p = Pattern.compile("%[YMDhms]");
+	static Pattern model_pattern = Pattern.compile("%[YMDhms]");
+	static Pattern system_pattern = Pattern.compile("%[YMNDEhmsz]");
 	private static PeriodFormatterBuilder format;
+	private static DateTimeFormatterBuilder systemFormat;
 	private static PeriodFormatter dateFormat;
 	private static PeriodFormatter timeFormat;
+	private static DateTimeFormatter systemDateFormat;
+	private static DateTimeFormatter systemTimeFormat;
 	private static GamaChronology chronology;
 
 	static PeriodFormatterBuilder getCustomFormat() {
@@ -53,6 +57,13 @@ public class Strings {
 		return format;
 	}
 
+	static DateTimeFormatterBuilder getSystemFormat() {
+		if ( systemFormat == null ) {
+			systemFormat = new DateTimeFormatterBuilder();
+		}
+		return systemFormat;
+	}
+
 	static PeriodFormatter getTimeFormat() {
 		if ( timeFormat == null ) {
 			timeFormat =
@@ -60,6 +71,20 @@ public class Strings {
 					.appendMinutes().appendLiteral(":").appendSeconds().toFormatter();
 		}
 		return timeFormat;
+	}
+
+	static DateTimeFormatter getSystemTimeFormat() {
+		if ( systemTimeFormat == null ) {
+			systemTimeFormat = DateTimeFormat.forPattern("HH:mm:ss");
+		}
+		return systemTimeFormat;
+	}
+
+	static DateTimeFormatter getSystemDateFormat() {
+		if ( systemDateFormat == null ) {
+			systemDateFormat = ISODateTimeFormat.yearMonthDay();
+		}
+		return systemDateFormat;
 	}
 
 	static PeriodFormatter getDateFormat() {
@@ -81,15 +106,15 @@ public class Strings {
 	}
 
 	@operator(value = IKeyword.PLUS, can_be_const = true, category = { IOperatorCategory.STRING })
-	public static
-		String opPlus(final String a, final String b) {
+	public static String opPlus(final String a, final String b) {
 		return a + b;
 	}
 
 	@operator(value = IKeyword.PLUS, can_be_const = true, category = { IOperatorCategory.STRING })
 	@doc(usages = @usage(value = "if the left-hand operand is a string, returns the concatenation of the two operands (the left-hand one beind casted into a string)",
 		examples = @example(value = "\"hello \" + 12", equals = "\"hello 12\"")))
-	public static String opPlus(final IScope scope, final String a, final Object b) throws GamaRuntimeException {
+	public static
+		String opPlus(final IScope scope, final String a, final Object b) throws GamaRuntimeException {
 		return a + Cast.asString(scope, b);
 	}
 
@@ -390,7 +415,7 @@ public class Strings {
 	}
 
 	@operator(value = "as_date", can_be_const = true, category = { IOperatorCategory.STRING, IOperatorCategory.TIME })
-	@doc(value = "converts a number into a string with year, month, day, hour, minutes, second following a given pattern (right-hand operand)",
+	@doc(value = "converts a number of seconds in the model (for instance, the 'time' variable) into a string that represents the period elapsed since the beginning of the simulation using year, month, day, hour, minutes and seconds following a given pattern (right-hand operand). GAMA uses a special calendar for internal model times, where months have 30 days and years 12 months. ",
 		masterDoc = true,
 		usages = @usage(value = "Pattern should include : \"%Y %M %D %h %m %s\" for outputting years, months, days, hours, minutes, seconds",
 			examples = @example(value = "22324234 as_date \"%M m %D d %h h %m m %s seconds\"",
@@ -403,7 +428,7 @@ public class Strings {
 		if ( pattern == null || pattern.isEmpty() ) { return asDate(time) + " " + asTime(time); }
 		getCustomFormat().clear();
 		List<String> dateList = new ArrayList();
-		final Matcher m = p.matcher(pattern);
+		final Matcher m = model_pattern.matcher(pattern);
 		int i = 0;
 		while (m.find()) {
 			String tmp = m.group();
@@ -453,8 +478,81 @@ public class Strings {
 
 	}
 
+	@operator(value = "as_system_date", can_be_const = true, category = { IOperatorCategory.STRING,
+		IOperatorCategory.TIME })
+	@doc(value = "converts a number of milliseconds in the system (for instance, the 'machine_time' variable) into a string that represents the current date represented by these milliseconds  using year, month, day, hour, minutes, seconds and time-zone offset following a given pattern (right-hand operand) ",
+		masterDoc = true,
+		usages = @usage(value = "Pattern should include : \"%Y %M %N %D %E %h %m %s %z\" for outputting years, months, name of month, days, name of days, hours, minutes, seconds and the time-zone. A null or empty pattern will return the complete date as defined by the ISO 8601 standard yyyy-MM-ddThh:mm:ss w/o the time-zone offset. Names are defined using the locale of the system",
+			examples = @example(value = "2147483647 as_date \" %D %Y %M / %h:%m:%s %z\"",
+				equals = "\"06 2015 05 / 23:58:57 +07\"")),
+		see = { "as_system_time" })
+	public static
+		String asSystemDate(final double time, final String pattern) {
+		// Pattern should include : "%Y %M %D %h %m %s" for outputting years, months, days, hours,
+		// minutes, seconds
+		if ( pattern == null || pattern.isEmpty() ) { return asSystemDate(time) + "T" + asSystemTime(time); }
+		getSystemFormat().clear();
+		List<String> dateList = new ArrayList();
+		final Matcher m = system_pattern.matcher(pattern);
+		int i = 0;
+		while (m.find()) {
+			String tmp = m.group();
+			if ( i != m.start() ) {
+				dateList.add(pattern.substring(i, m.start()));
+			}
+			dateList.add(tmp);
+			i = m.end();
+		}
+		if ( i != pattern.length() ) {
+			dateList.add(pattern.substring(i));
+		}
+		for ( i = 0; i < dateList.size(); i++ ) {
+			String s = dateList.get(i);
+			if ( s.charAt(0) == '%' && s.length() == 2 ) {
+				Character c = s.charAt(1);
+				switch (c) {
+					case 'Y':
+						getSystemFormat().appendYear(4, 4);
+						break;
+					case 'M':
+						getSystemFormat().appendMonthOfYear(2);
+						break;
+					case 'N':
+						getSystemFormat().appendMonthOfYearText();
+						break;
+					case 'D':
+						getSystemFormat().appendDayOfMonth(2);
+						break;
+					case 'E':
+						getSystemFormat().appendDayOfWeekText();
+						break;
+					case 'h':
+						getSystemFormat().appendHourOfDay(2);
+						break;
+					case 'm':
+						getSystemFormat().appendMinuteOfHour(2);
+						break;
+					case 's':
+						getSystemFormat().appendSecondOfMinute(2);
+						break;
+					case 'z':
+						getSystemFormat().appendTimeZoneOffset(null, false, 1, 2);
+						break;
+					default:
+						getSystemFormat().appendLiteral(s);
+				}
+			} else {
+				getSystemFormat().appendLiteral(s);
+			}
+		}
+
+		DateTimeFormatter pf = getSystemFormat().toFormatter();
+		return pf.print((long) time);
+
+	}
+
 	@operator(value = "as_date", can_be_const = true, category = { IOperatorCategory.STRING, IOperatorCategory.TIME })
-	@doc(value = "converts a number into a string with year, month, day, hour, minutes, second following a standard pattern",
+	@doc(value = "converts a number of seconds in the model (for example, the value of the 'time' variable) into a string that represents the period elapsed since the beginning of the simulation with years, months and days following a standard pattern. GAMA uses a special calendar for internal model times, where months have 30 days and years 12 months. ",
 		usages = @usage(value = "used as an unary operator, it uses a defined pattern with years, months, days",
 			examples = @example(value = "as_date(22324234)", equals = "\"8 months, 18 days\"")))
 	public static
@@ -465,14 +563,37 @@ public class Strings {
 	}
 
 	@operator(value = "as_time", can_be_const = true, category = { IOperatorCategory.STRING, IOperatorCategory.TIME })
-	@doc(value = "converts the given number into a string with hours, minutes and seconds",
+	@doc(value = "converts  a number of seconds in the model  (for example, the value of the 'time' variable)  into a string that represents the current number of hours, minutes and seconds of the period elapsed since the beginning of the simulation. As GAMA has no conception of time zones, the time is expressed as if the model was at GMT+00",
 		comment = "as_time operator is a particular case (using a particular pattern) of the as_date operator.",
 		examples = @example(value = "as_time(22324234)", equals = "\"09:10:34\""),
 		see = "as_date")
-	public static String asTime(final double cycles) {
+	public static
+		String asTime(final double time) {
 		PeriodType pt = PeriodType.yearMonthDayTime();
 		return getTimeFormat().print(
-			new Period(new Duration((long) cycles * 1000), getChronology()).normalizedStandard(pt));
+			new Period(new Duration((long) time * 1000), getChronology()).normalizedStandard(pt));
+	}
+
+	@operator(value = "as_system_time", can_be_const = true, category = { IOperatorCategory.STRING,
+		IOperatorCategory.TIME })
+	@doc(value = "converts  a value of milliseconds in the system  (for example, the value of the 'machine_time' variable)  into a string representing the current hours, minutes and seconds in the current time zone of the machine and the current Locale. This representation follows the ISO 8601 standard hh:mm:ss",
+		comment = "as_system_time operator is a particular case (using a particular pattern) of the as_system_date operator.",
+		examples = @example(value = "as_system_time(2147483647)", equals = "\"23:58:57\""),
+		see = "as_system_date")
+	public static
+		String asSystemTime(final double time) {
+		return getSystemTimeFormat().print((long) time);
+	}
+
+	@operator(value = "as_system_date", can_be_const = true, category = { IOperatorCategory.STRING,
+		IOperatorCategory.TIME })
+	@doc(value = "converts  a value of milliseconds in the system (for example, the value of the 'machine_time' variable)  into a string representing the current year, month and day in the current time zone of the machine and the current Locale. This representation follows the ISO 8601 standard yyyy-mm-dd",
+		comment = "as_system_date operator is a particular case (using a particular pattern) of the as_system_date operator.",
+		examples = @example(value = "as_system_date(2147483647)", equals = "\"2015-05-06\""),
+		see = "as_system_date")
+	public static
+		String asSystemDate(final double time) {
+		return getSystemDateFormat().print((long) time);
 	}
 
 }
