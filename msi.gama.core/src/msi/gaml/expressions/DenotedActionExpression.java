14cfc0330ca772f00fa8a064eace4cf78e26a518
alexis.drogoul@gmail.com <alexis.drogoul@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2015-05-23 16:18:49 +0000

    Small bug fix that could lead to infinite recursion
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@12253 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/expressions/DenotedActionExpression.java
+++ msi.gama.core/src/msi/gaml/expressions/DenotedActionExpression.java
@@ -18,7 +18,6 @@ import msi.gaml.types.Types;
 public class DenotedActionExpression extends VariableExpression {
 
 	StatementDescription description;
-	boolean recursiveSerializing;
 
 	public DenotedActionExpression(final StatementDescription action) {
 		super(action.getName(), Types.NO_TYPE, true, null);
@@ -35,18 +34,6 @@ public class DenotedActionExpression extends VariableExpression {
 		return description.getTitle();
 	}
 
-	@Override
-	public String serialize(final boolean includingBuiltIn) {
-		if ( recursiveSerializing ) {
-			recursiveSerializing = false;
-			return description.getName();
-		} else {
-			recursiveSerializing = true;
-			String result = description.serialize(includingBuiltIn);
-			recursiveSerializing = false;
-			return result;
-		}
-	}
 
 	/**
 	 * @see msi.gaml.expressions.IExpression#getDocumentation()
