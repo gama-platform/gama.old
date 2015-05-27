612a1d0e00888f8734d77b1977be5b31fcce02da
patrick.taillandier@gmail.com <patrick.taillandier@gmail.com@b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a>
2015-05-27 07:18:52 +0000

    Integration of the spatial_operator plugin (IDW, CRS...) to the core.
    git-svn-id: https://gama-platform.googlecode.com/svn/branches/GAMA_CURRENT@12282 b91ef52e-2b48-11df-9ab6-b1cdc0d4a89a

--- msi.gama.core/src/msi/gaml/operators/Spatial.java
+++ msi.gama.core/src/msi/gaml/operators/Spatial.java
@@ -24,6 +24,7 @@ import msi.gama.metamodel.shape.*;
 import msi.gama.metamodel.topology.ITopology;
 import msi.gama.metamodel.topology.filter.*;
 import msi.gama.metamodel.topology.grid.GamaSpatialMatrix;
+import msi.gama.metamodel.topology.projection.IProjection;
 import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.operator;
@@ -36,6 +37,7 @@ import msi.gama.util.file.*;
 import msi.gama.util.matrix.IMatrix;
 import msi.gama.util.path.*;
 import msi.gaml.types.*;
+
 import org.geotools.referencing.CRS;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
@@ -2721,6 +2723,47 @@ public abstract class Spatial {
 			}
 			return groups;
 		}
+		
+		@operator(value = { "IDW" }, category = { IOperatorCategory.SPATIAL })
+		@doc(value = "Inverse Distance Weighting (IDW) is a type of deterministic method for multivariate "
+			+ "interpolation with a known scattered set of points. The assigned values to each geometry are calculated with a weighted average of the values available at the known points. See: http://en.wikipedia.org/wiki/Inverse_distance_weighting "
+			+ "Usage: IDW (list of geometries, map of points (key: point, value: value), power parameter)",
+			examples = { @example(value = "IDW([ag1, ag2, ag3, ag4, ag5],[{10,10}::25.0, {10,80}::10.0, {100,10}::15.0], 2)",
+				equals = "for example, can return [ag1::12.0, ag2::23.0,ag3::12.0,ag4::14.0,ag5::17.0]",
+				isExecutable = false) })
+		public static
+			GamaMap<IShape, Double> primIDW(final IScope scope, final IContainer<?, ? extends IShape> geometries,
+				final GamaMap<GamaPoint, Double> points, final int power) {
+			GamaMap<IShape, Double> results = GamaMapFactory.create(Types.GEOMETRY, Types.FLOAT);
+			if ( points == null || points.isEmpty() ) { return null; }
+			if ( geometries == null || geometries.isEmpty(scope) ) { return results; }
+			for ( IShape geom : geometries.iterable(scope) ) {
+				double sum = 0;
+				double weight = 0;
+				double sumNull = 0;
+				int nbNull = 0;
+				for ( GamaPoint pt : points.keySet() ) {
+					double dist = geom.euclidianDistanceTo(pt);
+					if ( dist == 0 ) {
+						nbNull++;
+						sumNull += points.get(pt);
+					}
+					if ( nbNull == 0 ) {
+						double w = 1 / Math.pow(dist, power);
+						weight += w;
+						sum += w * points.get(pt);
+					}
+				}
+				if ( nbNull > 0 ) {
+					results.put(geom, sumNull / nbNull);
+				} else {
+					results.put(geom, sum / weight);
+				}
+
+			}
+			return results;
+		}
+
 	}
 
 	public static abstract class ThreeD {
@@ -2918,6 +2961,90 @@ public abstract class Spatial {
 				throw GamaRuntimeException.error("Impossible to compute the CRS for this type of file");
 			}
 		}
+		
+		@operator(value = {  "CRS_transform" }, category = { IOperatorCategory.SPATIAL,
+				IOperatorCategory.SP_TRANSFORMATIONS })
+			@doc(usages = { @usage(value = "returns the geometry corresponding to the transfomartion of the given geometry by the current CRS (Coordinate Reference System), the one corresponding to the world's agent one",
+				examples = { @example(value = "CRS_transform(shape)",
+					equals = "a geometry corresponding to the agent geometry transformed into the current CRS",
+					test = false) }) })
+			public static
+				IShape transform_CRS(final IScope scope, final IShape g) {
+				IProjection gis = scope.getSimulationScope().getProjectionFactory().getWorld();
+				if (gis == null) {
+					return g.copy(scope);
+				}
+				IShape s = new GamaShape(gis.inverseTransform(g.getInnerGeometry()));
+				if (g instanceof ILocation)
+					return s.getLocation();
+				return s;
+			}
+		
+		@operator(value = {  "to_GAMA_CRS" }, category = { IOperatorCategory.SPATIAL,
+				IOperatorCategory.SP_TRANSFORMATIONS })
+			@doc(usages = { @usage(value = "returns the geometry corresponding to the transfomartion of the given geometry to the GAMA CRS (Coordinate Reference System) assuming the given geometry is referenced by the current CRS, the one corresponding to the world's agent one",
+				examples = { @example(value = "to_GAMA_CRS({121,14})",
+					equals = "a geometry corresponding to the agent geometry transformed into the GAMA CRS",
+					test = false) }) })
+			public static
+				IShape to_GAMA_CRS(final IScope scope, final IShape g ) {
+				IProjection gis = scope.getSimulationScope().getProjectionFactory().getWorld();
+				if (gis == null) {
+					return g.copy(scope);
+				}
+				IShape s = new GamaShape(gis.transform(g.getInnerGeometry()));
+				if (g instanceof ILocation)
+					return s.getLocation();
+				return s;
+			}
+		
+		@operator(value = {  "to_GAMA_CRS" }, category = { IOperatorCategory.SPATIAL,
+				IOperatorCategory.SP_TRANSFORMATIONS })
+			@doc(usages = { @usage(value = "returns the geometry corresponding to the transfomartion of the given geometry to the GAMA CRS (Coordinate Reference System) assuming the given geometry is referenced by given CRS",
+				examples = { @example(value = "to_GAMA_CRS({121,14}, \"EPSG:4326\")",
+					equals = "a geometry corresponding to the agent geometry transformed into the GAMA CRS",
+					test = false) }) })
+			public static
+				IShape to_GAMA_CRS(final IScope scope, final IShape g, final String code ) {
+				IProjection gis;
+				try {
+					gis = scope.getSimulationScope().getProjectionFactory().forSavingWith(code);
+				} catch (FactoryException e) {
+					throw GamaRuntimeException.error("The code " + code +
+							" does not correspond to a known EPSG code", scope);
+				}
+				if (gis == null) {
+					return g.copy(scope);
+				}
+				IShape s = new GamaShape(gis.transform(g.getInnerGeometry()));
+				if (g instanceof ILocation)
+					return s.getLocation();
+				return s;
+			}
+		
+		@operator(value = {  "CRS_transform" }, category = { IOperatorCategory.SPATIAL,
+				IOperatorCategory.SP_TRANSFORMATIONS })
+			@doc(usages = { @usage(value = "returns the geometry corresponding to the transfomartion of the given geometry by the left operand CRS (Coordinate Reference System)",
+				examples = { @example(value = "shape CRS_transform(\"EPSG:4326\")",
+					equals = "a geometry corresponding to the agent geometry transformed into the EPSG:4326 CRS",
+					test = false) }) })
+			public static
+				IShape transform_CRS(final IScope scope, final IShape g, final String code) {
+				IProjection gis;
+				try {
+					gis = scope.getSimulationScope().getProjectionFactory().forSavingWith(code);
+				} catch (FactoryException e) {
+					throw GamaRuntimeException.error("The code " + code +
+							" does not correspond to a known EPSG code", scope);
+				}
+				if (gis == null) {
+					return g.copy(scope);
+				}
+				IShape s = new GamaShape(gis.inverseTransform(g.getInnerGeometry()));
+				if (g instanceof ILocation)
+					return s.getLocation();
+				return s;
+			}
 	}
 
 }
