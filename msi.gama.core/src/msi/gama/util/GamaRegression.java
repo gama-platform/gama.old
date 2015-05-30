9b16d2d095a19dd9f487c74d58d71a9d4a0d21f9
ptaillandier <patrick.taillandier@gmail.com>
2015-05-30 15:06:30 +0700

    Definition of new operators for regression

--- msi.gama.core/src/msi/gama/util/GamaRegression.java
+++ msi.gama.core/src/msi/gama/util/GamaRegression.java
@@ -1,6 +1,9 @@
 package msi.gama.util;
 
-import org.apache.commons.math3.stat.regression.UpdatingMultipleLinearRegression;
+import org.apache.commons.math3.stat.regression.AbstractMultipleLinearRegression;
+import org.apache.commons.math3.stat.regression.GLSMultipleLinearRegression;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
+import org.apache.commons.math3.stat.regression.RegressionResults;
 
 import msi.gama.common.interfaces.IValue;
 import msi.gama.precompiler.GamlAnnotations.getter;
@@ -8,21 +11,61 @@ import msi.gama.precompiler.GamlAnnotations.var;
 import msi.gama.precompiler.GamlAnnotations.vars;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gama.util.matrix.GamaFloatMatrix;
 import msi.gaml.types.IType;
 import msi.gaml.types.Types;
 
-@vars({ @var(name = "nb_instances", type = IType.INT),
+@vars({ @var(name = "parameters", type = IType.LIST, of = IType.FLOAT),
 	@var(name = "nb_features", type = IType.INT)})
 public class GamaRegression implements IValue{
 
-	UpdatingMultipleLinearRegression regressionMethod;
+	RegressionResults regressionResults;
 	int nbFeatures;
+	double param[] ;
+	
+	public void buildRegressionMethod(IScope scope, GamaFloatMatrix data, String method){
+		AbstractMultipleLinearRegression regressionMethod = null;
+		if (method.equals("GS"))
+			regressionMethod = new GLSMultipleLinearRegression();
+		else regressionMethod = new OLSMultipleLinearRegression();
+		int nbFeatures = data.numCols - 1;
+		int nbInstances = data.numRows - 1;
+		double[] instances = new double[data.numCols * data.numRows - 1 ];
+		for (int i = 0; i < data.length(scope); i++ ) {
+			instances[i] = data.getMatrix()[i];
+		}
+		regressionMethod.newSampleData(instances, nbFeatures, nbInstances);
+		param = regressionMethod.estimateRegressionParameters();
+	}
 	
 	
-	@getter("nb_instances")
-	public Integer getNbData() {
-		if (regressionMethod == null) return 0;
-		return (int) regressionMethod.getN();
+	
+	public GamaRegression(double[] param, int nbFeatures,RegressionResults regressionResults) {
+		super();
+		this.regressionResults = regressionResults;
+		this.nbFeatures = nbFeatures;
+		this.param = param;
+	}
+
+	
+
+
+	public Double predict(GamaList<Double> instance){
+		if (param == null)
+			return null;
+		double val = param[0];
+		for (int i = 1; i < param.length; i++) {
+			val += param[i]*instance.get(i);
+		}
+		return val;
+	}
+	
+	@getter("parameters")
+	public IList<Double> getParameters() {
+		if (param == null) return GamaListFactory.EMPTY_LIST;
+		IList<Double> vals = GamaListFactory.create(Types.FLOAT);
+		for (int i = 0; i < param.length; i++) vals.add(param[i]);
+		return vals;
 	}
 	
 	@getter("nb_features")
@@ -32,7 +75,7 @@ public class GamaRegression implements IValue{
 	
 	@Override
 	public String serialize(boolean includingBuiltIn) {
-		return null;
+		return getParameters().serialize(includingBuiltIn);
 	}
 
 	@Override
@@ -42,14 +85,18 @@ public class GamaRegression implements IValue{
 
 	@Override
 	public String stringValue(IScope scope) throws GamaRuntimeException {
-		return "Regression model with "+ getNbData()+" instances composed of " + nbFeatures + " features";
+		if (param == null) return "no function";
+		String st = "y = " + param[0];
+		for (int i = 1; i < param.length; i++) {
+			st += " + " + param[i] + " x" + i;
+		}
+		return st;
 	}
+	
 
 	@Override
 	public IValue copy(IScope scope) throws GamaRuntimeException {
-		GamaRegression gr = new GamaRegression();
-		gr.nbFeatures = nbFeatures;
-		gr.regressionMethod = regressionMethod;
+		GamaRegression gr = new GamaRegression(param.clone(),nbFeatures,regressionResults);
 		return gr;
 	}
 	
