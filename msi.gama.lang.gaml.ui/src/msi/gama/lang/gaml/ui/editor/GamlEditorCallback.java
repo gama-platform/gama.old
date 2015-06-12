d92678303c7aa5a82d02836815017315986c1005
AlexisDrogoul <alexis.drogoul@gmail.com>
2015-06-12 09:20:27 +0700

    Addressing issues #1240 and #1245.

--- msi.gama.lang.gaml.ui/src/msi/gama/lang/gaml/ui/editor/GamlEditorCallback.java
+++ msi.gama.lang.gaml.ui/src/msi/gama/lang/gaml/ui/editor/GamlEditorCallback.java
@@ -1,30 +1,45 @@
 /*********************************************************************************************
- * 
- * 
+ *
+ *
  * 'GamlEditorCallback.java', in plugin 'msi.gama.lang.gaml.ui', is part of the source code of the
  * GAMA modeling and simulation platform.
  * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
- * 
+ *
  * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
- * 
- * 
+ *
+ *
  **********************************************************************************************/
 package msi.gama.lang.gaml.ui.editor;
 
 import msi.gama.lang.gaml.resource.GamlResource;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.XtextEditor;
-import org.eclipse.xtext.ui.editor.validation.ValidatingEditorCallback;
+import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionProvider;
+import org.eclipse.xtext.ui.editor.validation.*;
+import org.eclipse.xtext.ui.validation.MarkerTypeProvider;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
+import org.eclipse.xtext.validation.*;
+import com.google.inject.Inject;
 
 /**
  * The class GamlEditorCallback. Calls the functionalities of ValidatingEditorCallback and marks the resource as "edited" or not, which allows them to process the online doc, etc.
- * 
+ *
  * @author drogoul
  * @since 11 avr. 2014
- * 
+ *
  */
 public class GamlEditorCallback extends ValidatingEditorCallback {
+
+	@Inject
+	private IResourceValidator resourceValidator;
+
+	@Inject
+	private MarkerCreator markerCreator;
+
+	@Inject
+	private MarkerTypeProvider markerTypeProvider;
+	@Inject
+	private IssueResolutionProvider issueResolutionProvider;
 
 	@Override
 	public void afterCreatePartControl(final XtextEditor editor) {
@@ -53,6 +68,32 @@ public class GamlEditorCallback extends ValidatingEditorCallback {
 			}
 		});
 		super.beforeDispose(editor);
+	}
+
+	@Override
+	public void afterSave(final XtextEditor editor) {
+		// super.afterSave(editor);
+		if ( editor.isEditable() ) {
+			ValidationJob validationJob = newValidationJob(editor);
+			validationJob.setName("Validating " + editor.getPartName());
+			// validationJob.
+			validationJob.schedule(2000);
+			// validationJob.schedule();
+		}
+	}
+
+	private ValidationJob newValidationJob(final XtextEditor editor) {
+		IValidationIssueProcessor issueProcessor;
+		if ( editor.getResource() == null ) {
+			issueProcessor =
+				new AnnotationIssueProcessor(editor.getDocument(), editor.getInternalSourceViewer()
+					.getAnnotationModel(), issueResolutionProvider);
+		} else {
+			issueProcessor = new MarkerIssueProcessor(editor.getResource(), markerCreator, markerTypeProvider);
+		}
+		ValidationJob validationJob =
+			new ValidationJob(resourceValidator, editor.getDocument(), issueProcessor, CheckMode.NORMAL_AND_FAST);
+		return validationJob;
 	}
 
 }
