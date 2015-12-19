fa86487bdb30c42949189525d83cec2db2c9af45
AlexisDrogoul <alexis.drogoul@gmail.com>
2015-12-20 00:36:20 +0700

    Fixes #781
    The scale factor of text now correctly takes the zoom level into
    account.

--- ummisco.gama.opengl/src/ummisco/gama/opengl/scene/StringDrawer.java
+++ ummisco.gama.opengl/src/ummisco/gama/opengl/scene/StringDrawer.java
@@ -1,31 +1,31 @@
 /*********************************************************************************************
- * 
- * 
+ *
+ *
  * 'StringDrawer.java', in plugin 'msi.gama.jogl2', is part of the source code of the
  * GAMA modeling and simulation platform.
  * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
- * 
+ *
  * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
- * 
- * 
+ *
+ *
  **********************************************************************************************/
 package ummisco.gama.opengl.scene;
 
 import java.awt.Font;
 import java.util.*;
-import ummisco.gama.opengl.JOGLRenderer;
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.fixedfunc.GLLightingFunc;
 import com.jogamp.opengl.util.awt.TextRenderer;
 import com.jogamp.opengl.util.gl2.GLUT;
+import ummisco.gama.opengl.JOGLRenderer;
 
 /**
- * 
+ *
  * The class StringDrawer.
- * 
+ *
  * @author drogoul
  * @since 4 mai 2013
- * 
+ *
  */
 
 public class StringDrawer extends ObjectDrawer<StringObject> {
@@ -75,7 +75,8 @@ public class StringDrawer extends ObjectDrawer<StringObject> {
 			TextRenderer r = get(s.font, s.size, s.style);
 			r.setColor(s.getColor());
 			r.begin3DRendering();
-			r.draw3D(s.string, x, y, z, (float) (renderer.data.getEnvHeight() / renderer.getHeight()));
+			r.draw3D(s.string, x, y, z, (float) (renderer.data.getEnvHeight() /
+				(renderer.getHeight() * renderer.displaySurface.getZoomLevel())));
 			r.end3DRendering();
 			gl.glPopMatrix();
 		} else {
@@ -84,8 +85,8 @@ public class StringDrawer extends ObjectDrawer<StringObject> {
 
 			gl.glDisable(GL.GL_BLEND);
 
-			gl.glColor4d(s.getColor().getRed() / 255.0, s.getColor().getGreen() / 255.0,
-				s.getColor().getBlue() / 255.0, s.getColor().getAlpha() / 255.0 * s.getAlpha());
+			gl.glColor4d(s.getColor().getRed() / 255.0, s.getColor().getGreen() / 255.0, s.getColor().getBlue() / 255.0,
+				s.getColor().getAlpha() / 255.0 * s.getAlpha());
 			gl.glRasterPos3d(x, y, z);
 
 			glut.glutBitmapString(GLUT.BITMAP_TIMES_ROMAN_24, s.string);
