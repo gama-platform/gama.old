1f079bfe1ac3659c3212ce2884c336dfe375b0cf
AlexisDrogoul <alexis.drogoul@gmail.com>
2015-12-29 01:16:59 +0700

    Revised Workaround for #1358

--- msi.gama.application/src/msi/gama/gui/swt/WorkaroundForIssue1358.java
+++ msi.gama.application/src/msi/gama/gui/swt/WorkaroundForIssue1358.java
@@ -5,6 +5,7 @@
 package msi.gama.gui.swt;
 
 import java.lang.Thread.UncaughtExceptionHandler;
+import msi.gama.gui.swt.swing.Platform;
 
 /**
  * Class WorkaroundForIssue1358. Only for MacOS X -- to be removed when Eclipse 4.5.2 will be out.
@@ -16,22 +17,24 @@ import java.lang.Thread.UncaughtExceptionHandler;
 public class WorkaroundForIssue1358 {
 
 	public static void install() {
-		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+		if ( Platform.isCocoa() ) {
+			Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
 
-			@Override
-			public void uncaughtException(final Thread t, final Throwable e) {
-				if ( e instanceof NullPointerException ) {
-					StackTraceElement[] traces = e.getStackTrace();
-					for ( StackTraceElement s : traces ) {
-						if ( s.getMethodName().contains("internal_new_GC") ) {
-							System.out.println(
-								"Harmless exception caught in Control.internal_new_GC() -- waiting from the fix in Eclipse 4.5.2");
-							return;
+				@Override
+				public void uncaughtException(final Thread t, final Throwable e) {
+					if ( e instanceof NullPointerException ) {
+						StackTraceElement[] traces = e.getStackTrace();
+						for ( StackTraceElement s : traces ) {
+							if ( s.getMethodName().contains("internal_new_GC") ) {
+								System.out.println(
+									"Harmless exception caught in Control.internal_new_GC() -- waiting from the fix in Eclipse 4.5.2");
+								return;
+							}
 						}
 					}
+					e.printStackTrace();
 				}
-				e.printStackTrace();
-			}
-		});
+			});
+		}
 	}
 }
