a5da82122a77bf14abe4d2e589cd65e14e44c880
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-01-06 01:37:50 +0700

    Fixes #1365

--- msi.gama.lang.gaml.ui/src/msi/gama/lang/gaml/ui/editor/GamlMarkOccurrenceActionContributor.java
+++ msi.gama.lang.gaml.ui/src/msi/gama/lang/gaml/ui/editor/GamlMarkOccurrenceActionContributor.java
@@ -1,35 +1,47 @@
 /*********************************************************************************************
- * 
- * 
+ *
+ *
  * 'GamlMarkOccurrenceActionContributor.java', in plugin 'msi.gama.lang.gaml.ui', is part of the source code of the
  * GAMA modeling and simulation platform.
  * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
- * 
+ *
  * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
- * 
- * 
+ *
+ *
  **********************************************************************************************/
 package msi.gama.lang.gaml.ui.editor;
 
-import msi.gama.common.*;
-import msi.gama.common.GamaPreferences.Entry;
-import msi.gama.common.GamaPreferences.IPreferenceChangeListener;
-import msi.gaml.types.IType;
+import org.eclipse.jface.action.*;
+import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.occurrences.MarkOccurrenceActionContributor;
 import org.eclipse.xtext.ui.editor.preferences.*;
 import com.google.inject.Singleton;
+import msi.gama.common.GamaPreferences;
+import msi.gama.common.GamaPreferences.*;
+import msi.gaml.types.IType;
 
 /**
  * The class GamlMarkOccurrenceActionContributor.
- * 
+ *
  * @author drogoul
  * @since 12 sept. 2013
- * 
+ *
  */
 @Singleton
 public class GamlMarkOccurrenceActionContributor extends MarkOccurrenceActionContributor implements IPreferenceStoreInitializer {
 
 	IPreferenceStoreAccess access;
+
+	@Override
+	public void contributeActions(final XtextEditor editor) {
+		super.contributeActions(editor);
+		IToolBarManager toolBarManager = editor.getEditorSite().getActionBars().getToolBarManager();
+		IContributionItem item = toolBarManager.find(getAction().getId());
+		if ( item != null ) {
+			toolBarManager.remove(item);
+		}
+
+	}
 
 	// Preference here is an instance variable, but only one will be created as this class is a singleton.
 	public final Entry<Boolean> EDITOR_MARK_OCCURENCES = GamaPreferences
