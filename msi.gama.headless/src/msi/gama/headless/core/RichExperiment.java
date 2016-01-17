af9a975c88895960629fe12ca3f915b52301e122
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-01-18 02:22:59 +0700

    Extending the Extending the use of the scope in various places

--- msi.gama.headless/src/msi/gama/headless/core/RichExperiment.java
+++ msi.gama.headless/src/msi/gama/headless/core/RichExperiment.java
@@ -14,47 +14,43 @@ package msi.gama.headless.core;
 import msi.gama.headless.job.ExperimentJob.OutputType;
 import msi.gama.kernel.model.IModel;
 import msi.gama.outputs.*;
-import msi.gama.runtime.*;
+import msi.gama.runtime.GAMA;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 
 public class RichExperiment extends Experiment implements IRichExperiment {
 
-	
-	
-	public RichExperiment(IModel mdl) {
+	public RichExperiment(final IModel mdl) {
 		super(mdl);
 	}
 
+	@Override
 	public OutputType getTypeOf(final String name) {
 		if ( currentExperiment == null ) { return OutputType.OUTPUT; }
 		if ( currentExperiment.hasVar(name) ) { return OutputType.EXPERIMENT_ATTRIBUTE; }
 		if ( currentExperiment.getModel().getSpecies().hasVar(name) ) { return OutputType.SIMULATION_ATTRIBUTE; }
 		return OutputType.OUTPUT;
 	}
-	
+
+	@Override
 	public RichOutput getRichOutput(final String parameterName) {
 		IOutput output =
 			((AbstractOutputManager) this.currentExperiment.getSimulationOutputs()).getOutputWithName(parameterName);
-		if ( output == null ) { throw GamaRuntimeException.error("Output unresolved"); }
+		if ( output == null ) { throw GamaRuntimeException.error("Output unresolved",
+			currentExperiment.getExperimentScope()); }
 		output.update();
-		
+
 		Object val = null;
-		
-		if(output instanceof MonitorOutput)
-		{
+
+		if ( output instanceof MonitorOutput ) {
 			val = ((MonitorOutput) output).getLastValue();
-		}
-		else if(output instanceof LayeredDisplayOutput)
-		{
+		} else if ( output instanceof LayeredDisplayOutput ) {
 			val = ((LayeredDisplayOutput) output).getImage();
+		} else if ( output instanceof LayeredDisplayOutput ) {
+			val = ((FileOutput) output).getFile();
 		}
-		else if(output instanceof LayeredDisplayOutput)
-		{
-			val = ((FileOutput)output).getFile();
-		}
-		return  new RichOutput(parameterName,this.currentStep,val);
+		return new RichOutput(parameterName, this.currentStep, val);
 	}
-		
+
 	@Override
 	public void dispose() {
 		GAMA.closeExperiment(currentExperiment);
