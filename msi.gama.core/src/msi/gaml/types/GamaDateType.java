4013d6bce7c021b0db7774720b2d27783760fc76
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-01-18 21:31:21 +0700

    Fixes #1426

--- msi.gama.core/src/msi/gaml/types/GamaDateType.java
+++ msi.gama.core/src/msi/gaml/types/GamaDateType.java
@@ -1,35 +1,31 @@
 /*********************************************************************************************
- * 
- * 
+ *
+ *
  * 'GamaColorType.java', in plugin 'msi.gama.core', is part of the source code of the
  * GAMA modeling and simulation platform.
  * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
- * 
+ *
  * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
- * 
- * 
+ *
+ *
  **********************************************************************************************/
 package msi.gaml.types;
 
+import org.joda.time.MutableDateTime;
 import msi.gama.precompiler.GamlAnnotations.type;
 import msi.gama.precompiler.ISymbolKind;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
-import msi.gama.util.GamaDate;
-import msi.gama.util.IContainer;
-import msi.gama.util.IList;
+import msi.gama.util.*;
 import msi.gaml.operators.Cast;
 
 /**
  * Written by Patrick Tallandier
- * 
+ *
  * @todo Description
- * 
+ *
  */
-@type(name = "date",
-	id = IType.DATE,
-	wraps = { GamaDate.class },
-	kind = ISymbolKind.Variable.REGULAR)
+@type(name = "date", id = IType.DATE, wraps = { GamaDate.class }, kind = ISymbolKind.Variable.REGULAR)
 public class GamaDateType extends GamaType<GamaDate> {
 
 	@Override
@@ -40,22 +36,20 @@ public class GamaDateType extends GamaType<GamaDate> {
 
 	public static GamaDate staticCast(final IScope scope, final Object obj, final Object param, final boolean copy)
 		throws GamaRuntimeException {
-		if (obj == null) return null;
+		if ( obj == null ) { return null; }
 		if ( obj instanceof GamaDate ) {
-			if (copy) return new GamaDate((GamaDate) obj);
+			if ( copy ) { return new GamaDate((GamaDate) obj); }
 			return (GamaDate) obj;
 		}
-		if ( obj instanceof IList ) {
-			return new GamaDate((IList) obj);
-		}
+		if ( obj instanceof IList ) { return new GamaDate((IList) obj); }
 		if ( obj instanceof IContainer ) { return staticCast(scope,
 			((IContainer) obj).listValue(scope, Types.NO_TYPE, false), param, copy); }
-		
+
 		if ( obj instanceof String ) {
-			if ("now".equals(obj.toString())) return new GamaDate(GamaDate.now());
+			if ( "now".equals(obj.toString()) ) { return new GamaDate(MutableDateTime.now()); }
 			return new GamaDate((String) obj);
 		}
-		if ( obj instanceof Boolean ) { return new GamaDate() ; }
+		if ( obj instanceof Boolean ) { return new GamaDate(); }
 		int i = Cast.asInt(scope, obj);
 		return new GamaDate(i);
 	}
@@ -65,11 +59,9 @@ public class GamaDateType extends GamaType<GamaDate> {
 		return null;
 	}
 
-	
 	@Override
 	public boolean canCastToConst() {
 		return true;
 	}
-	
 
 }
