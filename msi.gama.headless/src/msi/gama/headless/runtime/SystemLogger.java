73177a00652541cabb12dc1d28db7b291356ab2d
Nicolas <nicolas@192.168.0.105>
2016-01-20 12:25:48 +0700

    System logger bug fix

--- msi.gama.headless/src/msi/gama/headless/runtime/SystemLogger.java
+++ msi.gama.headless/src/msi/gama/headless/runtime/SystemLogger.java
@@ -3,26 +3,35 @@ package msi.gama.headless.runtime;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import com.sun.xml.internal.bind.v2.model.runtime.RuntimeMapPropertyInfo;
+
 public class SystemLogger extends PrintStream {
 	private static SystemLogger display;
+	private static SystemLogger error;
 	private static PrintStream oldOutStream = null;
+	private static PrintStream oldErrStream = null;
 	
 	
-	static void removeDisplay()
+	public static void removeDisplay()
 	{
 	 	if(display==null)
 	 	{
 			display = new SystemLogger(System.out);
+			error = new SystemLogger(System.err);
 			oldOutStream = System.out;
+			oldErrStream = System.err;
 	 	}
-		System.setOut(display);
+	 	System.setOut(display);
+	 	System.setErr(error);
 	}
 	
-	static void activeDisplay()
+	public static void activeDisplay()
 	{
+		
 		if(display!=null)
 		{
 			System.setOut(oldOutStream);
+			System.setErr(oldErrStream);
 			oldOutStream = null;
 			display = null;
 		}
