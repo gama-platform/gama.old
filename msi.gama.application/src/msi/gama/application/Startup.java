4fb991dab20eeb68c0581cd5ee0703dcd88d78d0
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-01-20 11:55:10 +0700

    Fixes an issue where the plugins would not start in the expected order

--- msi.gama.application/src/msi/gama/application/Startup.java
+++ msi.gama.application/src/msi/gama/application/Startup.java
@@ -9,7 +9,10 @@ public class Startup implements IStartup {
 	@Override
 	public void earlyStartup() {
 		System.err.println("Early startup of the SWT Regular UI plugin so as to ensure all services are setup");
-		GAMA.setRegularGui(new SwtGui());
+		// We make sure we do not replace the XText GUI
+		if ( GAMA.getRegularGui() == null ) {
+			GAMA.setRegularGui(new SwtGui());
+		}
 	}
 
 }
