d9458bd7b156237ef4dfc4cedad5db57b4b6b27b
benoitgaudou <benoit.gaudou@gmail.com>
2016-01-21 04:01:52 +0700

    Example of serialization and unserialization of SimulationAgent with dedicated agents

--- msi.gama.core/src/msi/gama/metamodel/agent/GamlAgent.java
+++ msi.gama.core/src/msi/gama/metamodel/agent/GamlAgent.java
@@ -13,7 +13,7 @@ package msi.gama.metamodel.agent;
 
 import java.util.*;
 import com.google.common.collect.Iterables;
-import gnu.trove.map.hash.THashMap;
+
 import msi.gama.common.interfaces.IKeyword;
 import msi.gama.common.util.RandomUtils;
 import msi.gama.kernel.simulation.*;
@@ -310,120 +310,6 @@ public class GamlAgent extends MinimalAgent implements IMacroAgent {
 		}
 
 		return immigrants;
-	}
-
-	/** Variables which are not saved during the capture and release process. */
-	private static final List<String> UNSAVABLE_VARIABLES = Arrays.asList(IKeyword.PEERS, IKeyword.AGENTS,
-		IKeyword.HOST, IKeyword.TOPOLOGY, IKeyword.MEMBERS, "populations");
-
-	/**
-	 * A helper class to save agent and restore/recreate agent as a member of a population.
-	 */
-		private class SavedAgent {
-
-		Map<String, Object> variables;
-		Map<String, List<SavedAgent>> innerPopulations;
-
-		SavedAgent(final IScope scope, final IAgent agent) throws GamaRuntimeException {
-			saveAttributes(scope, agent);
-			if ( agent instanceof IMacroAgent ) {
-				saveMicroAgents(scope, (IMacroAgent) agent);
-			}
-		}
-
-		/**
-		 * Saves agent's attributes to a map.
-		 *
-		 * @param agent
-		 * @throws GamaRuntimeException
-		 */
-		private void saveAttributes(final IScope scope, final IAgent agent) throws GamaRuntimeException {
-			variables = new THashMap<String, Object>(11, 0.9f);
-			final ISpecies species = agent.getSpecies();
-			for ( final String specVar : species.getVarNames() ) {
-				if ( UNSAVABLE_VARIABLES.contains(specVar) ) {
-					continue;
-				}
-
-				if ( species.getVar(specVar).value(scope, agent) instanceof IPopulation ) {
-					continue;
-				}
-
-				if ( specVar.equals(IKeyword.SHAPE) ) {
-					// variables.put(specVar, geometry.copy());
-					// Changed 3/2/12: is it necessary to make the things below ?
-					variables.put(specVar,
-						new GamaShape(((GamaShape) species.getVar(specVar).value(scope, agent)).getInnerGeometry()));
-					continue;
-				}
-				variables.put(specVar, species.getVar(specVar).value(scope, agent));
-			}
-		}
-
-		/**
-		 * Recursively save micro-agents of an agent.
-		 *
-		 * @param agent The agent having micro-agents to be saved.
-		 * @throws GamaRuntimeException
-		 */
-		private void saveMicroAgents(final IScope scope, final IMacroAgent agent) throws GamaRuntimeException {
-			innerPopulations = new THashMap<String, List<SavedAgent>>();
-
-			for ( final IPopulation microPop : agent.getMicroPopulations() ) {
-				final List<SavedAgent> savedAgents = new ArrayList<SavedAgent>();
-				final Iterator<IAgent> it = microPop.iterator();
-				while (it.hasNext()) {
-					savedAgents.add(new SavedAgent(scope, it.next()));
-				}
-
-				innerPopulations.put(microPop.getSpecies().getName(), savedAgents);
-			}
-		}
-
-		/**
-		 * @param scope
-		 * Restores the saved agent as a member of the target population.
-		 *
-		 * @param targetPopulation The population that the saved agent will be restored to.
-		 * @return
-		 * @throws GamaRuntimeException
-		 */
-		IAgent restoreTo(final IScope scope, final IPopulation targetPopulation) throws GamaRuntimeException {
-			final List<Map> agentAttrs = new ArrayList<Map>();
-			agentAttrs.add(variables);
-			final List<? extends IAgent> restoredAgents = targetPopulation.createAgents(scope, 1, agentAttrs, true);
-			restoreMicroAgents(scope, restoredAgents.get(0));
-
-			return restoredAgents.get(0);
-		}
-
-		/**
-		 *
-		 *
-		 * @param host
-		 * @throws GamaRuntimeException
-		 */
-		void restoreMicroAgents(final IScope scope, final IAgent host) throws GamaRuntimeException {
-
-			for ( final String microPopName : innerPopulations.keySet() ) {
-				final IPopulation microPop = ((IMacroAgent) host).getMicroPopulation(microPopName);
-
-				if ( microPop != null ) {
-					final List<SavedAgent> savedMicros = innerPopulations.get(microPopName);
-					final List<Map> microAttrs = new ArrayList<Map>();
-					for ( final SavedAgent sa : savedMicros ) {
-						microAttrs.add(sa.variables);
-					}
-
-					final List<? extends IAgent> microAgents =
-						microPop.createAgents(scope, savedMicros.size(), microAttrs, true);
-
-					for ( int i = 0; i < microAgents.size(); i++ ) {
-						savedMicros.get(i).restoreMicroAgents(scope, microAgents.get(i));
-					}
-				}
-			}
-		}
 	}
 
 	@Override
--- ummisco.gama.communicator/src/ummisco/gama/communicator/ReverseOperators.java
+++ ummisco.gama.communicator/src/ummisco/gama/communicator/ReverseOperators.java
@@ -1,29 +1,47 @@
 package ummisco.gama.communicator;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import msi.gama.runtime.IScope;
+import msi.gama.kernel.experiment.ExperimentAgent;
+import msi.gama.kernel.simulation.SimulationAgent;
+import msi.gama.metamodel.agent.SavedAgent;
 import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.operator;
-import ummisco.gama.serializer.gamaType.converters.GamaAgentConverter;
-import ummisco.gama.serializer.gamaType.converters.GamaMapConverter;
-import ummisco.gama.serializer.gamaType.converters.GamaPairConverter;
-import ummisco.gama.serializer.gamaType.converters.GamaPointConverter;
-import ummisco.gama.serializer.gamaType.converters.GamaScopeConverter;
-import ummisco.gama.serializer.gamaType.converters.GamaSimulationAgentConverter;
 
 public class ReverseOperators {
-	@operator(value = "saveScope")
+	@operator(value = "serializeSimulation")
 	@doc("")
-	public static String coucouScope(IScope scope, int i) {
+	public static String serializeSimulation(IScope scope, int i) {
 		XStream xstream = new XStream();
-		xstream.registerConverter(new GamaAgentConverter());
-		xstream.registerConverter(new GamaScopeConverter());
-		xstream.registerConverter(new GamaPointConverter());
-		xstream.registerConverter(new GamaPairConverter());
-		xstream.registerConverter(new GamaMapConverter());
-		xstream.registerConverter(new GamaSimulationAgentConverter());
+	//	xstream.registerConverter(new GamaAgentConverter());
+	//	xstream.registerConverter(new GamaScopeConverter());
+	//	xstream.registerConverter(new GamaPointConverter());
+	//	xstream.registerConverter(new GamaPairConverter());
+	//	xstream.registerConverter(new GamaMapConverter());
+	//	xstream.registerConverter(new GamaSimulationAgentConverter());
 		
-		return (String) xstream.toXML(scope);
+		ExperimentAgent expAgt = (ExperimentAgent) scope.getExperiment();
+		SimulationAgent simAgt = expAgt.getSimulation();
+		
+		return (String) xstream.toXML(new SavedAgent(scope,simAgt));
 	}
+	
+	@operator(value = "unSerializeSimulation")
+	@doc("")
+	public static int unSerializeSimulation(IScope scope, String simul) {
+		XStream xstream = new XStream(new DomDriver());
+	//	xstream.registerConverter(new GamaAgentConverter());
+	//	xstream.registerConverter(new GamaScopeConverter());
+	//	xstream.registerConverter(new GamaPointConverter());
+	//	xstream.registerConverter(new GamaPairConverter());
+	//	xstream.registerConverter(new GamaMapConverter());
+	//	xstream.registerConverter(new GamaSimulationAgentConverter());
+		
+		SavedAgent agt = (SavedAgent) xstream.fromXML(simul);
+
+		return 1;
+	}
+	
 }
