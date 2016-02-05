7c7ecf17668ecc210f075bb5a8e2ca169b9a736c
Julien Mazars <mazarsju@gmail.com>
2016-02-05 15:49:44 +0700

    update learningGraphDatabaseGenerator

--- msi.gama.documentation/src/msi/gama/doc/websiteGen/LearningConcept.java
+++ msi.gama.documentation/src/msi/gama/doc/websiteGen/LearningConcept.java
@@ -5,13 +5,15 @@ import java.util.List;
 class LearningConcept {
 	public String m_id;
 	public String m_name;
+	public String m_description;
 	public List<String> m_prerequisitesList;
 	public float m_xPos;
 	public float m_yPos;
 	
-	public LearningConcept(String id, String name, float xPos, float yPos, List<String> prerequisitesList) {
+	public LearningConcept(String id, String name, String description, float xPos, float yPos, List<String> prerequisitesList) {
 		m_id = id;
 		m_name = name;
+		m_description = description;
 		m_xPos = xPos;
 		m_yPos = yPos;
 		m_prerequisitesList = prerequisitesList;
