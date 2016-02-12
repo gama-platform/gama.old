8a03fcbbd88b9813b3669cbd012f20bf0e1a5bde
Nicolas <nicolas@mbp-de-nicolas>
2016-02-12 14:42:20 +0100

    warning suppress

--- msi.gama.application/src/msi/gama/application/projects/WorkspaceModelsManager.java
+++ msi.gama.application/src/msi/gama/application/projects/WorkspaceModelsManager.java
@@ -26,7 +26,6 @@ import org.eclipse.ui.actions.WorkspaceModifyOperation;
 import org.eclipse.ui.internal.ide.application.DelayedEventsProcessor;
 import msi.gama.runtime.GAMA;
 import msi.gaml.compilation.GamaBundleLoader;
-import msi.gaml.compilation.SystemLogger;
 
 /**
  * Class InitialModelOpener.
