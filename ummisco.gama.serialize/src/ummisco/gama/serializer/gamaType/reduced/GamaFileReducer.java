ae63f0facf8186c9727e5d151caa4294ff66657e
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-03-04 12:26:55 +0700

    Removes an error introduced in previous commit

--- ummisco.gama.serialize/src/ummisco/gama/serializer/gamaType/reduced/GamaFileReducer.java
+++ ummisco.gama.serialize/src/ummisco/gama/serializer/gamaType/reduced/GamaFileReducer.java
@@ -1,29 +1,23 @@
 package ummisco.gama.serializer.gamaType.reduced;
 
-import msi.gama.metamodel.shape.GamaPoint;
 import msi.gama.runtime.IScope;
 import msi.gama.util.IList;
-import msi.gama.util.IModifiableContainer;
-import msi.gama.util.file.GamaFile;
 import msi.gama.util.file.IGamaFile;
-import msi.gama.util.matrix.GamaMatrix;
 import msi.gaml.types.GamaFileType;
-import msi.gaml.types.GamaMatrixType;
-import msi.gaml.types.IType;
 
 public class GamaFileReducer {
-	private String path;
-	private IList attributes;
-	
-	public GamaFileReducer(IScope scope, IGamaFile f)
-	{		
+	private final String path;
+	private final IList attributes;
+
+	public GamaFileReducer(final IScope scope, final IGamaFile f)
+	{
 		path = f.getPath();
 		attributes = f.getAttributes(scope);
 	}
-	
-	public IGamaFile constructObject(IScope scope)
+
+	public IGamaFile constructObject(final IScope scope)
 	{
-		return (IGamaFile) GamaFileType.createFile(scope, path);//, attributes) ;
+		return GamaFileType.createFile(scope, path, null);// , attributes) ;
 		// return (GamaMatrix) GamaMatrixType.from(scope, valuesMatrixReducer, contentTypeMatrixReducer, new GamaPoint(nCols, nRows))	;
 
 	}
