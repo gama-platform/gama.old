f7816772e334240b12ecf8ba01f8e43c9d8645d4
Nicolas <nicolas@192.168.0.101>
2016-05-16 20:02:20 +0700

    new version of network

---
+++ ummisco.gama.network/src/ummisco/gama/mqtt/common/MQTTConnector.java
@@ -0,0 +1,45 @@
+package ummisco.gama.mqtt.common;
+
+import java.net.URISyntaxException;
+
+import org.fusesource.mqtt.client.CallbackConnection;
+import org.fusesource.mqtt.client.FutureConnection;
+import org.fusesource.mqtt.client.MQTT;
+
+public class MQTTConnector {
+	public static String DEFAULT_USER = env("ACTIVEMQ_USER", "admin");
+	public static String DEFAULT_PASSWORD = env("ACTIVEMQ_PASSWORD", "password");
+	public static String DEFAULT_HOST = env("ACTIVEMQ_HOST", "localhost");
+	public static int port = Integer.parseInt(env("ACTIVEMQ_PORT", "1883"));
+	public static final String DESTINATION_PREFIX = "";//"topic/sensors/";
+    
+    private static String env(String key, String defaultValue) {
+        String rc = System.getenv(key);
+        if( rc== null )
+            return defaultValue;
+        return rc;
+    }
+    
+    public static CallbackConnection connectReceiver( String server, String login, String pass) throws URISyntaxException
+    {
+        MQTT mqtt = new MQTT();
+        System.out.println("ln "+port );
+        mqtt.setHost(server, port);
+        mqtt.setUserName(login);
+        mqtt.setPassword(pass);
+        CallbackConnection connection = mqtt.callbackConnection();
+        return connection;
+    }
+    
+	public static FutureConnection connectSender( String server, String login, String pass) throws Exception
+	{
+		MQTT mqtt = new MQTT();
+        System.out.println("ln "+port );
+	    mqtt.setHost(server, port);
+	    mqtt.setUserName(login);
+	    mqtt.setPassword(pass);
+	    FutureConnection connection = mqtt.futureConnection();
+	    connection.connect().await();
+	    return connection;
+	}
+}
---
+++ ummisco.gama.network/src/ummisco/gama/network/common/INetworkSkill.java
@@ -0,0 +1,54 @@
+/*********************************************************************************************
+ * 
+ *
+ * 'ICommunicatorSkill.java', in plugin 'ummisco.gama.communicator', is part of the source code of the 
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 
+ * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
+ * 
+ * 
+ **********************************************************************************************/
+package ummisco.gama.network.common;
+
+import java.util.Observer;
+
+import msi.gama.runtime.IScope;
+import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gama.util.GamaMap;
+
+public interface INetworkSkill  {
+	public static String CONNECT_TOPIC = "connect";
+	public static String SERVER_URL = "to";
+	public static String GROUP = "with_group";
+	public static String WITHNAME = "with_name";
+	
+	//Agent Data
+	public static String NET_AGENT_NAME = "network_name";
+	public static String NET_AGENT_GROUPS = "network_groups";
+	public static String NET_AGENT_SERVER = "network_server";
+	
+	//MESSAGE CONTENTS
+	public static String TO = "to";
+	public static String FROM = "from";
+	public static String CONTENT = "content";
+
+	public static String BROADCAST = "all";
+	
+	
+	///// SKILL NETWORK
+	public static String NETWORK_SKILL = "network";
+	public static String SEND_MESSAGE = "send_message";
+	public static String FETCH_MESSAGE = "fetch_message";
+	public static String HAS_MORE_MESSAGE_IN_BOX="has_received_message";
+	
+	// SKILL SENSING
+	public static String SENSING_SKILL  = "sensor";
+	public static String SENSING_CONNECT = "connect_sensor";
+	public static String SENSOR_NAME = "sensor_name";
+	public static String FETCH_DATA = "fetch_data";
+	public static String EMPTY_BUFFER="has_received_data";
+	
+	
+	
+}
