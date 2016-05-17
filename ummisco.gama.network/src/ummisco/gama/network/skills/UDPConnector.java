a1f679840e7a7c60408a53dd7b7a118c7925427a
agrignard <agrignard@gmail.com>
2016-05-17 22:18:11 +0700

    Add 3 different Connector (empty one) (udp, tcp,  mqqt)

---
+++ ummisco.gama.network/src/ummisco/gama/network/skills/UDPConnector.java
@@ -0,0 +1,35 @@
+package ummisco.gama.network.skills;
+
+import java.util.Map;
+
+import msi.gama.metamodel.agent.IAgent;
+import msi.gama.runtime.IScope;
+import msi.gama.util.GamaMap;
+
+public class UDPConnector implements IConnector{
+
+	@Override
+	public void connectToServer(IAgent agent, String dest, String server, IScope scope) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendMessage(String dest, Map<String, String> data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GamaMap<String, String> fetchMessageBox(IAgent agt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean emptyMessageBox(IAgent agt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
