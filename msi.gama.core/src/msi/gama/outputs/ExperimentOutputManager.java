28ef5000933c897b88e22c6bc5db469aa1d7efa6
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-05-17 09:08:44 +0700

    Removes the possibility to declare the layout in models
    Signed-off-by: AlexisDrogoul <alexis.drogoul@gmail.com>

--- msi.gama.core/src/msi/gama/outputs/ExperimentOutputManager.java
+++ msi.gama.core/src/msi/gama/outputs/ExperimentOutputManager.java
@@ -14,8 +14,6 @@ package msi.gama.outputs;
 import msi.gama.common.interfaces.IKeyword;
 import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.example;
-import msi.gama.precompiler.GamlAnnotations.facet;
-import msi.gama.precompiler.GamlAnnotations.facets;
 import msi.gama.precompiler.GamlAnnotations.inside;
 import msi.gama.precompiler.GamlAnnotations.symbol;
 import msi.gama.precompiler.GamlAnnotations.usage;
@@ -25,7 +23,6 @@ import msi.gama.runtime.IScope;
 import msi.gaml.descriptions.IDescription;
 import msi.gaml.expressions.IExpression;
 import msi.gaml.operators.Cast;
-import msi.gaml.types.IType;
 
 /**
  * The Class OutputManager.
@@ -34,8 +31,12 @@ import msi.gaml.types.IType;
  */
 @symbol(name = IKeyword.PERMANENT, kind = ISymbolKind.OUTPUT, with_sequence = true, concept = { IConcept.BATCH,
 		IConcept.PARAMETER })
-@facets(omissible = IKeyword.LAYOUT, value = {
-		@facet(name = IKeyword.LAYOUT, type = IType.INT, optional = true, doc = @doc("Either #none, to indicate that no layout will be imposed, or one of the four possible predefined layouts: #stack, #split, #horizontal or #vertical. This layout will be applied to both experiment and simulation display views")) })
+// @facets(omissible = IKeyword.LAYOUT, value = {
+// @facet(name = IKeyword.LAYOUT, type = IType.INT, optional = true, doc =
+// @doc("Either #none, to indicate that no layout will be imposed, or one of the
+// four possible predefined layouts: #stack, #split, #horizontal or #vertical.
+// This layout will be applied to both experiment and simulation display
+// views")) })
 
 @inside(kinds = { ISymbolKind.EXPERIMENT })
 @doc(value = "Represents the outputs of the experiment itself. In a batch experiment, the permanent section allows to define an output block that will NOT be re-initialized at the beginning of each simulation but will be filled at the end of each simulation.", usages = {
