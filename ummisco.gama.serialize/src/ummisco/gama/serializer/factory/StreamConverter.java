0366c8e94e35392ff0c632f5a06de7a843466714
Nicolas <nicolas@192.168.0.112>
2016-05-18 13:52:44 +0700

    serializer API update

--- ummisco.gama.serialize/src/ummisco/gama/serializer/factory/StreamConverter.java
+++ ummisco.gama.serialize/src/ummisco/gama/serializer/factory/StreamConverter.java
@@ -8,6 +8,7 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
+import msi.gama.runtime.IScope;
 import msi.gama.util.GamaMapFactory;
 import msi.gaml.types.IType;
 
@@ -22,27 +23,27 @@ public abstract class StreamConverter {
 	
 	
 	
-	public static void loadAndBuild()
+	public static void loadAndBuild(IScope scope)
 	{
 		dataStreamer = new XStream(new DomDriver());
-		Converter[] cnv = Converters.converterFactory();
+		Converter[] cnv = Converters.converterFactory(scope);
 		for(Converter c:cnv)
 		{
 			StreamConverter.registerConverter(c);
 		}
 	}
 	
-	public static String convertObjectToStream(Object o)
+	public static String convertObjectToStream(IScope scope, Object o)
 	{
 		if(dataStreamer==null)
-			loadAndBuild();
+			loadAndBuild(scope);
 		return dataStreamer.toXML(o);
 	}
 	
-	public static Object convertStreamToObject(String data)
+	public static Object convertStreamToObject(IScope scope,String data)
 	{
 		if(dataStreamer==null)
-			loadAndBuild();
+			loadAndBuild(scope);
 		return dataStreamer.fromXML(data);
 	}
 	
