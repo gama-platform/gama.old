34ba871d72e2eb856da0c54c492500d4584b375c
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-05-21 22:16:14 +0700

    Add activator.

---
+++ msi.gama.ext/src/msi/gama/ext/Activator.java
@@ -0,0 +1,44 @@
+/*********************************************************************************************
+ * 
+ *
+ * 'Activator.java', in plugin 'msi.gama.headless', is part of the source code of the 
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2014 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 
+ * Visit https://code.google.com/p/gama-platform/ for license information and developers contact.
+ * 
+ * 
+ **********************************************************************************************/
+package msi.gama.ext;
+
+import org.osgi.framework.*;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(final BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(final BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
