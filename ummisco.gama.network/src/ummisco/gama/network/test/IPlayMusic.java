ddb4216dfbd79f36005426516f559634ee98e7ef
Nicolas <nicolas@192.168.0.104>
2016-05-22 13:04:03 +0700

    For Travis...

---
+++ ummisco.gama.network/src/ummisco/gama/network/test/IPlayMusic.java
@@ -0,0 +1,9 @@
+package ummisco.gama.network.test;
+
+public interface IPlayMusic {
+	public static String MUSIC_SKILL = "music";
+	public static String PLAY_NOTE = "play";
+	public static String LEVEL = "level";
+	public static String VOLUME = "volume";
+	public static String DURATION = "duration";
+}
