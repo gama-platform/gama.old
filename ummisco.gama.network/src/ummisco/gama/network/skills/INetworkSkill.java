2af5c538c08f554bb563a407197c3a35aa054af6
Nicolas <nmarilleau@gmail.com>
2016-06-13 09:39:54 +0200

    has_more_message command added

--- ummisco.gama.network/src/ummisco/gama/network/skills/INetworkSkill.java
+++ ummisco.gama.network/src/ummisco/gama/network/skills/INetworkSkill.java
@@ -51,7 +51,7 @@ public interface INetworkSkill  {
 	public static String SEND_MESSAGE = "send_message";
 	public static String FETCH_MESSAGE = "fetch_message";
 	public static String LISTEN = "listen";
-	public static String HAS_MORE_MESSAGE_IN_BOX="has_received_message";
+	public static String HAS_MORE_MESSAGE_IN_BOX="has_more_message";
 	public static String BROADCAST_RECEIVER = "all";
 	
 	///// GROUP MANAGEMENT
