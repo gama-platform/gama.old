fcb8f824390c3fd602bee2af97f5a05145cf69fd
hqnghi88 <hqnghi88@gmail.com>
2016-07-04 10:48:50 +0700

    Update github-release.sh

--- github-release.sh
+++ github-release.sh
@@ -24,26 +24,26 @@ COMMIT="${COMMIT:0:7}"
 
 timestamp=$(date '+_%D')
 
-SUFFIX="$timestamp.$COMMIT.zip"
+SUFFIX="$timestamp_$COMMIT.zip"
 echo $SUFFIX
 
 
 
 n=0
 RELEASEFILES[$n]="$thePATH-linux.gtk.x86.zip"
-NEWFILES[$n]='GAMA1.7_Linux_32_'$SUFFIX
+NEWFILES[$n]='GAMA1.7_Linux_32'$SUFFIX
 n=1
 RELEASEFILES[$n]="$thePATH-linux.gtk.x86_64.zip"
-NEWFILES[$n]='GAMA1.7_Linux_64_'$SUFFIX
+NEWFILES[$n]='GAMA1.7_Linux_64'$SUFFIX
 n=2
 RELEASEFILES[$n]="$thePATH-macosx.cocoa.x86_64.zip"
-NEWFILES[$n]='GAMA1.7_Mac_64_'$SUFFIX
+NEWFILES[$n]='GAMA1.7_Mac_64'$SUFFIX
 n=3
 RELEASEFILES[$n]="$thePATH-win32.win32.x86.zip"
-NEWFILES[$n]='GAMA1.7_Win_32_'$SUFFIX
+NEWFILES[$n]='GAMA1.7_Win_32'$SUFFIX
 n=4
 RELEASEFILES[$n]="$thePATH-win32.win32.x86_64.zip" 
-NEWFILES[$n]='GAMA1.7_Win_64_'$SUFFIX
+NEWFILES[$n]='GAMA1.7_Win_64'$SUFFIX
 
 i=0
 for (( i=0; i<5; i++ ))
