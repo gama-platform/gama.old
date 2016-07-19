f7f2144ce12241b339799863c55781f3776208ae
hqnghi88 <hqnghi88@gmail.com>
2016-07-19 23:20:22 +0700

    vs travis

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd msi.gama.parent &&
-mvn install:install-file -Dfile=file://home/travis/build/gama-platform/gama/msi.gama.processor/processor/plugins/msi.gama.processor-1.4.0.jar -DgroupId=msi.gama.processor -Dversion=1.4.0 -Dpackaging=jar && mvn -X clean install &&
+mvn -U clean && mvn -X dependency:purge-local-repository  clean install &&
 cd -
 
