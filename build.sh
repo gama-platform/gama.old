22f3f4666e07e3d870f96f6c75926d97aefc394d
hqnghi88 <hqnghi88@gmail.com>
2016-07-19 19:02:40 +0700

    Trying with travis

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 cd msi.gama.parent &&
-mvn install:install-file -Dfile=file://home/travis/build/gama-platform/gama/msi.gama.processor/processor/plugins/msi.gama.processor-1.4.0.jar -DgroupId=msi.gama.processor 
--DartifactId=msi.gama.processor -Dversion=1.4.0 -Dpackaging=jar && mvn -X clean install &&
+mvn install:install-file -Dfile=file://home/travis/build/gama-platform/gama/msi.gama.processor/processor/plugins/msi.gama.processor-1.4.0.jar -DgroupId=msi.gama.processor -Dversion=1.4.0 -Dpackaging=jar && mvn -X clean install &&
 cd -
 
