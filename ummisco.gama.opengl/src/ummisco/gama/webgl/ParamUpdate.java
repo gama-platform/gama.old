929c194c32af24941d81f07123fb82220c0c96c3
napieralaf <napieralaf@eisti.eu>
2016-09-01 17:46:13 +0700

    Add some contents for the web interface

---
+++ ummisco.gama.opengl/src/ummisco/gama/webgl/ParamUpdate.java
@@ -0,0 +1,11 @@
+package ummisco.gama.webgl;
+
+public class ParamUpdate {
+	public String id;
+	public Object data;
+	
+	public ParamUpdate(String id, Object data) {
+		this.id = id;
+		this.data = data;
+	}
+}
