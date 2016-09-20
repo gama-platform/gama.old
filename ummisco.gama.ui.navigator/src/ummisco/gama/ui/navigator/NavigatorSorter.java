b6333cda6cc1d417f7013ccf0b8161c3c0f8ff55
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-09-20 23:15:57 +0700

    Removes the old comparator code
    Signed-off-by: AlexisDrogoul <alexis.drogoul@gmail.com>

--- ummisco.gama.ui.navigator/src/ummisco/gama/ui/navigator/NavigatorSorter.java
+++ ummisco.gama.ui.navigator/src/ummisco/gama/ui/navigator/NavigatorSorter.java
@@ -2,7 +2,6 @@ package ummisco.gama.ui.navigator;
 
 import java.text.Collator;
 
-import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 
 import msi.gama.common.interfaces.IKeyword;
@@ -10,25 +9,8 @@ import ummisco.gama.ui.navigator.WrappedSyntacticContent.WrappedExperimentConten
 
 public class NavigatorSorter extends ViewerSorter {
 
-	public NavigatorSorter() {
-	}
-
 	public NavigatorSorter(final Collator collator) {
 		super(collator);
-	}
-
-	@Override
-	public int compare(final Viewer viewer, final Object e1, final Object e2) {
-		// if (e1 instanceof WrappedSyntacticContent && e2 instanceof
-		// WrappedSyntacticContent) {
-		// return ((WrappedSyntacticContent)
-		// e1).compareTo((WrappedSyntacticContent) e2);
-		// }
-		// if (e1 instanceof WrappedGamlObject && e2 instanceof
-		// WrappedGamlObject) {
-		// return ((WrappedGamlObject) e1).compareTo((WrappedGamlObject) e2);
-		// }
-		return super.compare(viewer, e1, e2);
 	}
 
 	@Override
