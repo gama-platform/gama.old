3c7f1d20dcb2180629f29ebb7870f2e81011677e
Nicolas <nmarilleau@gmail.com>
2016-10-10 01:50:36 +0200

    headless bug fix

--- ummisco.gama.product/extraresources/headless/windows/gama-headless.bat
+++ ummisco.gama.product/extraresources/headless/windows/gama-headless.bat
@@ -32,4 +32,4 @@ echo * http://gama-platform.googlecode.com                            *
 echo * (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC and Partners            *
 echo ******************************************************************
 rem @echo off
-call java  -cp ..\plugins\msi.gama.headless_1.7.0.201603131125.jar:..\plugins\org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar -Xms512m -Xmx%memory%  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data "%workDir%" !param! 
+call java  -cp ..\plugins\org.eclipse.equinox.launcher*.jar -Xms512m -Xmx%memory%  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data "%workDir%" !param! 
