8f284c9d6d66fddf928501e56627b4fd6e3b5ef0
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-11-03 00:17:42 +0700

    Changing the Changing the header to reflect current year
    Signed-off-by: AlexisDrogoul <alexis.drogoul@gmail.com>

--- msi.gama.application/src/msi/gama/application/workspace/WorkspacePreferences.java
+++ msi.gama.application/src/msi/gama/application/workspace/WorkspacePreferences.java
@@ -1,3 +1,13 @@
+/*********************************************************************************************
+ *
+ * 'WorkspacePreferences.java, in plugin msi.gama.application, is part of the source code of the
+ * GAMA modeling and simulation platform.
+ * (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC & Partners
+ *
+ * Visit https://github.com/gama-platform/gama for license information and developers contact.
+ * 
+ *
+ **********************************************************************************************/
 package msi.gama.application.workspace;
 
 import java.io.File;
