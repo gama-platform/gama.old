4a3b94611d1e9a7faf95bd98036dbc47756507ae
AlexisDrogoul <alexis.drogoul@gmail.com>
2016-12-06 05:46:41 +0100

    Fixes #1176 by implementing the new 'benchmark' statement.
    Signed-off-by: AlexisDrogoul <alexis.drogoul@gmail.com>

---
+++ msi.gama.core/src/msi/gaml/statements/BenchmarkStatement.java
@@ -0,0 +1,100 @@
+/*********************************************************************************************
+ *
+ * 'TraceStatement.java, in plugin msi.gama.core, is part of the source code of the GAMA modeling and simulation
+ * platform. (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC & Partners
+ *
+ * Visit https://github.com/gama-platform/gama for license information and developers contact.
+ * 
+ *
+ **********************************************************************************************/
+package msi.gaml.statements;
+
+import msi.gama.common.interfaces.IKeyword;
+import msi.gama.precompiler.GamlAnnotations.doc;
+import msi.gama.precompiler.GamlAnnotations.facet;
+import msi.gama.precompiler.GamlAnnotations.facets;
+import msi.gama.precompiler.GamlAnnotations.inside;
+import msi.gama.precompiler.GamlAnnotations.symbol;
+import msi.gama.precompiler.IConcept;
+import msi.gama.precompiler.ISymbolKind;
+import msi.gama.runtime.IScope;
+import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gaml.descriptions.IDescription;
+import msi.gaml.expressions.IExpression;
+import msi.gaml.operators.Cast;
+import msi.gaml.types.IType;
+
+/**
+ * Class TraceStatement.
+ * 
+ * @author drogoul
+ * @since 23 févr. 2014
+ * 
+ */
+@symbol (
+		name = "benchmark",
+		kind = ISymbolKind.SEQUENCE_STATEMENT,
+		with_sequence = true,
+		concept = { IConcept.TEST })
+@facets (
+		omissible = IKeyword.MESSAGE,
+		value = { @facet (
+				name = IKeyword.MESSAGE,
+				type = IType.NONE,
+				optional = true,
+				doc = @doc ("A message to display alongside the results. Should concisely describe the contents of the benchmark")),
+				@facet (
+						name = IKeyword.REPEAT,
+						type = IType.INT,
+						optional = true,
+						doc = @doc ("An int expression describing how many executions of the block must be handled. The output in this case will return the min, max and average durations")) })
+@inside (
+		kinds = { ISymbolKind.BEHAVIOR, ISymbolKind.SEQUENCE_STATEMENT, ISymbolKind.LAYER })
+@doc (
+		value = "Displays in the console the duration in ms of the execution of the statements included in the block. It is possible to indicate, with the 'repeat' facet, how many times the sequence should be run")
+public class BenchmarkStatement extends AbstractStatementSequence {
+
+	final IExpression repeat, message;
+
+	/**
+	 * @param desc
+	 */
+	public BenchmarkStatement(final IDescription desc) {
+		super(desc);
+		repeat = getFacet(IKeyword.REPEAT);
+		message = getFacet(IKeyword.MESSAGE);
+	}
+
+	@Override
+	public Object privateExecuteIn(final IScope scope) throws GamaRuntimeException {
+		final int repeatTimes = repeat == null ? 1 : Cast.asInt(scope, repeat.value(scope));
+		double min = Long.MAX_VALUE;
+		int timeOfMin = 0;
+		double max = Long.MIN_VALUE;
+		int timeOfMax = 0;
+		double total = 0;
+
+		for (int i = 0; i < repeatTimes; i++) {
+			final long begin = System.nanoTime();
+			super.privateExecuteIn(scope);
+			final long end = System.nanoTime();
+			final double duration = (end - begin) / 1000000d;
+			if (min > duration) {
+				min = duration;
+				timeOfMin = i;
+			}
+			if (max < duration) {
+				max = duration;
+				timeOfMax = i;
+			}
+			total += duration;
+		}
+		final String title = message == null ? "Execution time " : Cast.asString(scope, message.value(scope));
+		final String result = title + " (over " + repeatTimes + " iteration(s)): min = " + min + " ms (iteration #"
+				+ timeOfMin + ") | max = " + max + " ms (iteration #" + timeOfMax + ") | average = "
+				+ total / repeatTimes + "ms";
+		scope.getGui().getConsole().informConsole(result, scope.getRoot(), null);
+		return result;
+	}
+
+}
