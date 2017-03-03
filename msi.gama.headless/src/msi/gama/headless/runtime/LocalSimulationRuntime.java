fb9448e92e1ce472ea189016f53162e510a15a9d
AlexisDrogoul <alexis.drogoul@gmail.com>
2017-03-03 10:59:46 +0700

    Should fix #2138 by bounding the number of CPU cores more clearly.
    Signed-off-by: AlexisDrogoul <alexis.drogoul@gmail.com>

--- msi.gama.headless/src/msi/gama/headless/runtime/LocalSimulationRuntime.java
+++ msi.gama.headless/src/msi/gama/headless/runtime/LocalSimulationRuntime.java
@@ -21,235 +21,205 @@ import msi.gama.runtime.HeadlessListener;
 import msi.gaml.descriptions.IDescription;
 
 public class LocalSimulationRuntime extends Observable implements SimulationRuntime, RuntimeContext {
-	private Map<String, ExperimentJob> simulations;
-	private ArrayList<FakeApplication> queue ;
-	private ArrayList<FakeApplication> started ;
-	private HashMap<String,ArrayList<IModel> >loadedModels;
-	private HashMap<String,ArrayList<IModel>> availableLoadedModels;
-	private int allocatedProcessor ;
+	private final Map<String, ExperimentJob> simulations;
+	private final ArrayList<FakeApplication> queue;
+	private final ArrayList<FakeApplication> started;
+	private final HashMap<String, ArrayList<IModel>> loadedModels;
+	private final HashMap<String, ArrayList<IModel>> availableLoadedModels;
+	private final int allocatedProcessor;
 	private boolean isTraceKept;
-	
-	public boolean isTraceKept()
-	{
-		return this.isTraceKept;
-	}
-	public void keepTrace(boolean t)
-	{
-		this.isTraceKept=t;
-	}
-	public int getAllocatedProcessor() {
-		return allocatedProcessor;
+
+	public LocalSimulationRuntime() {
+		this(UNDEFINED_QUEUE_SIZE);
 	}
 
-	public void setAllocatedProcessor(int allocatedProcessor) {
-		this.allocatedProcessor = allocatedProcessor;
-	}
-	
-	public LocalSimulationRuntime(int proc) {
+	public LocalSimulationRuntime(final int numberOfCoresAsked) {
 		simulations = new HashMap<String, ExperimentJob>();
 		queue = new ArrayList<FakeApplication>();
 		started = new ArrayList<FakeApplication>();
-		loadedModels = new HashMap<String,ArrayList<IModel>>();
-		availableLoadedModels = new HashMap<String,ArrayList<IModel>>();
-		if(proc == UNDEFINED_QUEUE_SIZE)
-		{
-			proc = getSatisfiedThreads();
-		}
-		this.allocatedProcessor=proc;
+		loadedModels = new HashMap<String, ArrayList<IModel>>();
+		availableLoadedModels = new HashMap<String, ArrayList<IModel>>();
+		this.allocatedProcessor = getAvailableCores(numberOfCoresAsked);
 	}
-	
-	public LocalSimulationRuntime() {
-		simulations = new HashMap<String, ExperimentJob>();
-		queue = new ArrayList<FakeApplication>();
-		started = new ArrayList<FakeApplication>();
-		loadedModels = new HashMap<String,ArrayList<IModel>>();
-		availableLoadedModels = new HashMap<String,ArrayList<IModel>>();
-		this.allocatedProcessor=getSatisfiedThreads() ; //UNDEFINED_QUEUE_SIZE;
+
+	private static int getAvailableCores(final int asked) {
+		final int max = Runtime.getRuntime().availableProcessors();
+		final int cpus = Math.max(1, Math.min(max, asked));
+		System.out.println("Number of cpus used:" + cpus + " (available: " + max + ")");
+		return cpus;
 	}
-	
-	private static int getSatisfiedThreads() {
-		int cpus = Runtime.getRuntime().availableProcessors();
-		System.out.println("cpus :" + cpus);
-		int maxThreads = cpus;
-		maxThreads = (maxThreads > 0 ? maxThreads : 1);
-		return maxThreads;
-	}
-	
-	public void listenMe(Observer v)
-	{
+
+	public void listenMe(final Observer v) {
 		this.addObserver(v);
 	}
 
-	public void pushSimulation(ExperimentJob s) {
+	@Override
+	public boolean isTraceKept() {
+		return this.isTraceKept;
+	}
+
+	@Override
+	public void keepTrace(final boolean t) {
+		this.isTraceKept = t;
+	}
+
+	@Override
+	public void pushSimulation(final ExperimentJob s) {
 		simulations.put(s.getExperimentID(), s);
-		FakeApplication f = new FakeApplication(s,this);
-		if(started.size() < allocatedProcessor || allocatedProcessor==UNDEFINED_QUEUE_SIZE)
+		final FakeApplication f = new FakeApplication(s, this);
+		if (started.size() < allocatedProcessor)
 			this.startSimulation(f);
 		else
 			queue.add(f);
 	}
 
-	private void startSimulation(FakeApplication s)
-	{
+	private void startSimulation(final FakeApplication s) {
 		started.add(s);
 		s.start();
 		this.notifyListener();
 	}
-	
-	public void closeSimulation(FakeApplication s)
-	{
+
+	public void closeSimulation(final FakeApplication s) {
 		started.remove(s);
-		if(queue.size() > 0)
-		{
-			FakeApplication p = queue.get(0);
+		if (queue.size() > 0) {
+			final FakeApplication p = queue.get(0);
 			queue.remove(p);
 			this.startSimulation(p);
 		}
-		if(!this.isTraceKept)
+		if (!this.isTraceKept)
 			simulations.remove(s.getExperimentJob().getExperimentID());
 		this.notifyListener();
 	}
 
-
-	private void notifyListener()
-	{
+	private void notifyListener() {
 		this.setChanged();
 		this.notifyObservers();
 	}
-	
-	public SimulationState getSimulationState(String id) {
-		ExperimentJob tmp = simulations.get(id);
-		if(tmp ==null)
+
+	@Override
+	public SimulationState getSimulationState(final String id) {
+		final ExperimentJob tmp = simulations.get(id);
+		if (tmp == null)
 			return SimulationState.UNDEFINED;
-		if(started.contains(tmp))
+		if (started.contains(tmp))
 			return SimulationState.STARTED;
-		if(queue.contains(tmp))
+		if (queue.contains(tmp))
 			return SimulationState.ENQUEUED;
 		return SimulationState.ACHIEVED;
 	}
-	public boolean isPerformingSimulation()
-	{
+
+	@Override
+	public boolean isPerformingSimulation() {
 		return started.size() > 0 || queue.size() > 0;
 	}
-	
-	public synchronized void releaseModel(String key, IModel mdl)
-	{
-		//System.out.println("release simulation");
-		//String key = mdl.getFilePath();
-		//availableLoadedModels.get(key).add(mdl);
-		//System.out.println("remove " + mdl.getFilePath());
-		//lockUnLock(null,key, mdl);
-	//	System.out.println("model released ") ;
+
+	public synchronized void releaseModel(final String key, final IModel mdl) {
+		// System.out.println("release simulation");
+		// String key = mdl.getFilePath();
+		// availableLoadedModels.get(key).add(mdl);
+		// System.out.println("remove " + mdl.getFilePath());
+		// lockUnLock(null,key, mdl);
+		// System.out.println("model released ") ;
 	}
-	
-	private synchronized IModel lockUnLock(File fl, String key, IModel mdl) throws IOException
-	{
-		IModel mm =null;
-		if(mdl!=null)
-		{
+
+	private synchronized IModel lockUnLock(final File fl, final String key, final IModel mdl) throws IOException {
+		IModel mm = null;
+		if (mdl != null) {
 			availableLoadedModels.get(key).add(mdl);
-			mm= mdl;
+			mm = mdl;
 		}
-		if(fl != null)
-		{
-			mm= lockModel(fl);
+		if (fl != null) {
+			mm = lockModel(fl);
 		}
 		return mm;
 	}
-	
-	public synchronized IModel lockModel(File fl) throws IOException
-	{
+
+	public synchronized IModel lockModel(final File fl) throws IOException {
 		IModel mdl;
-		String key=fl.getAbsolutePath();
+		final String key = fl.getAbsolutePath();
 		ArrayList<IModel> arr = availableLoadedModels.get(fl.getAbsolutePath());
 		System.out.println(fl.getAbsolutePath());
-		if(arr ==null)
-		{
+		if (arr == null) {
 			arr = new ArrayList<IModel>();
 			availableLoadedModels.put(key, arr);
 			loadedModels.put(key, new ArrayList<IModel>());
 		}
-		if(arr.size() == 0)
-		{
-			mdl =HeadlessSimulationLoader.loadModel(fl);
+		if (arr.size() == 0) {
+			mdl = HeadlessSimulationLoader.loadModel(fl);
 			loadedModels.get(key).add(mdl);
-		}
-		else
-		{
+		} else {
 			mdl = arr.get(0);
 			arr.remove(0);
 		}
 		return mdl;
 	}
-	
-	public synchronized IModel loadModel(File fl) throws IOException
-	{
-		//return lockUnLock( fl,null, null) ; //lockModel(fl); //
-		return HeadlessSimulationLoader.loadModel(fl); //lockModel(fl); //mdl.c;
+
+	@Override
+	public synchronized IModel loadModel(final File fl) throws IOException {
+		// return lockUnLock( fl,null, null) ; //lockModel(fl); //
+		return HeadlessSimulationLoader.loadModel(fl); // lockModel(fl); //mdl.c;
 	}
 
-	public IExperimentPlan buildExperimentPlan(String expName, IModel mdl)
-	{
-		IDescription des = mdl.getExperiment(expName).getDescription();
-		IExperimentPlan expp= new ExperimentPlan(des);
+	@Override
+	public IExperimentPlan buildExperimentPlan(final String expName, final IModel mdl) {
+		final IDescription des = mdl.getExperiment(expName).getDescription();
+		final IExperimentPlan expp = new ExperimentPlan(des);
 		expp.setModel(mdl);
 		return expp;
 	}
-	
+
 	class FakeApplication extends Thread {// implements Runnable {
 
 		private ExperimentJob si = null;
 		private LocalSimulationRuntime runtime = null;
 
-		ExperimentJob getExperimentJob()
-		{
+		ExperimentJob getExperimentJob() {
 			return si;
 		}
+
 		public FakeApplication(final ExperimentJob sim, final LocalSimulationRuntime rn) {
 			si = sim;
-			this.runtime= rn;
+			this.runtime = rn;
 		}
+
 		@Override
 		public void run() {
 			try {
-				BufferedWriter file = new BufferedWriter(new FileWriter(Globals.OUTPUT_PATH + "/" + Globals.CONSOLE_OUTPUT_FILENAME+"-"+si.getExperimentID()+".txt"));
+				final BufferedWriter file = new BufferedWriter(new FileWriter(Globals.OUTPUT_PATH + "/"
+						+ Globals.CONSOLE_OUTPUT_FILENAME + "-" + si.getExperimentID() + ".txt"));
 				((HeadlessListener) GAMA.getHeadlessGui()).registerJob(file);
-			} catch (IOException e1) {
+			} catch (final IOException e1) {
 				e1.printStackTrace();
 			}
 			try {
-					si.loadAndBuild(this.runtime);
-				
-			} catch (InstantiationException e) {
+				si.loadAndBuild(this.runtime);
+
+			} catch (final InstantiationException e) {
 				e.printStackTrace();
-			} catch (IllegalAccessException e) {
+			} catch (final IllegalAccessException e) {
 				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
+			} catch (final ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (final IOException e) {
 				e.printStackTrace();
 			}
-				catch (IOException e) {
-					e.printStackTrace();
-				}
 			si.playAndDispose();
 			((HeadlessListener) GAMA.getHeadlessGui()).leaveJob();
-			runtime.closeSimulation(this); 
-			runtime.releaseModel(si.getSourcePath(),si.getSimulation().getModel());
-			
+			runtime.closeSimulation(this);
+			runtime.releaseModel(si.getSourcePath(), si.getSimulation().getModel());
+
 		}
 
 	}
 
 	@Override
 	public HashMap<String, Double> getSimulationState() {
-		
-		HashMap<String,Double > res = new HashMap<String,Double>();
-		for(ExperimentJob exp :simulations.values())
-		{
-			res.put(exp.getExperimentID(), new Double(exp.getStep()/exp.getFinalStep()));
+
+		final HashMap<String, Double> res = new HashMap<String, Double>();
+		for (final ExperimentJob exp : simulations.values()) {
+			res.put(exp.getExperimentID(), new Double(exp.getStep() / exp.getFinalStep()));
 		}
 		return res;
 	}
 
-
-	
 }
