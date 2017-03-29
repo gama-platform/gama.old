001d240596715c3fd1c13c205eaecfc71cd9a1cb
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-29 17:58:27 +0700

    add cron'

--- prepare.sh
+++ prepare.sh
@@ -2,19 +2,30 @@
 
 MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 echo $MESSAGE
-if  [[ ${MESSAGE} == *"ci deploy"* ]]; then		
-		if  [[ ${MESSAGE} == *"ci clean"* ]]; then
-				echo "Cleaning p2 update site"		
-				sshpass -e ssh gamaws@51.255.46.42 /var/www/gama_updates/clean.sh
-		fi		
-        echo "Deploy to p2 update site"		
-		sh ./publish.sh
-else
-        echo "Build GAMA project"		
-		sh ./build.sh
-fi
+if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
 
-if  [[ ${MESSAGE} == *"ci release"* ]]; then	
-		echo "Upload continuos release to github nothing"		
-		bash ./github-release.sh "$TRAVIS_COMMIT" 
-fi	
+	echo "Build GAMA project"		
+	sh ./build.sh			
+	echo "Deploy to p2 update site"		
+	sh ./publish.sh
+	echo "Upload continuos release to github nothing"		
+	bash ./github-release.sh "$TRAVIS_COMMIT" 
+	
+else
+	if  [[ ${MESSAGE} == *"ci deploy"* ]]; then		
+			if  [[ ${MESSAGE} == *"ci clean"* ]]; then
+					echo "Cleaning p2 update site"		
+					sshpass -e ssh gamaws@51.255.46.42 /var/www/gama_updates/clean.sh
+			fi		
+			echo "Deploy to p2 update site"		
+			sh ./publish.sh
+	else
+			echo "Build GAMA project"		
+			sh ./build.sh
+	fi
+
+	if  [[ ${MESSAGE} == *"ci release"* ]]; then	
+			echo "Upload continuos release to github nothing"		
+			bash ./github-release.sh "$TRAVIS_COMMIT" 
+	fi	
+fi
