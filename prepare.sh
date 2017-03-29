f4c97bd54d0d15e30cf5625bbb6b78615d1651f6
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-29 14:16:40 +0700

    ci clean ci deploy ci release

---
+++ prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+echo $MESSAGE
+if  [[ ${MESSAGE} == *"ci deploy"* ]]; then		
+		if  [[ ${MESSAGE} == *"ci clean"* ]]; then
+				echo "Cleaning p2 update site"		
+				sshpass -e ssh gamaws@51.255.46.42 /var/www/gama_updates/clean.sh
+		fi
+        echo "Deploy to p2 update site"		
+		sh ./publish.sh
+else
+        echo "Build GAMA project"		
+		sh ./build.sh
+fi
+
+
+if  [[ ${MESSAGE} == *"ci release"* ]]; then		
+		bash ./github-release.sh "$TRAVIS_COMMIT" 
+fi
+echo "do nothing"		
--- publish.sh
+++ publish.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd ummisco.gama.annotations &&
-mvn -q deploy -B -DuniqueVersion=false -DcreateChecksum=true --settings ../settings.xml  && 
-cd - &&
-cd msi.gama.processor &&
-mvn -q deploy -B -DuniqueVersion=false -DcreateChecksum=true --settings ../settings.xml  &&
-cd - &&
-cd msi.gama.parent &&
-mvn -q deploy -B -DuniqueVersion=false -DcreateChecksum=true --settings ../settings.xml &&
+mvn clean install -P p2Repo --settings ../settings.xml && 
 cd -
+
+cd msi.gama.processor &&
+mvn clean install -P p2Repo --settings ../settings.xml && 
+cd -
+
+cd msi.gama.parent &&
+mvn clean install -P p2Repo --settings ../settings.xml && 
+cd -
+
 
