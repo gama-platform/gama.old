b709a70d4c806f9944d333964a39999a5afadd5b
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-29 16:45:33 +0700

    Working with Travis CI, sorry for the inconvenience!! ci clean ci deploy

--- publish.sh
+++ publish.sh
@@ -2,11 +2,9 @@
 cd ummisco.gama.annotations &&
 mvn clean deploy -P p2Repo --settings ../settings.xml && 
 cd -
-
 cd msi.gama.processor &&
 mvn clean deploy -P p2Repo --settings ../settings.xml && 
 cd -
-
 cd msi.gama.parent &&
 mvn clean deploy -P p2Repo --settings ../settings.xml && 
 cd -
