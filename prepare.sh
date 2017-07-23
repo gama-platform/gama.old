7ba37a69cab35f822bfb1994dda84e34851747f5
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-07-24 00:29:39 +0700

    test travis, sorry for the spam

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,7 @@ commit_website_files() {
 	git config --global user.name "Travis CI"
 	git config --global push.default simple		
 	git clone --depth=50 --branch=master https://github.com/gama-platform/gama.wiki.git ../gama-platform/gama.wiki
-	cd /home/travis/build/gama-platform/gama/msi.gama.documentation
+	ls /home/travis/build/gama-platform/gama/
 	java -classpath "libs/jdom-2.0.1.jar;target/classes;../ummisco.gama.annotations/target/classes" msi.gama.doc.MainGenerateWiki	
 }
 
