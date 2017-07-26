b2c8cfe5d398cc30503ab75f79f5ab0992605017
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-07-26 14:46:32 +0700

    test travis, sorry for the spam

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@ cd ummisco.gama.annotations &&
 mvn -X clean install &&
 cd - &&
 cd msi.gama.processor &&
-mvn -X clean install &&
+mvn -q clean install &&
 cd - &&
 cd msi.gama.parent &&
-mvn -X clean compile && 
+mvn -q clean compile && 
 cd -
