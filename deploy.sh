36aceb10a08289b2752616d6830d058eebd71d2d
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-08-02 14:01:08 +0700

    try to fix travis again

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd msi.gama.processor &&
 mvn -q clean deploy -P p2Repo --settings ../settings.xml && 
 cd -
 cd msi.gama.parent &&
-mvn clean deploy -P p2Repo --settings ../settings.xml && 
+mvn -X clean deploy -P p2Repo --settings ../settings.xml && 
 cd -
 
 
