aaf6c4b1b8a70cb1d09417c1a5b1d5f5cab1fb9f
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-08-03 16:20:23 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,17 +3,17 @@
 compile (){
 	echo "Compile GAMA project"			
 	cd ummisco.gama.annotations &&
-	mvn clean install -DskipTests -T 8C &&
+	mvn clean install -DskipTests -T 80C &&
 	cd - &&
 	cd msi.gama.processor &&
-	mvn clean install -DskipTests -T 8C &&
+	mvn clean install -DskipTests -T 80C &&
 	cd - &&
 	cd msi.gama.parent &&
 	
 	if  [[ $MSG == *"ci debug"* ]]; then		
-		mvn -X clean compile -DskipTests -T 8C
+		mvn -X clean compile -DskipTests -T 80C
 	else
-		mvn clean compile -DskipTests -T 8C
+		mvn clean compile -DskipTests -T 80C
 	fi
 		
 	cd -
