85b508549c08f9cf6655147ed0a80bb9a4053327
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-09-26 17:27:47 +0700

    try to fix travis, sorry for the spam mails

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
-cd ummisco.gama.annotations &&
-mvn -q clean install &&
-cd - &&
-cd msi.gama.processor &&
-mvn -q clean install &&
-cd - &&
-cd msi.gama.parent &&
-mvn -q clean install &&
+cd ummisco.gama.annotations
+mvn -q clean install
+cd - 
+cd msi.gama.processor 
+mvn -q clean install 
+cd - 
+change=$(git log --pretty=format: --name-only --since="1 hour ago")
+if [[ ${change} == *"msi.gama.ext"* ]]; then		
+	cd msi.gama.ext 
+	mvn -q clean install 
+	cd -
+fi
+cd msi.gama.parent 
+mvn -q clean install 
 cd -
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-cd ummisco.gama.annotations && 
-mvn deploy -DskipTests -T 8C -P p2Repo --settings ../settings.xml && 
+cd ummisco.gama.annotations 
+mvn deploy -DskipTests -T 8C -P p2Repo --settings ../settings.xml
 cd -
-cd msi.gama.processor &&
-mvn deploy -DskipTests -T 8C -P p2Repo --settings ../settings.xml && 
+cd msi.gama.processor
+mvn deploy -DskipTests -T 8C -P p2Repo --settings ../settings.xml
 cd -
-cd msi.gama.parent &&
-mvn deploy -DskipTests -T 8C -P p2Repo --settings ../settings.xml && 
+
+change=$(git log --pretty=format: --name-only --since="1 hour ago")
+if [[ ${change} == *"msi.gama.ext"* ]]; then		
+	cd msi.gama.ext 
+	mvn deploy -DskipTests -T 8C -P p2Repo --settings ../settings.xml
+	cd -
+fi
+cd msi.gama.parent
+mvn deploy -DskipTests -T 8C -P p2Repo --settings ../settings.xml
 cd -
 
 
