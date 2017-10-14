a1e42d4b599dcb760a585798a3acf91f5990bdd6
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-10-14 09:10:51 +0700

    deploy only modified plugins

--- compile.sh
+++ compile.sh
@@ -39,53 +39,53 @@ install (){
 	
 	change=$(git log --pretty=format: --name-only --since="1 hour ago")
 	
-	if [[ ${change} == *"ummisco.gama.annotations"* ]] || [[ $MSG == *"ci ummisco.gama.annotations"* ]]; then
+	if [[ ${change} == *"ummisco.gama.annotations"* ]] || [[ $MSG == *"ci ummisco.gama.annotations"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.annotations 
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
-	if [[ ${change} == *"msi.gama.processor"* ]] || [[ $MSG == *"ci msi.gama.processor"* ]]; then
+	if [[ ${change} == *"msi.gama.processor"* ]] || [[ $MSG == *"ci msi.gama.processor"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gama.processor 
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 		
 	
 	
-	if [[ ${change} == *"msi.gama.ext"* ]] || [[ $MSG == *"ci ext"* ]]; then
+	if [[ ${change} == *"msi.gama.ext"* ]] || [[ $MSG == *"ci ext"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gama.ext 
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 		cd ummisco.gama.feature.dependencies 
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 	
-	if [[ ${change} == *"msi.gama.core"* ]] || [[ $MSG == *"ci msi.gama.core"* ]]; then
+	if [[ ${change} == *"msi.gama.core"* ]] || [[ $MSG == *"ci msi.gama.core"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gama.core 
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 
 	
-	if [[ ${change} == *"msi.gama.lang.gaml"* ]] || [[ $MSG == *"ci msi.gama.lang.gaml"* ]]; then
+	if [[ ${change} == *"msi.gama.lang.gaml"* ]] || [[ $MSG == *"ci msi.gama.lang.gaml"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gama.lang.gaml
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 
 	
-	if [[ ${change} == *"msi.gama.documentation"* ]] || [[ $MSG == *"ci msi.gama.documentation"* ]]; then
+	if [[ ${change} == *"msi.gama.documentation"* ]] || [[ $MSG == *"ci msi.gama.documentation"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gama.documentation
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
@@ -93,9 +93,9 @@ install (){
 
 	
 	
-	if [[ ${change} == *"ummisco.gama.ui.modeling"* ]] || [[ $MSG == *"ci ummisco.gama.ui.modeling"* ]]; then
+	if [[ ${change} == *"ummisco.gama.ui.modeling"* ]] || [[ $MSG == *"ci ummisco.gama.ui.modeling"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.ui.modeling
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
@@ -104,80 +104,80 @@ install (){
 	
 	
 	
-	if [[ ${change} == *"ummisco.gama.ui.shared"* ]] || [[ $MSG == *"ci ummisco.gama.ui.shared"* ]]; then
+	if [[ ${change} == *"ummisco.gama.ui.shared"* ]] || [[ $MSG == *"ci ummisco.gama.ui.shared"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.ui.shared
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 
 	
-	if [[ ${change} == *"ummisco.gama.ui.navigator"* ]] || [[ $MSG == *"ci ummisco.gama.ui.navigator"* ]]; then
+	if [[ ${change} == *"ummisco.gama.ui.navigator"* ]] || [[ $MSG == *"ci ummisco.gama.ui.navigator"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.ui.navigator
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
-	if [[ ${change} == *"ummisco.gama.ui.experiment"* ]] || [[ $MSG == *"ci ummisco.gama.ui.experiment"* ]]; then
+	if [[ ${change} == *"ummisco.gama.ui.experiment"* ]] || [[ $MSG == *"ci ummisco.gama.ui.experiment"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.ui.experiment
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
-	if [[ ${change} == *"msi.gama.application"* ]] || [[ $MSG == *"ci msi.gama.application"* ]]; then
+	if [[ ${change} == *"msi.gama.application"* ]] || [[ $MSG == *"ci msi.gama.application"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gama.application
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 	
 	
-	if [[ ${change} == *"msi.gaml.extensions.fipa"* ]] || [[ $MSG == *"ci msi.gaml.extensions.fipa"* ]]; then
+	if [[ ${change} == *"msi.gaml.extensions.fipa"* ]] || [[ $MSG == *"ci msi.gaml.extensions.fipa"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gaml.extensions.fipa
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
-	if [[ ${change} == *"msi.gama.headless"* ]] || [[ $MSG == *"ci msi.gama.headless"* ]]; then
+	if [[ ${change} == *"msi.gama.headless"* ]] || [[ $MSG == *"ci msi.gama.headless"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gama.headless
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 	
-	if [[ ${change} == *"simtools.gaml.extensions.traffic"* ]] || [[ $MSG == *"ci simtools.gaml.extensions.traffic"* ]]; then
+	if [[ ${change} == *"simtools.gaml.extensions.traffic"* ]] || [[ $MSG == *"ci simtools.gaml.extensions.traffic"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd simtools.gaml.extensions.traffic
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
-	if [[ ${change} == *"simtools.gaml.extensions.physics"* ]] || [[ $MSG == *"ci simtools.gaml.extensions.physics"* ]]; then
+	if [[ ${change} == *"simtools.gaml.extensions.physics"* ]] || [[ $MSG == *"ci simtools.gaml.extensions.physics"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd simtools.gaml.extensions.physics
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
-	if [[ ${change} == *"irit.gaml.extensions.database"* ]] || [[ $MSG == *"ci irit.gaml.extensions.database"* ]]; then
+	if [[ ${change} == *"irit.gaml.extensions.database"* ]] || [[ $MSG == *"ci irit.gaml.extensions.database"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd irit.gaml.extensions.database
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
-	if [[ ${change} == *"msi.gama.models"* ]] || [[ $MSG == *"ci msi.gama.models"* ]]; then
+	if [[ ${change} == *"msi.gama.models"* ]] || [[ $MSG == *"ci msi.gama.models"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gama.models
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 		
 		cd ummisco.gama.feature.models
-		mvn clean install -T 8C
+		mvn clean install
 		cd -		
 		
 	fi
@@ -186,84 +186,84 @@ install (){
 	
 	
 	
-	if [[ ${change} == *"msi.gaml.architecture.simplebdi"* ]] || [[ $MSG == *"ci msi.gaml.architecture.simplebdi"* ]]; then
+	if [[ ${change} == *"msi.gaml.architecture.simplebdi"* ]] || [[ $MSG == *"ci msi.gaml.architecture.simplebdi"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd msi.gaml.architecture.simplebdi
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
-	if [[ ${change} == *"simtools.graphanalysis.fr"* ]] || [[ $MSG == *"ci simtools.graphanalysis.fr"* ]]; then
+	if [[ ${change} == *"simtools.graphanalysis.fr"* ]] || [[ $MSG == *"ci simtools.graphanalysis.fr"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd simtools.graphanalysis.fr
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 		cd simtools.graphlayout.feature
-		mvn clean install -T 8C
+		mvn clean install
 		cd -		
 	fi
 	
 	
-	if [[ ${change} == *"ummisco.gama.opengl"* ]] || [[ $MSG == *"ci ummisco.gama.opengl"* ]]; then
+	if [[ ${change} == *"ummisco.gama.opengl"* ]] || [[ $MSG == *"ci ummisco.gama.opengl"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.opengl
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 	
-	if [[ ${change} == *"ummisco.gama.java2d"* ]] || [[ $MSG == *"ci ummisco.gama.java2d"* ]]; then
+	if [[ ${change} == *"ummisco.gama.java2d"* ]] || [[ $MSG == *"ci ummisco.gama.java2d"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.java2d
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 	
-	if [[ ${change} == *"ummisco.gama.ui.viewers"* ]] || [[ $MSG == *"ci ummisco.gama.ui.viewers"* ]]; then
+	if [[ ${change} == *"ummisco.gama.ui.viewers"* ]] || [[ $MSG == *"ci ummisco.gama.ui.viewers"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.ui.viewers
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 	
 	
-	if [[ ${change} == *"ummisco.gama.serialize"* ]] || [[ $MSG == *"ci ummisco.gama.serialize"* ]]; then
+	if [[ ${change} == *"ummisco.gama.serialize"* ]] || [[ $MSG == *"ci ummisco.gama.serialize"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.serialize
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 		cd ummisco.gama.feature.serialize
-		mvn clean install -T 8C
+		mvn clean install
 		cd -		
 	fi
 	
 	
 	
-	if [[ ${change} == *"ummisco.gama.network"* ]] || [[ $MSG == *"ci ummisco.gama.network"* ]]; then
+	if [[ ${change} == *"ummisco.gama.network"* ]] || [[ $MSG == *"ci ummisco.gama.network"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.network
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 		cd ummisco.gama.feature.network
-		mvn clean install -T 8C
+		mvn clean install
 		cd -		
 	fi
 	
 	
-	if [[ ${change} == *"ummisco.gaml.extensions.maths"* ]] || [[ $MSG == *"ci ummisco.gaml.extensions.maths"* ]]; then
+	if [[ ${change} == *"ummisco.gaml.extensions.maths"* ]] || [[ $MSG == *"ci ummisco.gaml.extensions.maths"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gaml.extensions.maths
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
 	
 	
 	
-	if [[ ${change} == *"ummisco.gaml.extensions.sound"* ]] || [[ $MSG == *"ci ummisco.gaml.extensions.sound"* ]]; then
+	if [[ ${change} == *"ummisco.gaml.extensions.sound"* ]] || [[ $MSG == *"ci ummisco.gaml.extensions.sound"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gaml.extensions.sound
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 		cd ummisco.gama.feature.audio
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 		
 	fi
@@ -271,12 +271,12 @@ install (){
 	
 	
 	
-	if [[ ${change} == *"ummisco.gaml.extensions.stats"* ]] || [[ $MSG == *"ci ummisco.gaml.extensions.stats"* ]]; then
+	if [[ ${change} == *"ummisco.gaml.extensions.stats"* ]] || [[ $MSG == *"ci ummisco.gaml.extensions.stats"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gaml.extensions.stats
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 		cd ummisco.gama.feature.stats
-		mvn clean install -T 8C
+		mvn clean install
 		cd -		
 	fi
 	
@@ -284,9 +284,9 @@ install (){
 	
 	
 	
-	if [[ ${change} == *"msi.gama.core"* ]] || [[ ${change} == *"msi.gama.headless"* ]] || [[ ${change} == *"msi.gama.lang.gaml"* ]] || [[ ${change} == *"msi.gama.processor"* ]] || [[ ${change} == *"ummisco.gama.annotations"* ]] || [[ ${change} == *"ummisco.gama.annotations"* ]] || [[ $MSG == *"ci ummisco.gama.feature.core"* ]]; then
+	if [[ ${change} == *"msi.gama.core"* ]] || [[ ${change} == *"msi.gama.headless"* ]] || [[ ${change} == *"msi.gama.lang.gaml"* ]] || [[ ${change} == *"msi.gama.processor"* ]] || [[ ${change} == *"ummisco.gama.annotations"* ]] || [[ ${change} == *"ummisco.gama.annotations"* ]] || [[ $MSG == *"ci ummisco.gama.feature.core"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.feature.core
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
@@ -294,9 +294,9 @@ install (){
 	
 	
 	
-	if [[ ${change} == *"irit.gaml.extensions.database"* ]] || [[ ${change} == *"msi.gaml.extensions.fipa"* ]] || [[ ${change} == *"simtools.gaml.extensions.traffic"* ]] || [[ ${change} == *"msi.gaml.architecture.simplebdi"* ]] || [[ ${change} == *"ummisco.gaml.extensions.maths"* ]] || [[ ${change} == *"simtools.gaml.extensions.physics"* ]] || [[ ${change} == *"ummisco.gama.network"* ]] || [[ ${change} == *"ummisco.gama.serialize"* ]] || [[ $MSG == *"ci ummisco.gama.feature.core.extensions"* ]]; then
+	if [[ ${change} == *"irit.gaml.extensions.database"* ]] || [[ ${change} == *"msi.gaml.extensions.fipa"* ]] || [[ ${change} == *"simtools.gaml.extensions.traffic"* ]] || [[ ${change} == *"msi.gaml.architecture.simplebdi"* ]] || [[ ${change} == *"ummisco.gaml.extensions.maths"* ]] || [[ ${change} == *"simtools.gaml.extensions.physics"* ]] || [[ ${change} == *"ummisco.gama.network"* ]] || [[ ${change} == *"ummisco.gama.serialize"* ]] || [[ $MSG == *"ci ummisco.gama.feature.core.extensions"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.feature.core.extensions
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
@@ -305,9 +305,9 @@ install (){
 	
 	
 	
-	if [[ ${change} == *"msi.gama.application"* ]] || [[ ${change} == *"ummisco.gama.ui.shared"* ]] || [[ $MSG == *"ci ummisco.gama.feature.core.ui"* ]]; then
+	if [[ ${change} == *"msi.gama.application"* ]] || [[ ${change} == *"ummisco.gama.ui.shared"* ]] || [[ $MSG == *"ci ummisco.gama.feature.core.ui"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.feature.core.ui
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
@@ -316,9 +316,9 @@ install (){
 	
 	
 	
-	if [[ ${change} == *"ummisco.gama.java2d"* ]] || [[ ${change} == *"ummisco.gama.ui.experiment"* ]] || [[ ${change} == *"ummisco.gama.opengl"* ]] || [[ $MSG == *"ci ummisco.gama.feature.experiment.ui"* ]]; then
+	if [[ ${change} == *"ummisco.gama.java2d"* ]] || [[ ${change} == *"ummisco.gama.ui.experiment"* ]] || [[ ${change} == *"ummisco.gama.opengl"* ]] || [[ $MSG == *"ci ummisco.gama.feature.experiment.ui"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.feature.experiment.ui
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
@@ -327,9 +327,9 @@ install (){
 	
 	
 	
-	if [[ ${change} == *"ummisco.gama.ui.modeling"* ]] || [[ ${change} == *"ummisco.gama.ui.navigator"* ]] || [[ ${change} == *"ummisco.gama.ui.viewers"* ]] || [[ $MSG == *"ci ummisco.gama.feature.modeling.ui"* ]]; then
+	if [[ ${change} == *"ummisco.gama.ui.modeling"* ]] || [[ ${change} == *"ummisco.gama.ui.navigator"* ]] || [[ ${change} == *"ummisco.gama.ui.viewers"* ]] || [[ $MSG == *"ci ummisco.gama.feature.modeling.ui"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		cd ummisco.gama.feature.modeling.ui
-		mvn clean install -T 8C
+		mvn clean install
 		cd -
 	fi
 	
@@ -343,9 +343,9 @@ install (){
 	cd msi.gama.parent
 	
 	if  [[ $MSG == *"ci debug"* ]]; then		
-		mvn -e clean install -DskipTests -T 8C
+		mvn -e clean install -DskipTests
 	else
-		mvn clean install -DskipTests -T 8C
+		mvn clean install -DskipTests
 	fi
 		
 	cd -
