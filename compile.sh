5aef6ed97102c4488abd18fbba930f5dc4ff09d7
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-10-19 00:45:13 +0700

    redo models validation on travis

--- compile.sh
+++ compile.sh
@@ -42,7 +42,7 @@ install (){
 	echo "Install GAMA project"			
 	
 	
-	change=$(git log --pretty=format: --name-only --since="1 hour ago")
+	change=$(git log --pretty=format: --name-only --since="30 minute ago")
 	
 	if [[ ${change} == *"ummisco.gama.annotations"* ]] || [[ $MSG == *"ci ummisco.gama.annotations"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
 		mvn_install ummisco.gama.annotations 		
