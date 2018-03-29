7a51affac2030ed98e3c643741d1660c158daf9f
AlexisDrogoul <alexis.drogoul@gmail.com>
2018-03-29 18:18:45 +0700

    Should fix #2375 by keeping the selection in the wizard

--- ummisco.gama.ui.navigator/src/ummisco/gama/ui/wizards/ExportProjectWizard.java
+++ ummisco.gama.ui.navigator/src/ummisco/gama/ui/wizards/ExportProjectWizard.java
@@ -2,19 +2,20 @@ package ummisco.gama.ui.wizards;
 
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IExportWizard;
-import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
 
+import ummisco.gama.ui.navigator.contents.ResourceManager;
 import ummisco.gama.ui.resources.GamaIcons;
 
 public class ExportProjectWizard extends Wizard implements IExportWizard {
 
 	private static final String EXTERNAL_PROJECT_SECTION = "ExternalProjectExportWizard";//$NON-NLS-1$
 	private ExportProjectWizardPage mainPage;
-	private final IStructuredSelection currentSelection = null;
+	private IStructuredSelection currentSelection = null;
 	private String initialPath = null;
 
 	public ExportProjectWizard() {
@@ -38,7 +39,7 @@ public class ExportProjectWizard extends Wizard implements IExportWizard {
 	 */
 	@Override
 	public boolean performCancel() {
-//		mainPage.performCancel();
+		// mainPage.performCancel();
 		return true;
 	}
 
@@ -47,18 +48,25 @@ public class ExportProjectWizard extends Wizard implements IExportWizard {
 	 */
 	@Override
 	public boolean performFinish() {
-		return mainPage.finish();//createProjects();
+		return mainPage.finish();// createProjects();
 	}
 
 	@Override
-	public void init(final IWorkbench workbench, final IStructuredSelection currentSelection) {
+	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
 		setDefaultPageImageDescriptor(GamaIcons.create("navigator/navigator.import.project2").descriptor());
+		final Object[] all = selection.toArray();
+		for (int i = 0; i < all.length; i++) {
+			all[i] = ResourceManager.getResource(all[i]);
+		}
+		this.currentSelection = new StructuredSelection(all);
+
 	}
 
 	@Override
 	public void addPages() {
-//		mainPage = new ExportProjectWizardPage("wizardExternalProjectsPage", initialPath, currentSelection); //$NON-NLS-1$
-		mainPage=new ExportProjectWizardPage(currentSelection);
+		// mainPage = new ExportProjectWizardPage("wizardExternalProjectsPage", initialPath, currentSelection);
+		// //$NON-NLS-1$
+		mainPage = new ExportProjectWizardPage(currentSelection);
 		addPage(mainPage);
 	}
 
