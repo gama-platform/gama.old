379278f77260306f71e29a5bce099200a461475f
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2018-06-30 19:30:50 +0700

    ci docs

--- msi.gama.documentation/src/msi/gama/doc/transform/XmlToWiki.java
+++ msi.gama.documentation/src/msi/gama/doc/transform/XmlToWiki.java
@@ -44,8 +44,8 @@ public class XmlToWiki {
 
 		System.out.print("Creation of the wiki page for Operators.....");
 		createWiki(Constants.DOCGAMA_GLOBAL_FILE,
-				Constants.XSL_XML2WIKI_FOLDER + File.separator + "docGama-Operators-xml2md.xsl",
-				Constants.XML2WIKI_FOLDER + File.separator + "Operators"+suffix+".md");
+				Constants.XSL_XML2WIKI_FOLDER + File.separator + "docGama-OperatorsSplitted-xml2md.xsl",
+				Constants.XML2WIKI_FOLDER + File.separator + "OperatorsSplitted"+suffix+".md");
 		createWiki(Constants.DOCGAMA_GLOBAL_FILE,
 				Constants.XSL_XML2WIKI_FOLDER + File.separator + "docGama-OperatorsAA-xml2md.xsl",
 				Constants.WIKI_FOLDER_WIKI_ONLY + File.separator + "OperatorsAA"+suffix+".md");
