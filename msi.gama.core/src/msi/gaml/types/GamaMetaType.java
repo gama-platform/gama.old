eebe1dc5260ba525d31a1c287c1d5972e4e69166
AlexisDrogoul <alexis.drogoul@gmail.com>
2018-07-02 09:14:22 +0700

    Change the name "type" to "gaml_type" to avoid collisions

--- msi.gama.core/src/msi/gaml/types/GamaMetaType.java
+++ msi.gama.core/src/msi/gaml/types/GamaMetaType.java
@@ -9,14 +9,14 @@
  **********************************************************************************************/
 package msi.gaml.types;
 
-import msi.gama.common.interfaces.IKeyword;
 import msi.gama.precompiler.GamlAnnotations.doc;
+import msi.gama.precompiler.GamlAnnotations.operator;
 import msi.gama.precompiler.GamlAnnotations.type;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 
 @type (
-		name = IKeyword.TYPE,
+		name = "gaml_type",
 		id = IType.TYPE,
 		wraps = { IType.class },
 		doc = @doc ("Metatype of all types in GAML"))
@@ -30,12 +30,24 @@ public class GamaMetaType extends GamaType<IType<?>> {
 	@Override
 	public IType<?> cast(final IScope scope, final Object obj, final Object param, final boolean copy)
 			throws GamaRuntimeException {
+		return staticCast(obj);
+	}
+
+	public static IType<?> staticCast(final Object obj) {
 		return GamaType.of(obj);
 	}
 
 	@Override
 	public IType<?> getDefault() {
 		return Types.NO_TYPE;
+	}
+
+	@operator (
+			value = { "type_of" },
+			can_be_const = true,
+			doc = @doc ("Returns the GAML type of the operand"))
+	public static IType<?> typeOf(final IScope scope, final Object obj) {
+		return staticCast(obj);
 	}
 
 }
