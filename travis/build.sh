25b8d92b5229536bc0729fdcc78eb08f00fab748
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2018-09-04 21:54:53 +0700

    try with eclipse compiler

--- travis/build.sh
+++ travis/build.sh
@@ -1,9 +1,9 @@
 cd ummisco.gama.annotations
-mvn -q clean install
+mvn clean install
 cd - 
 cd msi.gama.processor 
-mvn -q clean install
+mvn clean install
 cd - 
 cd msi.gama.parent 
-mvn -q clean install
+mvn clean install
 cd -
