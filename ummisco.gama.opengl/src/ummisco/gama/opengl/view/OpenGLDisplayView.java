19e151912132c56e11011e39f0d06f91fab1bb6f
AlexisDrogoul <alexis.drogoul@gmail.com>
2019-05-03 13:36:04 +0700

    Addresses #2628. Please test the fix on HiDPI Windows + Linux machines

--- ummisco.gama.opengl/src/ummisco/gama/opengl/view/OpenGLDisplayView.java
+++ ummisco.gama.opengl/src/ummisco/gama/opengl/view/OpenGLDisplayView.java
@@ -4,7 +4,7 @@
  * simulation platform. (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC & Partners
  *
  * Visit https://github.com/gama-platform/gama for license information and developers contact.
- * 
+ *
  *
  **********************************************************************************************/
 package ummisco.gama.opengl.view;
