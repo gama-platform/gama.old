gama-platform/gama.old

commit 0072d950a4a6c49b918366a2e21749061e9c93fb

headless bug fix

msi.gama.core/src/msi/gama/kernel/experiment/ExperimentAgent.java
@@ -246,7 +246,7 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 	}
 
 	protected boolean automaticallyCreateFirstSimulation() {
-		return !getSpecies().isHeadless() || ((GamlSpecies) this.getSpecies()).belongsToAMicroModel();
+		return /*!getSpecies().isHeadless() ||*/ ((GamlSpecies) this.getSpecies()).belongsToAMicroModel();
 	}
 
 	@Override
@@ -354,7 +354,7 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 	 */
 
 	public List<? extends IParameter.Batch> getDefaultParameters() {
-		if (!GamaPreferences.External.CORE_RND_EDITABLE.getValue()) { return new ArrayList<>(); }
+		if (!(getSpecies().isHeadless()) && !GamaPreferences.External.CORE_RND_EDITABLE.getValue()) { return new ArrayList<>(); }
 		final List<ExperimentParameter> params = new ArrayList<>();
 		final String cat = getExperimentParametersCategory();
 		ExperimentParameter p = new ExperimentParameter(getScope(), getSpecies().getVar(IKeyword.RNG),

msi.gama.core/src/msi/gama/kernel/experiment/ExperimentPlan.java
@@ -15,6 +15,7 @@ import static msi.gama.common.interfaces.IKeyword.TEST;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -379,6 +380,22 @@ public class ExperimentPlan extends GamlSpecies implements IExperimentPlan {
 		// DEBUG.LOG("ExperimentPlan.dipose END");
 	}
 
+	
+	public void createAgent(Double seed) {
+		final ExperimentPopulation pop = new ExperimentPopulation(this);
+		final IScope scope = getExperimentScope();
+		pop.initializeFor(scope);
+		List<Map<String, Object>> params =
+				seed == null ? Collections.EMPTY_LIST : Arrays.asList(new HashMap<String, Object>() {
+					{
+						put(IKeyword.SEED, seed);
+					}
+				});
+		agent = pop.createAgents(scope, 1, params, false, true).get(0);
+		addDefaultParameters();
+	}
+
+/*	
 	public void createAgent() {
 		final ExperimentPopulation pop = new ExperimentPopulation(this);
 		final IScope scope = getExperimentScope();
@@ -386,7 +403,7 @@ public class ExperimentPlan extends GamlSpecies implements IExperimentPlan {
 		agent = (ExperimentAgent) pop.createAgents(scope, 1, Collections.EMPTY_LIST, false, true).get(0);
 		addDefaultParameters();
 	}
-
+*/
 	@Override
 	public IModel getModel() {
 		return model;
@@ -493,7 +510,28 @@ public class ExperimentPlan extends GamlSpecies implements IExperimentPlan {
 		log = new FileOutput(output.getLiteral(IKeyword.TO), dataString, new ArrayList(parameters.keySet()), this);
 	}
 
+	
+	
+	public synchronized void open(Double seed) {
+
+		createAgent(seed);
+		myScope.getGui().prepareForExperiment(myScope, this);
+		agent.schedule(agent.getScope());
+		if (isBatch()) {
+			agent.getScope().getGui().getStatus(agent.getScope())
+					.informStatus(isTest() ? "Tests ready. Click run to begin." : " Batch ready. Click run to begin.");
+			agent.getScope().getGui().updateExperimentState(agent.getScope());
+		}
+	}
+
 	@Override
+	public synchronized void open() {
+		open(null);
+	}
+	
+	
+	
+	/*@Override
 	public synchronized void open() {
 
 		createAgent();
@@ -505,7 +543,7 @@ public class ExperimentPlan extends GamlSpecies implements IExperimentPlan {
 			agent.getScope().getGui().updateExperimentState(agent.getScope());
 		}
 	}
-
+*/
 	@Override
 	public void reload() {
 		if (isBatch()) {

msi.gama.core/src/msi/gama/runtime/GAMA.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 import msi.gama.common.interfaces.IBenchmarkable;
 import msi.gama.common.interfaces.IGui;
+import msi.gama.common.interfaces.IKeyword;
 import msi.gama.common.preferences.GamaPreferences;
 import msi.gama.kernel.experiment.ExperimentAgent;
 import msi.gama.kernel.experiment.ExperimentPlan;
@@ -147,8 +148,13 @@ public class GAMA {
 			}
 
 		}
-		currentExperiment.open();
-		if (seed != null) {
+		
+		
+		System.out.println("coucou "+ seed);
+		
+		//currentExperiment.setParameterValueByTitle(currentExperiment.getExperimentScope(), IKeyword.SEED,seed);
+		currentExperiment.open(seed);
+		/*if (seed != null) {
 			currentExperiment.getAgent().setSeed(seed);
 		}
 
@@ -156,7 +162,7 @@ public class GAMA {
 		if (!currentExperiment.isBatch()) {
 			currentExperiment.getAgent().createSimulation(new ParametersSet(), true);
 		}
-
+*/
 		controllers.add(currentExperiment.getController());
 		return currentExperiment;
 

msi.gama.headless/src/msi/gama/headless/core/Experiment.java
@@ -97,10 +97,11 @@ public class Experiment implements IExperiment {
 			currentExperiment.getController().getScheduler().paused = false;
 			currentExperiment.getAgent().step(currentExperiment.getAgent().getScope());
 		} else {
-			currentExperiment.getAgent().getSimulation().step(this.getScope());
+			currentExperiment.getAgent().step(this.getScope());
+			//currentExperiment.getAgent().getSimulation().step(this.getScope());
 		}
 		return currentStep++;
-
+ 
 	}
 
 	@Override

commit 746cab16b8c58ae7d4f4c6a056318d2879fdf8ae

Fixes a problem introduced in 0072d950a4a6c49b918366a2e21749061e9c93fb

msi.gama.core/src/msi/gama/kernel/experiment/ExperimentAgent.java
@@ -2,11 +2,11 @@
  *
  * msi.gama.kernel.experiment.ExperimentAgent.java, in plugin msi.gama.core, is part of the source code of the GAMA
  * modeling and simulation platform (v. 1.8)
- * 
+ *
  * (c) 2007-2018 UMI 209 UMMISCO IRD/SU & Partners
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
- * 
+ *
  ********************************************************************************************************/
 package msi.gama.kernel.experiment;
 
@@ -53,7 +53,6 @@ import msi.gama.util.GamaColor;
 import msi.gama.util.GamaListFactory;
 import msi.gama.util.IList;
 import msi.gama.util.TOrderedHashMap;
-import msi.gaml.species.GamlSpecies;
 import msi.gaml.species.ISpecies;
 import msi.gaml.statements.IExecutable;
 import msi.gaml.types.GamaGeometryType;
@@ -185,9 +184,9 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 	}
 
 	public String getDefinedRng() {
-		if (GamaPreferences.External.CORE_RND_EDITABLE
-				.getValue()) { return (String) ((ExperimentPlan) getSpecies()).parameters.get(IKeyword.RNG)
-						.value(ownScope); }
+		if (GamaPreferences.External.CORE_RND_EDITABLE.getValue()) {
+			return (String) ((ExperimentPlan) getSpecies()).parameters.get(IKeyword.RNG).value(ownScope);
+		}
 		return GamaPreferences.External.CORE_RNG.getValue();
 	}
 
@@ -231,7 +230,7 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 
 	/**
 	 * Redefinition of the callback method
-	 * 
+	 *
 	 * @see msi.gama.metamodel.agent.GamlAgent#_init_(msi.gama.runtime.IScope)
 	 */
 	@Override
@@ -246,7 +245,8 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 	}
 
 	protected boolean automaticallyCreateFirstSimulation() {
-		return /*!getSpecies().isHeadless() ||*/ ((GamlSpecies) this.getSpecies()).belongsToAMicroModel();
+		// Now creates simulations by default, even for the headless experiments (see #2654)
+		return true; // !getSpecies().isHeadless() || ((GamlSpecies) this.getSpecies()).belongsToAMicroModel();
 	}
 
 	@Override
@@ -263,7 +263,7 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 
 	/**
 	 * Method primDie()
-	 * 
+	 *
 	 * @see msi.gama.metamodel.agent.MinimalAgent#primDie(msi.gama.runtime.IScope)
 	 */
 	@Override
@@ -354,7 +354,9 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 	 */
 
 	public List<? extends IParameter.Batch> getDefaultParameters() {
-		if (!(getSpecies().isHeadless()) && !GamaPreferences.External.CORE_RND_EDITABLE.getValue()) { return new ArrayList<>(); }
+		if (!(getSpecies().isHeadless()) && !GamaPreferences.External.CORE_RND_EDITABLE.getValue()) {
+			return new ArrayList<>();
+		}
 		final List<ExperimentParameter> params = new ArrayList<>();
 		final String cat = getExperimentParametersCategory();
 		ExperimentParameter p = new ExperimentParameter(getScope(), getSpecies().getVar(IKeyword.RNG),
@@ -401,7 +403,7 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 
 	/**
 	 * Called normally from UI directly. Does not notify the GUI.
-	 * 
+	 *
 	 * @param d
 	 */
 	public void setMinimumDurationExternal(final Double d) {
@@ -647,7 +649,7 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 
 		/**
 		 * Method getRandom()
-		 * 
+		 *
 		 * @see msi.gama.runtime.IScope#getRandom()
 		 */
 		@Override
@@ -685,18 +687,21 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 		public Object getGlobalVarValue(final String varName) {
 
 			// First case: we have the variable inside the experiment.
-			if (ExperimentAgent.this.hasAttribute(varName)
-					|| getSpecies().hasVar(varName)) { return super.getGlobalVarValue(varName); }
+			if (ExperimentAgent.this.hasAttribute(varName) || getSpecies().hasVar(varName)) {
+				return super.getGlobalVarValue(varName);
+			}
 			// Second case: the simulation is not null, so it should handle it
 			final SimulationAgent sim = getSimulation();
 			if (sim != null && !sim.dead()) { return sim.getScope().getGlobalVarValue(varName); }
 			// Third case, the simulation is null but the model defines this variable (see #2044). We then grab its
 			// initial value if possible
-			if (this.getModel().getSpecies()
-					.hasVar(varName)) { return getModel().getSpecies().getVar(varName).getInitialValue(this); }
+			if (this.getModel().getSpecies().hasVar(varName)) {
+				return getModel().getSpecies().getVar(varName).getInitialValue(this);
+			}
 			// Fourth case: this is a parameter, so we get it from the species
-			if (getSpecies()
-					.hasParameter(varName)) { return getSpecies().getExperimentScope().getGlobalVarValue(varName); }
+			if (getSpecies().hasParameter(varName)) {
+				return getSpecies().getExperimentScope().getGlobalVarValue(varName);
+			}
 			// Fifth case: it is an extra parameter
 			return extraParametersMap.get(varName);
 		}
@@ -752,9 +757,10 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 	 */
 	public Iterable<IOutputManager> getAllSimulationOutputs() {
 		final SimulationPopulation pop = getSimulationPopulation();
-		if (pop != null) { return Iterables
-				.filter(Iterables.concat(Iterables.transform(pop, each -> each.getOutputManager()),
-						Collections.singletonList(getOutputManager())), each -> each != null); }
+		if (pop != null) {
+			return Iterables.filter(Iterables.concat(Iterables.transform(pop, each -> each.getOutputManager()),
+					Collections.singletonList(getOutputManager())), each -> each != null);
+		}
 		return Collections.EMPTY_LIST;
 	}
 
@@ -767,7 +773,7 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 
 	/**
 	 * Method closeSimulation()
-	 * 
+	 *
 	 * @see msi.gama.kernel.experiment.IExperimentAgent#closeSimulation(msi.gama.kernel.simulation.SimulationAgent)
 	 */
 	@Override
@@ -782,7 +788,7 @@ public class ExperimentAgent extends GamlAgent implements IExperimentAgent {
 
 	/**
 	 * Method getOutputManager()
-	 * 
+	 *
 	 * @see msi.gama.kernel.experiment.ITopLevelAgent#getOutputManager()
 	 */
 	@Override