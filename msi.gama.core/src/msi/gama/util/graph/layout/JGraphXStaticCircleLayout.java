66100f58b8f2b41e41d5db2441a4028cbbc90c85
chapuisk <kevin.chapuis@gmail.com>
2019-05-08 07:58:29 +0700

    trying to add jgraphx layout but depedency issues

--- msi.gama.core/src/msi/gama/util/graph/layout/JGraphXStaticCircleLayout.java
+++ msi.gama.core/src/msi/gama/util/graph/layout/JGraphXStaticCircleLayout.java
@@ -2,7 +2,7 @@ package msi.gama.util.graph.layout;
 
 import java.util.Map;
 
-import org.jgrapht.ext.JGraphXAdapter;
+import org.jgrapht.ext.GraphMLImporter;
 
 import com.mxgraph.layout.mxCircleLayout;
 import com.mxgraph.layout.mxIGraphLayout;
@@ -15,11 +15,13 @@ public class JGraphXStaticCircleLayout implements IStaticLayout {
 	@Override
 	public void doLayoutOneShot(IScope scope, GamaGraph<?, ?> graph, long timeout, Map<String, Object> options) {
 		// TODO Auto-generated method stub
-		JGraphXAdapter jgxAdapter = new JGraphXAdapter<>(graph);
+		//JGraphXAdapter jgxAdapter = new JGraphXAdapter<>(graph);
 		
-		mxIGraphLayout layout = new mxCircleLayout(jgxAdapter);
+		GraphMLImporter gmli = new GraphMLImporter<>(null, null);
 		
-		layout.execute(jgxAdapter.getDefaultParent());
+		mxIGraphLayout layout = new mxCircleLayout(null);
+		
+		//layout.execute(jgxAdapter.getDefaultParent());
 	}
 
 }
