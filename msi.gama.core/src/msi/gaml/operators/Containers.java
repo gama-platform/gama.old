ed8f3d66892622a83cb33d88c04d095e6f210cc1
tcquang <tcquang@users.noreply.github.com>
2019-05-08 12:03:58 +0700

    Adding example for distinct()

--- msi.gama.core/src/msi/gaml/operators/Containers.java
+++ msi.gama.core/src/msi/gaml/operators/Containers.java
@@ -319,7 +319,9 @@ public class Containers {
 	@doc (
 			value = "produces a set from the elements of the operand (i.e. a list without duplicated elements)",
 			usages = { @usage (
-					value = "if the operand is nil, remove_duplicates returns nil"),
+						value = "if the operand is nil, remove_duplicates returns nil",
+						examples = { @example (
+										value ="remove_duplicates([])", equals="[]")}),
 					@usage (
 							value = "if the operand is a graph, remove_duplicates returns the set of nodes"),
 					@usage (
@@ -328,7 +330,11 @@ public class Containers {
 									value = "remove_duplicates([1::3,2::4,3::3,5::7])",
 									equals = "[3,4,7]") }),
 					@usage (
-							value = "if the operand is a matrix, remove_duplicates returns a matrix without duplicated row") },
+							value = "if the operand is a matrix, remove_duplicates returns a matrix without duplicated row",
+							examples = { @example (
+									value = "remove_duplicates([[\"c11\",\"c12\",\"c13\",\"c13\"],[\"c21\",\"c22\",\"c23\",\"c23\"]])",
+									equals = "[[\"c11\",\"c12\",\"c13\"],[\"c21\",\"c22\",\"c23\"]]", test=false )})
+					},
 			examples = { @example (
 					value = "remove_duplicates([3,2,5,1,2,3,5,5,5])",
 					equals = "[3,2,5,1]") })
