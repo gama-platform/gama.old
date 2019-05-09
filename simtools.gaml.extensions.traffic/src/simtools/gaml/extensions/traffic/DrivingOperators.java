6c4620fb122d979f1b557c1f9a51635d818091b3
patrick taillandier <patrick.taillandier@gmail.com>
2019-05-09 16:48:15 +0700

    add no test

--- simtools.gaml.extensions.traffic/src/simtools/gaml/extensions/traffic/DrivingOperators.java
+++ simtools.gaml.extensions.traffic/src/simtools/gaml/extensions/traffic/DrivingOperators.java
@@ -16,6 +16,7 @@ import msi.gama.metamodel.shape.IShape;
 import msi.gama.metamodel.topology.graph.GamaSpatialGraph;
 import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.example;
+import msi.gama.precompiler.GamlAnnotations.no_test;
 import msi.gama.precompiler.GamlAnnotations.operator;
 import msi.gama.precompiler.IConcept;
 import msi.gama.precompiler.ITypeProvider;
@@ -34,6 +35,7 @@ public class DrivingOperators {
 	@doc(value = "creates a graph from the list/map of edges given as operand and connect the node to the edge", examples = {
 			@example(value = "as_driving_graph(road,node)  --:  build a graph while using the road agents as edges and the node agents as nodes", isExecutable = false) }, see = {
 					"as_intersection_graph", "as_distance_graph", "as_edge_graph" })
+	@no_test
 	public static IGraph spatialDrivingFromEdges(final IScope scope, final IContainer edges, final IContainer nodes) {
 		final IGraph graph = new GamaSpatialGraph(edges, nodes, scope);
 		for (final Object edge : edges.iterable(scope)) {
