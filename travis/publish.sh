475a063f52fe0e1d6d2d0dd2aeb7d730c201860f
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2019-05-16 15:34:34 +0700

    change trigger IO to sources branch ci docs

--- travis/publish.sh
+++ travis/publish.sh
@@ -31,9 +31,9 @@ commit_io_website_files() {
 	git remote rm origin
 	git remote add origin https://gama-bot:$BOT_TOKEN@github.com/gama-platform/gama-platform.github.io.git
 	git checkout -b sources
-	git branch --set-upstream-to=origin/sources sources
+	#git branch --set-upstream-to=origin/sources sources
 	echo "pulling"
-	git pull
+	git pull origin sources
 	echo "Travis build trigger from gama core at $(date)" > log.txt
 	git status
 	git add -A		
