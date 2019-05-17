d1d697cfa35fc1da0204ee4d49d069b0238a00cf
AlexisDrogoul <alexis.drogoul@gmail.com>
2019-05-17 18:22:18 +0700

    Fixes Travis !

--- ummisco.gama.ui.modeling/src-gen/ummisco/gama/ui/modeling/internal/ModelingActivator.java
+++ ummisco.gama.ui.modeling/src-gen/ummisco/gama/ui/modeling/internal/ModelingActivator.java
@@ -3,51 +3,52 @@
  */
 package ummisco.gama.ui.modeling.internal;
 
-import com.google.common.collect.Maps;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import java.util.Collections;
 import java.util.Map;
-import msi.gama.lang.gaml.GamlRuntimeModule;
-import msi.gama.lang.gaml.ui.GamlUiModule;
-import org.apache.log4j.Logger;
+
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.xtext.ui.shared.SharedStateModule;
 import org.eclipse.xtext.util.Modules2;
 import org.osgi.framework.BundleContext;
 
+import com.google.common.collect.Maps;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import msi.gama.lang.gaml.GamlRuntimeModule;
+import msi.gama.lang.gaml.ui.GamlUiModule;
+import ummisco.gama.dev.utils.DEBUG;
+
 /**
- * This class was generated. Customizations should only happen in a newly
- * introduced subclass. 
+ * This class was generated. Customizations should only happen in a newly introduced subclass.
  */
 public class ModelingActivator extends AbstractUIPlugin {
 
 	public static final String PLUGIN_ID = "ummisco.gama.ui.modeling";
 	public static final String MSI_GAMA_LANG_GAML_GAML = "msi.gama.lang.gaml.Gaml";
-	
-	private static final Logger logger = Logger.getLogger(ModelingActivator.class);
-	
+
 	private static ModelingActivator INSTANCE;
-	
-	private Map<String, Injector> injectors = Collections.synchronizedMap(Maps.<String, Injector> newHashMapWithExpectedSize(1));
-	
+
+	private Map<String, Injector> injectors =
+			Collections.synchronizedMap(Maps.<String, Injector> newHashMapWithExpectedSize(1));
+
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		INSTANCE = this;
 	}
-	
+
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		injectors.clear();
 		INSTANCE = null;
 		super.stop(context);
 	}
-	
+
 	public static ModelingActivator getInstance() {
 		return INSTANCE;
 	}
-	
+
 	public Injector getInjector(String language) {
 		synchronized (injectors) {
 			Injector injector = injectors.get(language);
@@ -57,7 +58,7 @@ public class ModelingActivator extends AbstractUIPlugin {
 			return injector;
 		}
 	}
-	
+
 	protected Injector createInjector(String language) {
 		try {
 			com.google.inject.Module runtimeModule = getRuntimeModule(language);
@@ -66,29 +67,24 @@ public class ModelingActivator extends AbstractUIPlugin {
 			com.google.inject.Module mergedModule = Modules2.mixin(runtimeModule, sharedStateModule, uiModule);
 			return Guice.createInjector(mergedModule);
 		} catch (Exception e) {
-			logger.error("Failed to create injector for " + language);
-			logger.error(e.getMessage(), e);
+			DEBUG.OUT("Failed to create injector for " + language);
+			DEBUG.OUT(e.getMessage());
 			throw new RuntimeException("Failed to create injector for " + language, e);
 		}
 	}
-	
+
 	protected com.google.inject.Module getRuntimeModule(String grammar) {
-		if (MSI_GAMA_LANG_GAML_GAML.equals(grammar)) {
-			return new GamlRuntimeModule();
-		}
+		if (MSI_GAMA_LANG_GAML_GAML.equals(grammar)) { return new GamlRuntimeModule(); }
 		throw new IllegalArgumentException(grammar);
 	}
-	
+
 	protected com.google.inject.Module getUiModule(String grammar) {
-		if (MSI_GAMA_LANG_GAML_GAML.equals(grammar)) {
-			return new GamlUiModule(this);
-		}
+		if (MSI_GAMA_LANG_GAML_GAML.equals(grammar)) { return new GamlUiModule(this); }
 		throw new IllegalArgumentException(grammar);
 	}
-	
+
 	protected com.google.inject.Module getSharedStateModule() {
 		return new SharedStateModule();
 	}
-	
-	
+
 }
