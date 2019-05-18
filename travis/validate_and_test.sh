47f5df7f54b0c0fa61b3d0b44018cbd53c2f6088
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2019-05-18 08:29:59 +0700

    Update validate_and_test.sh

--- travis/validate_and_test.sh
+++ travis/validate_and_test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd /home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless
-memory=2048m 
+memory=3048m 
 
 
   
@@ -16,25 +16,16 @@ echo "GAMA is starting..."
 #exec
 
 #GAMA=Gamaq
-exec java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -validate 
+java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -validate 
 
 res=$?
 		
 if [[ $res -gt 0 ]]; then	
 	rm -rf $passWork
-	
-echo "after first launch"
-cd /home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/configuration/org.eclipse.osgi
-find . | grep "\.gaml$"
-
-
-cd /home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless
-
-
 	exit $res
 fi
 
-exec java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -test -failed   
+java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -test -failed   
 res=$?			
 if [[ $res -gt 0 ]]; then
 	rm -rf $passWork
