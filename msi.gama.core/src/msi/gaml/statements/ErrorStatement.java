97a9fa1d313d2c1e00f647152520fad77b2e68fe
AlexisDrogoul <alexis.drogoul@gmail.com>
2019-06-18 20:55:48 +0700

    Gets rid of GraphStream library msi.gama.ext.

--- msi.gama.core/src/msi/gaml/statements/ErrorStatement.java
+++ msi.gama.core/src/msi/gaml/statements/ErrorStatement.java
@@ -1,12 +1,12 @@
 /*******************************************************************************************************
  *
- * msi.gaml.statements.ErrorStatement.java, in plugin msi.gama.core,
- * is part of the source code of the GAMA modeling and simulation platform (v. 1.8)
- * 
+ * msi.gaml.statements.ErrorStatement.java, in plugin msi.gama.core, is part of the source code of the GAMA modeling and
+ * simulation platform (v. 1.8)
+ *
  * (c) 2007-2018 UMI 209 UMMISCO IRD/SU & Partners
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
- * 
+ *
  ********************************************************************************************************/
 package msi.gaml.statements;
 
@@ -19,7 +19,8 @@ import msi.gama.precompiler.GamlAnnotations.facets;
 import msi.gama.precompiler.GamlAnnotations.inside;
 import msi.gama.precompiler.GamlAnnotations.symbol;
 import msi.gama.precompiler.GamlAnnotations.usage;
-import msi.gama.precompiler.*;
+import msi.gama.precompiler.IConcept;
+import msi.gama.precompiler.ISymbolKind;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
 import msi.gaml.descriptions.IDescription;
@@ -29,20 +30,30 @@ import msi.gaml.types.IType;
 
 /**
  * Written by drogoul Modified on 6 févr. 2010
- * 
+ *
  * @todo Description
- * 
+ *
  */
 
-@symbol(name = IKeyword.ERROR, kind = ISymbolKind.SINGLE_STATEMENT, with_sequence = false, concept = { IConcept.SYSTEM })
-@inside(kinds = { ISymbolKind.BEHAVIOR, ISymbolKind.SEQUENCE_STATEMENT, ISymbolKind.LAYER })
-@facets(value = { @facet(name = IKeyword.MESSAGE,
-	type = IType.STRING,
-	optional = false,
-	doc = @doc("the message to display in the error.")) }, omissible = IKeyword.MESSAGE)
-@doc(value = "The statement makes the agent output an error dialog (if the simulation contains a user interface). Otherwise displays the error in the console.",
-	usages = { @usage(value = "Throwing an error",
-		examples = { @example("error 'This is an error raised by ' + self;") }) })
+@symbol (
+		name = IKeyword.ERROR,
+		kind = ISymbolKind.SINGLE_STATEMENT,
+		with_sequence = false,
+		concept = { IConcept.SYSTEM })
+@inside (
+		kinds = { ISymbolKind.BEHAVIOR, ISymbolKind.SEQUENCE_STATEMENT, ISymbolKind.LAYER })
+@facets (
+		value = { @facet (
+				name = IKeyword.MESSAGE,
+				type = IType.STRING,
+				optional = false,
+				doc = @doc ("the message to display in the error.")) },
+		omissible = IKeyword.MESSAGE)
+@doc (
+		value = "The statement makes the agent output an error dialog (if the simulation contains a user interface). Otherwise displays the error in the console.",
+		usages = { @usage (
+				value = "Throwing an error",
+				examples = { @example ("error 'This is an error raised by ' + self;") }) })
 public class ErrorStatement extends AbstractStatement {
 
 	final IExpression message;
@@ -54,18 +65,13 @@ public class ErrorStatement extends AbstractStatement {
 
 	@Override
 	public Object privateExecuteIn(final IScope scope) throws GamaRuntimeException {
-		IAgent agent = scope.getAgent();
+		final IAgent agent = scope.getAgent();
 		String mes = null;
-		if ( agent != null && !agent.dead() ) {
+		if (agent != null && !agent.dead()) {
 			mes = Cast.asString(scope, message.value(scope));
 			throw GamaRuntimeException.error(mes, scope);
 		}
 		return mes;
 	}
-	//
-	// @Override
-	// public IType getType() {
-	// return Types.STRING;
-	// }
 
 }
