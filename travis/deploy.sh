c188ec82a6e5debf84fe67be3eeef606a1277e59
hqnghi88 <hqnghi88@gmail.com>
2019-08-18 21:46:42 +0700

    on  #2845 ci deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -12,9 +12,37 @@ function mvn_deploy() {
 	cd -
 }
 
+deploy(){	
+cd msi.gama.p2updatesite &&
+mvn -U clean install -P p2Repo --settings ../travis/settings.xml && 
+cd -
+}
+
+
 echo "Install GAMA project"			
+MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+echo $MESSAGE
+if  [[ ${MESSAGE} == *"ci ext"* ]]; then			
+	MSG+=" ci ext " 
+fi	
+
+if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]] || [[ $MSG == *"ci cron"* ]]; then 	
+	
+	deploy
+ 
+else
+	if  [[ ${MESSAGE} == *"ci deploy"* ]] || [[ $MSG == *"ci deploy"* ]]; then		
+		if  [[ ${MESSAGE} == *"ci clean"* ]] || [[ $MSG == *"ci clean"* ]]; then
+			clean
+			MSG+=" ci ext "
+			echo $MSG
+		fi 
+		deploy 
+	fi
+fi
 	
 	
+function tmp(){
 change=$(git log --pretty=format: --name-only --since="1 day ago")
 
 if [[ ${change} == *"ummisco.gama.annotations"* ]] || [[ $MSG == *"ci ummisco.gama.annotations"* ]] || [[ $MSG == *"ci fullbuild"* ]]; then
@@ -269,4 +297,4 @@ else
 	cd -
 
 fi
-
+}
