5337ac97f63abdfea4c60af916a954917510ce1a
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2019-10-07 17:06:22 +0700

    ci cron

--- travis/zip_withjdk.sh
+++ travis/zip_withjdk.sh
@@ -127,7 +127,6 @@ sudo cp jdk/win/gama-headless.bat /home/travis/build/gama-platform/gama/ummisco.
 
 sudo cp -R jdk/mac/64/1.8.171/jdk /home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/x86_64/Gama.app/Contents
 sudo cp jdk/mac/64/Gama.ini /home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/x86_64/Gama.app/Contents/Eclipse
-sudo cp jdk/mac/gama-headless.sh /home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/x86_64/Gama.app/Contents/headless
 	
 
 	
