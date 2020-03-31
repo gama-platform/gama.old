1fee0fb61034a504ef17d87020e0210ee3a7e73e
RoiArthurB <contact@arthurbrugiere.fr>
2020-03-31 19:34:02 +0700

    [HEADLESS OSX] Update script - Remove useless first variable - Fix if loop - Escape whitespaces in param arguments

--- ummisco.gama.product/extraresources/headless/osx/gama-headless.sh
+++ ummisco.gama.product/extraresources/headless/osx/gama-headless.sh
@@ -1,22 +1,16 @@
-#! /bin/sh
+#!/bin/bash
 memory=4096m
-declare -i i
-
-i=0
-echo ${!i}
 
 for ((i=1;i<=$#;i=$i+1))
 do
-if test ${!i} = "-m"
-then
+if [ "${!i}" = "-m" ]; then
     i=$i+1
     memory=${!i}
 else
-    PARAM=$PARAM\ ${!i}
-    i=$i+1
-    PARAM=$PARAM\ ${!i}
+    PARAM=$PARAM\ $(echo ${!i} | sed 's/ /\\ /g')
 fi
 done
+
 
 echo "******************************************************************"
 echo "* GAMA version 1.8                                               *"
@@ -25,8 +19,4 @@ echo "* (c) 2007-2019 UMI 209 UMMISCO IRD/SU & Partners                *"
 echo "******************************************************************"
 passWork=.work$RANDOM
 
-java -cp ../Eclipse/plugins/org.eclipse.equinox.launcher*.jar -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork $PARAM $mfull $outputFile
-rm -rf $passWork
-
-
-
+java -cp ../plugins/org.eclipse.equinox.launcher*.jar -Xms512m -Xmx$memory -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork $PARAM
