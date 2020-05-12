919851b06e69e4f1a324cc39e18e2a08d88caca0
AlexisDrogoul <alexis.drogoul@gmail.com>
2020-05-12 18:45:50 +0700

    Should normally fix #2969 (rebuild workspace prior to testing it)

--- msi.gama.lang.gaml/src/msi/gama/lang/gaml/resource/GamlResourceDescriptionStrategy.java
+++ msi.gama.lang.gaml/src/msi/gama/lang/gaml/resource/GamlResourceDescriptionStrategy.java
@@ -1,19 +1,18 @@
 /*********************************************************************************************
  *
- * 'GamlResourceDescriptionStrategy.java, in plugin msi.gama.lang.gaml, is part of the source code of the
- * GAMA modeling and simulation platform.
- * (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC & Partners
+ * 'GamlResourceDescriptionStrategy.java, in plugin msi.gama.lang.gaml, is part of the source code of the GAMA modeling
+ * and simulation platform. (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC & Partners
  *
  * Visit https://github.com/gama-platform/gama for license information and developers contact.
- * 
+ *
  *
  **********************************************************************************************/
 package msi.gama.lang.gaml.resource;
 
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.resource.IEObjectDescription;
-import org.eclipse.xtext.resource.IReferenceDescription;
 import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
 import org.eclipse.xtext.util.IAcceptor;
 
@@ -25,52 +24,34 @@ import msi.gama.lang.gaml.gaml.Statement;
 
 /**
  * The class GamlResourceDescriptionManager.
- * 
+ *
  * @author drogoul
  * @since 19 avr. 2012
- * 
+ *
  */
 public class GamlResourceDescriptionStrategy extends DefaultResourceDescriptionStrategy {
 
-	/**
-	 * @see org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy#createEObjectDescriptions(org.eclipse.emf.ecore.EObject,
-	 *      org.eclipse.xtext.util.IAcceptor)
-	 */
-
 	@Override
 	public boolean createEObjectDescriptions(final EObject o, final IAcceptor<IEObjectDescription> acceptor) {
-		if (o instanceof ActionArguments) {
-			return true;
-		} else if (o instanceof ArgumentDefinition) {
+		if (o instanceof ActionArguments || o instanceof Block || o instanceof Model) { return true; }
+		if (o instanceof ArgumentDefinition) {
 			super.createEObjectDescriptions(o, acceptor);
 		} else if (o instanceof Statement) {
-			try {
-				super.createEObjectDescriptions(o, acceptor);
-			} catch (final IllegalArgumentException e) {
-				return false;
-			}
-			return ((Statement) o).getBlock() != null; //
+			super.createEObjectDescriptions(o, acceptor);
+			return true;
 		}
-		return o instanceof Block || o instanceof Model;
-	}
-
-	@Override
-	public boolean createReferenceDescriptions(final EObject from, final URI exportedContainerURI,
-			final IAcceptor<IReferenceDescription> acceptor) {
-		// TODO Auto-generated method stub
-		return super.createReferenceDescriptions(from, exportedContainerURI, acceptor);
+		return false;
 	}
 
 	@Override
 	protected boolean isResolvedAndExternal(final EObject from, final EObject to) {
-		if (to == null)
-			return false;
-		if (to.eResource() == null)
-			return false;
-		if (to.eResource().getURI().lastSegment().endsWith("xmi"))
-			return false;
-		return super.isResolvedAndExternal(from, to);
-
+		if (to == null) { return false; }
+		if (!to.eIsProxy()) {
+			final Resource toR = to.eResource();
+			return toR != null && toR != from.eResource();
+		}
+		return !getLazyURIEncoder().isCrossLinkFragment(from.eResource(),
+				((InternalEObject) to).eProxyURI().fragment());
 	}
 
 }
