b8e32ae2e785c91463f00a47fc141dc907118e11
RoiArthurB <contact@arthurbrugiere.fr>
2020-10-28 11:04:52 +0700

    [HEADLESS] Script create workspace in output folder + fix display if error

--- ummisco.gama.product/extraresources/headless/osx/gama-headless.sh
+++ ummisco.gama.product/extraresources/headless/osx/gama-headless.sh
@@ -19,9 +19,12 @@ echo "* GAMA version 1.8.1                                             *"
 echo "* http://gama-platform.org                                       *"
 echo "* (c) 2007-2020 UMI 209 UMMISCO IRD/SU & Partners                *"
 echo "******************************************************************"
-passWork=.gama_headless-workspace-$(find ./ -maxdepth 1 -name ".gama_headless-workspace-*" | wc -l)
+# create workspace in output folder
+mkdir ${@: -1}
+passWork=${@: -1}/.workspace
 
 if ! java -cp "$( dirname "${BASH_SOURCE[0]}" )"/../Eclipse/plugins/org.eclipse.equinox.launcher*.jar -Xms512m -Xmx$memory -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork "$@"; then
     echo "Error in you command, here's the log :"
-    cat "$( dirname "${BASH_SOURCE[0]}" )"/$passWork/.metadata/.log
+    cat $passWork/.metadata/.log
+    exit 1
 fi
