7a9a2aeca94e5a9386bd8caf6ab2d87309ba9d53
RoiArthurB <contact@arthurbrugiere.fr>
2020-11-10 10:35:26 +0700

    [HEADLESS] Fix workspace creation on first run

--- ummisco.gama.product/extraresources/headless/unix/gama-headless.sh
+++ ummisco.gama.product/extraresources/headless/unix/gama-headless.sh
@@ -19,11 +19,15 @@ echo "* GAMA version 1.8.1                                             *"
 echo "* http://gama-platform.org                                       *"
 echo "* (c) 2007-2020 UMI 209 UMMISCO IRD/SU & Partners                *"
 echo "******************************************************************"
+# create output folder if not existing
+if [ ! -d "${@: -1}" ]; then
+    mkdir ${@: -1}
+fi
 # create workspace in output folder
-mkdir ${@: -1}
-passWork=${@: -1}/.workspace
+passWork=${@: -1}/.workspace$(find ${@: -1} -name ".workspace*" | wc -l)
+mkdir -p $passWork
 
-if ! java -cp "$( dirname "${BASH_SOURCE[0]}" )"/../plugins/org.eclipse.equinox.launcher*.jar -Xms512m -Xmx$memory -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork "$@"; then
+if ! java -cp "$( dirname "${BASH_SOURCE[0]}" )"/../plugins/org.eclipse.equinox.launcher*.jar -Xms512m -Xmx$memory -Djava.awt.headless=true org.eclipse.core.launcher.Main -application msi.gama.headless.id4 -data $passWork "$@"; then
     echo "Error in you command, here's the log :"
     cat /$passWork/.metadata/.log
     exit 1
