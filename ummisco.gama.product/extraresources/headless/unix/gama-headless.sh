5184eb9354779c88f783359e71efa1e301c05a79
RoiArthurB <contact@arthurbrugiere.fr>
2020-11-18 13:04:17 +0700

    [HEADLESS] Remove workspace creation when not launching simulations - #3049

--- ummisco.gama.product/extraresources/headless/unix/gama-headless.sh
+++ ummisco.gama.product/extraresources/headless/unix/gama-headless.sh
@@ -14,21 +14,38 @@ for arg do
   esac
 done
 
+workspaceCreate=0
+case "$@" in 
+  *-help*|*-version*|*-validate*|*-failed*|*-xml*)
+    workspaceCreate=1
+    ;;
+esac
+
+
 echo "******************************************************************"
 echo "* GAMA version 1.8.1                                             *"
 echo "* http://gama-platform.org                                       *"
 echo "* (c) 2007-2020 UMI 209 UMMISCO IRD/SU & Partners                *"
 echo "******************************************************************"
-# create output folder if not existing
-if [ ! -d "${@: -1}" ]; then
-    mkdir ${@: -1}
+passWork=.workspace
+# w/ output folder
+if [ $workspaceCreate -eq 0 ]; then
+  # create output folder if not existing
+  if [ ! -d "${@: -1}" ]; then
+      mkdir ${@: -1}
+  fi
+  # create workspace in output folder
+  passWork=${@: -1}/.workspace$(find ${@: -1} -name ".workspace*" | wc -l)
+  mkdir -p $passWork
+
+# w/o output folder
+else
+  # create workspace in current folder
+  passWork=.workspace$(find ./ -maxdepth 1 -name ".workspace*" | wc -l)
 fi
-# create workspace in output folder
-passWork=${@: -1}/.workspace$(find ${@: -1} -name ".workspace*" | wc -l)
-mkdir -p $passWork
 
 if ! java -cp "$( dirname "${BASH_SOURCE[0]}" )"/../plugins/org.eclipse.equinox.launcher*.jar -Xms512m -Xmx$memory -Djava.awt.headless=true org.eclipse.core.launcher.Main -application msi.gama.headless.id4 -data $passWork "$@"; then
     echo "Error in you command, here's the log :"
-    cat /$passWork/.metadata/.log
+    cat $passWork/.metadata/.log
     exit 1
 fi
