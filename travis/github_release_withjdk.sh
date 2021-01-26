d0c6f29e9f72e7f2b6e8caa8f81195eb071871c1
HUYNH Nghi <hqnghi88@gmail.com>
2021-01-26 14:26:14 +0700

    ci release alpha

--- travis/github_release_withjdk.sh
+++ travis/github_release_withjdk.sh
@@ -58,22 +58,22 @@ echo $SUFFIX
 
 n=0
 RELEASEFILES[$n]="$thePATH-linux.gtk.x86_64.zip"
-NEWFILES[$n]='GAMA1.8.2_Continuous_Linux'$SUFFIX 
+NEWFILES[$n]='GAMA_1.8.2_Alpha_Linux'$SUFFIX 
 n=1
 RELEASEFILES[$n]="$thePATH-macosx.cocoa.x86_64.zip"
-NEWFILES[$n]='GAMA1.8.2_Continuous_Mac'$SUFFIX
+NEWFILES[$n]='GAMA_1.8.2_Alpha_Mac'$SUFFIX
 n=2
 RELEASEFILES[$n]="$thePATH-win32.win32.x86_64.zip" 
-NEWFILES[$n]='GAMA1.8.2_Continuous_Win'$SUFFIX
+NEWFILES[$n]='GAMA_1.8.2_Alpha_Win'$SUFFIX
 n=3
 RELEASEFILES[$n]="$thePATH-linux.gtk.x86_64_withJDK.zip"
-NEWFILES[$n]='GAMA1.8.2_Continuous_withJDK_Linux'$SUFFIX
+NEWFILES[$n]='GAMA_1.8.2_Alpha_withJDK_Linux'$SUFFIX
 n=4
 RELEASEFILES[$n]="$thePATH-win32.win32.x86_64_withJDK.zip" 
-NEWFILES[$n]='GAMA1.8.2_Continuous_withJDK_Win'$SUFFIX
+NEWFILES[$n]='GAMA_1.8.2_Alpha_withJDK_Win'$SUFFIX
 n=5
 RELEASEFILES[$n]="$thePATH-macosx.cocoa.x86_64_withJDK.zip"
-NEWFILES[$n]='GAMA1.8.2_Continuous_withJDK_Mac'$SUFFIX
+NEWFILES[$n]='GAMA_1.8.2_Alpha_withJDK_Mac'$SUFFIX
  
 
 i=0
@@ -128,7 +128,7 @@ LK="https://api.github.com/repos/gama-platform/gama/releases"
   -H "X-Parse-REST-API-Key: sensitive" \
   -H "Authorization: token $BOT_TOKEN"   \
   -H "Content-Type: application/json" \
-  -d '{"tag_name": "'$RELEASE'", "name":"Continuous build","body":"Alpha release for GAMA 1.8.2, which adds compatibility with JDK 11+ . Please test and report issues","draft": false,"prerelease": true}' \
+  -d '{"tag_name": "'$RELEASE'", "name":"Alpha Version 1.8.2","body":"Alpha release for GAMA 1.8.2, which adds compatibility with JDK 11+ . Please test and report issues","draft": false,"prerelease": true}' \
     "$LK"`
 echo $RESULT	
 
