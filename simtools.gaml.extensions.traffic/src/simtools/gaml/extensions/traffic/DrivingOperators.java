e62782008ebee371348672a2551e5134da4d64e6
minhduc0711 <phamminhduc0711@gmail.com>
2021-02-24 14:47:52 +0700

    add new op extract_nodes in traffic plugin

--- simtools.gaml.extensions.traffic/src/simtools/gaml/extensions/traffic/DrivingOperators.java
+++ simtools.gaml.extensions.traffic/src/simtools/gaml/extensions/traffic/DrivingOperators.java
@@ -13,7 +13,14 @@
  **********************************************************************************************/
 package simtools.gaml.extensions.traffic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import msi.gama.metamodel.agent.IAgent;
+import msi.gama.metamodel.population.IPopulation;
+import msi.gama.metamodel.shape.ILocation;
 import msi.gama.metamodel.shape.IShape;
 import msi.gama.metamodel.topology.graph.GamaSpatialGraph;
 import msi.gama.precompiler.GamlAnnotations.doc;
@@ -27,6 +34,8 @@ import msi.gama.util.GamaListFactory;
 import msi.gama.util.IContainer;
 import msi.gama.util.IList;
 import msi.gama.util.graph.IGraph;
+import msi.gaml.operators.Containers;
+import msi.gaml.species.ISpecies;
 import msi.gaml.types.Types;
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
@@ -64,4 +73,40 @@ public class DrivingOperators {
 		return graph;
 	}
 
+	@operator(value = "extract_nodes", 
+			  content_type = ITypeProvider.CONTENT_TYPE_AT_INDEX + 2,
+			  index_type = ITypeProvider.CONTENT_TYPE_AT_INDEX + 2,
+			  concept = {IConcept.GRAPH, IConcept.TRANSPORT})
+	@doc(value = "A list of nodes that correspond to the given edges",
+		comment = "The first operand is a list of edges, and the second one is the species representing graph nodes",
+		examples = {
+			@example(value = "list<node> nodes <- extract_nodes(road, node); // `road` and `node` are species defined by the user",
+					isExecutable = false),
+		}
+	)
+	@no_test
+	public static IList extractNodes(IScope scope, IContainer edges, ISpecies nodeSpecies) {
+		// extract the end points of each edge
+		IList uniquePoints = GamaListFactory.create(Types.POINT);
+		for (Object edge : edges.iterable(scope)) {
+			if (edge instanceof IShape) {
+				IAgent agent = ((IShape) edge).getAgent();
+				IList points = agent.getGeometry().getPoints();
+				uniquePoints.add((ILocation) points.firstValue(scope));
+				uniquePoints.add((ILocation) points.lastValue(scope));
+			}
+		}
+		// remove duplicate points
+		uniquePoints = Containers.remove_duplicates(scope, uniquePoints);
+
+		// create node agents at these points
+		IPopulation population = scope.getAgent().getPopulationFor(nodeSpecies);
+		List<Map<String, Object>> attrMaps = new ArrayList<>();
+		for (Object point : uniquePoints.iterable(scope)) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("location", point);
+			attrMaps.add(map);
+		}
+		return population.createAgents(scope, uniquePoints.size(), attrMaps, false, true);
+	}
 }
