316ac9931e3e92fc8076daca3126c1b9e2cee522
hqnghi88 <hqnghi88@gmail.com>
2021-06-27 22:29:30 +0700

    ci deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -11,7 +11,9 @@ echo -e "IdentitiesOnly yes\n" >> ~/.ssh/config
 echo -e "Port 22\n" >> ~/.ssh/config 
 echo -e "StrictHostKeyChecking no\n" >> ~/.ssh/config 
 echo -e "PubkeyAuthentication no\n" >> ~/.ssh/config 
-cat ~/.ssh/config 
+cat ~/.ssh/config
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+ssh-keyscan -H 52.183.47.138 >> ~/.ssh/known_hosts
 ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts
 cd msi.gama.parent &&
 mvn clean install -P p2Repo --settings ../travis/settings.xml && 
