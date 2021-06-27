044a6b4131ef68a95e55bc3dda9a80d9a557e5a7
hqnghi88 <hqnghi88@gmail.com>
2021-06-27 21:36:02 +0700

    ci deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,7 +3,7 @@
 echo "DEPLOY"
 echo $SSH_USER_ID
 
-mkdir -m 0700 -p ~/.ssh && ssh-keyscan github.com | tee -a ~/.ssh/known_hosts
+mkdir -m 0700 -p ~/.ssh && ssh-keyscan $SSH_HOST | tee -a ~/.ssh/known_hosts
 echo -e "StrictHostKeyChecking no\n" >> ~/.ssh/config 
 ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts
 cd msi.gama.parent &&
