000f43d3e2a12e91984f11fc1b16dcfd59d32e3d
hqnghi88 <hqnghi88@gmail.com>
2021-06-28 01:35:05 +0700

    ci deploy

--- travis/decrypt_secret.sh
+++ travis/decrypt_secret.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $GITHUB_WORKSPACE/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $GITHUB_WORKSPACE/secrets/my_secret.json $GITHUB_WORKSPACE/travis/my_secret.json.gpg
+--output $GITHUB_WORKSPACE/travis/my_secret.json $GITHUB_WORKSPACE/travis/my_secret.json.gpg
