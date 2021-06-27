170cfd4bd38ee9b0828f17dec7ffaa5dd9b2dbed
hqnghi88 <hqnghi88@gmail.com>
2021-06-27 21:29:26 +0700

    ci deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,7 +5,7 @@ echo $SSH_USER_ID
 
 mkdir -m 0700 -p ~/.ssh && ssh-keyscan github.com | tee -a ~/.ssh/known_hosts
 echo -e "StrictHostKeyChecking no\n" >> ~/.ssh/config 
-ssh-keyscan -H ${{ secrets.SSH_HOST }} >> ~/.ssh/known_hosts
+ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts
 cd msi.gama.parent &&
 mvn clean install -P p2Repo --settings ../travis/settings.xml && 
 cd - 
