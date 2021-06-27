300bdaa5c469be6a9eef96d74acc3fd6a6b028b8
hqnghi88 <hqnghi88@gmail.com>
2021-06-27 20:59:19 +0700

    ci deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-
-function mvn_deploy() {
-	echo "Deploying " $1
-	cd $1
-	mvn deploy -DskipTests -Dcheckstyle.skip -T 8C -P p2Repo --settings ../travis/settings.xml
-	res=$?
-	if [[ $res -gt 0 ]]; then
-		exit $res
-	fi
-	cd -
-}
- 
+echo "DEPLOY"
+echo $SSH_USER_ID
 cd msi.gama.parent &&
 mvn clean install -P p2Repo --settings ../travis/settings.xml && 
 cd - 
