2684c6415ce08071332c806f70eab394258639fb
hqnghi88 <hqnghi88@gmail.com>
2021-06-28 10:34:40 +0700

    ci deploy

--- travis/decrypt_secret.sh
+++ travis/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SSH_USER_PWD" --output travis/settings.xml travis/settings.xml.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SSH_USER_PWD" --output travis/settings_auth.xml travis/settings_auth.xml.gpg
--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 bash $GITHUB_WORKSPACE/travis/decrypt_secret.sh
+
+header=$(<$GITHUB_WORKSPACE/travis/settings_header.xml)
+core=$(<$GITHUB_WORKSPACE/travis/settings_auth.xml)
+footer=$(<$GITHUB_WORKSPACE/travis/settings_footer.xml) 
+echo "$header $core $footer"> $GITHUB_WORKSPACE/travis/settings.xml
+
+
 echo "DEPLOY" 
 mkdir -m 0700 -p ~/.ssh 
 chmod 750 ~
@@ -12,7 +19,7 @@ echo -e "StrictHostKeyChecking no\n" >> ~/.ssh/config
 echo -e "PubkeyAuthentication no\n" >> ~/.ssh/config 
 cat ~/.ssh/config 
 ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts
-cd msi.gama.parent
+cd msi.gama.p2updatesite
 mvn deploy -P p2Repo --settings ../travis/settings.xml
 cd - 
 
