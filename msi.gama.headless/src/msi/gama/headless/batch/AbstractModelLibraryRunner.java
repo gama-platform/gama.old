ea12e40ec83294755ce4b0faae53f0146afb742b
RoiArthurB <contact@arthurbrugiere.fr>
2021-07-01 17:33:54 +0700

    [HL-MLR] Check files with GamlFileExtension functions

--- msi.gama.headless/src/msi/gama/headless/batch/AbstractModelLibraryRunner.java
+++ msi.gama.headless/src/msi/gama/headless/batch/AbstractModelLibraryRunner.java
@@ -2,10 +2,10 @@ package msi.gama.headless.batch;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 
 import com.google.inject.Injector;
 
+import msi.gama.common.GamlFileExtension;
 import msi.gama.lang.gaml.validation.GamlModelBuilder;
 import ummisco.gama.dev.utils.DEBUG;
 
@@ -21,12 +21,16 @@ public abstract class AbstractModelLibraryRunner {
 	}
 
 	protected boolean isModel(final URL url) {
-		final String file = url.getFile();
-		return file.endsWith(".gaml") || file.endsWith(".experiment");
+		return isModel(url.getFile());
+	}
+
+	protected boolean isModel(final String file) {
+		return GamlFileExtension.isGaml(file) || GamlFileExtension.isExperiment(file);
 	}
 
 	protected boolean isTest(final URL url) {
-		return isModel(url) && url.toString().contains("tests");
+		final String file = url.getFile();
+		return isModel(file) && (file.contains("test") || file.contains("Test"));
 	}
 
 	public abstract int start() throws IOException;
