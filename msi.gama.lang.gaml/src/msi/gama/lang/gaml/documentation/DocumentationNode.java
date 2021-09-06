c7d51264ae667e8b1654a0fde08fccfe4ff7747d
AlexisDrogoul <alexis.drogoul@gmail.com>
2021-09-06 11:47:22 +0700

    Removes temporarily the compression/uncompression of documentation

--- msi.gama.lang.gaml/src/msi/gama/lang/gaml/documentation/DocumentationNode.java
+++ msi.gama.lang.gaml/src/msi/gama/lang/gaml/documentation/DocumentationNode.java
@@ -1,60 +1,65 @@
-/*********************************************************************************************
+/*******************************************************************************************************
  *
- * 'DocumentationNode.java, in plugin msi.gama.lang.gaml, is part of the source code of the GAMA modeling and simulation
- * platform. (v. 1.8.1)
+ * DocumentationNode.java, in msi.gama.lang.gaml, is part of the source code of the GAMA modeling and simulation
+ * platform (v.1.8.2).
  *
- * (c) 2007-2020 UMI 209 UMMISCO IRD/UPMC & Partners
+ * (c) 2007-2021 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
- * Visit https://github.com/gama-platform/gama for license information and developers contact.
+ * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
- *
- **********************************************************************************************/
+ ********************************************************************************************************/
 package msi.gama.lang.gaml.documentation;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.InflaterInputStream;
 
 import msi.gama.common.interfaces.IGamlDescription;
 
+/**
+ * The Class DocumentationNode.
+ */
 public class DocumentationNode implements IGamlDescription {
 
-	public static byte[] compress(final String text) throws IOException {
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try (final OutputStream out = new DeflaterOutputStream(baos, true);) {
-			out.write(text.getBytes("ISO-8859-1"));
-		}
-		return baos.toByteArray();
-	}
+	// public static byte[] compress(final String text) throws IOException {
+	// final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	// try (final OutputStream out = new DeflaterOutputStream(baos, true);) {
+	// out.write(text.getBytes("ISO-8859-1"));
+	// }
+	// return baos.toByteArray();
+	// }
+	//
+	// public static String decompress(final byte[] bytes) throws IOException {
+	// final InputStream in = new InflaterInputStream(new ByteArrayInputStream(bytes));
+	// final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	// final byte[] buffer = new byte[8192];
+	// int len;
+	// while ((len = in.read(buffer)) > 0) {
+	// baos.write(buffer, 0, len);
+	// }
+	// return new String(baos.toByteArray(), "ISO-8859-1");
+	// }
+	//
+	// final byte[] title;
+	// final byte[] doc;
 
-	public static String decompress(final byte[] bytes) throws IOException {
-		final InputStream in = new InflaterInputStream(new ByteArrayInputStream(bytes));
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		final byte[] buffer = new byte[8192];
-		int len;
-		while ((len = in.read(buffer)) > 0) {
-			baos.write(buffer, 0, len);
-		}
-		return new String(baos.toByteArray(), "ISO-8859-1");
-	}
+	/** The title. */
+	String title;
 
-	final byte[] title;
-	final byte[] doc;
+	/** The doc. */
+	String doc;
 
+	/**
+	 * Instantiates a new documentation node.
+	 *
+	 * @param desc
+	 *            the desc
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
 	DocumentationNode(final IGamlDescription desc) throws IOException {
-
+		title = desc.getTitle();
+		doc = desc.getDocumentation();
 		final String plugin = desc.getDefiningPlugin();
-		final String title = desc.getTitle();
-		String documentation = desc.getDocumentation();
-		if (plugin != null) {
-			documentation += "\n<p/><i> [defined in " + plugin + "] </i>";
-		}
-		doc = compress(documentation);
-		this.title = compress(title);
+		if (plugin != null) { doc += "\n<p/><i> [defined in " + plugin + "] </i>"; }
 	}
 
 	/**
@@ -67,33 +72,31 @@ public class DocumentationNode implements IGamlDescription {
 
 	@Override
 	public String getDocumentation() {
-		try {
-			return decompress(doc);
-		} catch (final IOException e) {
-			e.printStackTrace();
-			return "Error";
-		}
+		return doc;
+		// try {
+		// return decompress(doc);
+		// } catch (final IOException e) {
+		// e.printStackTrace();
+		// return "Error";
+		// }
 	}
 
 	@Override
 	public String getTitle() {
-		try {
-			return decompress(title);
-		} catch (final IOException e) {
-			e.printStackTrace();
-			return "Error";
-		}
+		return title;
+		// try {
+		// return decompress(title);
+		// } catch (final IOException e) {
+		// e.printStackTrace();
+		// return "Error";
+		// }
 	}
 
 	@Override
-	public String getName() {
-		return "Online documentation";
-	}
+	public String getName() { return "Online documentation"; }
 
 	@Override
-	public String getDefiningPlugin() {
-		return "";
-	}
+	public String getDefiningPlugin() { return ""; }
 
 	@Override
 	public void setName(final String name) {
