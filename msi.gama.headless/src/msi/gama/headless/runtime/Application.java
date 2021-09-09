51492a193d156da88fb05250faf1302e287b56a1
ndgnuh <ndgnuh@protonmail.com>
2021-09-10 05:03:13 +0700

    Add help flag for -write-xmi

--- msi.gama.headless/src/msi/gama/headless/runtime/Application.java
+++ msi.gama.headless/src/msi/gama/headless/runtime/Application.java
@@ -144,6 +144,7 @@ public class Application implements IApplication {
 			+ "\n\t\t                             -- build an xml parameter file from a model"
 			+ "\n\t\t[xmlHeadlessFile.xml] [outputDirectory]"
 			+ "\n\t\t                             -- default usage of GAMA headless"
+			+ "\n\t\t" + WRITE_XMI + " -- write scope provider resource files to disk"
 		);
 		DEBUG.OFF();
 	}
