a3e25491e50dd94f66c717f72245e0de8e9bb5b4
RoiArthurB <contact@arthurbrugiere.fr>
2021-09-13 16:30:09 +0700

    [GHA] Raise bash error after cat log

--- travis/validate_and_test.sh
+++ travis/validate_and_test.sh
@@ -24,7 +24,7 @@ else
 
 	#GAMA=Gamaq
 	# Run -validate or cat log file and close
-	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -validate || cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/.work/.metadata/.log && exit $1
+	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -validate || cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/.work/.metadata/.log && exit 125
 	res=$?		
 	if [[ $res -gt 0 ]]; then	
 		rm -rf $passWork
@@ -32,7 +32,7 @@ else
 	fi
 
 	echo "GAMA is starting..."
-	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -test -failed || cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/.work/.metadata/.log && exit $1
+	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -test -failed || cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/.work/.metadata/.log && exit 125
 	res=$?			
 	if [[ $res -gt 0 ]]; then
 		rm -rf $passWork
