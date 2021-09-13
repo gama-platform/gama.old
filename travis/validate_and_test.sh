b15763d599032b4a34bf9e549b9fc621e4c40cea
RoiArthurB <contact@arthurbrugiere.fr>
2021-09-13 16:12:40 +0700

    [GHA] Display logs when validate fail

--- travis/validate_and_test.sh
+++ travis/validate_and_test.sh
@@ -3,7 +3,7 @@
 MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 echo $MESSAGE 
 if  [[ $MESSAGE == *"ci notest"* ]]; then	 
-	exit 0
+	exit 0 
 else
 
 	cd $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless
@@ -23,7 +23,8 @@ else
 	#exec
 
 	#GAMA=Gamaq
-	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -validate 
+	# Run -validate or cat log file and close
+	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -validate || cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/.work/.metadata/.log && exit $1
 	res=$?		
 	if [[ $res -gt 0 ]]; then	
 		rm -rf $passWork
@@ -31,7 +32,7 @@ else
 	fi
 
 	echo "GAMA is starting..."
-	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -test -failed   
+	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -test -failed || cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/.work/.metadata/.log && exit $1
 	res=$?			
 	if [[ $res -gt 0 ]]; then
 		rm -rf $passWork
