4637e48f09bc3b70b496ac6dca9c7491f1ff13f5
RoiArthurB <contact@arthurbrugiere.fr>
2021-09-15 13:00:18 +0700

    [GHA] Validate use gama-headless.sh helper Should prevent missing libraries and other problems

--- travis/validate_and_test.sh
+++ travis/validate_and_test.sh
@@ -6,33 +6,16 @@ if  [[ $MESSAGE == *"ci notest"* ]]; then
 	exit 0 
 else
 
-	cd $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless
-	memory=3048m 
-
-
-	  
-	DUMPLIST=$(ls  ../plugins/*.jar )
-
-	for fic in $DUMPLIST; do
-	GAMA=$GAMA:$fic
-	done
-
-	passWork=.work 
-
-	echo "GAMA is starting..."
-	#exec
-
-	#GAMA=Gamaq
-	# Run -validate or cat log file and close
-	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -validate || cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/.work/.metadata/.log && exit 125
+	echo "GAMA validate is starting..."
+	bash $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/gama-headless.sh -m 3048m -validate
 	res=$?		
 	if [[ $res -gt 0 ]]; then	
 		rm -rf $passWork
 		exit $res
 	fi
 
-	echo "GAMA is starting..."
-	java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.core.launcher.Main  -application msi.gama.headless.id4 -data $passWork -test -failed || cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/.work/.metadata/.log && exit 125
+	echo "GAMA test is starting..."
+	bash $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless/gama-headless.sh -m 3048m -test -failed
 	res=$?			
 	if [[ $res -gt 0 ]]; then
 		rm -rf $passWork
