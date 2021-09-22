f3694a70f7682c094736f515ece09ad48bb4a3ec
AlexisDrogoul <alexis.drogoul@gmail.com>
2021-09-22 21:50:28 +0700

    Removes useless workaround (for #2745, not relevant anymore)

--- ummisco.gama.java2d/src/ummisco/gama/java2d/AWTDisplayView.java
+++ ummisco.gama.java2d/src/ummisco/gama/java2d/AWTDisplayView.java
@@ -1,14 +1,13 @@
-/*********************************************************************************************
+/*******************************************************************************************************
  *
- * 'AWTDisplayView.java, in plugin ummisco.gama.java2d, is part of the source code of the GAMA modeling and simulation
- * platform. (v. 1.8.1)
+ * AWTDisplayView.java, in ummisco.gama.java2d, is part of the source code of the
+ * GAMA modeling and simulation platform (v.1.8.2).
  *
- * (c) 2007-2020 UMI 209 UMMISCO IRD/UPMC & Partners
+ * (c) 2007-2021 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
- * Visit https://github.com/gama-platform/gama for license information and developers contact.
- *
- *
- **********************************************************************************************/
+ * Visit https://github.com/gama-platform/gama for license information and contacts.
+ * 
+ ********************************************************************************************************/
 package ummisco.gama.java2d;
 
 import org.eclipse.swt.SWT;
@@ -18,12 +17,15 @@ import ummisco.gama.java2d.swing.SwingControl;
 import ummisco.gama.ui.views.WorkaroundForIssue1353;
 import ummisco.gama.ui.views.displays.LayeredDisplayView;
 
+/**
+ * The Class AWTDisplayView.
+ */
 public class AWTDisplayView extends LayeredDisplayView {
 
 	@Override
 	protected Composite createSurfaceComposite(final Composite parent) {
 
-		if (getOutput() == null) { return null; }
+		if (getOutput() == null) return null;
 
 		surfaceComposite = new SwingControl(parent, SWT.NO_FOCUS) {
 
@@ -35,7 +37,7 @@ public class AWTDisplayView extends LayeredDisplayView {
 		};
 		surfaceComposite.setEnabled(false);
 		WorkaroundForIssue1594.installOn(this, parent, surfaceComposite, (Java2DDisplaySurface) getDisplaySurface());
-		WorkaroundForIssue2745.installOn(this);
+		// WorkaroundForIssue2745.installOn(this);
 		WorkaroundForIssue1353.install();
 		return surfaceComposite;
 	}
