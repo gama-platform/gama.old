3730e24b9d5f68e4442e203610f17cc6ec5b1913
RoiArthurB <contact@arthurbrugiere.fr>
2022-03-21 15:29:03 +0700

    [TRAVIS] Add M1 build in zip w/ jdk script

--- travis/zip_withjdk.sh
+++ travis/zip_withjdk.sh
@@ -39,12 +39,14 @@ RELEASEFILES[$n]="$thePATH-linux.gtk.x86_64.zip"
 n=1
 RELEASEFILES[$n]="$thePATH-macosx.cocoa.x86_64.zip" 
 n=2
-RELEASEFILES[$n]="$thePATH-win32.win32.x86_64.zip"  
+RELEASEFILES[$n]="$thePATH-macosx.cocoa.aarch64.zip" 
 n=3
-RELEASEFILES[$n]="$thePATH-linux.gtk.x86_64_withJDK.zip" 
+RELEASEFILES[$n]="$thePATH-win32.win32.x86_64.zip"  
 n=4
-RELEASEFILES[$n]="$thePATH-win32.win32.x86_64_withJDK.zip"  
+RELEASEFILES[$n]="$thePATH-linux.gtk.x86_64_withJDK.zip" 
 n=5
+RELEASEFILES[$n]="$thePATH-win32.win32.x86_64_withJDK.zip"  
+n=6
 RELEASEFILES[$n]="$thePATH-macosx.cocoa.x86_64_withJDK.zip" 
 n=7
 RELEASEFILES[$n]="$thePATH-macosx.cocoa.aarch64_withJDK.zip" 
@@ -54,17 +56,25 @@ RELEASEFILES[$n]="$thePATH-macosx.cocoa.aarch64_withJDK.zip"
 wget -q $(curl https://api.github.com/repos/adoptium/temurin17-binaries/releases/latest | grep "/OpenJDK17U-jdk_x64_linux.*.gz\"" | cut -d ':' -f 2,3 | tr -d \") -O "jdk_linux_17.tar.gz"
 wget -q $(curl https://api.github.com/repos/adoptium/temurin17-binaries/releases/latest | grep "/OpenJDK17U-jdk_x64_window.*.zip\"" | cut -d ':' -f 2,3 | tr -d \") -O "jdk_win_17.zip"
 wget -q $(curl https://api.github.com/repos/adoptium/temurin17-binaries/releases/latest | grep "/OpenJDK17U-jdk_x64_mac.*.gz\"" | cut -d ':' -f 2,3 | tr -d \") -O "jdk_osx_17.tar.gz"
+wget -q $(curl https://api.github.com/repos/adoptium/temurin17-binaries/releases/latest | grep "/OpenJDK17U-jdk_aarch64_mac.*.gz\"" | cut -d ':' -f 2,3 | tr -d \") -O "jdk_osx_aarch_17.tar.gz"
 mkdir  jdk_linux
 mkdir  jdk_win
 mkdir  jdk_osx
+mkdir  jdk_osx_aarch
 
 
 echo "unzip jdk linux"	
 tar -zxf jdk_linux_17.tar.gz -C jdk_linux/
 mv jdk_linux/jdk-17* jdk_linux/jdk
+
 echo "unzip jdk osx"	
 tar -zxf jdk_osx_17.tar.gz -C jdk_osx/ 
 mv jdk_osx/jdk-17* jdk_osx/jdk 
+
+echo "unzip jdk osx_aarch"	
+tar -zxf jdk_osx_aarch_17.tar.gz -C jdk_osx_aarch/ 
+mv jdk_osx_aarch/jdk-17* jdk_osx_aarch/jdk 
+
 echo "unzip jdk win"	
 unzip -q jdk_win_17.zip -d jdk_win
 mv jdk_win/jdk-17* jdk_win/jdk 
@@ -119,11 +129,23 @@ sudo cp $GITHUB_WORKSPACE/travis/jdk/mac/gama-headless.sh $GITHUB_WORKSPACE/ummi
 
 
 
+sudo cp -R jdk_osx_aarch/jdk $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/aarch64/Gama.app/Contents
+#sudo cp $GITHUB_WORKSPACE/travis/jdk/mac/64/Gama.ini $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/aarch64/Gama.app/Contents/Eclipse
+echo "-vm" > Gama.ini
+echo "../jdk/Contents/Home/bin/java" >> Gama.ini
+cat $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/aarch64/Gama.app/Contents/Eclipse/Gama.ini >> Gama.ini
+rm $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/aarch64/Gama.app/Contents/Eclipse/Gama.ini
+mv Gama.ini $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/aarch64/Gama.app/Contents/Eclipse/Gama.ini
+
+sudo cp $GITHUB_WORKSPACE/travis/jdk/mac/gama-headless.sh $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/aarch64/Gama.app/Contents/headless
+
+
+
 
 	
 cd $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64
 
-sudo zip -9 -qyr "${RELEASEFILES[3]}" . && echo "compressed ${RELEASEFILES[3]}" || echo "compress fail ${RELEASEFILES[3]}"
+sudo zip -9 -qyr "${RELEASEFILES[4]}" . && echo "compressed ${RELEASEFILES[4]}" || echo "compress fail ${RELEASEFILES[4]}"
 
 cd ../../../../../../../
  
@@ -132,7 +154,7 @@ cd ../../../../../../../
 
 cd $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/win32/win32/x86_64
 
-sudo zip -9 -qr "${RELEASEFILES[4]}" . && echo "compressed ${RELEASEFILES[4]}" || echo "compress fail ${RELEASEFILES[4]}"
+sudo zip -9 -qr "${RELEASEFILES[5]}" . && echo "compressed ${RELEASEFILES[5]}" || echo "compress fail ${RELEASEFILES[5]}"
 
 cd ../../../../../../../
 
@@ -142,10 +164,14 @@ cd ../../../../../../../
 
 cd $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/x86_64
 
-sudo zip -9 -qyr "${RELEASEFILES[5]}" . && echo "compressed ${RELEASEFILES[5]}" || echo "compress fail ${RELEASEFILES[5]}"
+sudo zip -9 -qyr "${RELEASEFILES[6]}" . && echo "compressed ${RELEASEFILES[6]}" || echo "compress fail ${RELEASEFILES[6]}"
 
 cd ../../../../../../../
 
+
+cd $GITHUB_WORKSPACE/ummisco.gama.product/target/products/ummisco.gama.application.product/macosx/cocoa/aarch64
+
+sudo zip -9 -qyr "${RELEASEFILES[7]}" . && echo "compressed ${RELEASEFILES[7]}" || echo "compress fail ${RELEASEFILES[7]}"
 
 
 echo DONE
