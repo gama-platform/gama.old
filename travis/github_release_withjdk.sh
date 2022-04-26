585f5ed38ff9b9e4dc6631c1635a0e99bc6245ff
RoiArthurB <contact@arthurbrugiere.fr>
2022-04-26 19:27:14 +0700

    [GHA REL] Fix echo var instead of command ci release

--- travis/github_release_withjdk.sh
+++ travis/github_release_withjdk.sh
@@ -63,10 +63,10 @@ n=0
 suff=($SUFFIX $SUFFIX_DEB) 
 for s in ${suff[@]}; 
 do
-    RELEASEFILES[$n]=$GITHUB_WORKSPACE"/gama-platform_1.8.2-1_amd64."$($s | rev | cut -d "." -f 1 | rev)
+    RELEASEFILES[$n]=$GITHUB_WORKSPACE"/gama-platform_1.8.2-1_amd64."$(echo $s | rev | cut -d "." -f 1 | rev)
 		NEWFILES[$n]='GAMA_1.8.2_Linux'$s 
 		n=$n+1
-		RELEASEFILES[$n]=$GITHUB_WORKSPACE"/gama-platform-jdk_1.8.2-1_amd64."$($s | rev | cut -d "." -f 1 | rev)
+		RELEASEFILES[$n]=$GITHUB_WORKSPACE"/gama-platform-jdk_1.8.2-1_amd64."$(echo $s | rev | cut -d "." -f 1 | rev)
 		NEWFILES[$n]='GAMA_1.8.2_Linux_with_JDK'$s
 		n=$n+1
 done
