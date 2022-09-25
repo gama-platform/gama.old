ab89eb1af04614f816dd5960f17309be3c358ab7
AlexisDrogoul <alexis.drogoul@gmail.com>
2022-09-25 22:24:48 +0700

    Improves the thread skill to operate multiple skills in parallel.
    ATTENTION: the name of the action has been changed from 'runnable_action' to 'thread_action'
    ATTENTION: the unit for 'interval' now obeys the general scheme of GAML (default is #second)

--- msi.gama.core/src/msi/gaml/skills/ThreadSkill.java
+++ msi.gama.core/src/msi/gaml/skills/ThreadSkill.java
@@ -13,6 +13,7 @@ package msi.gaml.skills;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import msi.gama.common.interfaces.IKeyword;
+import msi.gama.metamodel.agent.AbstractAgent;
 import msi.gama.metamodel.agent.IAgent;
 import msi.gama.precompiler.GamlAnnotations.action;
 import msi.gama.precompiler.GamlAnnotations.arg;
@@ -32,6 +33,7 @@ import msi.gaml.types.IType;
  * getter, setter, init, action & args)
  *
  * @author taillandier 30 August 2022
+ * @revised AD 25 September 2022 regarding the unit of the interval, the names and the management of multiple threads
  */
 
 @doc ("The thread skill is intended to define the minimal set of behaviours required for agents that are able to run an action in a thread")
@@ -40,8 +42,17 @@ import msi.gaml.types.IType;
 		concept = { IConcept.SKILL, IConcept.SYSTEM })
 public class ThreadSkill extends Skill {
 
-	/** The current thread. */
-	private static ControlSubThread currentThread;
+	/** The Constant ACTION_NAME. */
+	private static final String ACTION_NAME = "thread_action";
+
+	/** The Constant START_THREAD. */
+	private static final String START_THREAD = "start_thread";
+
+	/** The Constant END_THREAD. */
+	private static final String END_THREAD = "end_thread";
+
+	/** The Constant THREAD_MEMORY. */
+	private static final String THREAD_MEMORY = "%%thread_memory%%";
 
 	/**
 	 * primStartThread
@@ -52,7 +63,7 @@ public class ThreadSkill extends Skill {
 	 *             the gama runtime exception
 	 */
 	@action (
-			name = "start_thread",
+			name = START_THREAD,
 			args = { @arg (
 					name = "continuous",
 					type = IType.BOOL,
@@ -71,12 +82,13 @@ public class ThreadSkill extends Skill {
 	public Boolean primStartThread(final IScope scope) throws GamaRuntimeException {
 		Boolean continuous = !scope.hasArg("continuous") ? true : (Boolean) scope.getArg("continuous", IType.BOOL);
 		Double interval = !scope.hasArg("interval") ? 0.1 : scope.getFloatArg("interval");
+		ControlSubThread currentThread = (ControlSubThread) scope.getAgent().getAttribute(THREAD_MEMORY);
 		if (currentThread == null) {
 			currentThread = new ControlSubThread(scope.getAgent(), continuous, (int) (interval * 1000));
+			scope.getAgent().setAttribute(THREAD_MEMORY, currentThread);
 			currentThread.start();
-			return true;
 		}
-		return false;
+		return true;
 	}
 
 	/**
@@ -89,18 +101,37 @@ public class ThreadSkill extends Skill {
 	 *             the gama runtime exception
 	 */
 	@action (
-			name = "end_thread",
+			name = END_THREAD,
 			doc = @doc (
 					examples = { @example ("do end_thread;") },
 					returns = "true if the thread was well stopped, false otherwise",
 					value = "End the current thread."))
 	public Boolean primEndThread(final IScope scope) throws GamaRuntimeException {
+		ControlSubThread currentThread = (ControlSubThread) scope.getAgent().getAttribute(THREAD_MEMORY);
 		if (currentThread != null) {
 			currentThread.stop();
-			currentThread = null;
+			scope.getAgent().setAttribute(THREAD_MEMORY, null);
 			return true;
 		}
 		return false;
+	}
+
+	/**
+	 * Prim die.
+	 *
+	 * @param scope
+	 *            the scope
+	 * @return the object
+	 * @throws GamaRuntimeException
+	 *             the gama runtime exception
+	 */
+	@action (
+			name = "die",
+			doc = @doc ("Kills the agent and disposes of it. Once dead, the agent cannot behave anymore"))
+	public Object primDie(final IScope scope) throws GamaRuntimeException {
+		primEndThread(scope);
+		((AbstractAgent) scope.getAgent()).primDie(scope);
+		return null;
 	}
 
 	/**
@@ -113,7 +144,7 @@ public class ThreadSkill extends Skill {
 	 *             the gama runtime exception
 	 */
 	@action (
-			name = "runnable_action")
+			name = ACTION_NAME)
 	public Object primExternalFactorOnRemainingTime(final IScope scope) throws GamaRuntimeException {
 		return null;
 	}
@@ -175,7 +206,7 @@ public class ThreadSkill extends Skill {
 		public void run() {
 			if (continuous) {
 				running.set(true);
-				while (running.get()) {
+				while (running.get() && !agent.dead() && !agent.getScope().interrupted()) {
 					try {
 						Thread.sleep(interval);
 					} catch (InterruptedException e) {
@@ -184,16 +215,13 @@ public class ThreadSkill extends Skill {
 					}
 
 					ISpecies context = agent.getSpecies();
-
-					IStatement action = context.getAction("runnable_action");
-					action.executeOn(agent.getScope().copy("ThreadScope"));
+					IStatement action = context.getAction(ACTION_NAME);
+					agent.getScope().copy("ThreadScope").execute(action, agent, null);
 				}
 			} else {
 				ISpecies context = agent.getSpecies();
-				IStatement action = context.getAction("runnable_action");
-				action.executeOn(agent.getScope().copy("ThreadScope"));
-				this.stop();
-
+				IStatement action = context.getAction(ACTION_NAME);
+				agent.getScope().copy("ThreadScope").execute(action, agent, null);
 			}
 		}
 
--- msi.gama.models/models/GAML Syntax/System/RunThread.gaml
+++ msi.gama.models/models/GAML Syntax/System/RunThread.gaml
@@ -2,9 +2,11 @@
 /***
 * Name: RunThread 
 * Author: Patrick Taillandier
-* Description: This model illustrates the possibility of GAMA in terms of running a model in a specific thread
-* A skill called thread was defined that allows to run the built-in runnable_action action. The runnable_action has to be overrided.
-* Two other built-in actions are provided: start_thread that starts the thread, and end_thread, that ends the thread
+* Description: This model illustrates the possibility of GAMA to run a model or any action in a specific thread. 
+* A skill called 'thread' allows to run in a thread the built-in 'thread_action' action. If overriden, this 'thread_action' is run in a thread.
+* Two other built-in actions are provided: start_thread that starts the thread, and end_thread, that ends the thread.
+* In this model, the skill is attached to the global species and to a species of agents. It can be attached to any species, and multiple agents can run multiple threads.
+* When the agents are killed, their thread is automatically stopped if it is running
 * Tags: system, thread, skill
 ***/
 
@@ -12,26 +14,39 @@
 model testThread
 
 global skills: [thread]{
-	init {
-		//if there is one, stop the existing thread
-		do end_thread;
-			
+	bool create_agents <- false;
+	init {			
 		//create and start a new thread - the runnable_action model will be activated continuously every 10ms
-		do start_thread continuous: true interval: 10;
+		
+		if (create_agents) {
+			create thread_agent number: 2;
+		} else {
+			do start_thread continuous: true interval: 1#s;
+		}	
 	}
 	
-	reflex behavior {
-		if cycle = 10000 {
-			//end the thread
-			do end_thread;
-			do pause;
-		}
-	}
+
 	
 	//the action that will be concerned by the new thread
-	action runnable_action {
+	action thread_action {
 		write "current time: " + #now;
 	}	
 }
 
-experiment run_a_thread autorun: true;
+species thread_agent skills: [thread] {
+	init {
+		do start_thread continuous: true interval: 1#s;
+	}
+	
+	action thread_action {
+		write " > " + self + "current time: " + #now;
+	}	
+}
+  
+experiment "Run global thread" autorun: true;
+
+experiment "Run several threads" autorun: true {
+	action _init_ {
+		create simulation with: [create_agents:: true];
+	}
+}
