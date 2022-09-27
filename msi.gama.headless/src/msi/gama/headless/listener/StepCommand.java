ff3a653bed9be3356408db82319cf0e4c136b27e
Baptiste Lesquoy <lesquoy.baptiste@gmail.com>
2022-09-27 09:29:29 +0700

    GamaServer add  nb_step parameter - ci release

--- msi.gama.headless/src/msi/gama/headless/listener/StepCommand.java
+++ msi.gama.headless/src/msi/gama/headless/listener/StepCommand.java
@@ -1,28 +1,18 @@
 package msi.gama.headless.listener;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.java_websocket.WebSocket;
 
-import msi.gama.common.GamlFileExtension;
-import msi.gama.headless.common.Globals;
-import msi.gama.headless.core.GamaHeadlessException;
-import msi.gama.headless.job.ManualExperimentJob;
 import msi.gama.util.IMap;
-import msi.gama.util.file.json.GamaJsonList;
 import ummisco.gama.dev.utils.DEBUG;
 
 public class StepCommand extends SocketCommand {
 	@Override
 	public void execute(final WebSocket socket, IMap<String, Object> map) {
 
-		String exp_id = map.get("exp_id") != null ? map.get("exp_id").toString() : "";
-		String socket_id = map.get("socket_id").toString();
-		final String cmd_type = map.get("type").toString();
+		final String	exp_id 		= map.get("exp_id") != null ? map.get("exp_id").toString() : "";
+		final String 	socket_id 	= map.get("socket_id").toString();
+		final String 	cmd_type 	= map.get("type").toString();
+		final int 		nb_step		= map.get("nb_step") != null ? (int) map.get("nb_step") : 0 ;
 		final GamaWebSocketServer gamaWebSocketServer = (GamaWebSocketServer) map.get("server");
 		DEBUG.OUT("launch");
 		DEBUG.OUT(map.get("model"));
@@ -30,7 +20,9 @@ public class StepCommand extends SocketCommand {
 
 		if (gamaWebSocketServer.get_listener().getExperiment(socket_id, exp_id) != null
 				&& gamaWebSocketServer.get_listener().getExperiment(socket_id, exp_id).getSimulation() != null) {
-			gamaWebSocketServer.get_listener().getExperiment(socket_id, exp_id).controller.userStep();
+			for (int i = 0 ; i < nb_step ; i++) {
+				gamaWebSocketServer.get_listener().getExperiment(socket_id, exp_id).controller.userStep();				
+			}
 		}
 		socket.send(cmd_type);
 	}
