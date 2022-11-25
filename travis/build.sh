80a310be322cfa98b28211f2608b3f59c7a57bfe
RoiArthurB <contact@arthurbrugiere.fr>
2022-11-25 11:34:43 +0700

    [GHA] Revert cec7afe Didn't work on GHA and annoying for local debug

--- travis/build.sh
+++ travis/build.sh
@@ -1,9 +1,9 @@
 cd ummisco.gama.annotations
-mvn -ntp clean install $1
+mvn clean install $1
 cd - 
 cd msi.gama.processor 
-mvn -ntp clean install $1
+mvn clean install $1
 cd - 
 cd msi.gama.parent 
-mvn -ntp clean install $1
+mvn clean install $1
 cd -
