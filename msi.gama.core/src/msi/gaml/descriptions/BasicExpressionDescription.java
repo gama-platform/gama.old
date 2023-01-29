6bc901ea91d9ce469a32a2e370a7b977b4fc2778
AlexisDrogoul <alexis.drogoul@gmail.com>
2023-01-29 21:44:04 +0700

    Fixes a bug that prevented the online doc from being displayed

--- msi.gama.core/src/msi/gaml/descriptions/BasicExpressionDescription.java
+++ msi.gama.core/src/msi/gaml/descriptions/BasicExpressionDescription.java
@@ -3,7 +3,7 @@
  * BasicExpressionDescription.java, in msi.gama.core, is part of the source code of the GAMA modeling and simulation
  * platform (v.1.9.0).
  *
- * (c) 2007-2022 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -161,8 +161,11 @@ public class BasicExpressionDescription implements IExpressionDescription {
 		if (type == Types.STRING && expression.isConst())
 			return context.getTypeNamed(GamaStringType.staticCast(null, expression.getConstValue(), true));
 		final String s = expression.literalValue();
-		final ITypesManager tm = context.getModelDescription().getTypesManager();
-		if (tm.containsType(s)) return tm.get(s);
+		ModelDescription md = context.getModelDescription();
+		if (md != null) {
+			final ITypesManager tm = md.getTypesManager();
+			if (tm.containsType(s)) return tm.get(s);
+		}
 		return expression.getGamlType();
 	}
 
