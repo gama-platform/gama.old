453f296c00486463ab3e4f0ab538ac8e44b8224e
AlexisDrogoul <alexis.drogoul@gmail.com>
2023-02-05 22:30:03 +0700

    Uses the complete URL (incl. query) to cache downloaded files

--- msi.gama.core/src/msi/gama/common/util/FileUtils.java
+++ msi.gama.core/src/msi/gama/common/util/FileUtils.java
@@ -2,7 +2,7 @@
  *
  * FileUtils.java, in msi.gama.core, is part of the source code of the GAMA modeling and simulation platform (v.1.9.0).
  *
- * (c) 2007-2022 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -738,7 +738,7 @@ public class FileUtils {
 	 */
 	private static String constructRelativeTempFilePath(final IScope scope, final URL url) {
 		return "" + CacheLocationProvider.NAME + "" + SEPARATOR + url.getHost() + URL_SEPARATOR_REPLACEMENT
-				+ url.getPath().replace(SEPARATOR, URL_SEPARATOR_REPLACEMENT);
+				+ url.getPath().replace(SEPARATOR, URL_SEPARATOR_REPLACEMENT) + url.getQuery();
 
 	}
 
