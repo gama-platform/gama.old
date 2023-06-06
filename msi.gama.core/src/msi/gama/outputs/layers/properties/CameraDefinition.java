526c4c6eb274300f07a8023330476e9318c620c7
AlexisDrogoul <alexis.drogoul@gmail.com>
2023-06-06 09:33:11 +0700

    Fixes #3821, verifying an IGraphics is associated with the scope

--- msi.gama.core/src/msi/gama/outputs/layers/properties/CameraDefinition.java
+++ msi.gama.core/src/msi/gama/outputs/layers/properties/CameraDefinition.java
@@ -1,11 +1,11 @@
 /*******************************************************************************************************
  *
  * CameraDefinition.java, in msi.gama.core, is part of the source code of the GAMA modeling and simulation platform
- * (v.2.0.0).
+ * (v.1.9.2).
  *
  * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
- * Visit https://github.com/gama-platform/gama2 for license information and contacts.
+ * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
  ********************************************************************************************************/
 package msi.gama.outputs.layers.properties;
@@ -95,7 +95,7 @@ public class CameraDefinition extends AbstractDefinition implements ICameraDefin
 		if (temp instanceof String pos) {
 			// If it is a symbolic position
 			double coeff = 1.4;
-			if (scope instanceof GraphicsScope gs) {
+			if ((scope instanceof GraphicsScope gs) && (gs.getGraphics() != null)) {
 				coeff = gs.getGraphics().getSurface().getData().getCameraDistanceCoefficient();
 			}
 			double w = scope.getSimulation().getWidth();
