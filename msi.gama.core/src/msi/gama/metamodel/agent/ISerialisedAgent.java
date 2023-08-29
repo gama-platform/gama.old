967961d2e3a319ce4ff43f27402bea3fa1418f37
AlexisDrogoul <alexis.drogoul@gmail.com>
2023-08-29 23:59:55 +0700

    Interface for gathering all the "serialisations" of agents

---
+++ msi.gama.core/src/msi/gama/metamodel/agent/ISerialisedAgent.java
@@ -0,0 +1,53 @@
+/*******************************************************************************************************
+ *
+ * ISerialisedAgent.java, in msi.gama.core, is part of the source code of the GAMA modeling and simulation platform
+ * (v.1.9.2).
+ *
+ * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ *
+ * Visit https://github.com/gama-platform/gama for license information and contacts.
+ *
+ ********************************************************************************************************/
+package msi.gama.metamodel.agent;
+
+/**
+ * The Interface ISerialisedAgent.
+ *
+ * @author Alexis Drogoul (alexis.drogoul@ird.fr)
+ * @date 8 août 2023
+ */
+public interface ISerialisedAgent {
+
+	/**
+	 * Gets the index.
+	 *
+	 * @author Alexis Drogoul (alexis.drogoul@ird.fr)
+	 * @return the index
+	 * @date 8 août 2023
+	 */
+	int getIndex();
+
+	/**
+	 * Gets the attribute.
+	 *
+	 * @author Alexis Drogoul (alexis.drogoul@ird.fr)
+	 * @param var
+	 *            the var
+	 * @return the attribute
+	 * @date 8 août 2023
+	 */
+	Object getAttributeValue(String var);
+
+	/**
+	 * Sets the attribute value.
+	 *
+	 * @author Alexis Drogoul (alexis.drogoul@ird.fr)
+	 * @param var
+	 *            the var
+	 * @param val
+	 *            the val
+	 * @date 8 août 2023
+	 */
+	void setAttributeValue(String var, Object val);
+
+}
