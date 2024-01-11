47073dfcc737953a25d83c98f45db7546c80e2fd
AlexisDrogoul <alexis.drogoul@gmail.com>
2024-01-12 00:11:27 +0700

    Adds Syntactic Errors as a possible category for GAML issues

--- msi.gama.core/src/msi/gaml/interfaces/IGamlIssue.java
+++ msi.gama.core/src/msi/gaml/interfaces/IGamlIssue.java
@@ -1,9 +1,8 @@
 /*******************************************************************************************************
  *
- * IGamlIssue.java, in ummisco.gama.annotations, is part of the source code of the GAMA modeling and simulation platform
- * (v.1.9.3).
+ * IGamlIssue.java, in msi.gama.core, is part of the source code of the GAMA modeling and simulation platform (v.1.9.3).
  *
- * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -172,5 +171,8 @@ public interface IGamlIssue {
 
 	/** The missing plugin. */
 	String MISSING_PLUGIN = "gaml.missing.plugin";
+
+	/** The syntactic error. */
+	String SYNTACTIC_ERROR = "gaml.syntactic.error";
 
 }
