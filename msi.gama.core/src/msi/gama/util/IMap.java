0a3defe65892f9f8baae2a9eedd9a25c35e9dded
AlexisDrogoul <alexis.drogoul@gmail.com>
2024-01-14 00:43:23 +0700

    Small changes to IMap iteration algo

--- msi.gama.core/src/msi/gama/util/IMap.java
+++ msi.gama.core/src/msi/gama/util/IMap.java
@@ -2,7 +2,7 @@
  *
  * IMap.java, in msi.gama.core, is part of the source code of the GAMA modeling and simulation platform (v.1.9.3).
  *
- * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -12,7 +12,6 @@ package msi.gama.util;
 import static msi.gama.util.GamaMapFactory.createWithoutCasting;
 
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.Spliterator;
@@ -658,11 +657,7 @@ public interface IMap<K, V>
 	 * @return true, if successful
 	 */
 	default boolean forEachValue(final ConsumerWithPruning<? super V> visitor) {
-		final Iterator<Map.Entry<K, V>> it = entrySet().iterator();
-		while (it.hasNext()) {
-			final Map.Entry<K, ? extends V> entry = it.next();
-			if (!visitor.process(entry.getValue())) return false;
-		}
+		for (Entry<K, V> entry : entrySet()) { if (!visitor.process(entry.getValue())) return false; }
 		return true;
 	}
 
