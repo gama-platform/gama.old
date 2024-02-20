f468c3020be31b1ccf8e6a5c919e30cd6e7a69ce
AlexisDrogoul <alexis.drogoul@gmail.com>
2024-02-20 15:39:37 +0700

    Supports a better deserialisation of agents

--- msi.gama.core/src/msi/gama/metamodel/agent/SerialisedAgent.java
+++ msi.gama.core/src/msi/gama/metamodel/agent/SerialisedAgent.java
@@ -3,7 +3,7 @@
  * SerialisedAgent.java, in msi.gama.core, is part of the source code of the GAMA modeling and simulation platform
  * (v.1.9.3).
  *
- * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -259,7 +259,9 @@ public record SerialisedAgent(int index, String species, Map<String, Object> att
 		IPopulation<?> p = scope.getSimulation().getPopulationFor(species);
 		if (p == null)
 			throw GamaRuntimeException.error("No population named" + species + " exist in this simulation", scope);
-		return restoreInto(scope, p);
+		IAgent a = p.getOrCreateAgent(scope, index);
+		restoreAs(scope, a);
+		return a;
 	}
 
 	/**
