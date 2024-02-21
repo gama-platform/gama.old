7f275698df2108216b57f0cdb85df5f666ba5810
AlexisDrogoul <alexis.drogoul@gmail.com>
2024-02-21 15:39:35 +0700

    Addresses #3984 -- Please test

--- msi.gama.core/src/msi/gama/util/file/GamaGridFile.java
+++ msi.gama.core/src/msi/gama/util/file/GamaGridFile.java
@@ -3,7 +3,7 @@
  * GamaGridFile.java, in msi.gama.core, is part of the source code of the GAMA modeling and simulation platform
  * (v.1.9.3).
  *
- * (c) 2007-2023 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
  *
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -857,7 +858,7 @@ public class GamaGridFile extends GamaGisFile implements IFieldMatrixProvider {
 	public double[] getBand(final IScope scope, final int index) {
 		createCoverage(scope);
 		read(scope, true, false);
-		return records.bands.get(index);
+		return Arrays.copyOf(records.bands.get(index), length(scope));
 	}
 
 }
